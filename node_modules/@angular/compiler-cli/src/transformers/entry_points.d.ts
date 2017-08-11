598f203be78fab73b6343a2171c21947f26e2f1f
paulbert <paul.r.albert@gmail.com>
2017-08-11 17:58:02 -0400

    Remove node_modules from cache

--- node_modules/@angular/compiler-cli/src/transformers/entry_points.d.ts
+++
@@ -1,9 +0,0 @@
-import * as ts from 'typescript';
-import { CompilerHost, CompilerOptions } from './api';
-import { createModuleFilenameResolver } from './module_filename_resolver';
-export { createProgram } from './program';
-export { createModuleFilenameResolver };
-export declare function createHost({tsHost, options}: {
-    tsHost: ts.CompilerHost;
-    options: CompilerOptions;
-}): CompilerHost;
