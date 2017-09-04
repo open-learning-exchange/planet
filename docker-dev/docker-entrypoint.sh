f80e48dc7abe86a8ca2ecfa4b4900e2b9a31ffe5
mappuji <a.mappuji@gmail.com>
2017-09-05 02:41:13 +0700

    Create Docker for Development (WIP)

---
+++ docker-dev/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Add CORS to CouchDB so app has access to databases
+git clone https://github.com/pouchdb/add-cors-to-couchdb.git
+cd add-cors-to-couchdb
+npm install
+node bin.js http://couchdb:5984
+# End add CORS to CouchDB
+
+# Add initial Couch databases here
+curl -X PUT http://couchdb:5984/_users
+curl -X PUT http://couchdb:5984/_replicator
+curl -X PUT http://couchdb:5984/_global_changes
+curl -X PUT http://couchdb:5984/meetups
+# End Couch database addition
+
+npm install
+ng serve
