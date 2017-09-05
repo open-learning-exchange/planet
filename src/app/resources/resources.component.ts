890751fcf53e2ce5593959d5f447bd3aecb8bc20
nirojdyola <nirojdyola@gmail.com>
2017-09-05 14:25:55 +0545

    [#16] removed underscore.js

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { CouchService } from '../shared/couchdb.service';
 import { Router } from '@angular/router';
 import { Headers, Http } from '@angular/http';
-import * as _ from 'underscore';
 
 @Component({
   templateUrl: './resources.component.html',
@@ -13,7 +12,6 @@ export class ResourcesComponent implements OnInit {
   upload_files = [];
   attachments = [];
   message = "";
-  _: any = _;
  
   constructor(private couchService: CouchService,private http: Http,private router: Router) { }
 
@@ -22,24 +20,16 @@ export class ResourcesComponent implements OnInit {
   }
 
   onChange(event) {
-    var files = event.target.files;
-    this.upload_files.push(
-      {
-        "name": files[0].name,
-        "type": files[0].type
-      });
-
-    var filetype = this.upload_files[0].type;
-    var headers = new Headers({'Content-Type': filetype});
-    var filename = this.upload_files[0].name;
-    var defaultOpts = {headers:headers,withCredentials:true};
-
-    this.couchService.post('/resources',{}).then(
+    var files = event.target.files[0];
+    var filename = files.name;
+    var filetype = files.type;
+    
+    this.couchService.post('resources',{'filename' : filename}).then(
       (data) => {
         //upload files in the given id
         var url = 'resources/' + data.id + '/' + filename + '?rev=' + data.rev;
         this.couchService.saveAttachment(
-          url,files[0],filetype)
+          url,files,filetype)
           .then(
             (data)=>{
               this.message = "Success";
