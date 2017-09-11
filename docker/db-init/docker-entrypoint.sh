d5979eb16266b1ef0853fd42c9bfbd8f1e3000cc
mappuji <a.mappuji@gmail.com>
2017-09-11 18:08:42 +0000

    wait before configuring CORS and delete unneccessary docker-entrypoint

--- docker/db-init/docker-entrypoint.sh
+++ docker/db-init/docker-entrypoint.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-#CORS
+#CORS DOWNLOAD
 git clone https://github.com/pouchdb/add-cors-to-couchdb.git
-cd add-cors-to-couchdb
-npm install
-node bin.js http://couchdb:5984
-cd -
 
 #WAIT_TIME
 echo  "Waiting for couchdb to start"
 WAIT_TIME=0
 until curl couchdb:5984 || [ $WAIT_TIME -eq 180 ]; do
-echo "..."
-sleep 5
-WAIT_TIME=$(expr $WAIT_TIME + 5)
+    echo "..."
+    sleep 5
+    WAIT_TIME=$(expr $WAIT_TIME + 5)
 done
+
+#CORS SETUP
+cd add-cors-to-couchdb
+npm install
+node bin.js http://couchdb:5984
+cd -
 
 #MIGRATOR
 curl -X PUT http://couchdb:5984/_users
