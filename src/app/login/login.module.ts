6236ff32cdbef2f83e87dd756b5f81c835fd7fca
nirojdyola <nirojdyola@gmail.com>
2017-09-11 16:10:10 +0545

    [#38] - MultiLanguage Integrated

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { TranslateModule } from "ng2-Translate";
 
 import { LoginComponent } from './login.component';
 
@@ -9,7 +10,8 @@ import { LoginRouterModule } from './login-router.module'
 
 @NgModule({
   imports: [
-    LoginRouterModule,FormsModule,CommonModule
+    LoginRouterModule,FormsModule,CommonModule,
+    TranslateModule.forRoot()
   ],
   declarations: [
     LoginComponent
