8f8cfdda03d376c0b7224136dce00784912ad7f1
Jingting <jchen136@hawk.iit.edu>
2017-10-01 23:08:39 -0500

    unit test to meetup component

--- src/app/meetups/meetups.component.spec.ts
+++ src/app/meetups/meetups.component.spec.ts
@@ -4,9 +4,7 @@ import { MeetupsComponent } from './meetups.component';
 import { HttpModule } from '@angular/http';
 
 describe('MeetupsComponent', () => {
-  let component: MeetupsComponent;
-  let fixture: ComponentFixture<MeetupsComponent>;
-
+  
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
@@ -18,13 +16,82 @@ describe('MeetupsComponent', () => {
     .compileComponents();
   }));
 
+  let component: MeetupsComponent;
+  let fixture: ComponentFixture<MeetupsComponent>;
+  let spy: any;
+  let couchService;
+  let de;
+  let statusElement;
+  let testModel;
+
   beforeEach(() => {
     fixture = TestBed.createComponent(MeetupsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    couchService = fixture.debugElement.injector.get(CouchService);
+    testModel = {id:'1', rev: 'qwrjksf'};
+    de = fixture.debugElement;
+    statusElement = de.nativeElement.querySelector('p');
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
-});
+  
+  it('should make a get request to couchService', () => {
+    spy = spyOn(couchService, 'get').and.returnValue(Promise.resolve());
+    component.getMeetups();
+    fixture.whenStable().then(() => {
+        fixture.detectChanges();
+        expect(spy).toHaveBeenCalledWith('meetups/_all_docs?include_docs=true');
+    });
+  });
+
+  
+  it('should getmeetups',() =>{
+    
+      spy = spyOn(couchService, 'get').and.returnValue(Promise.resolve()); 
+      fixture.whenStable().then(() => {
+          fixture.detectChanges();
+          expect(component.getMeetups()).toBeUndefined();
+      });
+  });
+
+  it('should show There was a problem getting meetups', async() =>{
+    spy = spyOn(couchService, 'get').and.returnValue(Promise.reject({}));
+    component.getMeetups();
+    fixture.whenStable().then(() => {
+        fixture.detectChanges();
+        expect(statusElement.textContent).toBe('There was a problem getting meetups');
+    });
+  });
+  
+  
+  it('should make a delete request to couchService', () => {
+   
+    spy = spyOn(couchService, 'delete').and.returnValue(Promise.resolve());
+    component.deleteMeetup(testModel.id, testModel.rev);
+    fixture.whenStable().then(() => {
+        fixture.detectChanges();
+        expect(spy).toHaveBeenCalledWith('meetups/' + testModel.id + '?rev=' + testModel.rev);
+    });
+  });
+
+  it('should delete a specific meetup', async() =>{
+    spy = spyOn(couchService, 'delete').and.returnValue(Promise.resolve());
+      component.deleteMeetup(testModel.id, testModel.rev);
+      fixture.whenStable().then(() => {
+          fixture.detectChanges();
+          expect(component.deleteMeetup(testModel.id, testModel.rev)).toBeUndefined();
+      });
+  });
+  
+  it('should There was a problem deleting this meetup', async() =>{
+    spy = spyOn(couchService, 'delete').and.returnValue(Promise.reject({}));
+    component.deleteMeetup(testModel.id, testModel.rev);
+    fixture.whenStable().then(() => {
+        fixture.detectChanges();
+        expect(statusElement.textContent).toBe('There was a problem deleting this meetup');
+    });
+  });
+
+})
