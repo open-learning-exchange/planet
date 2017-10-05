40b5b6c522cc264ea971757877d11a92f9d759cd
LKhadka <lawrence_2009@hotmail.com>
2017-10-05 10:27:35 -0500

    add course to dockfile

--- docker/db-init/docker-entrypoint.sh
+++ docker/db-init/docker-entrypoint.sh
@@ -24,3 +24,4 @@ curl -X PUT http://couchdb:5984/_replicator
 curl -X PUT http://couchdb:5984/_global_changes
 curl -X PUT http://couchdb:5984/meetups
 curl -X PUT http://couchdb:5984/courses
+curl -X PUT http://couchdb:5984/courses/_design/course-validators -d @../../design/courses/course-validators.json
