354cf2ad958bed72296140727a1a1163ea5315dd
mappuji <a.mappuji@gmail.com>
2017-10-10 02:19:00 +0700

    Typo with COMMIT_ID

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+DOCKER_ORG=treehouses
+DOCKER_REPO=planet-dev
+VERSION=$(cat package.json | grep version | awk '{print$2}' | awk '{print substr($0, 2, length($0) - 3)}')
+BRANCH=$TRAVIS_BRANCH
+COMMIT=${TRAVIS_COMMIT::8}
+
 build_message(){
     # $1 = build message
     echo
@@ -11,20 +17,20 @@ build_message(){
 
 build_docker() {
   build_message Build the docker images ...
-  docker build -f ./docker/planet/Dockerfile  ./docker/planet -t $DOCKER_ORG/$DOCKER_REPO:$VERSION-$BRANCH-$COMMIT_ID
-  docker build -f ./docker/db-init/Dockerfile ./docker/db-init -t $DOCKER_ORG/$DOCKER_REPO:db-init-$VERSION-$BRANCH-$COMMIT_ID
+  docker build -f ./docker/planet/Dockerfile  ./docker/planet -t $DOCKER_ORG/$DOCKER_REPO:$VERSION-$BRANCH-$COMMIT
+  docker build -f ./docker/db-init/Dockerfile ./docker/db-init -t $DOCKER_ORG/$DOCKER_REPO:db-init-$VERSION-$BRANCH-$COMMIT
 }
 
 tag_latest_docker() {
   build_message Tag latest docker images ...
-  docker tag $DOCKER_ORG/$DOCKER_REPO:$VERSION-$BRANCH-$COMMIT_ID $DOCKER_ORG/$DOCKER_REPO:latest
-  docker tag $DOCKER_ORG/$DOCKER_REPO:db-init-$VERSION-$BRANCH-$COMMIT_ID $DOCKER_ORG/$DOCKER_REPO:db-init-latest
+  docker tag $DOCKER_ORG/$DOCKER_REPO:$VERSION-$BRANCH-$COMMIT $DOCKER_ORG/$DOCKER_REPO:latest
+  docker tag $DOCKER_ORG/$DOCKER_REPO:db-init-$VERSION-$BRANCH-$COMMIT $DOCKER_ORG/$DOCKER_REPO:db-init-latest
 }
 
 push_docker() {
   build_message Pushing docker images ...
-  docker push $DOCKER_ORG/$DOCKER_REPO:$VERSION-$BRANCH-$COMMIT_ID
-  docker push $DOCKER_ORG/$DOCKER_REPO:db-init-$VERSION-$BRANCH-$COMMIT_ID
+  docker push $DOCKER_ORG/$DOCKER_REPO:$VERSION-$BRANCH-$COMMIT
+  docker push $DOCKER_ORG/$DOCKER_REPO:db-init-$VERSION-$BRANCH-$COMMIT
 }
 
 push_latest_docker() {
@@ -32,11 +38,6 @@ push_latest_docker() {
   docker push $DOCKER_ORG/$DOCKER_REPO:latest
   docker push $DOCKER_ORG/$DOCKER_REPO:db-init-latest
 }
-
-DOCKER_ORG=treehouses
-DOCKER_REPO=planet-dev
-VERSION=$(cat package.json | grep version | awk '{print$2}' | awk '{print substr($0, 2, length($0) - 3)}')
-BRANCH=$TRAVIS_BRANCH
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 build_docker
