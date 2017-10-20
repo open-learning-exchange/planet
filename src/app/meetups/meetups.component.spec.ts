96382a911f428af313e6180abb73b3c6d6f86aab
Paul <paul.r.albert@gmail.com>
2017-10-20 11:40:21 -0400

    Update getMeetups test and fix linting

--- src/app/meetups/meetups.component.spec.ts
+++ src/app/meetups/meetups.component.spec.ts
@@ -1,102 +1,91 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CouchService } from '../shared/couchdb.service';
 import { MeetupsComponent } from './meetups.component';
 import { HttpModule } from '@angular/http';
 
+import { By } from '@angular/platform-browser';
+
 describe('MeetupsComponent', () => {
-  
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
-        HttpModule
-      ],
-      declarations: [ MeetupsComponent ],
-      providers:[CouchService]
-    })
-    .compileComponents();
-  }));
 
   let component: MeetupsComponent;
   let fixture: ComponentFixture<MeetupsComponent>;
-  let spy: any;
+  let getSpy: any;
+  let deleteSpy: any;
   let couchService;
   let de;
-  let statusElement;
-  //let testModel;
   let meetupdata1;
   let meetupdata2;
   let meetuparray;
 
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpModule
+      ],
+      declarations: [ MeetupsComponent ],
+      providers: [CouchService]
+    });
     fixture = TestBed.createComponent(MeetupsComponent);
     component = fixture.componentInstance;
     couchService = fixture.debugElement.injector.get(CouchService);
-    //testModel = {id:'1', rev: 'qwrjksf'};
     de = fixture.debugElement;
-    statusElement = de.nativeElement.querySelector('p');
-    meetupdata1 ={id:'1', rev:'qwrjksf',title:'happyhangout', description:'once a week'}; 
-    meetupdata2 ={id:'2', rev:'ghjjdrt',title:'angularhangout', description:'twice a week'};
-    meetuparray ={meetupdata1, meetupdata2}
+    meetupdata1 = {id: '1', rev: 'qwrjksf', title: 'happyhangout', description: 'once a week'};
+    meetupdata2 = {id: '2', rev: 'ghjjdrt', title: 'angularhangout', description: 'twice a week'};
+    meetuparray = {rows: [{doc: meetupdata1}, {doc: meetupdata2}]};
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
-  
+
   it('should make a get request to couchService', () => {
-    spy = spyOn(couchService, 'get').and.returnValue(Promise.resolve());
+    getSpy = spyOn(couchService, 'get').and.returnValue(Promise.resolve({rows: []}));
+    fixture.detectChanges();
+    expect(getSpy).toHaveBeenCalledWith('meetups/_all_docs?include_docs=true');
+  });
+
+  it('should display meetups in a table', () => {
+    const meetupRows = de.queryAll(By.css('.km-meetup-row'));
+    getSpy = spyOn(couchService, 'get').and.returnValue(Promise.resolve(meetuparray));
     component.getMeetups();
     fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        expect(spy).toHaveBeenCalledWith('meetups/_all_docs?include_docs=true');
-    });
-  });
-  
-  it('should getmeetups',() =>{
-    
-      spy = spyOn(couchService, 'get').and.returnValue(Promise.resolve(meetuparray)); 
-      component.getMeetups();
-      fixture.whenStable().then(() => {
-          fixture.detectChanges();
-          expect(meetuparray.calls.allArgs()).toEqual([{id:'1', rev:'qwrjksf',title:'happyhangout', description:'once a week'}, {id:'2', rev:'ghjjdrt',title:'angularhangout', description:'twice a week'}]);
+      fixture.detectChanges();
+      // Iterate over rows in the meetup table to check title & description match mock data
+      meetupRows.map((rowElement, index) => {
+        const title = rowElement.query(By.css('.km-meetup-title')).nativeElement;
+        const description = rowElement.query(By.css('.km-meetup-description')).nativeElement;
+        expect(title.textContent).toBe(meetuparray.rows[index].doc.title);
+        expect(description.textContent).toBe(meetuparray.rows[index].doc.description);
       });
-  });
-
-  it('should show There was a problem getting meetups', async() =>{
-    spy = spyOn(couchService, 'get').and.returnValue(Promise.reject({}));
-    component.getMeetups();
-    fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        expect(statusElement.textContent).toBe('There was a problem getting meetups');
     });
   });
 
-  
+  it('should show There was a problem getting meetups', () => {
+    const statusElement = de.query(By.css('.km-message')).nativeElement;
+    getSpy = spyOn(couchService, 'get').and.returnValue(Promise.reject({}));
+    component.getMeetups();
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(statusElement.textContent).toBe('There was a problem getting meetups');
+    });
+  });
+
   it('should make a delete request to couchService', () => {
-   
-    spy = spyOn(couchService, 'delete').and.returnValue(Promise.resolve());
     component.deleteMeetup(meetupdata1.id, meetupdata1.rev);
     fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        expect(spy).toHaveBeenCalledWith('meetups/' + meetupdata1.id + '?rev=' + meetupdata1.rev);
+      fixture.detectChanges();
+      expect(deleteSpy).toHaveBeenCalledWith('meetups/' + meetupdata1.id + '?rev=' + meetupdata1.rev);
     });
   });
 
-  it('should delete a specific meetup', async() =>{
-    spy = spyOn(couchService, 'delete').and.returnValue(Promise.resolve(meetuparray));
-      component.deleteMeetup('1', 'qwrjksf');
-      fixture.whenStable().then(() => {
-          fixture.detectChanges();
-          expect(meetuparray.calls.allArgs()).toEqual({id:'2', rev:'ghjjdrt',title:'angularhangout', description:'twice a week'});
-      });
-  });
-
-  it('should There was a problem deleting this meetup', async() =>{
-    spy = spyOn(couchService, 'delete').and.returnValue(Promise.reject({}));
+  it('should There was a problem deleting this meetup', () => {
+    const statusElement = de.query(By.css('.km-message')).nativeElement;
+    deleteSpy = spyOn(couchService, 'delete').and.returnValue(Promise.reject({}));
     component.deleteMeetup(meetupdata1.id, meetupdata1.rev);
     fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        expect(statusElement.textContent).toBe('There was a problem deleting this meetup');
+      fixture.detectChanges();
+      expect(statusElement.textContent).toBe('There was a problem deleting this meetup');
     });
   });
-})
+
+});
--- src/app/meetups/meetupsadd.component.spec.ts
+++ src/app/meetups/meetupsadd.component.spec.ts
@@ -6,19 +6,19 @@ import { HttpModule } from '@angular/http';
 
 
 describe('MeetupsaddComponent', () => {
-  
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ FormsModule,HttpModule ],
+      imports: [ FormsModule, HttpModule ],
       declarations: [ MeetupsaddComponent ],
-      providers:[CouchService]
+      providers: [CouchService]
     })
     .compileComponents();
   }));
 
   let component: MeetupsaddComponent;
   let fixture: ComponentFixture<MeetupsaddComponent>;
-  let spy:any;
+  let spy: any;
   let couchService;
   let testModel;
   let de;
@@ -26,57 +26,54 @@ describe('MeetupsaddComponent', () => {
   let compiled;
 
   beforeEach(() => {
-     fixture = TestBed.createComponent(MeetupsaddComponent);
-     component = fixture.debugElement.componentInstance;
-     couchService = fixture.debugElement.injector.get(CouchService);
-     testModel = { title:'hangout', description:'once a week' };
-     de = fixture.debugElement;
-     compiled = fixture.debugElement.nativeElement;
-     statusElement = de.nativeElement.querySelector('p');
+    fixture = TestBed.createComponent(MeetupsaddComponent);
+    component = fixture.debugElement.componentInstance;
+    couchService = fixture.debugElement.injector.get(CouchService);
+    testModel = { title: 'hangout', description: 'once a week' };
+    de = fixture.debugElement;
+    compiled = fixture.debugElement.nativeElement;
+    statusElement = de.nativeElement.querySelector('p');
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
 
-  
+
   it('should make a post request to couchService', () => {
     spy = spyOn(couchService, 'post').and.returnValue(Promise.resolve(testModel));
     component.onSubmit(testModel);
     fixture.whenStable().then(() => {
-        fixture.detectChanges();
-        expect(spy).toHaveBeenCalled();
+      fixture.detectChanges();
+      expect(spy).toHaveBeenCalled();
     });
-  });
-  
-  it('should show meetup created: title correctly',() =>{
-      
-      spy = spyOn(couchService, 'post').and.returnValue(Promise.resolve(testModel.title));
-      component.onSubmit(testModel);
-      fixture.whenStable().then(() =>{
-          fixture.detectChanges();
-          expect(statusElement.textContent).toBe('Meetup created: ' + testModel.title);
-      });
   });
 
-  it('should message Please complete the form',() =>{
-      testModel.title='';
-      testModel.description='';
-      component.onSubmit(testModel);
-      fixture.whenStable().then(()=>{
-        fixture.detectChanges();
-        expect(statusElement.textContent).toBe('Please complete the form');
+  it('should show meetup created: title correctly', () => {
+    spy = spyOn(couchService, 'post').and.returnValue(Promise.resolve(testModel.title));
+    component.onSubmit(testModel);
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(statusElement.textContent).toBe('Meetup created: ' + testModel.title);
     });
   });
-  
-  it('should message There was a problem creating the meetup',() =>{
-    
-      spy = spyOn(couchService, 'post').and.returnValue(Promise.reject({}));
-      component.onSubmit(testModel);
-      fixture.whenStable().then(() =>{
-          fixture.detectChanges();
-          let compiled = fixture.debugElement.nativeElement;
-          expect(statusElement.textContent).toBe('There was a problem creating the meetup');
-      });
+
+  it('should message Please complete the form', () => {
+    testModel.title = '';
+    testModel.description = '';
+    component.onSubmit(testModel);
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(statusElement.textContent).toBe('Please complete the form');
+    });
   });
-})
+
+  it('should message There was a problem creating the meetup', () => {
+    spy = spyOn(couchService, 'post').and.returnValue(Promise.reject({}));
+    component.onSubmit(testModel);
+    fixture.whenStable().then(() => {
+      fixture.detectChanges();
+      expect(statusElement.textContent).toBe('There was a problem creating the meetup');
+    });
+  });
+});
