f51f9c7fc81553b6285b772f6c131543e0e839bf
John Kiragu <mutuakiragu@gmail.com>
2017-10-22 19:38:44 +0300

    Fix relative imports on module

--- src/app/meetups/meetups.module.ts
+++ src/app/meetups/meetups.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { MeetupsComponent } from './meetups.component';
-import { MeetupsaddComponent } from './meetups-add.component';
+import { MeetupsAddComponent } from './meetups-add.component';
 
 import { MeetupsRouterModule } from './meetups-router.module';
 
