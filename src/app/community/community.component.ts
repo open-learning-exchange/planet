5d788933b9be1fdbfd2a9f50199f45516b91dc2e
Rupesh Manandhar <rupandroo@gmail.com>
2017-10-23 17:23:21 +0545

    [#96] Added filter

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
-
 import { CouchService } from '../shared/couchdb.service';
 
 @Component({
@@ -10,10 +9,42 @@ import { CouchService } from '../shared/couchdb.service';
   styleUrls: ['./community.component.scss']
 })
 export class CommunityComponent implements OnInit {
+  message = '';
+  community = [];
+  filter = '';
 
-  constructor() { }
+  constructor(
+    private couchService: CouchService
+    ) { }
+
+  getcommunitylist() {
+      this.couchService.post('community/_find',{
+                    "selector": {
+                       "_id": {
+                          "$gt": null
+                        },
+                        "nationName":  { $regex: '.*' + this.filter + '.*' }
+                      }
+                    }
+                )
+      .then((data) => {
+        this.community = data.docs;
+      }, (error) => this.message = 'There was a problem getting community');
+  }
+
+  deleteCommunity(communityId, communityRev) {
+    console.log(communityId, communityRev)
+    this.couchService.delete('community/' + communityId + '?rev=' + communityRev)
+      .then((data) => {
+        this.getcommunitylist();
+      }, (error) => this.message = 'There was a problem deleting this community');
+  }
+  onKey() {
+    this.getcommunitylist();
+  }
 
   ngOnInit() {
+    this.getcommunitylist();
   }
 
 }
