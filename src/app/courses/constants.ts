4b580767b47e1b12c4c7a2d085a4750e41e9f947
Paul <paul.r.albert@gmail.com>
2017-11-02 13:38:53 -0400

    Remove searchDocuments - use mangoQueries instead

--- src/app/courses/constants.ts
+++ src/app/courses/constants.ts
@@ -28,15 +28,3 @@ export const days = [
   'Thursday',
   'Friday'
 ];
-
-export function searchDocuments(selector, query) {
-  return JSON.parse(`
-    {
-      "selector": {
-        "${selector}": "${query}"
-      },
-      "fields": ["${selector}"],
-      "limit": 1
-    }
-    `);
-}
