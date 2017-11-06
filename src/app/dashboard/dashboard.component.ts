b2a67c98977dc8eb7bdd7973476e2f138c5ce2a7
Rupesh87 <rupandroo@gmail.com>
2017-11-06 15:12:03 +0545

    [#155] Nation and community button in dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,50 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CouchService } from '../shared/couchdb.service';
 import { UserService } from '../shared/user.service';
+import { Router } from '@angular/router';
 
 // Main page once logged in.  At this stage is more of a placeholder.
 @Component({
-  template: `
-    <div id="greeting">Hi, {{name}}</div>
-  `
+  templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent implements OnInit {
   name = '';
+  message = '';
+  communities = [];
+  nation =[];
   roles: string[] = [];
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private couchService: CouchService,
+    private router: Router
   ) {}
+
+  getcommunitylist() {
+    this.couchService.get('communityregistrationrequests/_all_docs?include_docs=true&limit=3')
+     .then((data) => {
+       this.communities = data.rows;
+     }, (error) => this.message = 'There was a problem getting Communities');
+  }
+
+  getNationList() {
+    this.couchService.get('nations/_all_docs?include_docs=true&limit=3')
+      .then((data) => {
+        this.nation = data.rows;
+      }, (error) => this.message = 'There was a problem getting NationList');
+  }
+
+  viewNation() {
+    this.router.navigateByUrl('/nation');
+  }
+
+  viewCommunity() {
+    this.router.navigateByUrl('/community');
+  }
 
   ngOnInit() {
     Object.assign(this, this.userService.get());
+    this.getcommunitylist();
+    this.getNationList();
   }
 }
