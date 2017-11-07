aa2c88541d7909d4f167e2877f5d11f352329956
Paul <paul.r.albert@gmail.com>
2017-11-07 00:59:35 -0500

    Fix courses.component merge

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -20,53 +20,11 @@ export class CoursesComponent implements OnInit {
       }, (error) => this.message = 'There was a problem getting the courses');
   }
 
-<<<<<<< HEAD
-  onSubmit() {
-    if (this.courseForm.valid) {
-      this.addCourse(this.courseForm.value);
-    } else {
-      Object.keys(this.courseForm.controls).forEach(field => {
-        const control = this.courseForm.get(field);
-        control.markAsTouched({ onlySelf: true });
-      });
-    }
-  }
-
-  async addCourse(courseInfo) {
-    // ...is the rest syntax for object destructuring
-    try {
-      await this.couchService.post(this.dbName, { ...courseInfo });
-      this.router.navigate([ '/' ]);
-    } catch (err) {
-      // Connect to an error display component to show user that an error has occurred
-      console.log(err);
-    }
-  }
-
-  cancel() {
-    this.location.back();
-  }
-
-  /* FOR TOGGLING DAILY/WEEKLY DAYS */
-
-  onDayChange(day: string, isChecked: boolean) {
-    const dayFormArray = <FormArray>this.courseForm.controls.day;
-
-    if (isChecked) {
-      // add to day array if checked
-      dayFormArray.push(new FormControl(day));
-    } else {
-      // remove from day array if unchecked
-      const index = dayFormArray.controls.findIndex(x => x.value === day);
-      dayFormArray.removeAt(index);
-    }
-=======
   deleteCourse(courseId, courseRev) {
     this.couchService.delete('courses/' + courseId + '?rev=' + courseRev)
       .then((data) => {
         this.getCourses();
       }, (error) => this.message = 'There was a problem deleting this course');
->>>>>>> origin/master
   }
 
   ngOnInit() {
