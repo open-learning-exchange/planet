cc3527a435f66181af4cfb9c2d20a77681c26ba0
Paul <paul.r.albert@gmail.com>
2017-11-08 16:44:27 -0500

    Add documentation as comment

--- src/app/shared/alerts/alerts-delete.component.ts
+++ src/app/shared/alerts/alerts-delete.component.ts
@@ -1,10 +1,20 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+/*
+ * Bootstrap modal for deleting from Planet
+ * type - Required.  Sets the text of the main message on the modal.  See HTML for options.
+ * deleteItem - Required.  Object which is the main argument for the delete method located
+ *  in the parent component this is called from.
+ * message - Optional.  Error message that is displayed when value is truthy.
+ * displayName - Optional. If deleteItem does not have a 'name' property, set this to
+ *  display to the user what is being deleted.
+ * okClick - Required.  Method from parent which is called to delete.
+ */
+import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'planet-alerts-delete',
   templateUrl: './alerts-delete.component.html'
 })
-export class AlertsDeleteComponent {
+export class AlertsDeleteComponent implements OnChanges {
 
   @Input() type: string;
   @Input() deleteItem: any;
