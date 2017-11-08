3c7372a72d5855e72aee9fd0f185474d65ac15a3
Paul <paul.r.albert@gmail.com>
2017-11-08 16:03:30 -0500

    Implement for nation and readd nation delete button

--- src/app/nation/nation.component.ts
+++ src/app/nation/nation.component.ts
@@ -23,6 +23,7 @@ export class NationComponent implements OnInit {
   message = '';
   nations = [];
   nationForm: FormGroup;
+  deleteItem = {};
 
   constructor(
     private location: Location,
@@ -57,14 +58,18 @@ export class NationComponent implements OnInit {
       }, (error) => this.message = 'There was a problem getting NationList');
   }
 
-  deleteNation(nationId, nationRev, index) {
-    const nationDelete = confirm('Are you sure you want to delete?');
-    if (nationDelete) {
-      this.couchService.delete('nations/' + nationId + '?rev=' + nationRev)
+  deleteClick(nation,index) {
+    this.deleteItem = { ...nation.doc, index: index };
+    jQuery('#planetDelete').modal('show');
+  }
+
+  deleteNation(nation) {
+    const { _id: nationId, _rev: nationRev, index } = nation;
+    this.couchService.delete('nations/' + nationId + '?rev=' + nationRev)
       .then((data) => {
         this.nations.splice(index, 1);
-      }, (error) => this.message = 'There was a problem deleting this meetup');
-    }
+        jQuery('#planetDelete').modal('hide');
+      }, (error) => this.message = 'There was a problem deleting this nation');
   }
 
   onSubmit(nation) {
