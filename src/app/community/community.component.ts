b416012217669ae86af5f48caf9e569c75dcb328
Paul <paul.r.albert@gmail.com>
2017-11-08 15:07:30 -0500

    Implement for communities

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
 import { Router } from '@angular/router';
 import { CouchService } from '../shared/couchdb.service';
+declare var jQuery: any;
 
 @Component({
   templateUrl: './community.component.html'
@@ -11,9 +12,11 @@ export class CommunityComponent implements OnInit {
   communities = [];
   filter = '';
   selectedValue = '';
+  deleteItem = {};
+
   constructor(
     private couchService: CouchService
-    ) { }
+  ) { }
 
   getcommunitylist() {
     this.couchService.post('communityregistrationrequests/_find', {
@@ -40,14 +43,19 @@ export class CommunityComponent implements OnInit {
     this.getcommunitylist();
   }
 
-  deleteCommunity(communityId, communityRev) {
-    const val = confirm('Are you sure you want to delete it?');
-    if (val) {
-      this.couchService.delete('communityregistrationrequests/' + communityId + '?rev=' + communityRev)
-        .then((data) => {
-          this.getcommunitylist();
-        }, (error) => this.message = 'There was a problem deleting this community');
-    }
+  deleteClick(community,index) {
+    this.deleteItem = { ...community, index: index };
+    jQuery('#planetDelete').modal('show');
+  }
+
+  deleteCommunity(community) {
+    // With object destructuring colon means different variable name assigned, i.e. 'id' rather than '_id'
+    let { _id: id, _rev: rev, index } = community;
+    this.couchService.delete('communityregistrationrequests/' + id + '?rev=' + rev)
+      .then((data) => {
+        this.getcommunitylist();
+        jQuery('#planetDelete').modal('hide');
+      }, (error) => this.message = 'There was a problem deleting this community');
   }
 
   ngOnInit() {
