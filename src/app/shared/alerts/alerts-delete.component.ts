b675a47ba0339154e1a137993df53769fbafa47b
Paul <paul.r.albert@gmail.com>
2017-11-08 16:24:23 -0500

    Implement optional displayName where there isn't a name property

--- src/app/shared/alerts/alerts-delete.component.ts
+++ src/app/shared/alerts/alerts-delete.component.ts
@@ -9,8 +9,15 @@ export class AlertsDeleteComponent {
   @Input() type: string;
   @Input() deleteItem: any;
   @Input() message: string;
+  @Input() displayName: string;
   @Output() okClick = new EventEmitter();
   @Output() cancelClick = new EventEmitter();
+
+  name: string;
+
+  ngOnChanges() {
+    this.name = this.deleteItem ? this.displayName || this.deleteItem.name : this.displayName;
+  }
 
   ok() {
     this.okClick.emit(this.deleteItem);
