ce2bc23510435c3eb16d1718f5b94417ec532538
amanuely <aafework@middlebury.edu>
2017-11-09 18:53:17 -0500

    fixed feedback based on recviews

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/user.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { CouchService } from '../shared/couchdb.service';
+
+export class Feedback{
+  name:string
+  isUrgent:boolean;
+  feedbackType:string;
+  feedbackMsg:string;
+}
 
 @Component({
-  selector: 'app-feedback',
-  templateUrl: './feedback.component.html',
-  styleUrls: ['./feedback.component.scss']
+  selector: 'feedback',
+  templateUrl: './feedback.component.html'
 })
-export class FeedbackComponent implements OnInit {
 
-  constructor() { }
+export class FeedbackComponent implements OnInit {
+  msgForUsr:string;
+  isFeedbackMsg :boolean=true;
+  feedback:Feedback= new Feedback();
+  message:string;
+  fedbkSubmitted:boolean= false;
+
+  constructor(
+    private userService: UserService,
+    private couchService:CouchService
+    ) { }
 
   ngOnInit() {
+    this.feedback.name= this.userService.get().name;
   }
 
+  submitfeedback(){
+    if (this.feedback.feedbackMsg===undefined|| this.feedback.feedbackMsg=="") {
+      this. msgForUsr="Feedback  cannot be empty";
+      this.isFeedbackMsg=false;
+    }else
+    {
+      this.isFeedbackMsg=true;
+      this.couchService.post('feedback/', this.feedback)
+      .then((data) => {
+        this. msgForUsr='';
+        this.isFeedbackMsg=false;
+        this.fedbkSubmitted=true;
+        this. msgForUsr= "Thank you! We have received your feedback";
+      },
+      (error) => {
+        this.isFeedbackMsg=true;
+        this.fedbkSubmitted=true;
+        this.msgForUsr = "Error with submitting your feedback";
+      });
+    }
+  }
 }
--- src/app/home/home-router.module.ts
+++ src/app/home/home-router.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 import { DashboardComponent } from '../dashboard/dashboard.component';
 import { UsersComponent } from '../users/users.component';
 import { HomeComponent } from './home.component';
