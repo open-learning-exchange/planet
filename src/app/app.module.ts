618abee4adc1681dc6e82199bc10833df471fccb
Paul <paul.r.albert@gmail.com>
2017-11-13 23:33:14 -0500

    Upgrade to Angular 5 (Fixes #160) (#174)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
-import './shared/rxjs-imports';
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -15,7 +13,7 @@ import { UserService } from './shared/user.service';
 
 @NgModule({
   imports: [
-    BrowserModule, AppRoutingModule, HttpModule
+    BrowserModule, AppRoutingModule, HttpClientModule
   ],
   declarations: [
     AppComponent, PageNotFoundComponent
