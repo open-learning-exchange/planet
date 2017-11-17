1987f0c35b49fc8a4b741a3a5737d8a44ba28551
Paul <paul.r.albert@gmail.com>
2017-11-17 09:26:06 -0500

    Finish users table and add material dropdown

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material';
 import { MatChipsModule } from '@angular/material';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   exports: [
-    MatTableModule, MatChipsModule, MatIconModule
+    MatTableModule, MatChipsModule, MatIconModule, MatSelectModule, MatFormFieldModule, MatButtonModule
   ]
 })
 export class MaterialModule {}
