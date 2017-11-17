8cb3f99e1fb1ee27993382f9fc022003a0466457
Raju Chaudhary <razu_latakta@hotmail.com>
2017-11-17 17:10:13 +0545

    material design for community

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material';
 import { MatChipsModule } from '@angular/material';
 import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule, MatButtonModule, MatFormFieldModule, MatSelectModule} from '@angular/material';
 
 @NgModule({
   exports: [
-    MatTableModule, MatChipsModule, MatIconModule
+    MatTableModule, MatChipsModule, MatIconModule, MatPaginatorModule, MatButtonModule, MatFormFieldModule, MatSelectModule
   ]
 })
 export class MaterialModule {}
