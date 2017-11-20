53eb2ee51c37334e4222ec6ffd0af655400e15a0
amanuely <aafework@middlebury.edu>
2017-11-20 17:08:36 -0500

    updated feedback page

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { CouchService } from '../shared/couchdb.service';
 
-export class Feedback{
-  name:string
-  isUrgent:boolean;
-  feedbackType:string;
-  feedbackMsg:string;
+export class Feedback {
+  name: string;
+  isUrgent: boolean;
+  feedbackType: string;
+  feedbackMsg: string;
 }
 
 @Component({
@@ -17,37 +17,36 @@ export class Feedback{
 })
 
 export class FeedbackComponent implements OnInit {
-  msgForUsr:string;
-  feedback:Feedback= new Feedback();
-  message:string;
-  fedbkSubmitted:string;
+  msgForUsr: string;
+  feedback: Feedback= new Feedback();
+  message: string;
+  fedbkSubmitted: string;
 
   constructor(
     private userService: UserService,
-    private couchService:CouchService
+    private couchService: CouchService
     ) { }
 
   ngOnInit() {
-    this.feedback.name= this.userService.get().name;
+    this.feedback.name = this.userService.get().name;
   }
 
-  submitfeedback(){
+  submitfeedback() {
     if (!this.feedback.feedbackMsg) {
-      this.message='';
-      this.fedbkSubmitted='';
-      this.msgForUsr="Feedback cannot be empty!";
-    }else
-    {
+      this.message = '';
+      this.fedbkSubmitted = '';
+      this. msgForUsr = 'Feedback cannot be empty!';
+    } else {
       this.couchService.post('feedback/', this.feedback)
       .then((data) => {
-        this.msgForUsr='';
-        this.message='';
-        this.fedbkSubmitted="Thank you! We have received your feedback.";
+        this.msgForUsr = '';
+        this.message = '';
+        this.fedbkSubmitted = 'Thank you! We have received your feedback.';
       },
       (error) => {
-        this.msgForUsr='';
-        this.fedbkSubmitted='';
-        this.message = "Sorry, your feedback could not be submitted!";
+        this.msgForUsr = '';
+        this.fedbkSubmitted = '';
+        this.message = 'Sorry, your feedback could not be submitted!';
       });
     }
   }
