93cd90d9a1179755a31c790cd77193b508376f72
Raju Chaudhary <razu_latakta@hotmail.com>
2017-11-21 16:39:43 +0545

    nation validator

---
+++ design/nations/nation-validators.js
@@ -0,0 +1,13 @@
+unction (newDoc, oldDoc, userCtx, secObj) {
+  function require(field) {
+    var errMessage = field + ' is required';
+    if (!newDoc[field]) {
+      throw { forbidden: errMessage };
+    }
+  }
+  if (!newDoc) {
+    require('name');
+    require('adminName');
+    require('nationUrl');
+  }
+}
