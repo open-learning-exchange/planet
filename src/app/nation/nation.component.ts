3f14923c2cd81a616a104576bd479732ebb59024
pr0chin <smaharjan@himalayantechies.com>
2017-11-23 16:32:18 +0545

    [#194] Fixed submit nation and data render

--- src/app/nation/nation.component.ts
+++ src/app/nation/nation.component.ts
@@ -44,9 +44,7 @@ export class NationComponent implements OnInit, AfterViewInit {
     private nationValidatorService: NationValidatorService,
     private dialog: MatDialog,
     private dialogsFormService: DialogsFormService
-  ) {
-    this.createForm();
-  }
+  ) {}
 
   ngOnInit() {
     this.getNationList();
@@ -63,22 +61,12 @@ export class NationComponent implements OnInit, AfterViewInit {
     this.nations.filter = filterValue;
   }
 
-  createForm() {
-    this.nationForm = this.fb.group({
-      adminName: [ '', Validators.required,
-        // an arrow function is for lexically binding 'this' otherwise 'this' would be undefined
-        ac => this.nationValidatorService.nationCheckerService$(ac)
-      ],
-      name: [ '', Validators.required ],
-      nationUrl: [ '', Validators.required ],
-    });
-  }
-
   getNationList() {
     this.couchService.get(this.dbName + '/_all_docs?include_docs=true')
       .then((data) => {
         // _all_docs returns object with rows array of objects with 'doc' property that has an object with the data.
         // Map over data.rows to remove the 'doc' property layer
+        console.log(data.rows.map(nation => nation.doc))
         this.nations.data = data.rows.map(nation => nation.doc);
       }, (error) => this.message = 'There was a problem getting NationList');
   }
@@ -111,7 +99,6 @@ export class NationComponent implements OnInit, AfterViewInit {
   }
 
   onSubmit(nation) {
-    console.log(this.nationForm.valid);
     if (nation) {
       const formdata = {
         'admin_name': nation.adminName,
@@ -124,7 +111,7 @@ export class NationComponent implements OnInit, AfterViewInit {
           formdata[ '_id' ] = data.id;
           formdata[ '_rev' ] = data.rev;
           this.nations.data.push(formdata);
-          console.log(this.nations.data);
+          this.getNationList();
         }, (error) => this.message = 'Error');
     } else {
       // Using (<any>Object) allows you to iterate over the actual object refs rather than the keys in TypeScript
@@ -154,7 +141,6 @@ export class NationComponent implements OnInit, AfterViewInit {
     this.dialogsFormService
       .confirm(title, type, fields, validation, '')
       .subscribe((res) => {
-        console.log('Res', res);
         this.onSubmit(res);
       });
   }
