a8e9aa1f2390d88ebe5d5ac762ab89e612ff4bc1
Laxman Maharjan <email@mlaxman.com.np>
2017-11-28 10:00:02 +0545

    removing oninit

--- src/app/manager-dashboard/manager-dashboard.component.ts
+++ src/app/manager-dashboard/manager-dashboard.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   template: `
     <div><a routerLink="/community" i18n mat-raised-button>Communities</a> <a routerLink="/nation" i18n mat-raised-button>Nations</a></div>
   `
 })
-export class ManagerDashboardComponent implements OnInit {
+export class ManagerDashboardComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
