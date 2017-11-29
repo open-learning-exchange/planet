da6f87c6866a458d2c2890cfe811cfe0a4f81e00
Paul <paul.r.albert@gmail.com>
2017-11-29 15:37:52 -0500

    One request function

--- src/app/shared/couchdb.service.ts
+++ src/app/shared/couchdb.service.ts
@@ -12,38 +12,30 @@ export class CouchService {
     return Object.assign({}, this.defaultOpts, opts) || this.defaultOpts;
   }
 
+  private couchDBReq(type: string, db: string, opts: any, data?: any) {
+    const url = this.baseUrl + db;
+    if (type === 'post' || type === 'put') {
+      return this.http[type](url, data, opts).toPromise();
+    }
+    return this.http[type](url, opts).toPromise();
+  }
+
   constructor(private http: HttpClient) {}
 
   put(db: string, data: any, opts?: any): Promise<any> {
-
-    return this.http
-      .put(this.baseUrl + db, JSON.stringify(data) || '', this.setOpts(opts))
-      .toPromise();
+    return this.couchDBReq('put', db, this.setOpts(opts), JSON.stringify(data) || '');
   }
 
   post(db: string, data: any, opts?: any): Promise<any> {
-
-    return this.http
-      .post(this.baseUrl + db, JSON.stringify(data) || '', this.setOpts(opts))
-      .toPromise();
+    return this.couchDBReq('post', db, this.setOpts(opts), JSON.stringify(data) || '');
   }
 
   get(db: string, opts?: any): Promise<any> {
-    const url = this.baseUrl + db;
-    opts = this.setOpts(opts);
-
-    return this.http
-      .get(url, opts)
-      .toPromise();
+    return this.couchDBReq('get', db, this.setOpts(opts));
   }
 
   delete(db: string, opts?: any): Promise<any> {
-    const url = this.baseUrl + db;
-    opts = this.setOpts(opts);
-
-    return this.http
-      .delete(url, opts)
-      .toPromise();
+    return this.couchDBReq('delete', db, this.setOpts(opts));
   }
 
 }
