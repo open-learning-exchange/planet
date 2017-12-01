63e6c976390121b90ffb3799cbbab82aed4dada2
pr0chin <smaharjan@himalayantechies.com>
2017-12-01 12:46:39 +0545

    Laguage translation with AOT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -21,7 +21,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppComponent, PageNotFoundComponent
   ],
   providers: [
-    CouchService, AuthService, UserService
+    CouchService, AuthService, UserService, { provide: LOCALE_ID, useValue: 'ne' }
   ],
   bootstrap: [ AppComponent ]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, TRANSLATIONS, TRANSLATIONS_FORMAT  } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -8,4 +8,17 @@ if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// use the require method provided by webpack
+declare const require;
+// we use the webpack raw-loader to return the content as a string
+
+const translations = require(`raw-loader!./i18n/messages.ne.xlf`);
+
+platformBrowserDynamic().bootstrapModule(AppModule, {
+
+  providers: [
+    { provide: TRANSLATIONS, useValue: translations },
+    { provide: TRANSLATIONS_FORMAT, useValue: 'xlf' }
+  ]
+
+});
