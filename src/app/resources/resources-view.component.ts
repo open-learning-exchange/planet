242a900f06787401debe1021e2f0698560372037
Rupesh87 <rupandroo@gmail.com>
2017-12-05 15:28:45 +0545

    [#181] Resourse view

--- src/app/resources/resources-view.component.ts
+++ src/app/resources/resources-view.component.ts
@@ -20,6 +20,7 @@ import { environment } from '../../environments/environment';
         Browser not supported
       </audio>
       <object [data]="pdfSrc" *ngSwitchCase="'pdf'" width="800" height="600"></object>
+      <div *ngSwitchCase="'other'"><a class='btn btn-primary' href={{resourceSrc}}>Open File</a></div>
     </div>
   `
 })
@@ -37,6 +38,7 @@ export class ResourcesViewComponent implements OnInit {
   pdfSrc: any;
   contentType = '';
   urlPrefix = environment.couchAddress + 'resources/';
+  url: any;
 
   ngOnInit() {
     this.route.paramMap.pipe(switchMap((params: ParamMap) => this.getResource(params.get('id'))))
@@ -47,8 +49,8 @@ export class ResourcesViewComponent implements OnInit {
     return this.couchService.get('resources/' + id)
       .then((data) => {
         this.mediaType = data.mediaType;
-        this.contentType = data._attachments[data.filename].content_type;
-        this.resourceSrc = this.urlPrefix + data._id + '/' + data.filename;
+        this.contentType = data._attachments[Object.keys(data._attachments)[0]].content_type;
+        this.resourceSrc = this.urlPrefix + data._id + '/' + Object.keys(data._attachments)[0];
         if (this.mediaType === 'pdf') {
           this.pdfSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.resourceSrc);
         }
