e475f75450c9770181ae6a3f54c0e8f6128d85b1
Laxman Maharjan <email@mlaxman.com.np>
2017-12-06 12:50:29 +0545

    [#198] adding direction to template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'planet-app',
-  template: '<router-outlet></router-outlet>'
+  template: '<router-outlet i18n-dir dir="ltr"></router-outlet>'
 })
 export class AppComponent { }
