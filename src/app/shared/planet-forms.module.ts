9be8ea45fd6b5403012c7bba14c65eb6f7cdba60
Paul <paul.r.albert@gmail.com>
2017-12-05 20:38:26 -0500

    Update form error messages for translation & material

--- src/app/shared/planet-forms.module.ts
+++ src/app/shared/planet-forms.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from './material.module';
 import { FormErrorMessagesComponent } from './form-error-messages.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule, MaterialModule
   ],
   exports: [
     FormErrorMessagesComponent
