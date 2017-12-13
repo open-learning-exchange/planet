e4500261d4336f37de26f5b5d5269c078fd9c625
nirojdyola <nirojdyola@gmail.com>
2017-12-13 15:28:07 +0000

    Delete Button added in Resource list (Fixes#229) (#236)

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CouchService } from '../shared/couchdb.service';
 import { Headers } from '@angular/http';
+import { DialogsDeleteComponent } from '../shared/dialogs/dialogs-delete.component';
+import { MatDialog } from '@angular/material';
 
 @Component({
   templateUrl: './resources.component.html'
@@ -13,6 +15,7 @@ export class ResourcesComponent implements OnInit {
   message = '';
   file: any;
   resource = { mediaType: '' };
+  deleteDialog: any;
 
   getRating(sum, timesRated) {
     this.rating = 0;
@@ -24,7 +27,7 @@ export class ResourcesComponent implements OnInit {
     return this.rating;
   }
 
-  constructor(private couchService: CouchService) {}
+  constructor(private couchService: CouchService, private dialog: MatDialog) {}
 
   ngOnInit() {
     this.getResources();
@@ -40,4 +43,30 @@ export class ResourcesComponent implements OnInit {
         this.resources = data.rows;
       }, error => (this.message = 'Error'));
   }
+
+  deleteClick(resource) {
+    this.deleteDialog = this.dialog.open(DialogsDeleteComponent, {
+      data: {
+        okClick: this.deleteResource(resource),
+        type: 'resource',
+        displayName: resource.title
+      }
+    });
+    // Reset the message when the dialog closes
+    this.deleteDialog.afterClosed().subscribe(() => {
+      this.message = '';
+    });
+  }
+
+  deleteResource(resource) {
+    return () => {
+      const { _id: resourceId, _rev: resourceRev } = resource;
+      this.couchService.delete('resources/' + resourceId + '?rev=' + resourceRev)
+        .then((data) => {
+          this.resources = this.resources.filter((res: any) => data.id !== res.id);
+          this.deleteDialog.close();
+        }, (error) => this.deleteDialog.componentInstance.message = 'There was a problem deleting this resource.');
+    };
+  }
+
 }
