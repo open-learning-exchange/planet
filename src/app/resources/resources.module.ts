721b8b693d83ed873a2c6be2d0e88a5cec8de5ec
Paul <paul.r.albert@gmail.com>
2017-12-15 17:53:33 -0500

    Use iframe to view HTML resources

--- src/app/resources/resources.module.ts
+++ src/app/resources/resources.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PlanetFormsModule } from '../shared/planet-forms.module';
 import { ResourcesComponent } from './resources.component';
-import { ResourcesViewComponent } from './resources-view.component';
+import { ResourcesViewComponent } from './view-resources/resources-view.component';
 import { ResourcesAddComponent } from './resources-add.component';
 import { ResourcesRouterModule } from './resources-router.module';
 import { MaterialModule } from '../shared/material.module';
--- src/app/resources/view-resources/resources-view.component.ts
+++ src/app/resources/view-resources/resources-view.component.ts
@@ -1,27 +1,19 @@
 import { switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
-import { CouchService } from '../shared/couchdb.service';
+import { CouchService } from '../../shared/couchdb.service';
 
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Router } from '@angular/router';
-import { environment } from '../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Component({
-  template: `
-    <div class="km-resource-view" [ngSwitch]="mediaType">
-      <img [src]="resourceSrc" *ngSwitchCase="'image'">
-      <video controls *ngSwitchCase="'video'">
-        <source [src]="resourceSrc" [type]="contentType" />
-        Browser not supported
-      </video>
-      <audio controls *ngSwitchCase="'audio'">
-        <source [src]="resourceSrc" [type]="contentType" />
-        Browser not supported
-      </audio>
-      <object [data]="pdfSrc" *ngSwitchCase="'pdf'" width="800" height="600"></object>
-      <div *ngSwitchCase="'other'"><a class='btn btn-primary' href={{resourceSrc}}>Open File</a></div>
-    </div>
+  templateUrl: './resources-view.component.html',
+  styles: [ `
+    :host iframe {
+      width: 80vw,
+      height: 80vh
+    }
   `
 })
 export class ResourcesViewComponent implements OnInit {
@@ -53,12 +45,8 @@ export class ResourcesViewComponent implements OnInit {
         this.mediaType = data.mediaType;
         this.contentType = data._attachments[filename].content_type;
         this.resourceSrc = this.urlPrefix + data._id + '/' + filename;
-        if (this.mediaType === 'pdf') {
+        if (this.mediaType === 'pdf' || this.mediaType === 'HTML') {
           this.pdfSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.resourceSrc);
-        }
-        if (this.mediaType === 'zip') {
-          this.router.navigate([ '/resources' ]);
-          window.open(this.resourceSrc);
         }
         return data;
       }, (error) => console.log('Error'));
