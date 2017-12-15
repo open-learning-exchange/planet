f76c6ec4c253e99899b3906a3211df628cf12a6a
Paul <paul.r.albert@gmail.com>
2017-12-15 16:07:15 -0500

    Modify userHandler to use credentials when deleting

--- e2e/userHandler.ts
+++ e2e/userHandler.ts
@@ -17,11 +17,11 @@ module.exports = function(timeStamp) {
       });
     },
     delete: function() {
-      return rp('http://127.0.0.1:5984/_users/org.couchdb.user:' + user).then(function(res) {
+      return rp('http://' + user + ':e2e@127.0.0.1:5984/_users/org.couchdb.user:' + user).then(function(res) {
         const rev = JSON.parse(res)._rev;
         return rp({
           method: 'DELETE',
-          uri: 'http://127.0.0.1:5984/_users/org.couchdb.user:' + user + '?rev=' + rev,
+          uri: 'http://' + user + ':e2e@127.0.0.1:5984/_users/org.couchdb.user:' + user + '?rev=' + rev,
           headers: { 'Content-Type': 'application/json' },
           json: true
         });
