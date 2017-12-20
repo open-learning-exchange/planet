305f16c0aa883e83b28aca5d1307a97973ad6d1a
Raju Chaudhary <razu_latakta@hotmail.com>
2017-12-20 16:53:21 +0545

    [#260] scss lint tested on login.scss

--- src/app/courses/request-courses/courses-request.component.ts
+++ src/app/courses/request-courses/courses-request.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: 'courses-request.component.html'
+  templateUrl: 'courses-request.component.html',
+  styleUrls: [ 'courses-request.component.scss' ]
 })
 export class CoursesRequestComponent {}
