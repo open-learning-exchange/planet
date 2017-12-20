9d0e4a478ece1dfc7ee0ff4881b637947ded8b37
Xing Yao <yaoxing1990@gmail.com>
2017-12-20 14:20:43 -0600

    Changed courses to course

--- src/app/courses/view-course/course-view.component.ts
+++ src/app/courses/view-course/course-view.component.ts
@@ -7,7 +7,7 @@ import { CouchService } from '../../shared/couchdb.service';
 })
 export class CourseViewComponent implements OnInit, OnDestroy {
   id: string;
-  courses: any;
+  course: any;
   private sub: any;
 
   constructor(
@@ -21,7 +21,7 @@ export class CourseViewComponent implements OnInit, OnDestroy {
     });
     this.couchService.get('courses/' + this.id)
       .then((data) => {
-        this.courses = data.courseTitle;
+        this.course = data.courseTitle;
       });
   }
 
