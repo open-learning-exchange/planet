a439eaddb6ac2448d2bf9d6d5f03d4852490c139
Xing Yao <yaoxing1990@gmail.com>
2017-12-19 23:38:27 -0600

    Changed id to title inside each button function

--- src/app/courses/open-course/course-open.component.ts
+++ src/app/courses/open-course/course-open.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CouchService } from '../../shared/couchdb.service';
 
 @Component({
   templateUrl: './course-open.component.html'
 })
 export class CourseOpenComponent implements OnInit, OnDestroy {
   id: string;
+  courses: any;
   private sub: any;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private couchService: CouchService
+  ) {}
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = params['id'];
     });
+    this.couchService.get('courses/' + this.id)
+      .then((data) => {
+        this.courses = data.courseTitle;
+      }); 
   }
 
   ngOnDestroy() {
