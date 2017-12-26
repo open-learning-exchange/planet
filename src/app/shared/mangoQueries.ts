82280212e85baacf93a6ee1f475a02ae67cb840e
Paul <paul.r.albert@gmail.com>
2017-12-25 22:31:13 -0500

    Add option to omit fields or sort from mangoQuery

--- src/app/shared/mangoQueries.ts
+++ src/app/shared/mangoQueries.ts
@@ -16,12 +16,17 @@ export function findAllDocuments(selector, query) {
 }
 
 // Creates more general find query that can search with multiple selectors & fields
-export function findDocuments(selectors, fields, sort = {}, limit = 1, skip = 0) {
-  return {
+export function findDocuments(selectors, fields: any = 0, sort: any = 0, limit = 1, skip = 0) {
+  let query: any = {
     'selector': selectors,
-    'fields': fields,
-    'sort': sort,
     'limit': limit,
     'skip': skip
   };
+  if(fields !== 0) {
+    query.fields = fields;
+  }
+  if(sort !== 0) {
+    query.sort = sort;
+  }
+  return query;
 }
