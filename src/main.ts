42eddb82d3fa8deff43cbc0b636927bf9bb555b7
Paul <paul.r.albert@gmail.com>
2017-12-27 11:29:30 -0500

    Fix debug operator for unit tests

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import { debugOperator } from './debug-operator';
+import { debugOperator } from './app/debug-operator';
 import { Observable } from 'rxjs/Observable';
 
 Observable.prototype.debug = debugOperator;
