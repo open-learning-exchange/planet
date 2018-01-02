d9a7058205c70665a4c95fe767b4d9ecf15ddb4a
Rupesh87 <rupandroo@gmail.com>
2018-01-02 10:17:54 +0545

    Show resources from remote nation (fixes #234) (#238)

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { CouchService } from '../shared/couchdb.service';
 import { DialogsDeleteComponent } from '../shared/dialogs/dialogs-delete.component';
 import { MatTableDataSource, MatPaginator, MatFormField, MatFormFieldControl, MatDialog, MatDialogRef } from '@angular/material';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
 
 @Component({
   templateUrl: './resources.component.html'
@@ -16,6 +20,7 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
   message = '';
   file: any;
   deleteDialog: any;
+  nationName = '';
 
   getRating(sum, timesRated) {
     let rating = 0;
@@ -26,7 +31,12 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
     return (rating * 20) + '%';
   }
 
-  constructor(private couchService: CouchService, private dialog: MatDialog) {}
+  constructor(
+    private couchService: CouchService,
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private httpclient: HttpClient
+  ) {}
 
   ngOnInit() {
     this.getResources();
@@ -43,12 +53,37 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
     this.resources.filter = filterResValue.trim().toLowerCase();
   }
 
-  getResources() {
-    this.couchService
-      .get('resources/_all_docs?include_docs=true')
-      .subscribe(data => {
-        this.resources.data = data.rows.map(res => res.doc);
+  getExternalResources() {
+    this.couchService.post('nations/_find',
+    { 'selector': { 'name': this.nationName },
+    'fields': [ 'name', 'nationurl' ] })
+      .pipe(switchMap(data => {
+        this.nationName = data.docs[0].name;
+        const nationUrl = data.docs[0].nationurl;
+        if (nationUrl) {
+          return this.httpclient.jsonp('http://' + nationUrl +
+            '/resources/_all_docs?include_docs=true&callback=JSONP_CALLBACK',
+            'callback'
+          );
+        }
+        // If there is no url, return an observable of an empty array
+        return of([]);
+      })).subscribe((res: any) => {
+        this.resources.data = res.rows.map(r => r.doc);
       }, error => (this.message = 'Error'));
+  }
+
+  getResources() {
+    this.nationName = this.route.snapshot.paramMap.get('nationname');
+    if (this.nationName !== null) {
+      this.getExternalResources();
+    } else {
+      this.couchService
+        .get('resources/_all_docs?include_docs=true')
+        .subscribe(data => {
+          this.resources.data = data.rows.map(res => res.doc);
+        }, error => (this.message = 'Error'));
+    }
   }
 
   deleteClick(resource) {
