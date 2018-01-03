107e6c44e284ec449cd515464085d351da96d796
Rupesh87 <rupandroo@gmail.com>
2018-01-03 16:54:02 +0545

    [283] Change password

--- src/app/users/user-profile/user-profile.component.ts
+++ src/app/users/user-profile/user-profile.component.ts
@@ -3,6 +3,10 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 import { CouchService } from '../../shared/couchdb.service';
 import { environment } from '../../../environments/environment';
 import { UserService } from '../../shared/user.service';
+import { CustomValidators } from '../../validators/custom-validators';
+import { Validators } from '@angular/forms';
+import { DialogsFormService } from '../../shared/dialogs/dialogs-form.service';
+import { useAnimation } from '@angular/core/src/animation/dsl';
 
 @Component({
   templateUrl: './user-profile.component.html'
@@ -18,7 +22,8 @@ export class UserProfileComponent implements OnInit {
   constructor(
     private couchService: CouchService,
     private route: ActivatedRoute,
-    private userService: UserService
+    private userService: UserService,
+    private dialogsFormService: DialogsFormService
   ) { }
 
   ngOnInit() {
@@ -39,5 +44,51 @@ export class UserProfileComponent implements OnInit {
     }, (error) => {
       console.log(error);
     });
+  }
+
+  onSubmit(data, userDetail) {
+      const formdata = {
+        'password': data.password,
+        '_rev': userDetail._rev,
+        'name': userDetail.name,
+        'type':'user',
+        'roles': userDetail.roles,
+        'firstName': userDetail.firstName,
+        'middleName': userDetail.middleName,
+        'lastName': userDetail.lastName,
+        'email': userDetail.email,
+        'language': userDetail.language,
+        'phoneNumber': userDetail.phoneNumber,
+        'birthDate': userDetail.birthDate,
+        'gender': userDetail.gender,
+        'level': userDetail.level,
+        '_attachments':userDetail._attachments
+      };
+      this.couchService.put(this.dbName+'/org.couchdb.user:'+userDetail.name, formdata)
+        .subscribe((res) => {
+          console.log('Success');
+        }, (error) => (error));
+  }
+
+  changePasswordForm(userDetail) {
+    const title = 'Change Password';
+    const type = 'user';
+    const fields =
+      [
+        { 'label': 'Password', 'type': 'textbox', 'name': 'password', 'placeholder': 'Password', 'required': true },
+        { 'label': 'Repeat Password', 'type': 'textbox', 'name': 'repeatPassword', 'placeholder': 'Repeat Password', 'required': true }
+      ];
+    const validation = {
+      password: [ '', Validators.required ],
+      repeatPassword: [ '', Validators.required ]
+    };
+    this.dialogsFormService
+      .confirm(title, type, fields, validation, '')
+      .debug('Dialog confirm')
+      .subscribe((res) => {
+        if (res !== undefined) {
+          this.onSubmit(res, userDetail);
+        }
+      });
   }
 }
