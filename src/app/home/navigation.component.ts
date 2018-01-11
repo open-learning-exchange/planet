fc2e33b61aeee2fd9dd6eae1f015a737430405b5
Niroj Dyola <nirojdyola@gmail.com>
2018-01-11 12:38:08 +0545

    [#287] data placed in shared folder

--- src/app/home/navigation.component.ts
+++ src/app/home/navigation.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { CouchService } from '../shared/couchdb.service';
 import { Router } from '@angular/router';
-import * as languages from '../../../design/languages/languages-mockup.json';
+import * as languages from '../shared/languages.json';
 
 @Component({
   selector: 'planet-navigation',
@@ -12,12 +12,13 @@ import * as languages from '../../../design/languages/languages-mockup.json';
       <li><a href="#" class="km-logout" (click)="logoutClick()">LOGOUT</a></li>
       <li><a routerLink="/manager"><mat-icon>person</mat-icon></a></li>
       <li>
-        <img *ngIf="selected_flag" src="{{selected_flag}}" i18n-alt alt="{{selected_lang}}" i18n-title title="{{selected_lang}}" />
+        <img *ngIf="current_flag" src="assets/flags/{{current_flag}}.png" i18n-alt alt="{{current_lang}}" i18n-title
+        title="{{current_lang}}" />
         <button mat-icon-button [matMenuTriggerFor]="language_menu">
           <mat-icon>more_vert</mat-icon>
         </button>
         <mat-menu #language_menu="matMenu">
-          <button mat-menu-item *ngFor="let language of languages" (click)="switchLanguage(language)">
+          <button mat-menu-item *ngFor="let language of languages" (click)="switchLanguage(language.served_url)">
             <img src="{{language.flag}}" i18n-title title="{{language.name}}" i18n-alt alt="{{language.name}}" />
             <span>{{language.short_code}}</span>
           </button>
@@ -33,8 +34,8 @@ import * as languages from '../../../design/languages/languages-mockup.json';
 export class NavigationComponent implements OnInit {
 
   languages = [];
-  selected_flag = '';
-  selected_lang = '';
+  current_flag = 'en';
+  current_lang = 'English';
   name = '';
   roles: string[] = [];
 
@@ -56,12 +57,19 @@ export class NavigationComponent implements OnInit {
 
   ngOnInit() {
     Object.assign(this, this.userService.get());
-    this.languages = (<any>languages);
+    this.languages = (<any>languages).map(language => {
+      if (language.served_url === document.baseURI) {
+        this.current_flag = language.short_code;
+        this.current_lang = language.name;
+      }
+      return language;
+    }).filter(lang  => {
+      return lang['active'] !== 'N';
+    });
   }
 
-  switchLanguage(lang) {
-    this.selected_flag = lang.flag;
-    this.selected_lang = lang.name;
+  switchLanguage(served_url) {
+    alert('You are going to switch in ' + served_url + ' environment');
   }
 
   logoutClick() {
