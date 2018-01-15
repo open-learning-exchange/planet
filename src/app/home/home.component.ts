54de4037de47cd5993c836a5917d275946e5bc9f
Paul <paul.r.albert@gmail.com>
2018-01-15 18:25:20 -0500

    Add new navigation (top & side) to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../shared/user.service';
+import { CouchService } from '../shared/couchdb.service';
+import { Router } from '@angular/router';
 
 // Main page once logged in.  At this stage is more of a placeholder.
 @Component({
-  template: `
-    <planet-navigation></planet-navigation>
-    <main>
-      <router-outlet></router-outlet>
-    </main>
-  `,
+  templateUrl: './home.component.html',
   styleUrls: [ './home.scss' ]
 })
-export class HomeComponent {}
+export class HomeComponent {
+  name = '';
+  roles: string[] = [];
+  constructor(
+    private couchService: CouchService,
+    private router: Router,
+    private userService: UserService
+  ) {}
+
+  logoutClick() {
+    this.couchService.delete('_session', { withCredentials: true }).subscribe((data: any) => {
+      if (data.ok === true) {
+        this.router.navigate([ '/login' ], {});
+      }
+    });
+  }
+
+  ngOnInit() {
+    Object.assign(this, this.userService.get());
+  }
+
+
+  backgroundRoute() {
+    const routesWithBackground = [ 'resources' ];
+    return routesWithBackground.findIndex((route) => this.router.url.indexOf(route) > -1) > -1;
+  }
+}
