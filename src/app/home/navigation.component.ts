50c8f8d4bfe48b773a34efc8c81daabc186c60c2
nirojdyola <nirojdyola@gmail.com>
2018-01-16 05:15:22 +0000

    Added language menu (Fixes #287) (#297)

--- src/app/home/navigation.component.ts
+++ src/app/home/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { CouchService } from '../shared/couchdb.service';
 import { Router } from '@angular/router';
+import { languages } from '../shared/languages';
 
 @Component({
   selector: 'planet-navigation',
@@ -9,6 +10,19 @@ import { Router } from '@angular/router';
     <ul>
       <li *ngFor="let comp of components"><a [routerLink]="'/' + comp.link">{{comp.name.toUpperCase()}}</a></li>
       <li><a href="#" class="km-logout" (click)="logoutClick()">LOGOUT</a></li>
+      <li><a routerLink="/manager"><mat-icon>person</mat-icon></a></li>
+      <li>
+        <button mat-icon-button [matMenuTriggerFor]="language_menu">
+          <img *ngIf="current_flag" src="assets/flags/{{current_flag}}.png" i18n-alt alt="{{current_lang}}" i18n-title
+        title="{{current_lang}}" />
+        </button>
+        <mat-menu #language_menu="matMenu">
+          <button mat-menu-item *ngFor="let language of languages" (click)="switchLanguage(language.served_url)">
+            <img src="assets/flags/{{language.short_code}}.png" i18n-title title="{{language.name}}" i18n-alt alt="{{language.name}}" />
+            <span>{{language.short_code}}</span>
+          </button>
+        </mat-menu>
+      </li>
       <li><a routerLink="/manager"><i class="material-icons">settings</i></a></li>
       <li *ngIf="roles.indexOf('_admin') === -1"><a routerLink="/users/profile/{{name}}"><mat-icon>person</mat-icon></a></li>
       <li><mat-icon [matMenuTriggerFor]="notification">notifications</mat-icon></li>
@@ -23,9 +37,15 @@ import { Router } from '@angular/router';
   `,
   styleUrls: [ './navigation.scss' ]
 })
+
 export class NavigationComponent implements OnInit {
+
+  languages = [];
+  current_flag = 'en';
+  current_lang = 'English';
   name = '';
   roles: string[] = [];
+
   constructor(
     private couchService: CouchService,
     private router: Router,
@@ -42,16 +62,29 @@ export class NavigationComponent implements OnInit {
     { link: 'feedback', name: 'Feedback' },
   ];
 
+  ngOnInit() {
+    Object.assign(this, this.userService.get());
+    this.languages = (<any>languages).map(language => {
+      if (language.served_url === document.baseURI) {
+        this.current_flag = language.short_code;
+        this.current_lang = language.name;
+      }
+      return language;
+    }).filter(lang  => {
+      return lang['active'] !== 'N';
+    });
+  }
+
+  switchLanguage(served_url) {
+    alert('You are going to switch in ' + served_url + ' environment');
+  }
+
   logoutClick() {
     this.couchService.delete('_session', { withCredentials: true }).subscribe((data: any) => {
       if (data.ok === true) {
         this.router.navigate([ '/login' ], {});
       }
     });
-  }
-
-  ngOnInit() {
-    Object.assign(this, this.userService.get());
   }
 
 }
