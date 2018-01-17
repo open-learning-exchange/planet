1d9627c9b7caead015eeb57d4f8caa7251613dac
Paul <paul.r.albert@gmail.com>
2018-01-16 21:44:49 -0500

    Sidenav expand to include words on click

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,22 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 import { UserService } from '../shared/user.service';
 import { CouchService } from '../shared/couchdb.service';
 import { Router } from '@angular/router';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { interval } from 'rxjs/observable/interval';
+import { tap } from 'rxjs/operators';
 
 // Main page once logged in.  At this stage is more of a placeholder.
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: [ './home.scss' ]
+  styleUrls: [ './home.scss' ],
+  animations: [
+    trigger('sidenavState', [
+      state('closed', style({
+        width: '72px';
+      })),
+      state('open', style({
+        width: '150px';
+      })),
+      transition('closed <=> open', animate('500ms ease'))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
   name = '';
   roles: string[] = [];
+  sidenavState: string = 'closed';
+  @ViewChild('content') private mainContent;
+
+  // Sets the margin for the main content to match the sidenav width
+  animObs = interval(15).pipe(tap(() => {
+    this.mainContent._updateContentMargins();
+    this.mainContent._changeDetectorRef.markForCheck();
+  }));
+  // For disposable returned by observer to unsubscribe
+  animDisp: any;
+
   constructor(
     private couchService: CouchService,
     private router: Router,
     private userService: UserService
   ) {}
+
+  ngOnInit() {
+    Object.assign(this, this.userService.get());
+  }
+
+  ngAfterViewInit() {
+    this.mainContent._updateContentMargins();
+    this.mainContent._changeDetectorRef.markForCheck();
+  }
+
+  backgroundRoute() {
+    const routesWithBackground = [ 'resources' ];
+    return routesWithBackground.findIndex((route) => this.router.url.indexOf(route) > -1) > -1;
+  }
+
+  toggleNav() {
+    this.sidenavState = this.sidenavState === 'open' ? 'closed' : 'open';
+    this.animDisp = this.animObs.subscribe();
+  }
+
+  endAnimation() {
+    if(this.animDisp) {
+      this.animDisp.unsubscribe();
+    }
+  }
 
   logoutClick() {
     this.couchService.delete('_session', { withCredentials: true }).subscribe((data: any) => {
@@ -24,14 +74,5 @@ export class HomeComponent implements OnInit {
         this.router.navigate([ '/login' ], {});
       }
     });
-  }
-
-  ngOnInit() {
-    Object.assign(this, this.userService.get());
-  }
-
-  backgroundRoute() {
-    const routesWithBackground = [ 'resources' ];
-    return routesWithBackground.findIndex((route) => this.router.url.indexOf(route) > -1) > -1;
   }
 }
