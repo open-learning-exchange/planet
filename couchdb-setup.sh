e015b1ab0f5f94bc414ae977c6345e3e0ac8717a
badri guragain <badri305@gmail.com>
2018-01-18 11:21:17 +0545

    [#310] changes as per comment on PR

--- couchdb-setup.sh
+++ couchdb-setup.sh
@@ -87,12 +87,13 @@ curl -X PUT $COUCHURL/_replicator
 curl -X PUT $COUCHURL/_global_changes
 
 # Add planet app databases
-curl -X PUT $COUCHURL/meetups
-curl -X PUT $COUCHURL/resources
-curl -X PUT $COUCHURL/courses
-curl -X PUT $COUCHURL/nations
 curl -X PUT $COUCHURL/communityregistrationrequests
+curl -X PUT $COUCHURL/courses
 curl -X PUT $COUCHURL/feedback
+curl -X PUT $COUCHURL/meetups
+curl -X PUT $COUCHURL/nations
+curl -X PUT $COUCHURL/ratings
+curl -X PUT $COUCHURL/resources
 
 # Add or update design docs
 upsert_doc courses _design/course-validators @./design/courses/course-validators.json
