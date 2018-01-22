644c3f3909b7db18ab2a2fa39a77aa59ac8e9d96
Jingting <jchen136@hawk.iit.edu>
2018-01-21 22:08:30 -0600

    add timestamp to articleDate

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -122,7 +122,7 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
         .get('resources/_all_docs?include_docs=true')
         .subscribe(data => {
           // Sort in descending articleDate order, so the new resource can be shown on the top
-          data.rows.sort((a, b) => 0 - (new Date(a.doc.articleDate) > new Date(b.doc.articleDate) ? 1 : -1));
+          data.rows.sort((a, b) => b.doc.articleDate - a.doc.articleDate);
           this.resources.data = data.rows.map(res => res.doc);
         }, error => (this.message = 'Error'));
     }
