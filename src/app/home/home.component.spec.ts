cbdf35b358bd8629c95486bfd55faac6a3c44778
Niroj Dyola <nirojdyola@gmail.com>
2018-01-23 14:12:37 +0545

    [#320] removed navigaton component from home spec

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { MaterialModule } from '../shared/material.module';
 import { HomeComponent } from './home.component';
-import { NavigationComponent } from './navigation.component';
 import { CouchService } from '../shared/couchdb.service';
 import { of } from 'rxjs/observable/of';
 
@@ -13,7 +12,7 @@ describe('Home', () => {
   const setup = () => {
     TestBed.configureTestingModule({
       imports: [ RouterTestingModule, CommonModule, HttpClientModule, MaterialModule ],
-      declarations: [ HomeComponent, NavigationComponent ],
+      declarations: [ HomeComponent ],
       providers: [ CouchService ]
     });
     const fixture = TestBed.createComponent(HomeComponent),
