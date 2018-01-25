df1bc0e4a63e8c2c25aa07464b2cc3c7371ed785
Niroj Dyola <nirojdyola@gmail.com>
2018-01-25 12:39:18 +0545

    [#319] Removed unused Directive

--- src/app/resources/view-resources/resources-view.component.ts
+++ src/app/resources/view-resources/resources-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, Directive } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CouchService } from '../../shared/couchdb.service';
 
 import { ActivatedRoute, ParamMap } from '@angular/router';
