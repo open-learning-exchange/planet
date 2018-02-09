554f2edad3b35193b70452e136dec0527ddde60a
MacNew <newmac100@gmail.com>
2018-02-09 10:58:40 +0545

    [#352] Dashboard dynamic style

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,10 +7,46 @@ import { UserService } from '../shared/user.service';
   templateUrl: './dashboard.component.html',
   styleUrls: [ './dashboard.scss' ]
 })
-export class DashboardComponent implements OnInit {
-  name = '';
-  roles: string[] = [];
 
+export class DashboardComponent implements OnInit {
+  myLibrary = [
+    { text: 'library one' },
+    { text: 'library two' },
+    { text: 'library three' },
+    { text: 'library four' },
+    { text: 'library five' },
+    { text: 'library six' },
+    { text: 'library seven' },
+    { text: 'library Eight' },
+    { text: 'library nine' },
+    { text: 'library ten' },
+  ];
+  myTeam = [
+    { text: 'team one' },
+    { text: 'team two' },
+    { text: 'team three' },
+    { text: 'team four' },
+    { text: 'team five' },
+    { text: 'team six' },
+  ];
+  myMeetups = [
+    { text: 'meetup one' },
+    { text: 'meetup two' },
+    { text: 'meetup three' },
+    { text: 'meetup four' },
+    { text: 'meetup five' },
+    { text: 'meetup six' },
+    { text: 'meetup seven' },
+  ];
+  myCourses = [
+  { text: 'courses one' },
+  { text: 'courses two' },
+  { text: 'courses three' },
+  { text: 'courses four' },
+  { text: 'courses five' },
+  { text: 'courses six' },
+  { text: 'courses seven' },
+  ];
   constructor(
     private userService: UserService
   ) {}
@@ -18,4 +54,44 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     Object.assign(this, this.userService.get());
   }
+
+  nextLibrary() {
+    this.myLibrary = this.myLibrary.concat( this.myLibrary.splice( 0, 4 ) );
+  }
+
+  previousLibrary() {
+    for ( let i = 0; i < 4; i++) {
+      this.myLibrary.unshift(this.myLibrary.pop());
+    }
+  }
+
+  nextMyTeam() {
+    this.myTeam = this.myTeam.concat( this.myTeam.splice( 0, 4) );
+  }
+
+  previousMyTeam() {
+    for ( let i = 0; i < 4; i++ ) {
+      this.myTeam.unshift( this.myTeam.pop() );
+    }
+  }
+
+  nextMyMeetups() {
+    this.myMeetups = this.myMeetups.concat( this.myMeetups.splice( 0, 4 ) );
+  }
+
+  previousMyMeetups() {
+    for ( let i = 0; i < 4; i++ ) {
+      this.myMeetups.unshift( this.myMeetups.pop() );
+    }
+  }
+
+  nextMyCourses() {
+    this.myCourses = this.myCourses.concat( this.myCourses.splice( 0, 4 ) );
+  }
+
+  previousMyCourses() {
+    for ( let i = 0; i < 4; i++ ) {
+      this.myCourses.unshift( this.myCourses.pop() );
+    }
+ }
 }
