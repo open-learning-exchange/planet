1eb82b4047046638a614768204c88cc03a771fd1
Paul <paul.r.albert@gmail.com>
2018-02-12 14:26:26 -0500

    Add new icons for dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,24 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 @Component({
   selector: 'planet-app',
   template: '<div i18n-dir dir="ltr"><router-outlet></router-outlet></div>'
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+    iconRegistry.addSvgIcon(
+      'myLibrary',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/library.svg'));
+    iconRegistry.addSvgIcon(
+      'myCourses',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/courses.svg'));
+    iconRegistry.addSvgIcon(
+      'myMeetups',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/meetups.svg'));
+    iconRegistry.addSvgIcon(
+      'myTeams',
+      sanitizer.bypassSecurityTrustResourceUrl('assets/icons/team.svg'));
+  }
+}
