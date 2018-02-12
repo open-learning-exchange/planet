4d2cbe4b1d85a097c0783548a91340e54cc8e000
Paul <paul.r.albert@gmail.com>
2018-02-12 16:58:48 -0500

    Add resource links to myLibrary and scroll button functionality

--- src/app/dashboard/dashboard-tile.component.ts
+++ src/app/dashboard/dashboard-tile.component.ts
@@ -1,34 +1,48 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 // Main page once logged in.  At this stage is more of a placeholder.
 @Component({
   selector: 'planet-dashboard-tile',
-  template: `
-    <mat-card class="dashboard-card">
-      <div class="left-tile accent-color">
-        <mat-icon svgIcon={{cardTitle}}></mat-icon>
-        <span>{{cardTitle}}</span>
-      </div>
-      <div class="right-tile">
-        <div class="dashboard-items">
-          <span class="dashboard-item" *ngFor="let i of items;let odd=odd" [ngClass]="{'bg-grey': odd}">{{'Item '+i}}</span>
-        </div>
-        <div class="dashboard-arrows bg-grey">
-          <span><mat-icon>keyboard_arrow_right</mat-icon></span>
-          <span><mat-icon>keyboard_arrow_left</mat-icon></span>
-        </div>
-      </div>
-    </mat-card>
-  `,
+  templateUrl: './dashboard-tile.component.html',
   styleUrls: [ './dashboard-tile.scss' ]
 })
-export class DashboardTileComponent {
+export class DashboardTileComponent implements OnInit, OnChanges {
   @Input() cardTitle: string;
   @Input() color: string;
-  items = Array(10).fill(0).map((val, ind, arr) => {
-    return ind
+  @Input() itemData;
+  items = Array(5).fill(0).map((val, ind, arr) => {
+    return ind;
   });
 
+  displayIndex = 0;
+  displayItems = [];
+
   constructor() { }
+
+  ngOnInit() {
+    if (this.itemData) {
+      this.items = [];
+    } else {
+      this.itemData = { items: [] };
+    }
+  }
+
+  ngOnChanges() {
+    this.resetDisplayItems();
+  }
+
+  resetDisplayItems() {
+    if (this.itemData) {
+      this.displayItems = this.itemData.items.slice(this.displayIndex, this.displayIndex + 5);
+    }
+  }
+
+  arrowClick(direction: number) {
+    let newIndex = this.displayIndex + (direction * 5);
+    newIndex = (newIndex < 0 || newIndex > this.itemData.items.length) ? 0 : newIndex;
+    this.displayIndex = newIndex;
+    this.resetDisplayItems();
+  }
+
 
 }
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 import { UserService } from '../shared/user.service';
+import { CouchService } from '../shared/couchdb.service';
 
 // Main page once logged in.  At this stage is more of a placeholder.
 @Component({
   template: `
-    <planet-dashboard-tile [cardTitle]="'myLibrary'" class="planet-library-theme"></planet-dashboard-tile>
+    <planet-dashboard-tile [cardTitle]="'myLibrary'" class="planet-library-theme" [itemData]="data.resources"></planet-dashboard-tile>
     <planet-dashboard-tile [cardTitle]="'myCourses'" class="planet-courses-theme"></planet-dashboard-tile>
     <planet-dashboard-tile [cardTitle]="'myMeetups'" class="planet-meetups-theme"></planet-dashboard-tile>
     <planet-dashboard-tile [cardTitle]="'myTeams'" class="planet-teams-theme"></planet-dashboard-tile>
@@ -22,12 +23,21 @@ import { UserService } from '../shared/user.service';
 export class DashboardComponent implements OnInit {
   name = '';
   roles: string[] = [];
+  data = { resources: { items: [] } };
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private couchService: CouchService
   ) {}
 
   ngOnInit() {
     Object.assign(this, this.userService.get());
+    this.getData('resources').subscribe((response) => {
+      this.data.resources = { linkPrefix: 'resources/view/', items: response.rows.map((item) => item.doc) };
+    });
+  }
+
+  getData(db: string) {
+    return this.couchService.get(db + '/_all_docs?include_docs=true');
   }
 }
