2ff5b806b5d2a1f23a68f1e65fa2a7b1ed4f398b
Samriddhi Basnet <sbasnet@himalayantechies.com>
2018-02-12 11:17:36 +0545

    [#267] worked on approval feature

--- src/app/shared/dialogs/planet-dialogs.module.ts
+++ src/app/shared/dialogs/planet-dialogs.module.ts
@@ -7,22 +7,22 @@ import { DialogsDeleteComponent } from './dialogs-delete.component';
 import { DialogsViewComponent } from './dialogs-view.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { PlanetFormsModule } from '../planet-forms.module';
-
+import { DialogsChangeComponent } from './dialogs-change.component';
 @NgModule({
   imports: [
     CommonModule, MaterialModule, FormsModule, ReactiveFormsModule, PlanetFormsModule
   ],
   exports: [
-    DialogsFormComponent, DialogsDeleteComponent, DialogsViewComponent
+    DialogsFormComponent, DialogsDeleteComponent, DialogsViewComponent, DialogsChangeComponent
   ],
   declarations: [
-    DialogsFormComponent, DialogsDeleteComponent, DialogsViewComponent
+    DialogsFormComponent, DialogsDeleteComponent, DialogsViewComponent, DialogsChangeComponent
   ],
   providers: [
     DialogsFormService
   ],
   entryComponents: [
-    DialogsFormComponent, DialogsDeleteComponent, DialogsViewComponent
+    DialogsFormComponent, DialogsDeleteComponent, DialogsViewComponent, DialogsChangeComponent
   ]
 })
 export class PlanetDialogsModule {}
