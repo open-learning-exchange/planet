0bd588e005998915d4b80c2f19bf52639e185a01
LKhadka <lawrence_2009@hotmail.com>
2018-02-13 18:20:47 -0600

    initialize pouchdb service with some config

--- src/app/shared/pouchdb.service.ts
+++ src/app/shared/pouchdb.service.ts
@@ -1,7 +1,22 @@
 import { Injectable } from '@angular/core';
 import PouchDB from 'pouchdb';
+import { environment } from '../../environments/environment';
+PouchDB.plugin(require('pouchdb-authentication'));
+// WHAT ARE WE TRYING TO DO ?
+// Create a database per new user sign up
+// Remember this is NoSQL not SQL
+// @TODO: Use a different method to configure the remote db in the app or login component
+// @FIXME: Probably will have to create multiple instances of remotedb to handle all different databases
 
 @Injectable()
 export class PouchdbService {
+  private baseUrl = environment.couchAddress;
+  private remoteDb: any;
+  private localDb: any;
+
   constructor() {}
+
+  public configureStore() {
+    this.localDb = new PouchDB('planet-local');
+  }
 }
