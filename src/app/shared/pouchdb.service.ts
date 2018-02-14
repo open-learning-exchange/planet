e411aad7aa56fbf9d0025e0bcf9d1df39a80141c
LKhadka <lawrence_2009@hotmail.com>
2018-02-13 18:35:11 -0600

    add basic config to add local and remote dbs

--- src/app/shared/pouchdb.service.ts
+++ src/app/shared/pouchdb.service.ts
@@ -14,9 +14,42 @@ export class PouchdbService {
   private remoteDb: any;
   private localDb: any;
 
-  constructor() {}
+  constructor() {
+    this.localDb = null;
+    this.remoteDb = null;
+  }
 
-  public configureStore() {
+  // call deconfigureStore whenever new user is signed in to configure a new db for the user and end the previous user's session
+  async configureStore(userId: string) {
+    await this.deconfigureStore();
+
     this.localDb = new PouchDB('planet-local');
+    this.remoteDb = new PouchDB(this.baseUrl + '/' + userId, {
+      skip_setup: true
+    });
+
+    this.localDb
+      .sync('planet-local', { live: true, retry: true })
+      .on('error', console.log.bind(console));
+  }
+
+  getDb() {
+    if (!this.localDb) {
+      throw new Error('Database has not yet been configured!');
+    }
+
+    return this.localDb;
+  }
+
+  async deconfigureStore() {
+    if (!this.localDb) {
+      return;
+    }
+
+    await this.localDb.close();
+    this.localDb = null;
+
+    await this.remoteDb.close();
+    this.remoteDb = null;
   }
 }
