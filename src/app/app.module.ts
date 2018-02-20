f1542bf64b5d5aead583d545b88a63fd3b8ea90f
Lawrence Khadka <LKhadka@users.noreply.github.com>
2018-02-19 23:04:39 -0600

    Add Service worker (#346)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -15,9 +16,18 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PlanetMessageService } from './shared/planet-message.service';
 import { MaterialModule } from './shared/material.module';
 
+import { environment } from '../environments/environment';
+
 @NgModule({
   imports: [
-    BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, MaterialModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    environment.production
+      ? ServiceWorkerModule.register('/ngsw-worker.js')
+      : []
   ],
   declarations: [
     AppComponent, PageNotFoundComponent
@@ -25,6 +35,6 @@ import { MaterialModule } from './shared/material.module';
   providers: [
     CouchService, AuthService, UserService, ValidatorService, PlanetMessageService
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
