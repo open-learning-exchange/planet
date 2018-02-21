bf6022f269deb53d71610a67904f009cc44d08a1
Machhindra <newmac1000@gmail.com>
2018-02-21 21:07:43 +0545

    Login form reactive & password required (fixes #413) (#414)

--- src/app/login/login-form.component.ts
+++ src/app/login/login-form.component.ts
@@ -3,6 +3,8 @@ import { CouchService } from '../shared/couchdb.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '../shared/user.service';
 import { switchMap } from 'rxjs/operators';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { CustomValidators } from '../validators/custom-validators';
 import { PlanetMessageService } from '../shared/planet-message.service';
 
 @Component({
@@ -10,23 +12,35 @@ import { PlanetMessageService } from '../shared/planet-message.service';
   styleUrls: [ './login.scss' ]
 })
 export class LoginFormComponent {
+  public userForm: FormGroup;
   constructor(
     private couchService: CouchService,
     private router: Router,
     private route: ActivatedRoute,
     private userService: UserService,
+    private formBuilder: FormBuilder,
     private planetMessageService: PlanetMessageService
-  ) {}
+  ) {
+    const formObj = this.createMode ? Object.assign({}, loginForm, repeatPassword) : loginForm;
+    this.userForm = this.formBuilder.group(formObj);
+  }
 
   createMode: boolean = this.router.url.split('?')[0] === '/login/newuser';
   returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  message = '';
   model = { name: '', password: '', repeatPassword: '' };
 
   onSubmit() {
-    if (this.createMode) {
-      this.createUser(this.model);
+    if (this.userForm.valid) {
+      if (this.createMode) {
+        this.createUser(this.userForm.value);
+      } else {
+        this.login(this.userForm.value, false);
+      }
     } else {
-      this.login(this.model, false);
+      Object.keys(this.userForm.controls).forEach(fieldName => {
+        this.userForm.controls[fieldName].markAsTouched();
+      });
     }
   }
 
@@ -34,7 +48,7 @@ export class LoginFormComponent {
     const data = {
       'user': user_id,
       'message': 'Welcome ' + user_id.replace('org.couchdb.user:', '') + ' to the Planet Learning',
-     'link': '',
+      'link': '',
       'type': 'register',
       'priority': 1,
       'status': 'unread',
@@ -48,18 +62,14 @@ export class LoginFormComponent {
     this.router.navigate([ this.returnUrl ]);
   }
 
-  createUser({ name, password, repeatPassword }: {name: string, password: string, repeatPassword: string}) {
-    if (password === repeatPassword) {
-      this.couchService.put('_users/org.couchdb.user:' + name.toLowerCase(),
-      { 'name': name.toLowerCase(), 'password': password, 'roles': [], 'type': 'user', 'isUserAdmin': false })
+  createUser({ name, password }: {name: string, password: string}) {
+    this.couchService.put('_users/org.couchdb.user:' + name,
+      { 'name': name, 'password': password, 'roles': [], 'type': 'user', 'isUserAdmin': false })
         .subscribe((data) => {
           this.planetMessageService.showMessage('User created: ' + data.id.replace('org.couchdb.user:', ''));
           this.welcomeNotification(data.id);
-          this.login(this.model, true);
-        }, (error) => this.planetMessageService.showMessage('There was an error creating a new user'));
-    } else {
-        this.planetMessageService.showMessage('Passwords do not match');
-    }
+          this.login(this.userForm.value, true);
+        }, (error) => this.message = '');
   }
 
   login({ name, password }: {name: string, password: string}, isCreate: boolean) {
@@ -76,3 +86,19 @@ export class LoginFormComponent {
       }, (error) => this.planetMessageService.showMessage('Username and/or password do not match'));
   }
 }
+
+const repeatPassword = {
+  password: [ '', Validators.compose([
+    Validators.required,
+    CustomValidators.matchPassword('repeatPassword', false)
+    ]) ],
+  repeatPassword: [ '', Validators.compose([
+    Validators.required,
+    CustomValidators.matchPassword('password', true)
+    ]) ]
+};
+
+const loginForm = {
+  name: [ '', Validators.required ],
+  password: [ '', Validators.required ]
+};
