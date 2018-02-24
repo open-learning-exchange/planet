352e221f830b9d4a99c1682336bc42871224090e
LKhadka <l.khadka@outlook.com>
2018-02-24 13:25:33 -0600

    create basic auth flow in pouch

--- src/app/login/login-form.component.ts
+++ src/app/login/login-form.component.ts
@@ -3,18 +3,20 @@ import { CouchService } from '../shared/couchdb.service';
 import { Router, ActivatedRoute } from '@angular/router';
 import { UserService } from '../shared/user.service';
 import { switchMap } from 'rxjs/operators';
+import { PouchdbService } from '../shared/pouchdb.service';
 
 @Component({
   templateUrl: './login-form.component.html',
-  styleUrls: [ './login.scss' ]
+  styleUrls: ['./login.scss']
 })
 export class LoginFormComponent {
   constructor(
     private couchService: CouchService,
+    private pouchdbService: PouchdbService,
     private router: Router,
     private route: ActivatedRoute,
     private userService: UserService
-  ) { }
+  ) {}
 
   createMode: boolean = this.router.url.split('?')[0] === '/login/newuser';
   returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
@@ -31,50 +33,74 @@ export class LoginFormComponent {
 
   welcomeNotification(user_id) {
     const data = {
-      'user': user_id,
-      'message': 'Welcome ' + user_id.replace('org.couchdb.user:', '') + ' to the Planet Learning',
-     'link': '',
-      'type': 'register',
-      'priority': 1,
-      'status': 'unread',
-      'time': Date.now()
+      user: user_id,
+      message:
+        'Welcome ' +
+        user_id.replace('org.couchdb.user:', '') +
+        ' to the Planet Learning',
+      link: '',
+      type: 'register',
+      priority: 1,
+      status: 'unread',
+      time: Date.now()
     };
-    this.couchService.post('notifications', data)
-      .subscribe((res) => {
-        console.log(res);
-      }, (error) => this.message = 'Error');
+    this.couchService.post('notifications', data).subscribe(res => {
+      console.log(res);
+    }, error => (this.message = 'Error'));
   }
 
   reRoute() {
-    this.router.navigate([ this.returnUrl ]);
+    this.router.navigate([this.returnUrl]);
   }
 
-  createUser({ name, password, repeatPassword }: {name: string, password: string, repeatPassword: string}) {
+  async createUser({
+    name,
+    password,
+    repeatPassword
+  }: {
+    name: string;
+    password: string;
+    repeatPassword: string;
+  }) {
     if (password === repeatPassword) {
-      this.couchService.put('_users/org.couchdb.user:' + name,
-      { 'name': name, 'password': password, 'roles': [], 'type': 'user', 'isUserAdmin': false })
-        .subscribe((data) => {
-          this.message = 'User created: ' + data.id.replace('org.couchdb.user:', '');
-          this.welcomeNotification(data.id);
-          this.login(this.model, true);
-        }, (error) => this.message = '');
+      this.pouchdbService.signupUser(name, password);
+      this.reRoute();
+      // this.couchService
+      //   .put('_users/org.couchdb.user:' + name, {
+      //     name: name,
+      //     password: password,
+      //     roles: [],
+      //     type: 'user',
+      //     isUserAdmin: false
+      //   })
+      //   .subscribe(data => {
+      //     this.message =
+      //       'User created: ' + data.id.replace('org.couchdb.user:', '');
+      //     this.welcomeNotification(data.id);
+      //     this.login(this.model, true);
+      //   }, error => (this.message = ''));
     } else {
       this.message = 'Passwords do not match';
     }
   }
 
-  login({ name, password }: {name: string, password: string}, isCreate: boolean) {
-    this.couchService.post('_session', { 'name': name, 'password': password }, { withCredentials: true })
-      .pipe(switchMap((data) => {
-        // Post new session info to login_activity
-        this.userService.set(data);
-        return this.userService.newSessionLog();
-      })).subscribe((res) => {
-        if (isCreate) {
-          this.router.navigate( [ 'users/update/' + name ]);
-        } else {
-          this.reRoute();
-        }
-      }, (error) => this.message = 'Username and/or password do not match');
+  async login(
+    { name, password }: { name: string; password: string },
+    isCreate: boolean
+  ) {
+    this.pouchdbService.login(name, password);
+    this.reRoute();
+    // this.couchService.post('_session', { 'name': name, 'password': password }, { withCredentials: true })
+    //   .pipe(switchMap((data) => {
+    //     // Post new session info to login_activity
+    //     this.userService.set(data);
+    //     return this.userService.newSessionLog();
+    //   })).subscribe((res) => {
+    //     if (isCreate) {
+    //       this.router.navigate( [ 'users/update/' + name ]);
+    //     } else {
+    //       this.reRoute();
+    //     }
+    //   }, (error) => this.message = 'Username and/or password do not match');
   }
 }
--- src/app/shared/pouchdb.service.ts
+++ src/app/shared/pouchdb.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import PouchDB from 'pouchdb';
+import PouchDBAuth from 'pouchdb-authentication';
 import { environment } from '../../environments/environment';
-PouchDB.plugin(require('pouchdb-authentication'));
+PouchDB.plugin(PouchDBAuth);
 // WHAT ARE WE TRYING TO DO ?
 // Create a database per new user sign up
 // Remember this is NoSQL not SQL
@@ -11,7 +12,7 @@ PouchDB.plugin(require('pouchdb-authentication'));
 @Injectable()
 export class PouchdbService {
   private baseUrl = environment.couchAddress;
-  private remoteDb: any;
+  private remoteDb: any; // we use remote db for authentication and stuff!!
   private localDb: any;
 
   constructor() {
@@ -24,21 +25,25 @@ export class PouchdbService {
     await this.deconfigureStore();
 
     this.localDb = new PouchDB('planet-local');
-    this.remoteDb = new PouchDB(this.baseUrl + '/' + userId, {
+    this.remoteDb = new PouchDB(this.baseUrl + '/user-' + window.btoa(userId), {
       skip_setup: true
     });
-
-    this.localDb
-      .sync('planet-local', { live: true, retry: true })
-      .on('error', console.log.bind(console));
   }
 
-  getDb() {
+  getLocalDb() {
     if (!this.localDb) {
       throw new Error('Database has not yet been configured!');
     }
 
     return this.localDb;
+  }
+
+  getRemoteDb() {
+    if (!this.remoteDb) {
+      throw new Error('Database has not yet been configured!');
+    }
+
+    return this.remoteDb;
   }
 
   async deconfigureStore() {
@@ -51,5 +56,51 @@ export class PouchdbService {
 
     await this.remoteDb.close();
     this.remoteDb = null;
+  }
+
+  // @FIXME: move to user services
+  // @TODO: rewrite with Observables
+  async login(name, password) {
+    await this.configureStore(name);
+
+    const ajaxOpts = {
+      ajax: {
+        headers: {
+          Authorization: 'Basic ' + window.btoa(name + ':' + password)
+        }
+      }
+    };
+
+    await this.remoteDb.login(name, password).catch(error => {
+      this.deconfigureStore();
+      console.error(error);
+    });
+  }
+  async signupUser(name, password) {
+    await this.configureStore(name);
+
+    await this.remoteDb
+      .signUp(name, password)
+      .then(function() {
+        // So it seems that we first need to login to the remotedb otherwise we get a 400 error about the http adapters
+        this.remoteDb.login(name, password).then(() => {
+          this.localDb
+            .sync(this.remoteDb, { live: true, retry: true })
+            .on('error', console.log.bind(console));
+        });
+        this.login(name, password);
+      })
+      .catch(err => {
+        this.deconfigureStore();
+        if (err) {
+          if (err.name === 'conflict') {
+            // "batman" already exists, choose another username
+          } else if (err.name === 'forbidden') {
+            // invalid username
+          } else {
+            // HTTP error, cosmic rays, etc.
+          }
+        }
+      });
   }
 }
