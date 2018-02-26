f1e9b57fb936d36f1899592f03117251843f07c0
MacNew <newmac1000@gmail.com>
2018-02-26 21:50:18 +0545

    [#438] modified notification-view.component.ts

--- src/app/notification-view/notification-view.component.ts
+++ src/app/notification-view/notification-view.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
 @Component({
   template: `{{message}}`,
 })
-
 export class NotificationViewComponent implements OnInit {
   message: string;
   constructor(
