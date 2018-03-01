2b934f79fd445bc9756bf863dca65cc1437fa0db
Bosun Adepoju <adepojuolatunbosun@gmail.com>
2018-03-01 11:53:02 -0500

    Materialize meetups and keep from touching sidebar/topbar (connects #375) (#422)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -69,7 +69,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
   // Used to swap in different background.
   // Should remove when background is finalized.
   backgroundRoute() {
-    const routesWithBackground = [ 'resources', 'courses', 'users' ];
+    const routesWithBackground = [ 'resources', 'courses', 'users', 'meetups' ];
     // Leaving the exception variable in so we can easily use this while still testing backgrounds
     const routesWithoutBackground = [ 'users/update', 'users/profile' ];
     const isException = routesWithoutBackground
