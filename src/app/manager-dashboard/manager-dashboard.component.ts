6733de2a7f450ba6acc893d6bd95280f1109e42d
razu9861 <razu9861@users.noreply.github.com>
2018-03-06 09:19:12 +0545

    Hide setting from non admin (fixes #457) (#466)

--- src/app/manager-dashboard/manager-dashboard.component.ts
+++ src/app/manager-dashboard/manager-dashboard.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../shared/user.service';
 
 @Component({
   template: `
-    <div><a routerLink="/community" i18n mat-raised-button>Communities</a> <a routerLink="/nation" i18n mat-raised-button>Nations</a></div>
+    <div *ngIf="displayDashboard"><a routerLink="/community" i18n mat-raised-button>Communities</a> <a routerLink="/nation" i18n mat-raised-button>Nations</a></div>
+    <div>{{message}}</div>
   `
 })
-export class ManagerDashboardComponent {
+export class ManagerDashboardComponent implements OnInit {
+  isUserAdmin = false;
+  displayDashboard = true;
+  message = '';
 
-  constructor() { }
+  constructor( private userService: UserService) { }
+
+  ngOnInit() {
+    Object.assign(this, this.userService.get());
+    if (!this.isUserAdmin) {
+      // A non-admin user cannot receive all user docs
+      this.displayDashboard = false;
+      this.message = 'Access restricted to admins';
+    }
+  }
 
 }
