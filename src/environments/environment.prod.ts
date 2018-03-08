9e39de3fff4ecb0c1c4279c15c179146b3f6253d
Rupesh87 <rupandroo@gmail.com>
2018-03-08 16:22:36 +0545

    [#433] Travis fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   test: false,
   // Change this to Docker address
-  couchAddress: 'planet-db-host:planet-db-port/'
+  couchAddress: 'planet-db-host:planet-db-port/',
+  centerAddress: 'earth.np.ole.org:5986'
 };
