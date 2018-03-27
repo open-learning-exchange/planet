58261f10f05db8911e6b92cbf65ebb48a2611f45
Paul <paul.r.albert@gmail.com>
2018-03-27 12:24:41 -0400

    Cleanup ResourcesComponent

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -1,22 +1,19 @@
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { CouchService } from '../shared/couchdb.service';
 import { DialogsPromptComponent } from '../shared/dialogs/dialogs-prompt.component';
-import { MatTableDataSource, MatPaginator, MatSort, MatFormField, MatFormFieldControl, MatDialog, MatDialogRef } from '@angular/material';
+import { MatTableDataSource, MatPaginator, MatSort, MatDialog } from '@angular/material';
 import { SelectionModel } from '@angular/cdk/collections';
 import { Router } from '@angular/router';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { switchMap, catchError } from 'rxjs/operators';
+import { switchMap, catchError,  } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
 import { PlanetMessageService } from '../shared/planet-message.service';
 import { UserService } from '../shared/user.service';
-import { findDocuments } from '../shared/mangoQueries';
-import { forkJoin } from 'rxjs/observable/forkJoin';
 import { DialogsFormService } from '../shared/dialogs/dialogs-form.service';
-import { Validators } from '@angular/forms';
 import { filterSpecificFields } from '../shared/table-helpers';
-import { environment } from '../../environments/environment';
 import { ResourcesService } from './resources.service';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   templateUrl: './resources.component.html',
@@ -44,11 +41,10 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
   displayedColumns = [ 'select', 'info', 'rating' ];
   readonly dbName = 'resources';
   message = '';
-  file: any;
   deleteDialog: any;
   nationName = '';
   selection = new SelectionModel(true, []);
-  urlPrefix = environment.couchAddress + this.dbName + '/';
+  onDestroy$ = new Subject<void>();
 
   constructor(
     private couchService: CouchService,
@@ -75,6 +71,11 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
     this.resources.paginator = this.paginator;
   }
 
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
     const numSelected = this.selection.selected.length;
@@ -93,57 +94,6 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
     this.resources.data.forEach(row => this.selection.select(row));
   }
 
-  setupList(resourcesRes, ratings) {
-    this.resources.data = resourcesRes.map((r: any) => {
-      const resource = r.doc || r;
-      const ratingIndex = ratings.findIndex(rating => {
-        return resource._id === rating.item;
-      });
-      if (ratingIndex > -1) {
-        const ratingInfo = this.addRatingToResource(resource._id, ratingIndex, ratings, {});
-        return { ...resource, rating: ratingInfo };
-      }
-      return { ...resource,  rating: { rateSum: 0, totalRating: 0, maleRating: 0, femaleRating: 0, userRating: {}  } };
-    });
-  }
-
-  addRatingToResource(id, index, ratings, ratingInfo: any) {
-    const rating = ratings[index];
-    // If totalRating is undefined, will start count at 1
-    ratingInfo.totalRating = ratingInfo.totalRating + 1 || 1;
-    ratingInfo.rateSum = ratingInfo.rateSum + rating.rate || rating.rate;
-    if (rating.user.gender) {
-      switch (rating.user.gender) {
-        case 'male':
-          ratingInfo.maleRating = ratingInfo.maleRating + 1 || 1;
-          break;
-        case 'female':
-          ratingInfo.femaleRating = ratingInfo.femaleRating + 1 || 1;
-          break;
-      }
-    }
-    ratingInfo.userRating = rating.user.name === this.userService.get().name ? rating : ratingInfo.userRating;
-    if (ratings.length > index + 1 && ratings[index + 1].item === id) {
-      // Ratings are sorted by resource id,
-      // so this recursion will add all ratings to resource
-      return this.addRatingToResource(id, index + 1, ratings, ratingInfo);
-    }
-    return ratingInfo;
-  }
-
-  getRatings() {
-    return this.couchService.post('ratings/_find', findDocuments({
-      // Selector
-      'type': 'resource',
-      // Must have sorted property in selector to sort correctly
-      'item': { '$gt': null }
-    }, 0, [ { 'item': 'desc' } ], 1000)).pipe(catchError(err => {
-      // If there's an error, return a fake couchDB empty response
-      // so resources can be displayed.
-      return of({ docs: [] });
-    }));
-  }
-
   getExternalResources() {
     return this.couchService.post('nations/_find',
     { 'selector': { 'name': this.nationName },
@@ -160,14 +110,6 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
         // If there is no url, return an observable of an empty array
         return of([]);
     }));
-  }
-
-  getResources() {
-    this.nationName = this.route.snapshot.paramMap.get('nationname');
-    if (this.nationName !== null) {
-      return this.getExternalResources();
-    }
-    return this.couchService.get(this.dbName + '/_all_docs?include_docs=true');
   }
 
   deleteClick(resource) {
@@ -232,12 +174,6 @@ export class ResourcesComponent implements OnInit, AfterViewInit {
 
   goBack() {
     this.router.navigate([ '/' ]);
-  }
-
-  updateRatings(newRating: any) {
-    this.getRatings().subscribe((res) => {
-      this.setupList(this.resources.data, res.docs);
-    });
   }
 
 }
