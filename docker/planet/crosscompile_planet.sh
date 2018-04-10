1d761f734be4c64b7f157586957684f8fe1c3fa7
Sahil Phule <sahil@sahil.xyz>
2018-04-10 06:10:25 -0500

    Made Lots of changes

---
+++ docker/planet/crosscompile_planet.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ARCH=$1
+ACT=$2
+
+if [[ "${ARCH}" == "armv7" ]]; then
+  TRIPLE="arm-linux-gnueabihf"
+  GCC="4.8"
+elif [[ "${ARCH}" == "armv8" ]]; then
+  TRIPLE="aarch64-linux-gnu"
+  GCC="4.8"
+else
+  exit 1
+fi
+
+PACKAGES="gcc-${GCC}-${TRIPLE} g++-${GCC}-${TRIPLE}"
+export CC="${TRIPLE}-gcc-${GCC}"
+export CXX="${TRIPLE}-g++-${GCC}"
+export STRIP="${TRIPLE}-strip"
+export ZMQ_BUILD_OPTIONS="--host=${TRIPLE}"
+
+echo "Building Planet for ${ARCH}"
+
+if [[ "${ACT}" == "install"  ]]; then
+   echo "Install stage"
+   npm install "--arch=${TRIPLE}"
+   mkdir /ng-app
+   cp -R ./node_modules ./ng-app
+elif [[ "${ACT}" == "build"  ]]; then
+   echo "Build the angular app in production mode stage"
+   $(npm bin)/ng build "--arch=${TRIPLE}" --prod
+else
+   echo "Error: No action Specified"
+fi
