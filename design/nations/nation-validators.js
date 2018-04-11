196f524d7e498f1868c477e76937495ad3c83503
Laxman Maharjan <email@mlaxman.com.np>
2018-04-11 13:59:42 +0545

    [#541] making learner readonly for nations

--- design/nations/nation-validators.js
+++ design/nations/nation-validators.js
@@ -5,6 +5,24 @@ function (newDoc, oldDoc, userCtx, secObj) {
       throw { forbidden: errMessage };
     }
   }
+  function canWrite() {
+    var CAN_WRITE = true;
+    var readOnly = ['learner'];
+    if (!userCtx.isUserAdmin) {
+      if (userCtx.roles.length < 1) {
+        CAN_WRITE = false;
+      } else {
+        for (var i = 0; i < userCtx.roles.length; i++) {
+          if (~ readOnly.indexOf(userCtx.roles[i])) {
+            CAN_WRITE = false;
+          }
+        }
+      }
+    }
+    if(!CAN_WRITE)
+      throw { forbidden: 'You have only read-only access' };
+  }
+  canWrite();
   if (!newDoc) {
     require('name');
     require('adminName');
