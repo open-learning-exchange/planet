b9447a647768923cef7ae6308dfbbb649221d734
mappuji <mappuji@protonmail.com>
2018-04-15 00:34:17 +0700

    I know this is not going to work, but I need you opinion

--- docker/planet/docker-entrypoint.sh
+++ docker/planet/docker-entrypoint.sh
@@ -1,24 +1,3 @@
 #!/bin/sh
 
-if [ -z "$HOST_PROTOCOL" ]
-then
-  PROTOCOL="http"
-elif [ "$HOST_PROTOCOL" = "https" ]
-then
-  PROTOCOL="https"
-else
-  PROTOCOL="http"
-fi
-
-if [ "$MULTIPLE_IPS" = "true" ]
-then
-    sed -i -e 's#couchAddress:"planet-db-host:planet-db-port/"#couchAddress:window.location.protocol+"//"+window.location.hostname+":planet-db-port/"#g' /usr/share/nginx/html/main*
-else
-    sed -i -e "s#planet-db-host#$PROTOCOL://$DB_HOST#g" /usr/share/nginx/html/main*
-fi
-
-sed -i -e "s#planet-db-port#$DB_PORT#g" /usr/share/nginx/html/main*
-sed -i -e "s#planet-center-address#$CENTER_ADDRESS#g" /usr/share/nginx/html/main*
-
 nginx -g "daemon off;"
-
