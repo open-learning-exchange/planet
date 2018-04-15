934a2bbc39123ff38eeaf8fae315385474a99e64
Paul <paul.r.albert@gmail.com>
2018-04-15 11:05:28 -0400

    Remove currentDate

--- src/app/resources/resources-add.component.ts
+++ src/app/resources/resources-add.component.ts
@@ -30,7 +30,6 @@ export class ResourcesAddComponent implements OnInit {
   media: string[];
   openWith: string[];
   resourceType: string[];
-  currentDate = new Date();
   file: any;
   resourceForm: FormGroup;
   readonly dbName = 'resources'; // make database name a constant
@@ -72,7 +71,7 @@ export class ResourcesAddComponent implements OnInit {
       openWith: '',
       resourceFor: [],
       medium: '',
-      articleDate: '',
+      articleDate: Date.now(),
       resourceType: '',
       addedBy: '',
       openUrl: [],
@@ -142,8 +141,6 @@ export class ResourcesAddComponent implements OnInit {
   }
 
   addResource(resourceInfo) {
-    // convert articleDate in Date format to timestamp format
-    resourceInfo.articleDate = Date.now();
     // ...is the rest syntax for object destructuring
     this.couchService.post(this.dbName, { ...resourceInfo }).subscribe(() => {
       this.router.navigate([ '/resources' ]);
