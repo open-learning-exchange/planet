21c70ff1d8b9fb5c87f10c03678b1828d6811ac6
Laxman Maharjan <email@mlaxman.com.np>
2018-04-16 08:46:02 +0545

    updated canWrite function

--- design/nations/nation-validators.js
+++ design/nations/nation-validators.js
@@ -6,17 +6,17 @@ function (newDoc, oldDoc, userCtx, secObj) {
     }
   }
   function canWrite() {
-    var CAN_WRITE = true;
-    var readOnly = ['learner'];
-    if (!userCtx.isUserAdmin) {
-      if (userCtx.roles.length < 1) {
-        CAN_WRITE = false;
-      } else {
-        for (var i = 0; i < userCtx.roles.length; i++) {
-          if (readOnly.indexOf(userCtx.roles[i]) !== -1) {
-            CAN_WRITE = false;
-          }
-        }
+    var CAN_WRITE = false;
+    var writeAccess = ['_admin'];
+    if(secObj && secObj.admins && secObj.admins.names) {
+      CAN_WRITE = (secObj.admins.names.indexOf(userCtx.name) !== -1);
+    }
+    if(!CAN_WRITE && secObj && secObj.admins && secObj.admins.roles) {
+      writeAccess.concat(secObj.admins.roles);
+    }
+    for (var i = 0; (!CAN_WRITE && i < userCtx.roles.length); i++) {
+      if (writeAccess.indexOf(userCtx.roles[i]) !== -1) {
+        CAN_WRITE = true;
       }
     }
     if(!CAN_WRITE) {
