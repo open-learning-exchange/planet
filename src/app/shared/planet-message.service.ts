652ff385bb4c1fee55f02b2bfe668b70ee350597
Samriddhi Basnet <sbasnet@himalayantechies.com>
2018-04-19 17:10:35 +0545

    [#633] dynamic snackbar created

--- src/app/shared/planet-message.service.ts
+++ src/app/shared/planet-message.service.ts
@@ -4,18 +4,20 @@ import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
 @Injectable()
 export class PlanetMessageService {
   private config: MatSnackBarConfig;
-  constructor( private snackBar: MatSnackBar ) {
+  constructor( private snackBar: MatSnackBar ) { 
     this.config = new MatSnackBarConfig();
     this.config.duration = 3000;
   }
 
-  showMessage(message: string, duration?: number, action?: string) {
-    this.config = duration ? Object.assign(this.config, { 'duration': duration }) : this.config;
-    this.snackBar.open(message, action, this.config);
+  showMessage(message: string) {
+    this.snackBar.open(message, ' ', {
+      duration: 3000,
+      extraClasses: [ 'show-snackbar' ]
+    });
   }
 
   showAlert(message: string, duration?: number, action?: string) {
     this.config = duration ? Object.assign(this.config, { 'duration': duration }) : this.config;
     this.snackBar.open(message, action, this.config);
-  }
+    }
 }
