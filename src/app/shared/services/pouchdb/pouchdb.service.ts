3c2d765d7edadbe5a7fd43bef2d738af902086f1
LKhadka <l.khadka@outlook.com>
2018-04-19 21:39:33 -0500

    use authdb for auth guards

--- src/app/shared/services/pouchdb/pouchdb.service.ts
+++ src/app/shared/services/pouchdb/pouchdb.service.ts
@@ -1,19 +1,33 @@
-import { Injectable } from '@angular/core';
-import PouchDB from 'pouchdb';
-import { environment } from '../../../../environments/environment';
+import { Injectable } from "@angular/core";
+import PouchDB from "pouchdb";
+import { environment } from "../../../../environments/environment";
 
-type RemoteDatabases = 'auth';
+type RemoteDatabases = "auth";
 
 @Injectable()
 export class PouchdbService {
   private baseUrl = environment.couchAddress;
   private localDb;
   private remoteDbs = {
-    auth: ''
+    auth: null
   };
 
   constructor() {
-    this.localDb = new PouchDB('local-pouchdb');
+    this.localDb = new PouchDB("local-pouchdb");
+
+    this.remoteDbs.auth = new PouchDB(this.baseUrl + "/notifications", {
+      skip_setup: true
+    });
+
+    // Object.keys(this.remoteDbs).forEach(key => {
+    //   this.remoteDbs[key] = new PouchDB()
+    // })
+  }
+
+  syncDb() {
+    this.localDb
+      .sync(this.remoteDbs.auth, { live: true, retry: true })
+      .on("error", console.log.bind(console));
   }
 
   getLocalPouchDB() {
