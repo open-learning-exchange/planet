c89d04854d80a795e49a5be5a5902d2a0c5836ae
LKhadka <l.khadka@outlook.com>
2018-04-19 21:39:33 -0500

    use authdb for auth guards

--- src/app/shared/services/pouchdb/pouchdb-auth.service.ts
+++ src/app/shared/services/pouchdb/pouchdb-auth.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { Observable } from 'rxjs/Observable';
+import { Observable } from "rxjs/Observable";
 
-import PouchDB from 'pouchdb';
-import PouchDBAuth from 'pouchdb-authentication';
-import { environment } from '../../../../environments/environment';
+import PouchDB from "pouchdb";
+import PouchDBAuth from "pouchdb-authentication";
+import { environment } from "../../../../environments/environment";
 PouchDB.plugin(PouchDBAuth);
 
-import { PouchdbService } from './pouchdb.service';
-import { catchError, flatMap, map } from 'rxjs/operators';
-import 'rxjs/add/observable/fromPromise';
+import { PouchdbService } from "./pouchdb.service";
+import { catchError, flatMap, switchMap } from "rxjs/operators";
+import "rxjs/add/observable/fromPromise";
 
 @Injectable()
 export class PouchdbAuthService {
@@ -17,27 +17,35 @@ export class PouchdbAuthService {
   private authDB;
 
   constructor(private pouchdbService: PouchdbService) {
-    this.authDB = this.pouchdbService.getRemotePouchDB('auth');
+    this.authDB = this.pouchdbService.getRemotePouchDB("auth");
   }
 
   private configureStore(userId: string) {
-    this.authDB = new PouchDB(this.baseUrl + '/user-' + window.btoa(userId), {
+    // instead of userID (1 database per user), we can use the user's roles and sync to the corresponding document
+    this.authDB = new PouchDB(this.baseUrl + "/test", {
       skip_setup: true
     });
-    this.pouchdbService.updateRemotePouchDB('auth', this.authDB);
+    this.pouchdbService.updateRemotePouchDB("auth", this.authDB);
   }
 
   private deconfigureStore() {
-    this.authDB = '';
-    this.pouchdbService.updateRemotePouchDB('auth', '');
+    this.authDB = "";
+    this.pouchdbService.updateRemotePouchDB("auth", "");
+  }
+
+  getSessionInfo() {
+    return Observable.fromPromise(this.authDB.getSession()).pipe(
+      catchError(this.handleError)
+    );
   }
 
   login(username, password) {
-    this.configureStore(username);
-
-    return Observable.fromPromise(this.authDB.login(username, password)).pipe(
-      catchError(this.handleError)
-    );
+    // this.configureStore(username);
+    return Observable.fromPromise(
+      this.authDB.login(username, password).then(() => {
+        this.pouchdbService.syncDb();
+      })
+    ).pipe(catchError(this.handleError));
   }
 
   signupUser(username, password, opts?) {
@@ -49,7 +57,7 @@ export class PouchdbAuthService {
   }
 
   private handleError(err) {
-    console.error('An error occurred while signing in the user', err);
+    console.error("An error occurred while signing in the user", err);
     this.deconfigureStore();
     return Observable.throw(err.message || err);
   }
