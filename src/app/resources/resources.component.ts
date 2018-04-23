a48067bf6a9c7a7541bcc2c7336212d8faca85b3
BasnetSamriddhi <35338384+BasnetSamriddhi@users.noreply.github.com>
2018-04-24 03:55:56 +0545

    Added replaced ids in shelf (fixes#713) (#715)

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -237,19 +237,17 @@ export class ResourcesComponent implements OnInit, AfterViewInit, OnDestroy {
     });
     this.couchService.post(`shelf/_find`, { 'selector': { '_id': this.userService.get()._id } })
       .pipe(
-        map(data => {
-          return { rev: { _rev: data.docs[0]._rev }, resourceIds: data.docs[0].resourceIds || [] };
-        }),
         // If there are no matches, CouchDB throws an error
         // User has no "shelf", and it needs to be created
         catchError(err => {
-          // Observable of continues stream
-          return of({ rev: {}, resourceIds: [] });
+          // Observable of continues stream, send fake response with empty resourceIds array
+          return of({ docs: [ { _rev: '', resourceIds: [] } ] });
         }),
         switchMap(data => {
-          const resourceIds = resourceIdArray.concat(data.resourceIds).reduce(this.dedupeShelfReduce, []);
+          const oldShelf = data.docs[0];
+          const resourceIds = resourceIdArray.concat(oldShelf.resourceIds).reduce(this.dedupeShelfReduce, []);
           return this.couchService.put('shelf/' + this.userService.get()._id,
-            Object.assign(data.rev, { resourceIds }));
+            Object.assign(oldShelf, { resourceIds }));
         })
       ).subscribe((res) =>  {
         this.updateAddLibrary();
