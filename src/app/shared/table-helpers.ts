537a85b03a27a7585d6dee9f5ade45ba08941eeb
Laxman Maharjan <email@mlaxman.com.np>
2018-04-27 09:16:11 +0545

    [#770] Add multilevel filter specific fields

--- src/app/shared/table-helpers.ts
+++ src/app/shared/table-helpers.ts
@@ -9,13 +9,27 @@ const checkFilterItems = (data: any) => ((includeItem: boolean, [ field, val ]) 
 
 export const filterSpecificFields = (filterFields: string[]): any => {
   return (data: any, filter: string) => {
+    let dt = '';
     for (let i = 0; i < filterFields.length; i++) {
-      if (data[filterFields[i]].toLowerCase().indexOf(filter.trim().toLowerCase()) > -1) {
+      const keys = filterFields[i].split('.');
+      dt = testCase(data, keys);
+      if (dt.toLowerCase().indexOf(filter.trim().toLowerCase()) > -1) {
         return true;
       }
     }
   };
 };
+
+function testCase(data: any, filterFields: string[]) {
+  if (filterFields.length > 1 && data[filterFields[0]]) {
+      data = data[filterFields[0]];
+      filterFields.splice(0, 1);
+      return testCase(data, filterFields);
+  } else if (data[filterFields[0]]) {
+      return data[filterFields[0]];
+  }
+  return '';
+}
 
 export const filterDropdowns = (filterObj: any) => {
   return (data: any, filter: string) => {
