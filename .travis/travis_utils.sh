a37d8814167619eaca2e6eb05769f595c8a657a0
Sahil Phule <sahil@sahil.xyz>
2018-05-02 17:28:30 -0500

    Revert "Changed to planet-test for testing purpose"
    This reverts commit e1439ffb3681af2526bd21cbe581dcf83f868902.

--- .travis/travis_utils.sh
+++ .travis/travis_utils.sh
@@ -167,12 +167,12 @@ bell() {
 
 create_multiarch_manifest_planet(){
     build_message Creating Planet Multiarch Manifests
-    if [ "$BRANCH" = "multiarch_suport" ]
+    if [ "$BRANCH" = "master" ]
     then
         build_message Creating Planet Multiarch Manifest for Latest
         # $1: latest arm
         # $2: latest amd64        
-        yq n image treehouses/planet-test:latest | \
+        yq n image treehouses/planet:latest | \
         yq w - manifests[0].image $1 | \
         yq w - manifests[0].platform.architecture arm | \
         yq w - manifests[0].platform.os linux | \
@@ -187,7 +187,7 @@ create_multiarch_manifest_planet(){
             build_message Creating Planet Multiarch Manifest for Versioned
             # $3: versioned arm
             # $4: versioned amd64
-            yq n image treehouses/planet-test:$VERSION | \
+            yq n image treehouses/planet:$VERSION | \
             yq w - manifests[0].image $3 | \
             yq w - manifests[0].platform.architecture arm | \
             yq w - manifests[0].platform.os linux | \
@@ -203,12 +203,12 @@ create_multiarch_manifest_planet(){
 
 create_multiarch_manifest_dbinit(){
     build_message Creating db init Multiarch Manifests
-    if [ "$BRANCH" = "multiarch_suport" ]
+    if [ "$BRANCH" = "master" ]
     then
         build_message Creating Multiarch Manifest for db-init
         # $1: db-init arm
         # $2: db-init amd64        
-        yq n image treehouses/planet-test:db-init | \
+        yq n image treehouses/planet:db-init | \
         yq w - manifests[0].image $1 | \
         yq w - manifests[0].platform.architecture arm | \
         yq w - manifests[0].platform.os linux | \
@@ -223,7 +223,7 @@ create_multiarch_manifest_dbinit(){
             build_message Creating Multiarch Manifest for db-init Versioned
             # $3: db-init versioned arm
             # $4: db-init versioned amd64
-            yq n image treehouses/planet-test:db-init-$VERSION | \
+            yq n image treehouses/planet:db-init-$VERSION | \
             yq w - manifests[0].image $3 | \
             yq w - manifests[0].platform.architecture arm | \
             yq w - manifests[0].platform.os linux | \
@@ -239,7 +239,7 @@ create_multiarch_manifest_dbinit(){
 
 push_multiarch_manifests(){
     build_message Pushing Multiarch Manifests to cloud
-    if [ "$BRANCH" = "multiarch_suport" ]
+    if [ "$BRANCH" = "master" ]
     then
         /tmp/manifest_tool push from-spec /tmp/MA_manifests/MA_planet_latest.yaml
         /tmp/manifest_tool push from-spec /tmp/MA_manifests/MA_planet_versioned.yaml
