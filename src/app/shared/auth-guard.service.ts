55cd4df0c2ab8a372dbe6d72f07a28fd96080405
LKhadka <l.khadka@outlook.com>
2018-05-19 01:14:29 -0500

    replace couch service with auth service

--- src/app/shared/auth-guard.service.ts
+++ src/app/shared/auth-guard.service.ts
@@ -4,49 +4,68 @@ import { UserService } from './user.service';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { switchMap, map } from 'rxjs/operators';
+import { AuthService as PouchDBAuthService } from './services';
 
-import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  Router,
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot
+} from '@angular/router';
 
 @Injectable()
 export class AuthService {
-
-  constructor(private couchService: CouchService, private userService: UserService, private router: Router) {}
+  constructor(
+    private couchService: CouchService,
+    private userService: UserService,
+    private router: Router,
+    private authService: PouchDBAuthService
+  ) {}
 
   private checkUser(url: any): Observable<boolean> {
-    return this.couchService
-      .get('_session', { withCredentials: true })
-      .pipe(switchMap((res: any) => {
+    return this.authService.getSessionInfo().pipe(
+      switchMap(res => {
         if (res.userCtx.name) {
-          // If user already matches one on the user service, do not make additional call to CouchDB
           if (res.userCtx.name === this.userService.get().name) {
             return of(true);
           }
           return this.userService.setUserConfigAndShelf(res.userCtx);
         }
         this.userService.unset();
-        this.router.navigate([ '/login' ], { queryParams: { returnUrl: url }, replaceUrl: true });
+        this.router.navigate(['/login'], {
+          queryParams: { returnUrl: url },
+          replaceUrl: true
+        });
         return of(false);
       }),
-      map((isLoggedIn) => {
+      map(isLoggedIn => {
         return isLoggedIn;
-      }));
+      })
+    );
   }
 
   // For main app (which requires login).  Uses canActivateChild to check on every route
   // change if session has expired
-  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+  canActivateChild(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
     return this.checkUser(state.url);
   }
 
   // For login route will redirect to main app if there is an active session
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.couchService.get('_session', { withCredentials: true }).pipe(map(res => {
-      if (res.userCtx.name) {
-        this.router.navigate([ '' ]);
-        return false;
-      }
-      return true;
-    }));
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean> {
+    return this.authService.getSessionInfo().pipe(
+      map(res => {
+        if (res.userCtx.name) {
+          this.router.navigate(['']);
+          return false;
+        }
+        return true;
+      })
+    );
   }
-
 }
