70e2e99f2d73d602ca7c925f0005c287c95c4aca
LKhadka <l.khadka@outlook.com>
2018-05-19 00:36:32 -0500

    add service for handling auth

---
+++ src/app/shared/services/pouchDB/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromPromise';
+import { catchError } from 'rxjs/operators';
+import { environment } from '../../../../environments/environment';
+import { PouchDBService } from './pouchDB.service';
+
+@Injectable()
+export class AuthService {
+  private authDB;
+
+  constructor(private pouchdbService: PouchDBService) {
+    this.authDB = this.pouchdbService.getAuthDB();
+  }
+
+  getSessionInfo() {
+    return Observable.fromPromise(this.authDB.getSession()).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  login(username, password) {
+    return Observable.fromPromise(this.authDB.login(username, password)).pipe(
+      catchError(this.handleError)
+    );
+  }
+
+  signupUser(username, password, opts?) {
+    return Observable.fromPromise(
+      this.authDB.signUp(username, password, opts)
+    ).pipe(catchError(this.handleError));
+  }
+
+  private handleError(err) {
+    console.error('An error occurred while signing in the user', err);
+    return Observable.throw(err.message || err);
+  }
+}
