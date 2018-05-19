f1602a385bf197f330f6c39a17b88fb67c1f7326
LKhadka <l.khadka@outlook.com>
2018-05-19 00:48:26 -0500

    export pouch auth service

--- src/app/shared/services/pouchDB/index.ts
+++ src/app/shared/services/pouchDB/index.ts
@@ -1,5 +1,7 @@
 import { PouchDBService } from './pouchDB.service';
+import { AuthService } from './auth.service';
 
 export { PouchDBService } from './pouchDB.service';
+export { AuthService } from './auth.service';
 
-export const SHARED_SERVICES = [PouchDBService];
+export const SHARED_SERVICES = [PouchDBService, AuthService];
