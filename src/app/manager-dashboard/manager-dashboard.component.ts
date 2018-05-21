a2e24aaf6f6e56bbe5343e220c02fe59f95dc6e5
Rupesh87 <rupandroo@gmail.com>
2018-05-22 05:03:47 +0545

    [#949] Resend registration request (connects #949) (#1028)
    * [#949] Resend registration request
    
    * Update manager-dashboard.component.ts
    
    * Remove deep nesting
    
    * Fix shelf url and grammar

--- src/app/manager-dashboard/manager-dashboard.component.ts
+++ src/app/manager-dashboard/manager-dashboard.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../shared/user.service';
+import { CouchService } from '../shared/couchdb.service';
+import { findOneDocument } from '../shared/mangoQueries';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs/observable/of';
+import { forkJoin } from 'rxjs/observable/forkJoin';
+import { PlanetMessageService } from '../shared/planet-message.service';
 
 @Component({
   template: `
@@ -9,6 +15,8 @@ import { UserService } from '../shared/user.service';
         <a routerLink="/associated/{{ planetType === 'center' ? 'nation' : 'community' }}"
           i18n mat-raised-button>{{ planetType === 'center' ? 'Nation' : 'Community' }}</a>
       </span>
+      <button *ngIf="planetType !== center && showResendConfiguration"
+        (click)="resendConfig()" i18n mat-raised-button>Resend Registration Request</button>
       <a routerLink="/feedback" i18n mat-raised-button>Feedback</a>
     </div>
     <div class="view-container" *ngIf="displayDashboard && planetType !== 'center'">
@@ -26,9 +34,12 @@ export class ManagerDashboardComponent implements OnInit {
   displayDashboard = true;
   message = '';
   planetType = this.userService.getConfig().planetType;
+  showResendConfiguration = false;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private couchService: CouchService,
+    private planetMessageService: PlanetMessageService
   ) {}
 
   ngOnInit() {
@@ -38,6 +49,39 @@ export class ManagerDashboardComponent implements OnInit {
       this.displayDashboard = false;
       this.message = 'Access restricted to admins';
     }
+    // Check parent if configuration exists
+    this.couchService.post(`communityregistrationrequests/_find`, findOneDocument('name', this.userService.get().name),
+      { domain: this.userService.getConfig().parentDomain }).subscribe((data: any) => {
+        if (data.docs.length === 0) {
+          this.showResendConfiguration = true;
+        }
+      });
+  }
+
+  resendConfig() {
+    const { _id, _rev, ...config } = this.userService.getConfig();
+    const { _rev: userRev, ...userDetail } = this.userService.get();
+    return this.couchService.post('communityregistrationrequests', config, { domain: this.userService.getConfig().parentDomain })
+      .pipe(switchMap((res: any) => {
+        userDetail.requestId = res.id;
+        return this.couchService.post(`_users/_find`,
+          { 'selector': { '_id': userDetail._id }, 'fields': [ '_id', '_rev' ] },
+          { domain: this.userService.getConfig().parentDomain });
+      }), switchMap((user) => {
+        if (user.docs[0]) {
+          userDetail._rev = user.docs[0]._rev;
+        }
+        userDetail.isUserAdmin = false;
+        return forkJoin([
+          this.couchService.put('_users/org.couchdb.user:' + userDetail.name,
+            userDetail, { domain: this.userService.getConfig().parentDomain }),
+          this.couchService.put('shelf/org.couchdb.user:' + userDetail.name,
+            {}, { domain: this.userService.getConfig().parentDomain })
+        ]);
+      })).subscribe((res: any) => {
+        this.planetMessageService.showMessage('Registration request has been sent successfully.');
+        this.showResendConfiguration = false;
+      }, error => this.planetMessageService.showAlert('An error occurred please try again.'));
   }
 
 }
