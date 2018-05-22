eec04fb3df03e9f95f62612ae997c05d99ad69f1
Laxman Maharjan <email@mlaxman.com.np>
2018-05-22 06:38:00 +0545

    Notification for community request (fixes #1004) (#1007)

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -185,7 +185,19 @@ export class ConfigurationComponent implements OnInit {
             userDetail['isUserAdmin'] =  false;
             return this.couchService.put('_users/org.couchdb.user:' + credentials.name,
               userDetail, { domain: configuration.parentDomain });
-          })),
+          }), switchMap(data => {
+            const requestNotification = {
+              'user': 'SYSTEM',
+              'message': 'New ' + configuration.planetType + ' "' + configuration.name + '" has requested to connect.',
+              'link': '/requests',
+              'type': 'request',
+              'priority': 1,
+              'status': 'unread',
+              'time': Date.now()
+            };
+            // Send notification to parent
+            return this.couchService.post('notifications', requestNotification, { domain: configuration.parentDomain });
+          }))
       ]).debug('Sending request to parent planet').subscribe((data) => {
         this.planetMessageService.showMessage('Admin created: ' + data[1].id.replace('org.couchdb.user:', ''));
         this.router.navigate([ '/login' ]);
