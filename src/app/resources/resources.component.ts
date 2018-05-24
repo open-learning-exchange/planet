877973ea07602ac38395c45826fb411d172c5541
Laxman Maharjan <email@mlaxman.com.np>
2018-05-24 17:48:11 +0545

    [#901] Pull selected resource from parent

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -14,6 +14,8 @@ import { ResourcesService } from './resources.service';
 import * as constants from './resources-constants';
 import { environment } from '../../environments/environment';
 import { debug } from '../debug-operator';
+import { DialogsFormService } from '../shared/dialogs/dialogs-form.service';
+import { Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './resources.component.html',
@@ -45,7 +47,7 @@ export class ResourcesComponent implements OnInit, AfterViewInit, OnDestroy {
   selection = new SelectionModel(true, []);
   onDestroy$ = new Subject<void>();
   parent = this.route.snapshot.data.parent;
-  displayedColumns = this.parent ? [ 'title', 'rating' ] : [ 'select', 'title', 'rating' ];
+  displayedColumns = [ 'select', 'title', 'rating' ];
   getOpts = this.parent ? { domain: this.userService.getConfig().parentDomain } : {};
   subjectList: any = constants.subjectList;
   levelList: any = constants.levelList;
@@ -71,7 +73,8 @@ export class ResourcesComponent implements OnInit, AfterViewInit, OnDestroy {
     private httpclient: HttpClient,
     private planetMessageService: PlanetMessageService,
     private userService: UserService,
-    private resourcesService: ResourcesService
+    private resourcesService: ResourcesService,
+    private dialogsFormService: DialogsFormService
   ) {}
 
   ngOnInit() {
@@ -252,6 +255,60 @@ export class ResourcesComponent implements OnInit, AfterViewInit, OnDestroy {
     }).concat(currentShelf.resourceIds).reduce(this.dedupeShelfReduce, []);
     const msg = resources.length === 1 ? resources[0].title + ' have been added to' : resources.length + ' resources have been added to';
     this.updateShelf(Object.assign({}, currentShelf, { resourceIds }), msg);
+  }
+
+  fetchResource(resources) {
+    const title = 'Confirmation';
+    const fields = [
+      {
+        'label': 'Old Password',
+        'type': 'textbox',
+        'inputType': 'password',
+        'name': 'password',
+        'placeholder': 'Password',
+        'required': true
+      }
+    ];
+    const formGroup = {
+      password: [ '', Validators.required ]
+    };
+    this.dialogsFormService
+      .confirm(title, fields, formGroup)
+      .debug('Dialog confirm')
+      .subscribe((response) => {
+        if (response !== undefined) {
+          const adminPassword = response.password;
+          const resourceIds = resources.map((res) => {
+            return { _id: res._id, _rev: res._rev };
+          });
+          this.couchService.post('_session', { name: this.userService.get().name, password: adminPassword })
+          .pipe(switchMap(data => {
+            const adminName = this.userService.get().name + '@' + this.userService.getConfig().code;
+            const resourceFetchDown = {
+              '_id': 'resource_from_parent' + Date.now(),
+              'source': {
+                'headers': {
+                  'Authorization': 'Basic ' + btoa(adminName + ':' + adminPassword)
+                },
+                'url': 'https://' + this.userService.getConfig().parentDomain + '/resources'
+              },
+              'target': {
+                'headers': {
+                  'Authorization': 'Basic ' + btoa(this.userService.get().name + ':' + adminPassword)
+                },
+                'url': environment.couchAddress + 'resources'
+              },
+              'document': resourceIds,
+              'create_target':  false,
+              'continuous': false,
+              'owner': this.userService.get().name
+            };
+            return this.couchService.post('_replicator', resourceFetchDown);
+          })).subscribe(data => {
+            this.planetMessageService.showMessage(resourceIds.length + ' ' + 'resources queued to fetch');
+          }, error => this.planetMessageService.showMessage('Invalid password'));
+        }
+      });
   }
 
   removeFromLibrary(resourceId, resourceTitle) {
