717ea3b361121c8a8c62deb70da0edf449d4a207
Lawrence Khadka <l.khadka@outlook.com>
2018-05-29 16:37:19 -0500

    export courses service

--- src/app/shared/services/pouch/index.ts
+++ src/app/shared/services/pouch/index.ts
@@ -1,7 +1,8 @@
 import { PouchService } from './pouch.service';
 import { AuthService } from './auth.service';
+import { CoursesService } from './courses.service';
 
 export { PouchService } from './pouch.service';
 export { AuthService } from './auth.service';
-
+export { CoursesService } from './courses.service';
 export const SHARED_SERVICES = [ PouchService, AuthService ];
