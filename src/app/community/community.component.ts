84f432004e1216ba65288cac5354678e2f1a073d
Laxman Maharjan <email@mlaxman.com.np>
2018-06-07 07:04:57 +0545

    Delete user on community reject (fixes #1221) (#1243)

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -97,7 +97,7 @@ export class CommunityComponent implements OnInit, AfterViewInit {
   updateClick(community, change) {
     this.editDialog = this.dialog.open(DialogsPromptComponent, {
       data: {
-        okClick: change === 'delete' ? this.deleteCommunity(community) : this.updateCommunity(community, change),
+        okClick: this.updateCommunity(community, change),
         changeType: change,
         type: 'community',
         displayName: community.name
@@ -112,15 +112,13 @@ export class CommunityComponent implements OnInit, AfterViewInit {
       // Split community object into id, rev, and all other props in communityInfo
       const { _id: communityId, _rev: communityRev, ...communityInfo } = community;
       switch (change) {
+        case 'delete':
+          this.deleteCommunity(community);
+          break;
         case 'reject':
         case 'unlink':
           const updatedCommunity = { ...community, registrationRequest: 'rejected' };
-          this.couchService.put('communityregistrationrequests/' + communityId, updatedCommunity)
-            .subscribe((data) => {
-              this.updateRev(data, this.communities.data);
-              this.getCommunityList();
-              this.editDialog.close();
-            }, (error) => this.editDialog.componentInstance.message = 'There was a problem accepting this community');
+          this.rejectCommunity(updatedCommunity);
           break;
         case 'accept':
           forkJoin([
@@ -142,31 +140,50 @@ export class CommunityComponent implements OnInit, AfterViewInit {
   addDeleteObservable(res, db) {
     if (res.docs.length > 0) {
       const doc = res.docs[0];
-      return [ this.couchService.delete(db + doc._id + '?rev=' + doc._rev) ];
+      return this.couchService.delete(db + doc._id + '?rev=' + doc._rev);
     }
-    return [];
+    return of({ 'ok': true });
   }
 
   deleteCommunity(community) {
     // Return a function with community on its scope to pass to delete dialog
-    return () => {
-    // With object destructuring colon means different variable name assigned, i.e. 'id' rather than '_id'
-      const { _id: id, _rev: rev } = community;
-      forkJoin([
-        this.couchService.post('_users/_find', { 'selector': { '_id': 'org.couchdb.user:' + community.adminName } }),
-        this.couchService.post('shelf/_find', { 'selector': { '_id': 'org.couchdb.user:' + community.adminName } })
-      ]).pipe(switchMap(([ user, shelf ]) => {
-        const deleteObs = [ this.couchService.delete('communityregistrationrequests/' + id + '?rev=' + rev) ].concat(
-          this.addDeleteObservable(user, '_users/'),
-          this.addDeleteObservable(shelf, 'shelf/')
-        );
-        return forkJoin(deleteObs);
-      })).subscribe((data) => {
-        // It's safer to remove the item from the array based on its id than to splice based on the index
-        this.communities.data = this.communities.data.filter((comm: any) => data[0].id !== comm._id);
-        this.editDialog.close();
-      }, (error) => this.editDialog.componentInstance.message = 'There was a problem deleting this community');
-    };
+    const { _id: id, _rev: rev } = community;
+    return this.pipeRemovePlanetUser(this.couchService.delete('communityregistrationrequests/' + id + '?rev=' + rev), community)
+    .subscribe(([ data, userRes ]) => {
+      // It's safer to remove the item from the array based on its id than to splice based on the index
+      this.communities.data = this.communities.data.filter((comm: any) => data.id !== comm._id);
+      this.editDialog.close();
+    }, (error) => this.editDialog.componentInstance.message = 'There was a problem deleting this community');
+  }
+
+  rejectCommunity(community: any) {
+    // Return a function with community on its scope to pass to delete dialog
+    return this.pipeRemovePlanetUser(this.couchService.put('communityregistrationrequests/' + community._id, community), community)
+    .subscribe(([ data, userRes ]) => {
+      this.updateRev(data, this.communities.data);
+      this.getCommunityList();
+      this.editDialog.close();
+    }, (error) => this.editDialog.componentInstance.message = 'There was a problem rejecting this community');
+  }
+
+  pipeRemovePlanetUser(obs: any, community) {
+    return obs.pipe(
+      switchMap(data => {
+        return forkJoin([ of(data), this.removePlanetUser(community) ]);
+      })
+    );
+  }
+
+  removePlanetUser(community) {
+    return forkJoin([
+      this.couchService.post('_users/_find', { 'selector': { '_id': 'org.couchdb.user:' + community.adminName } }),
+      this.couchService.post('shelf/_find', { 'selector': { '_id': 'org.couchdb.user:' + community.adminName } })
+    ]).pipe(switchMap(([ user, shelf ]) => {
+      return forkJoin([
+        this.addDeleteObservable(user, '_users/'),
+        this.addDeleteObservable(shelf, 'shelf/')
+      ]);
+    }));
   }
 
   // Gives the requesting user the 'learner' role & access to all DBs (as of April 2018)
