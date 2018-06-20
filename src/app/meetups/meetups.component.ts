c5acaa3fb4cbb9a55417d77670d3facd6747ce6f
nirojdyola <nirojdyola@gmail.com>
2018-06-20 23:55:35 +0545

    [#1313] Removed useservice from html

--- src/app/meetups/meetups.component.ts
+++ src/app/meetups/meetups.component.ts
@@ -38,6 +38,7 @@ export class MeetupsComponent implements OnInit, AfterViewInit, OnDestroy {
   displayedColumns = this.parent ? [ 'title' ] : [ 'select', 'title', 'info' ];
   getOpts = this.parent ? { domain: this.userService.getConfig().parentDomain } : {};
   pageEvent: PageEvent;
+  userName = this.userService.get().name;
 
   constructor(
     private couchService: CouchService,
