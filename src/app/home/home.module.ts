2fc8ec3a7e986630af1db7eae8b67c66f9032593
nirojdyola <nirojdyola@gmail.com>
2018-06-21 22:00:48 +0545

    [#1415] Feedback Popup (connects #1415)

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -12,7 +12,7 @@ import { PlanetFormsModule } from '../shared/planet-forms.module';
 import { MaterialModule } from '../shared/material.module';
 import { DashboardTileComponent } from '../dashboard/dashboard-tile.component';
 import { NotificationsComponent } from '../notifications/notifications.component';
-
+import { PlanetDialogsModule } from '../shared/dialogs/planet-dialogs.module';
 import { PulsateIconDirective } from './pulsate-icon.directive';
 
 @NgModule({
@@ -24,7 +24,8 @@ import { PulsateIconDirective } from './pulsate-icon.directive';
     PlanetFormsModule,
     MaterialModule,
     HttpClientModule,
-    HttpClientJsonpModule
+    HttpClientJsonpModule,
+    PlanetDialogsModule
   ],
   declarations: [
     HomeComponent,
