77ea261dea49e4963534670618edcd89b9496b38
Paul <paul.r.albert@gmail.com>
2018-06-22 09:34:26 -0400

    Fix current language name

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -62,7 +62,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     this.getNotification();
     this.onUserUpdate();
     this.currentLanguage = this.languages.find(language => {
-      return document.baseURI.indexOf('/' + language.name + '/') > -1;
+      return this.router.url.indexOf('/' + language.shortCode + '/') > -1;
     }) || this.currentLanguage;
     this.userService.notificationStateChange$.pipe(takeUntil(this.onDestroy$)).subscribe(() => {
       this.getNotification();
