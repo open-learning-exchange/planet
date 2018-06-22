e3102f0e4c601abf7d34efdcdb3bb03f57eeb479
mappuji <mappuji@protonmail.com>
2018-06-23 01:19:02 +0700

    fix sourcing problem

--- docker/planet/scripts/crosscompile_planet.sh
+++ docker/planet/scripts/crosscompile_planet.sh
@@ -6,14 +6,14 @@ source build_planet.sh
 ARCH=$1
 ACT=$2
 
-if [ -z "${I18N}" ]; then
+if [[ -z "${I18N}" ]]; then
   I18N="single"
 fi
 
-if [ "${ARCH}" = "armv7" ]; then
+if [[ "${ARCH}" == "armv7" ]]; then
   TRIPLE="arm-linux-gnueabihf"
   GCC="4.8"
-elif [ "${ARCH}" = "armv8" ]; then
+elif [[ "${ARCH}" == "armv8" ]]; then
   TRIPLE="aarch64-linux-gnu"
   GCC="4.8"
 else
@@ -28,20 +28,20 @@ export ZMQ_BUILD_OPTIONS="--host=${TRIPLE}"
 
 echo "Building Planet for ${ARCH}"
 
-if [ "${ACT}" = "install"  ]; then
+if [[ "${ACT}" == "install"  ]]; then
    echo "Install stage"
    apt-get update -qq
    apt-get install -y curl gnupg
    curl -sL https://deb.nodesource.com/setup_8.x | bash -
    apt-get install -y nodejs build-essential "${PACKAGES}"
    npm install "--arch=${TRIPLE}"
-elif [ "${ACT}" = "build"  ]; then
+elif [[ "${ACT}" == "build"  ]]; then
    echo "Build the angular app in production mode stage"
-   if [ "${I18N}" = "multi" ]; then
+   if [[ "${I18N}" == "multi" ]]; then
      build_multi
    else
      build_single
    fi
- else
+else
    echo "Error: No action Specified"
 fi
