4b698349f8d5cb63b700d08b3c4546ab1005fa9c
Laxman Maharjan <email@mlaxman.com.np>
2018-06-27 17:52:29 +0545

    [#1494] Send item to parent

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -265,14 +265,15 @@ export class CoursesComponent implements OnInit, AfterViewInit {
     this.updateShelf(userShelf, 'Course added to your dashboard');
   }
 
-  fetchCourse(courses) {
+  shareCourse(type, courses) {
+    const msg = (type === 'pull' ? 'fetch' : 'send');
     const { resources, exams } = this.coursesService.attachedItemsOfCourses(courses);
-    this.syncService.confirmPasswordAndRunReplicators([
+    this.syncService.confirmPasswordAndRunReplicators([ 
       { db: this.dbName, items: courses, type: 'pull', date: true },
       { db: 'resources', items: resources, type: 'pull', date: true },
       { db: 'exams', items: exams, type: 'pull', date: true }
     ]).subscribe((response: any) => {
-      this.planetMessageService.showMessage(courses.length + ' ' + this.dbName + ' ' + 'queued to fetch');
+      this.planetMessageService.showMessage(courses.length + ' ' + this.dbName + ' ' + 'queued to ' + msg);
     }, () => error => this.planetMessageService.showMessage(error));
   }
 
