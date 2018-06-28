c851a201a2dceb340e5447b6d07ef7938701cc60
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-06-27 21:12:17 -0300

    Keep route after lenguage change

--- src/app/shared/planet-language.component.ts
+++ src/app/shared/planet-language.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { languages } from '../shared/languages';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'planet-language',
@@ -10,7 +11,7 @@ export class PlanetLanguageComponent implements OnInit {
   languages = languages;
   currentLanguage: any = { name: 'English', shortCode: 'eng' };
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.currentLanguage = this.languages.find(language => {
@@ -21,4 +22,12 @@ export class PlanetLanguageComponent implements OnInit {
       language => language.shortCode !== this.currentLanguage.shortCode);
   }
 
+  getRouterUrl(language) {
+    const newRoute = '/' + language.shortCode + this.router.url;
+    if (this.router.url.indexOf('/' + this.currentLanguage.shortCode + '/') === 0) {
+      newRoute.replace('/' + this.currentLanguage.shortCode + '/', '/' + language.shortCode + '/');
+    }
+
+    return newRoute;
+  }
 }
