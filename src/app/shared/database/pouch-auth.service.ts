853ecb8468375e699788e562e390595e45282ebb
lawrencek0 <l.khadka@outlook.com>
2018-06-28 13:41:35 -0500

    create service to handle auth actions

---
+++ src/app/shared/database/pouch-auth.service.ts
@@ -0,0 +1,32 @@
+import { Injectable, Inject } from '@angular/core';
+import { PouchService } from './pouch.service';
+import { from, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable()
+export class PouchAuthService {
+    private authDB;
+
+    constructor(private pouchService: PouchService) {
+        this.authDB = this.pouchService.getAuthDB();
+    }
+
+    login(username, password) {
+        return from(this.authDB.login(username, password)).pipe(
+            catchError(this.handleError)
+        );
+    }
+
+    // opts: for passing in extra data about the user
+    signup(username, password, opts?) {
+        return from(this.authDB.signUp(username, password, opts)).pipe(
+            catchError(this.handleError)
+        );
+    }
+
+    private handleError(err) {
+        console.error('An error occured while signing in the user', err);
+        return throwError(err.message || err);
+    }
+}
