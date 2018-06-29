8ccc90284573ecad32c765c83d9249163cd6cd8c
Paul <paul.r.albert@gmail.com>
2018-06-29 14:25:39 -0400

    Fix number converted to string

--- src/app/courses/progress-courses/courses-progress-chart.component.ts
+++ src/app/courses/progress-courses/courses-progress-chart.component.ts
@@ -24,7 +24,7 @@ export class CoursesProgressChartComponent implements OnChanges {
     this.sets = this.inputs.map(input => ({
       ...input,
       items: fillEmptyItems(input.items),
-      total: input.items.reduce((total, item) => total + item.number, 0)
+      total: input.items.reduce((total, item) => total + (item.number || 0), 0)
     }));
     this.horizTotals = this.sets.reduce((totals, set) => {
       return set.items.map((item, index) => (item.number || 0) + (totals[index] || 0));
