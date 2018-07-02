dcdf25c659afd76df9561b1f15a19617fe1cfcd1
gcantieni <gus.cantieni@gmail.com>
2018-07-02 17:37:54 -0400

    Clean code Remove comments and remove previous fix for submit button

--- src/app/resources/resources-add.component.ts
+++ src/app/resources/resources-add.component.ts
@@ -33,7 +33,6 @@ export class ResourcesAddComponent implements OnInit {
   userDetail: any = {};
   pageType = 'Add new';
   disableDownload = true;
-  isSubmitted = false;
   resourceFilename = '';
 
   constructor(
@@ -118,7 +117,6 @@ export class ResourcesAddComponent implements OnInit {
 
   onSubmit() {
     if (this.resourceForm.valid) {
-      this.isSubmitted = true;
       const fileObs: Observable<any> = this.createFileObs();
       fileObs.pipe(debug('Preparing file for upload')).subscribe(({ resource, file }) => {
         const { _id, _rev } = this.existingResource;
