f67ff16aad56b6041f29138b9a0ea83752c2cbaf
gcantieni <gus.cantieni@gmail.com>
2018-07-02 17:37:54 -0400

    Clean code, remove comments and remove previous fix for submit button

--- src/app/shared/dialogs/dialogs-loading.component.ts
+++ src/app/shared/dialogs/dialogs-loading.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
-  selector: 'dialogs-loading',
   templateUrl: './dialogs-loading.component.html'
 })
 
--- src/app/shared/submit.directive.ts
+++ src/app/shared/submit.directive.ts
@@ -1,13 +1,6 @@
 import { Directive, HostListener, Input } from '@angular/core';
-import { MatDialog } from '@angular/material'
-import { DialogsLoadingComponent } from './dialogs/dialogs-loading.component'
-
-/*
- * TODO:
- * change planet-language module to planet-shared module
- * disable-close = true
- * closeOnNavigation = true
- */
+import { MatDialog } from '@angular/material';
+import { DialogsLoadingComponent } from './dialogs/dialogs-loading.component';
 
 @Directive({
   selector: '[planetSubmit]'
@@ -28,22 +21,3 @@ export class SubmitDirective {
     }
   }
 }
-/*
-import { Directive, HostListener } from '@angular/core';
-import { MatDialog } from '@angular/material'
-import { DialogsLoadingComponent } from './dialogs-loading.component'
-
-@Directive({selector: '[planetSubmit]'})
-export class PlanetSubmit {
-  constructor(private dialog: MatDialog) { }
-
-  @HostListener('click') onClick() {
-      this.openDialog();
-  }
-  openDialog(): void {
-    const dialogRef = this.dialog.open(DialogsLoadingComponent, {
-      // width: '250px'
-    });
-  }
-}
-*/
