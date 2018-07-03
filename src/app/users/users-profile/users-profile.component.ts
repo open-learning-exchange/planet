fac272aab2a8ce86dcdbf963c71a2517c29c7b3a
Paul <paul.r.albert@gmail.com>
2018-07-02 20:32:05 -0400

    Implement change password directive

--- src/app/users/users-profile/users-profile.component.ts
+++ src/app/users/users-profile/users-profile.component.ts
@@ -3,14 +3,6 @@ import { ActivatedRoute, ParamMap, Router } from '@angular/router';
 import { CouchService } from '../../shared/couchdb.service';
 import { environment } from '../../../environments/environment';
 import { UserService } from '../../shared/user.service';
-import { Validators } from '@angular/forms';
-import { DialogsFormService } from '../../shared/dialogs/dialogs-form.service';
-import { CustomValidators } from '../../validators/custom-validators';
-import { forkJoin, of } from 'rxjs';
-import { switchMap, catchError } from 'rxjs/operators';
-import { PlanetMessageService } from '../../shared/planet-message.service';
-import { ValidatorService } from '../../validators/validator.service';
-import { debug } from '../../debug-operator';
 
 @Component({
   templateUrl: './users-profile.component.html',
@@ -38,9 +30,6 @@ export class UsersProfileComponent implements OnInit {
     private couchService: CouchService,
     private route: ActivatedRoute,
     private userService: UserService,
-    private dialogsFormService: DialogsFormService,
-    private planetMessageService: PlanetMessageService,
-    private validatorService: ValidatorService,
     private router: Router
   ) { }
 
@@ -62,130 +51,6 @@ export class UsersProfileComponent implements OnInit {
     }, (error) => {
       console.log(error);
     });
-  }
-
-  onSubmit(credentialData, userDetail) {
-    const updateDoc = Object.assign({ password: credentialData.password }, userDetail);
-    this.changePasswordRequest(updateDoc).pipe(switchMap((responses) => {
-      return forkJoin([ ...responses.map(r => of(r)), this.reinitSession(userDetail.name, credentialData.password) ]);
-    })).subscribe((responses) => {
-      const errors = responses.filter(r => !r.ok);
-      if (errors.length === 0) {
-        this.planetMessageService.showMessage('Password successfully updated');
-      } else {
-        this.planetMessageService.showAlert(errors.map(e => e.reason).join(' & '));
-      }
-    }, (error) => this.planetMessageService.showAlert('Error changing password'));
-  }
-
-  changePasswordRequest(userData) {
-    // Manager role also has isUserAdmin true so check role to be empty
-    const isUserAdmin = (this.userService.get().isUserAdmin && !this.userService.get().roles.length);
-    const observables = [ this.couchService.put(this.dbName + '/' + userData._id, userData) ];
-    if (isUserAdmin) {
-      // Update user in parent planet
-      observables.push(this.couchService.get('_users/' + userData._id , { domain: this.userService.getConfig().parentDomain })
-        .pipe(catchError(this.passwordError('Error changing password in parent planet')),
-        switchMap((data) => {
-          if (data.ok === false) {
-            return of(data);
-          }
-          const { derived_key, iterations, password_scheme, salt, ...profile } = data;
-          profile.password = userData.password;
-          return this.couchService.put(this.dbName + '/' + profile._id, profile,
-            { domain: this.userService.getConfig().parentDomain });
-        }))
-      );
-      // Add response ok if there is not error on changing admin password
-      observables.push(
-        this.couchService.put('_node/nonode@nohost/_config/admins/' + userData.name, userData.password)
-        .pipe(catchError(this.passwordError('Error changing admin password')),
-        switchMap((response) => {
-          return of(response);
-        }))
-      );
-    }
-    return forkJoin(observables);
-  }
-
-  passwordError(reason: string) {
-    return () => {
-      return of({ ok: false, reason: reason });
-    };
-  }
-
-  reinitSession(username, password) {
-    return forkJoin([
-      this.couchService.post('_session', { 'name': username, 'password': password }, { withCredentials: true }),
-      this.couchService.post('_session', { 'name': username, 'password': password },
-        { withCredentials: true, domain: this.userService.getConfig().parentDomain })
-    ]).pipe(catchError(() => {
-      // Silent error for now so other specific messages are shown
-      return of({ ok: true });
-    }));
-  }
-
-  changePasswordForm(userDetail) {
-    const title = 'Change Password';
-    const fields = this.newChangePasswordFormFields();
-    const formGroup = this.newChangePasswordFormGroup();
-    this.dialogsFormService
-      .confirm(title, fields, formGroup)
-      .pipe(debug('Dialog confirm'))
-      .subscribe((res) => {
-        if (res !== undefined) {
-          this.onSubmit(res, userDetail);
-        }
-      });
-  }
-
-  newChangePasswordFormFields() {
-    return [
-      {
-        'label': 'Old Password',
-        'type': 'textbox',
-        'inputType': 'password',
-        'name': 'oldPassword',
-        'placeholder': 'Old Password',
-        'required': true
-      },
-      {
-        'label': 'Password',
-        'type': 'textbox',
-        'inputType': 'password',
-        'name': 'password',
-        'placeholder': 'Password',
-        'required': true
-      },
-      {
-        'label': 'Confirm Password',
-        'type': 'textbox',
-        'inputType': 'password',
-        'name': 'confirmPassword',
-        'placeholder': 'Confirm Password',
-        'required': true
-      }
-    ];
-  }
-
-  newChangePasswordFormGroup() {
-    return {
-      oldPassword: [ '', Validators.required, ac => this.validatorService.checkOldPassword$(ac) ],
-      password: [
-        '',
-        Validators.compose([
-          Validators.required,
-          CustomValidators.matchPassword('confirmPassword', false)
-        ])
-      ],
-      confirmPassword: [
-        '',
-        Validators.compose([
-          Validators.required,
-          CustomValidators.matchPassword('password', true)
-        ])
-      ]
-    };
   }
 
   goBack() {
