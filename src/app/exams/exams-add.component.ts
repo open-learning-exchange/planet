5fcbcefa7f68f2cfcd89e9da6d490bbf91e09144
Arpita Singh <singh.arpi92@gmail.com>
2018-07-05 13:21:07 +0545

    exam add fixes

--- src/app/exams/exams-add.component.ts
+++ src/app/exams/exams-add.component.ts
@@ -82,9 +82,9 @@ export class ExamsAddComponent implements OnInit {
     } else {
       Object.keys(this.examForm.controls).forEach(field => {
         const control = this.examForm.get(field);
-        control.markAsTouched({ onlySelf: true });
-        this.formNotComplete = true;
+        control.markAsTouched({ onlySelf: true });  
       });
+      this.formNotComplete = true;
     }
   }
 
