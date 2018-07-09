3b94259d7ff4ff7756c0f3bcdba089374de159e6
singharpita <singh.arpi92@gmail.com>
2018-07-09 22:37:10 +0545

    Update config back button (fixes #1548) (#1572)

---
+++ src/app/manager-dashboard/manager-dashboard-configuration.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  template: `
+    <mat-toolbar>
+      <a mat-icon-button i18n routerLink="/manager"><mat-icon>arrow_back</mat-icon></a>
+      <span i18n>Planet Configuration</span>
+    </mat-toolbar>
+    <planet-configuration></planet-configuration>
+  `
+})
+export class ManagerDashboardConfigurationComponent { }
