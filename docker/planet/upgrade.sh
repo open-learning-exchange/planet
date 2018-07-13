331aa86dc82a1760c5863fb2b92d6ab02297b3c6
Ignacio Rodriguez <ignacio@sugarlabs.org>
2018-07-13 19:18:22 -0300

    Remove usage of bash

--- docker/planet/upgrade.sh
+++ docker/planet/upgrade.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 echo "HTTP/1.0 200 OK"
 echo "Content-type: text/plain"
 echo ""
 
-PULL_IMAGES=(
-    treehouses/planet:db-init
-    treehouses/planet:latest
-    treehouses/couchdb:2.1.1
-)
+PULL_IMAGES="treehouses/planet:db-init"
+PULL_IMAGES="${PULL_IMAGES} treehouses/planet:latest"
+PULL_IMAGES="${PULL_IMAGES} treehouses/couchdb:2.1.1"
 
-for image in "${PULL_IMAGES[@]}" ; do
-  curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/create?fromImage=$image"
+for image in ${PULL_IMAGES}; do
+    curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/create?fromImage=$image"
 done
