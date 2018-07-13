9181f2d38cce3e728f773d555deb71aaa7c3d2a7
Paul <paul.r.albert@gmail.com>
2018-07-13 15:04:10 -0400

    Add teams to route with background (fixes #1637) (#1656)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -77,7 +77,9 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   // Used to swap in different background.
   // Should remove when background is finalized.
   backgroundRoute() {
-    const routesWithBackground = [ 'resources', 'courses', 'feedback', 'users', 'meetups', 'requests', 'associated', 'submissions' ];
+    const routesWithBackground = [
+      'resources', 'courses', 'feedback', 'users', 'meetups', 'requests', 'associated', 'submissions', 'teams'
+    ];
     // Leaving the exception variable in so we can easily use this while still testing backgrounds
     const routesWithoutBackground = [];
     const isException = routesWithoutBackground
