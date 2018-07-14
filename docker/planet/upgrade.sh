09680c790caf90de405d36e67c6b0b2474c578f8
Ignacio Rodriguez <ignacio@sugarlabs.org>
2018-07-13 22:29:09 -0300

    Use function

--- docker/planet/upgrade.sh
+++ docker/planet/upgrade.sh
@@ -3,10 +3,10 @@ echo "HTTP/1.0 200 OK"
 echo "Content-type: text/plain"
 echo ""
 
-PULL_IMAGES="treehouses/planet:db-init"
-PULL_IMAGES="${PULL_IMAGES} treehouses/planet:latest"
-PULL_IMAGES="${PULL_IMAGES} treehouses/couchdb:2.1.1"
+function upgrade {
+  curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/create?fromImage=$1"
+}
 
-for image in ${PULL_IMAGES}; do
-    curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/create?fromImage=$image"
-done
+upgrade "treehouses/planet:db-init"
+upgrade "treehouses/planet:latest"
+upgrade "treehouses/couchdb:2.1.1"
