68f598292ef3df9990c498ca996c75d030cf9bd1
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-07-20 21:54:36 -0300

    Revert "testing..."
    This reverts commit ee7f50f54dc10f676942eb689be3736cfb22dbc1.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,7 @@ export const environment = {
   production: true,
   test: false,
   // Change this to Docker address
-  // couchAddress: 'planet-db-host:planet-db-port/',
-  couchAddress: window.location.origin + '/di/',
+  couchAddress: 'planet-db-host:planet-db-port/',
   centerAddress: 'planet-center-address',
   centerProtocol: 'https',
   parentProtocol: 'https',
