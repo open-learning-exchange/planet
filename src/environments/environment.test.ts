abcce7d220de6a023177b8a1e3ab8ca4cdf472fe
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-07-20 21:38:49 -0300

    testing...

--- src/environments/environment.test.ts
+++ src/environments/environment.test.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
   test: true,
-  couchAddress: 'http://127.0.0.1:5984/',
+  // couchAddress: 'http://127.0.0.1:5984/',
+  couchAddress: window.location.origin + '/di/',
   centerAddress: 'earth.ole.org:2200',
   centerProtocol: 'https',
   parentProtocol: 'https',
