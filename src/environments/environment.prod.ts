694eba49d0dc3c349e7adc8e65d70923eb3a35cf
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-07-20 22:19:40 -0300

    ............

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   test: false,
   // Change this to Docker address
   // couchAddress: 'planet-db-host:planet-db-port/',
-  upgradeAddress: window.location.origin + '/di/',
+  couchAddress: window.location.origin + '/di/',
   centerAddress: 'planet-center-address',
   centerProtocol: 'https',
   parentProtocol: 'https',
