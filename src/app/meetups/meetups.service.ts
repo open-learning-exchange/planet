d3dce61c6494143f8048e69f65cba71494f22ee1
Paul <paul.r.albert@gmail.com>
2018-07-25 15:28:24 -0400

    Rework MeetupService to not update before success

--- src/app/meetups/meetups.service.ts
+++ src/app/meetups/meetups.service.ts
@@ -60,19 +60,25 @@ export class MeetupService {
   }
 
   attendMeetup(meetupId, participate) {
-    participate ? this.userShelf.meetupIds.splice(meetupId, 1)
-      : this.userShelf.meetupIds.push(meetupId);
-    return this.meetupShelf(participate);
+    const newMeetupIds = [ ...this.userShelf.meetupIds ];
+    if (participate) {
+      newMeetupIds.splice(meetupId, 1);
+    } else {
+      newMeetupIds.push(meetupId);
+    }
+    return this.updateMeetupShelf(newMeetupIds, participate);
   }
 
   attendMeetups(meetups) {
-    this.userShelf.meetupIds = meetups.concat(this.userShelf.meetupIds).reduce(dedupeShelfReduce, []);
-    return this.meetupShelf(true);
+    const newMeetupIds = meetups.concat(this.userShelf.meetupIds).reduce(dedupeShelfReduce, []);
+    return this.updateMeetupShelf(newMeetupIds, true);
   }
 
-  meetupShelf(participate) {
-    return this.couchService.put('shelf/' + this.userService.get()._id, this.userShelf)
+  updateMeetupShelf(meetupIds, participate) {
+    const newShelf = { ...this.userShelf, meetupIds };
+    return this.couchService.put('shelf/' + this.userService.get()._id, newShelf)
       .pipe(map((response) => {
+        this.userShelf = newShelf;
         this.userShelf._rev = response.rev;
         this.userService.shelf = this.userShelf;
         return { response, participate };
