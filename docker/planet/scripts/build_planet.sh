1b95ec52315894de705ff09bd49babe7c60994d4
Laxman Maharjan <email@mlaxman.com.np>
2018-07-28 00:25:09 +0545

    enable somali build

--- docker/planet/scripts/build_planet.sh
+++ docker/planet/scripts/build_planet.sh
@@ -6,6 +6,7 @@ build_multi(){
   "$(npm bin)"/ng build --prod --base-href /spa/ --output-path=dist/spa --aot --i18n-file=src/i18n/messages.spa.xlf --i18n-locale=es --i18n-format=xlf
   "$(npm bin)"/ng build --prod --base-href /fra/ --output-path=dist/fra --aot --i18n-file=src/i18n/messages.fra.xlf --i18n-locale=fr --i18n-format=xlf
   "$(npm bin)"/ng build --prod --base-href /nep/ --output-path=dist/nep --aot --i18n-file=src/i18n/messages.nep.xlf --i18n-locale=ne --i18n-format=xlf
+  "$(npm bin)"/ng build --prod --base-href /som/ --output-path=dist/som --aot --i18n-file=src/i18n/messages.som.xlf --i18n-locale=so --i18n-format=xlf
 }
 
 build_single(){
