6d30b8e8ee8b4e2f1c470f9a4866f17338bf22ce
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-07-28 00:32:37 -0300

    Rename img

--- docker/planet/upgrade.sh
+++ docker/planet/upgrade.sh
@@ -7,5 +7,10 @@ function upgrade {
   curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/create?fromImage=$1"
 }
 
+function rename {
+  curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/$1/tag?repo=$2"
+}
+
 upgrade "treehouses/planet:db-init"
 upgrade "treehouses/planet:$PLANET_VERSION"
+rename "treehouses/planet:$PLANET_VERSION" "treehouses/planet:local"
