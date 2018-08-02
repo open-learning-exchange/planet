9f0875a543ece559edf3c53545073c75f95069ed
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-08-02 07:54:11 -0300

    Upgrade to nation version only (fix #1670, #1671, #1774) (#1758)

--- docker/planet/upgrade.sh
+++ docker/planet/upgrade.sh
@@ -7,6 +7,12 @@ function upgrade {
   curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/create?fromImage=$1"
 }
 
-upgrade "treehouses/planet:db-init"
-upgrade "treehouses/planet:latest"
-upgrade "treehouses/couchdb:2.1.1"
+function rename {
+  curl --unix-socket /var/run/docker.sock -X POST "http://localhost/images/$1/tag?repo=$2"
+}
+
+upgrade "treehouses/planet:db-init-$PLANET_VERSION"
+upgrade "treehouses/planet:$PLANET_VERSION"
+
+rename "treehouses/planet:$PLANET_VERSION" "treehouses/planet:local"
+rename "treehouses/planet:db-init-$PLANET_VERSION" "treehouses/planet:db-init-local"
