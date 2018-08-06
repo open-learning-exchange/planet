46530479887bdb0f03279468e4210ccf5b9efda1
Arpita Singh <singh.arpi92@gmail.com>
2018-08-06 14:16:22 +0545

    removed unnecessary call in dialogcomponent

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -11,7 +11,6 @@ import { ConfigurationGuard } from '../configuration/configuration-guard.service
 import { MatTooltipModule } from '@angular/material';
 import { ConfigurationModule } from '../configuration/configuration.module';
 import { PlanetLanguageModule } from '../shared/planet-language.module';
-import { PlanetLanguageModule } from '../shared/planet-language.module';
 
 @NgModule({
   imports: [
@@ -22,7 +21,6 @@ import { PlanetLanguageModule } from '../shared/planet-language.module';
     ReactiveFormsModule,
     PlanetFormsModule,
     ConfigurationModule,
-    PlanetLanguageModule,
     PlanetLanguageModule
   ],
   declarations: [
