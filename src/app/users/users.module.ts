9ec51b33d86b613b75593526329b6a5a9f83fa9e
singharpita <singh.arpi92@gmail.com>
2018-08-06 15:43:59 +0545

    Spinner on submit (fixes #1785) (#1793)

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -10,6 +10,7 @@ import { UsersRouterModule } from './users-router.module';
 import { PlanetFormsModule } from '../shared/forms/planet-forms.module';
 import { MaterialModule } from '../shared/material.module';
 import { PlanetDialogsModule } from '../shared/dialogs/planet-dialogs.module';
+import { PlanetLanguageModule } from '../shared/planet-language.module';
 
 @NgModule({
   imports: [
@@ -20,7 +21,8 @@ import { PlanetDialogsModule } from '../shared/dialogs/planet-dialogs.module';
     ReactiveFormsModule,
     PlanetFormsModule,
     PlanetDialogsModule,
-    MaterialModule
+    MaterialModule,
+    PlanetLanguageModule
   ],
   declarations: [
     UsersComponent,
