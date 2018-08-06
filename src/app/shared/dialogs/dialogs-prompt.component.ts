66500785c73eaf8667635d48e98ff2569c96320c
Paul <paul.r.albert@gmail.com>
2018-08-06 14:02:04 -0400

    Clean up DialogsPromptComponent header comments

--- src/app/shared/dialogs/dialogs-prompt.component.ts
+++ src/app/shared/dialogs/dialogs-prompt.component.ts
@@ -1,12 +1,14 @@
 /*
- * Material dialog for deleting from Planet
- * type - Required.  Sets the text of the main message on the modal.  See HTML for options.
- * deleteItem - Required.  Object which is the main argument for the delete method located
- *  in the parent component this is called from.
+ * Material dialog for confirming action or prompting user with information
+ * showMainParagraph - Optional, defaults to true. Boolean which toggles the longer main message.
+ * type,
+ * changeType,
+ * amount - Required if showMainParagraph.  Sets the text of the main message on the modal.  See HTML for options.
+ * cancelable - Optional. Shows/hides the cancel button.
  * message - Optional.  Error message that is displayed when value is truthy.
  * displayName - Optional. If deleteItem does not have a 'name' property, set this to
  *  display to the user what is being deleted.
- * okClick - Required.  Method from parent which is called to delete.
+ * okClick - Optional.  Function to call when user clicks OK.
  */
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
