a0958c32a04524070d42e5d293f75bebe16399a1
Ignacio Rodríguez <ignacio@sugarlabs.org>
2018-08-07 16:57:08 -0300

    Pass database to http://host/db/ (connects #1716) (#1724)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,8 @@ export const environment = {
   production: true,
   test: false,
   // Change this to Docker address
-  couchAddress: 'planet-db-host:planet-db-port/',
+  // couchAddress: 'planet-db-host:planet-db-port/',
+  couchAddress: window.location.origin + '/db/',
   centerAddress: 'planet-center-address',
   centerProtocol: 'https',
   parentProtocol: 'https',
