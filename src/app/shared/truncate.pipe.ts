4c2d40ba1b7e13deddbd9ceefef4f0646dfd3015
Bhargavi Nadendla <bhargavinadendla@gmail.com>
2018-08-08 20:32:20 -0500

    Truncating the list view of text throughout planet(fixes #1787)

---
+++ src/app/shared/truncate.pipe.ts
@@ -0,0 +1,22 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate'
+})
+
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string, length: number): string {
+    const biggestWord = 50;
+    const elipses = "...";
+    if(typeof value === "undefined") return value;
+    if(value.length <= length) return value;
+    let truncatedText = value.slice(0, length + biggestWord);
+    while (truncatedText.length > length - elipses.length) {
+      let lastSpace = truncatedText.lastIndexOf(" ");
+      if(lastSpace === -1) break;
+      truncatedText = truncatedText.slice(0, lastSpace).replace(/[!,.?;:]$/, '');
+    }
+    return truncatedText + elipses;
+    }
+}
