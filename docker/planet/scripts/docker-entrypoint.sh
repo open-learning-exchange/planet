6e99a6ce30d3d3d09393e55af65cf1764b1b7ad9
Laxman Maharjan <email@mlaxman.com.np>
2018-08-10 20:47:09 +0545

    Parent protocol from yml file (fixes #1813) (#1817)

--- docker/planet/scripts/docker-entrypoint.sh
+++ docker/planet/scripts/docker-entrypoint.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env sh
 
-if [ -z "$HOST_PROTOCOL" ]
-then
-  PROTOCOL="http"
-elif [ "$HOST_PROTOCOL" = "https" ]
-then
-  PROTOCOL="https"
-else
-  PROTOCOL="http"
-fi
+# Function for protocol
+check_protocol() {
+  PARAM=$1
+  DEFAULT=$2
+  if [ -z "$PARAM" ]
+  then
+    PROTO="$DEFAULT"
+  elif [ "$PARAM" = "https" ]
+  then
+    PROTO="https"
+  else
+    PROTO="http"
+  fi
+  echo "$PROTO"
+}
+
+PROTOCOL=$(check_protocol $HOST_PROTOCOL "http")
+P_PROTOCOL=$(check_protocol $PARENT_PROTOCOL "https")
 
 if [ "$MULTIPLE_IPS" = "true" ]
 then
@@ -19,6 +28,7 @@ fi
 
 sed -i -e "s#planet-db-port#$DB_PORT#g" /usr/share/nginx/html/**/main*
 sed -i -e "s#planet-center-address#$CENTER_ADDRESS#g" /usr/share/nginx/html/**/main*
+sed -i -e "s#planet-parent-protocol#$P_PROTOCOL#g" /usr/share/nginx/html/**/main*
 
 spawn-fcgi -s /run/fcgi.sock -U nginx -G nginx /usr/bin/fcgiwrap
 nginx -g "daemon off;"
