c40d235a127e9687c344808fcec8e26a40f399ec
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-08-15 20:46:12 +0545

    [1848] logo-instead-spinner fixed

--- src/app/shared/dialogs/dialogs-loading.component.ts
+++ src/app/shared/dialogs/dialogs-loading.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
-  templateUrl: './dialogs-loading.component.html'
+  templateUrl: './dialogs-loading.component.html',
+  styles: [`.logo-spinner {
+    animation: loading-icon-linear-rotate 4s linear infinite;
+  }
+  @keyframes loading-icon-linear-rotate{
+    0% { transform: rotate(0); }
+    100% { transform: rotate(360deg); }
+  }`]
 })
 
 export class DialogsLoadingComponent {
