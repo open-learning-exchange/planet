eb5cb64de21a969749ad700e0d8c792f3fe0af8f
Bhargavi Nadendla <bhargavinadendla@gmail.com>
2018-08-14 20:18:10 -0500

    Deselecting the resources in course-step (fixes #1847)

--- src/app/shared/dialogs/dialogs-list.component.ts
+++ src/app/shared/dialogs/dialogs-list.component.ts
@@ -45,7 +45,7 @@ export class DialogsListComponent implements AfterViewInit {
     this.tableData.data = this.data.tableData;
     this.tableColumns = this.data.columns;
     this.disableRowClick = this.data.disableSelection || false;
-    this.emptySubmit = this.data.selectionOptional || false;
+    this.emptySubmit = this.data.selectionOptional || true;
     if (this.data.filterPredicate) {
       this.tableData.filterPredicate = this.data.filterPredicate;
     }
