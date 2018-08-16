2cd76fb745e3d07e8a308ca05e407096753c4940
Paul <paul.r.albert@gmail.com>
2018-08-15 22:45:15 -0400

    Clean up HTML and style fixes

--- src/app/shared/dialogs/dialogs-loading.component.ts
+++ src/app/shared/dialogs/dialogs-loading.component.ts
@@ -3,13 +3,15 @@ import { MatDialog, MatDialogRef } from '@angular/material';
 
 @Component({
   templateUrl: './dialogs-loading.component.html',
-  styles: [`.logo-spinner {
-    animation: loading-icon-linear-rotate 4s linear infinite;
-  }
-  @keyframes loading-icon-linear-rotate{
-    0% { transform: rotate(0); }
-    100% { transform: rotate(360deg); }
-  }`]
+  styles: [ `
+    .logo-spinner {
+      animation: loading-icon-linear-rotate 4s linear infinite;
+    }
+    @keyframes loading-icon-linear-rotate {
+      0% { transform: rotate(0); }
+      100% { transform: rotate(360deg); }
+    }
+  ` ]
 })
 
 export class DialogsLoadingComponent {
