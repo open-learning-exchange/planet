fe56bc106b8d0c04cf3af35629b0f35c7ae6f0df
Paul <paul.r.albert@gmail.com>
2018-08-27 17:02:27 -0400

    Rename directory and files

--- docker/planet/nginx/credentials.sh
+++ docker/planet/nginx/credentials.sh
@@ -6,10 +6,10 @@ echo ""
 PLANET_USER=${PLANET_CREDENTIALS%%,*}
 PLANET_PASS=${PLANET_CREDENTIALS##*,}
 
-rm admin/admin.yml
+rm credentials/credentials.yml
 echo "services:
   db-init:
     environment:
       - COUCHDB_USER=$PLANET_USER
       - COUCHDB_PASS=$PLANET_PASS
-version: \"2\"" >> admin/admin.yml
+version: \"2\"" >> credentials/credentials.yml
