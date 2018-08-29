e80517531748b4a42f48d51bf3fc88c01f8fab71
Paul <paul.r.albert@gmail.com>
2018-08-29 02:02:53 -0400

    Planet-step-list always has reorder & remove buttons

--- src/app/shared/forms/planet-step-list.component.ts
+++ src/app/shared/forms/planet-step-list.component.ts
@@ -1,11 +1,57 @@
-import { Component, Input, EventEmitter, Output, Directive, ContentChildren, ViewChild, TemplateRef } from '@angular/core';
+import {
+  Component,
+  Input,
+  EventEmitter,
+  Output,
+  Directive,
+  ContentChildren,
+  ViewChild,
+  TemplateRef,
+  Injectable,
+  OnDestroy
+} from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlanetStepListService {
+
+  stepMoveClick$ = new Subject<any>();
+
+  constructor() {}
+
+  moveStep(index, direction) {
+    this.stepMoveClick$.next({ index, direction });
+  }
+
+}
 
 @Component({
   selector: 'planet-step-list-item',
-  template: '<ng-template><ng-content></ng-content></ng-template>'
+  template: `
+    <ng-template>
+      <ng-content></ng-content>
+      <button mat-icon-button *ngIf="!isFirst" (click)="moveStep($event,-1)"><mat-icon>arrow_upward</mat-icon></button>
+      <button mat-icon-button *ngIf="!isLast" (click)="moveStep($event,1)"><mat-icon>arrow_downward</mat-icon></button>
+      <button mat-icon-button (click)="moveStep($event,i)"><mat-icon>delete</mat-icon></button>
+    </ng-template>
+  `
 })
 export class PlanetStepListItemComponent {
   @ViewChild(TemplateRef) template: TemplateRef<any>;
+  index: number;
+  isFirst: boolean;
+  isLast: boolean;
+
+  constructor(private planetStepListService: PlanetStepListService) {}
+
+  moveStep(event, direction = 0) {
+    event.stopPropagation();
+    this.planetStepListService.moveStep(this.index, direction);
+  }
+
 }
 
 @Component({
@@ -18,20 +64,36 @@ export class PlanetStepListItemComponent {
     }
   ` ]
 })
-export class PlanetStepListComponent {
+export class PlanetStepListComponent implements OnDestroy {
 
   @Input() steps: any[];
+  @Output() stepsChange = new EventEmitter<any[]>();
   @Input() nameProp: string;
   @Input() defaultName = 'Step';
   @Output() stepClicked = new EventEmitter<number>();
-  @Output() stepUpdated = new EventEmitter<number>();
 
   @ContentChildren(PlanetStepListItemComponent) stepListItems;
 
   listMode = true;
   openIndex = -1;
+  private onDestroy$ = new Subject<void>();
 
-  constructor() {}
+  constructor(private planetStepListService: PlanetStepListService) {
+    this.planetStepListService.stepMoveClick$.pipe(takeUntil(this.onDestroy$)).subscribe(this.moveStep.bind(this));
+  }
+
+  ngAfterContentChecked() {
+    this.stepListItems.forEach((item, index, array) => {
+      item.index = index;
+      item.isFirst = index === 0;
+      item.isLast = index === (array.length - 1);
+    });
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
 
   stepClick(index: number) {
     this.listMode = false;
@@ -41,7 +103,14 @@ export class PlanetStepListComponent {
 
   toList() {
     this.listMode = true;
-    this.stepUpdated.emit(this.openIndex);
+  }
+
+  moveStep({ index, direction }) {
+    const step = this.steps.splice(index, 1)[0];
+    if (direction !== 0) {
+      this.steps.splice(index + direction, 0, step);
+    }
+    this.stepsChange.emit(this.steps);
   }
 
 }
