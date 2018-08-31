e27a18042401fcd262646d0f6306f0a5b3b33ca3
Bhargavi Nadendla <bhargavinadendla@gmail.com>
2018-08-31 17:18:58 -0500

    Fixing header empty in Exam and survey(fixes #1994)

--- src/app/exams/exams-view.component.ts
+++ src/app/exams/exams-view.component.ts
@@ -26,7 +26,7 @@ export class ExamsViewComponent implements OnInit, OnDestroy {
   grade;
   submissionId: string;
   fromSubmission = false;
-  currentRoute;
+  examType = this.route.snapshot.data.mySurveys === true ? 'survey' : 'exam';
 
   constructor(
     private router: Router,
@@ -62,7 +62,6 @@ export class ExamsViewComponent implements OnInit, OnDestroy {
         this.submissionsService.openSubmission({ submissionId, 'status': params.get('status') });
       }
     });
-    this.currentRoute = this.route.snapshot.data.mySurveys === true ? 'survey' : 'exam';
   }
 
   ngOnDestroy() {
