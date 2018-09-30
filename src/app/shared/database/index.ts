41eeaca974270f9b6273b25e8780dce8e6573152
lawrencek0 <l.khadka@outlook.com>
2018-09-30 15:32:59 -0500

    add feedback service to exports

--- src/app/shared/database/index.ts
+++ src/app/shared/database/index.ts
@@ -1,6 +1,8 @@
 import { PouchService } from './pouch.service';
 import { PouchAuthService } from './pouch-auth.service';
+import { FeedbackService } from './feedback.service';
 
 export { PouchService } from './pouch.service';
 export { PouchAuthService } from './pouch-auth.service';
-export const SHARED_SERVICES = [ PouchService, PouchAuthService ];
+export { FeedbackService } from './feedback.service';
+export const SHARED_SERVICES = [ PouchService, PouchAuthService, FeedbackService ];
