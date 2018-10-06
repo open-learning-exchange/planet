8e9bf33041e5de4de358381aaf041ae36eb20f9f
lawrencek0 <l.khadka@outlook.com>
2018-10-06 11:35:04 -0500

    configure databases on successful login

--- src/app/shared/database/pouch-auth.service.ts
+++ src/app/shared/database/pouch-auth.service.ts
@@ -28,6 +28,7 @@ export class PouchAuthService {
   }
 
   login(username, password) {
+    this.pouchService.configureDBs();
     return from(this.authDB.logIn(username, password)).pipe(
       catchError(this.handleError)
     );
