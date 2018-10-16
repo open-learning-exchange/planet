3d53f8100b7b88c9056efa9c75820dcd27318e74
YangLou1 <bloglog@hotmail.com>
2018-10-16 17:44:20 -0400

    Make reports scroll within view-container (connects #2282)

--- src/app/manager-dashboard/reports/reports-detail.component.ts
+++ src/app/manager-dashboard/reports/reports-detail.component.ts
@@ -5,6 +5,7 @@ import { StateService } from '../../shared/state.service';
 
 @Component({
   templateUrl: './reports-detail.component.html',
+  styleUrls: [ './reports-detail.scss' ]
 })
 export class ReportsDetailComponent {
 
