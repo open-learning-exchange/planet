2cb922d46ef84ead4218d88175a35f77a174e2ec
Paul <paul.r.albert@gmail.com>
2018-10-19 22:25:19 -0400

    Add top horizontal navbar design (#2345)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,49 +1,24 @@
-import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { UserService } from '../shared/user.service';
 import { CouchService } from '../shared/couchdb.service';
 import { Router } from '@angular/router';
-import { trigger, state, style, animate, transition } from '@angular/animations';
-import { languages } from '../shared/languages';
-import { interval, Subject, forkJoin } from 'rxjs';
-import { tap, switchMap, takeUntil } from 'rxjs/operators';
+import { Subject, forkJoin } from 'rxjs';
+import { switchMap, takeUntil } from 'rxjs/operators';
 import { findDocuments } from '../shared/mangoQueries';
-import { debug } from '../debug-operator';
 import { PouchAuthService } from '../shared/database';
 import { StateService } from '../shared/state.service';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: [ './home.scss' ],
-  animations: [
-    trigger('sidenavState', [
-      state('closed', style({
-        width: '72px'
-      })),
-      state('open', style({
-        width: '150px'
-      })),
-      transition('closed <=> open', animate('500ms ease'))
-    ])
-  ]
 })
-export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
-  sidenavState = 'closed';
+export class HomeComponent implements OnInit {
+
   notifications = [];
-  @ViewChild('content') private mainContent;
   user: any = {};
   userImgSrc = '';
-
-  // Sets the margin for the main content to match the sidenav width
-  animObs = interval(15).pipe(
-    debug('Menu animation'),
-    tap(() => {
-      this.mainContent._updateContentMargins();
-      this.mainContent._changeDetectorRef.markForCheck();
-    }
-  ));
-  // For disposable returned by observer to unsubscribe
-  animDisp: any;
+  layout: string;
 
   private onDestroy$ = new Subject<void>();
 
@@ -58,6 +33,7 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
       .subscribe(() => {
         this.onUserUpdate();
       });
+    this.couchService.get('_node/nonode@nohost/_config/planet').subscribe((res: any) => this.layout = res.layout || 'classic');
   }
 
   ngOnInit() {
@@ -68,34 +44,22 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     });
   }
 
-  ngAfterViewInit() {
-    this.mainContent._updateContentMargins();
-    this.mainContent._changeDetectorRef.markForCheck();
-  }
-
-  ngOnDestroy() {
-    this.onDestroy$.next();
-    this.onDestroy$.complete();
-  }
-
   // Used to swap in different background.
   // Should remove when background is finalized.
   backgroundRoute() {
-    const routesWithBackground = [
-      'resources', 'courses', 'feedback', 'users', 'meetups', 'requests', 'associated', 'submissions', 'teams'
-    ];
-    // Leaving the exception variable in so we can easily use this while still testing backgrounds
-    const routesWithoutBackground = [];
-    const isException = routesWithoutBackground
-      .findIndex((route) => this.router.url.indexOf(route) > -1) > -1;
-    const isRoute = routesWithBackground
-      .findIndex((route) => this.router.url.indexOf(route) > -1) > -1;
-    return isRoute && !isException;
-  }
-
-  toggleNav() {
-    this.sidenavState = this.sidenavState === 'open' ? 'closed' : 'open';
-    this.animDisp = this.animObs.subscribe();
+    const router = this.router;
+    return () => {
+      const routesWithBackground = [
+        'resources', 'courses', 'feedback', 'users', 'meetups', 'requests', 'associated', 'submissions', 'teams'
+      ];
+      // Leaving the exception variable in so we can easily use this while still testing backgrounds
+      const routesWithoutBackground = [];
+      const isException = routesWithoutBackground
+        .findIndex((route) => router.url.indexOf(route) > -1) > -1;
+      const isRoute = routesWithBackground
+        .findIndex((route) => router.url.indexOf(route) > -1) > -1;
+      return isRoute && !isException;
+    };
   }
 
   onUserUpdate() {
@@ -108,31 +72,23 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  endAnimation() {
-    if (this.animDisp) {
-      this.animDisp.unsubscribe();
-    }
-  }
-
-  switchLanguage(servedUrl) {
-    alert('You are going to switch in ' + servedUrl + ' environment');
-  }
-
   logoutClick() {
-    const configuration = this.stateService.configuration;
-    this.userService.endSessionLog().pipe(switchMap(() => {
-      const obsArr = [ this.pouchAuthService.logout() ];
-      const localAdminName = configuration.adminName.split('@')[0];
-      if (localAdminName === this.userService.get().name) {
-        obsArr.push(
-          this.couchService.delete('_session', { withCredentials: true, domain: configuration.parentDomain }),
-        );
-      }
-      return forkJoin(obsArr);
-    })).subscribe((response: any) => {
-        this.userService.unset();
-        this.router.navigate([ '/login' ], {});
-    }, err => console.log(err));
+    return () => {
+      const configuration = this.stateService.configuration;
+      this.userService.endSessionLog().pipe(switchMap(() => {
+        const obsArr = [ this.pouchAuthService.logout() ];
+        const localAdminName = configuration.adminName.split('@')[0];
+        if (localAdminName === this.userService.get().name) {
+          obsArr.push(
+            this.couchService.delete('_session', { withCredentials: true, domain: configuration.parentDomain }),
+          );
+        }
+        return forkJoin(obsArr);
+      })).subscribe((response: any) => {
+          this.userService.unset();
+          this.router.navigate([ '/login' ], {});
+      }, err => console.log(err));
+    };
   }
 
   getNotification() {
@@ -155,11 +111,13 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
     }, (error) => console.log(error));
   }
 
-  readNotification(notification) {
-    const updateNotificaton =  { ...notification, 'status': 'read' };
-    this.couchService.put('notifications/' + notification._id, updateNotificaton).subscribe((data) => {
-      this.userService.setNotificationStateChange();
-    },  (err) => console.log(err));
+  readNotification() {
+    return (notification) => {
+      const updateNotificaton =  { ...notification, 'status': 'read' };
+      this.couchService.put('notifications/' + notification._id, updateNotificaton).subscribe((data) => {
+        this.userService.setNotificationStateChange();
+      },  (err) => console.log(err));
+    };
   }
 
 }
