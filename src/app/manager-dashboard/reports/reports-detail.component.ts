e37f9d6fdf86d728e537e601ae1b432d5e7b7e9e
Svitlana Lesiv <lesivsvitlana@gmail.com>
2018-10-25 03:18:33 -0400

    change report label style (fixes #2373)

--- src/app/manager-dashboard/reports/reports-detail.component.ts
+++ src/app/manager-dashboard/reports/reports-detail.component.ts
@@ -4,7 +4,8 @@ import { ReportsService } from './reports.service';
 import { StateService } from '../../shared/state.service';
 
 @Component({
-  templateUrl: './reports-detail.component.html'
+  templateUrl: './reports-detail.component.html',
+  styleUrls: ['./reports.scss'],
 })
 export class ReportsDetailComponent {
 
