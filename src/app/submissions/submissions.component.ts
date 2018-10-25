578ca311a20763caf432c150716e3737acb01a1f
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-10-25 13:12:37 +0545

    issues with complete and grade fixed

--- src/app/submissions/submissions.component.ts
+++ src/app/submissions/submissions.component.ts
@@ -110,7 +110,7 @@ export class SubmissionsComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   submissionAction(submission) {
-    if ((this.mode === 'survey' && submission.status !== 'complete') || (this.mode !== 'survey' && submission.status !== 'pending')) {
+    if (this.mode === 'survey' || (this.mode !== 'survey' && submission.status !== 'pending')) {
       this.router.navigate([
         './exam',
         { submissionId: submission._id, questionNum: 1, status: submission.status, mode: this.surveyMode(this.mode, submission.type) }
