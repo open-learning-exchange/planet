42b2eaa0badbdf6c4dfed3fc9b47e357038f6e5e
Paul <paul.r.albert@gmail.com>
2018-11-14 17:28:12 -0500

    Remove chip input for labels

--- src/app/shared/forms/planet-tag-input.component.ts
+++ src/app/shared/forms/planet-tag-input.component.ts
@@ -1,12 +1,10 @@
 import {
-  Component, Input, Optional, Self, OnDestroy, HostBinding, EventEmitter, Output, ViewChild, ElementRef, OnInit, Inject
+  Component, Input, Optional, Self, OnDestroy, HostBinding, EventEmitter, Output, ElementRef, Inject
 } from '@angular/core';
 import { ControlValueAccessor, NgControl, FormControl } from '@angular/forms';
-import { MatFormFieldControl, MatAutocomplete, MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+import { MatFormFieldControl, MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 import { FocusMonitor } from '@angular/cdk/a11y';
-import { ENTER, COMMA } from '@angular/cdk/keycodes';
-import { Subject, Observable } from 'rxjs';
-import { startWith, map, takeUntil, auditTime } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 import { TagsService } from './tags.service';
 
 @Component({
@@ -59,7 +57,7 @@ export class PlanetTagInputDialogComponent {
     { provide: MatFormFieldControl, useExisting: PlanetTagInputComponent }
   ]
 })
-export class PlanetTagInputComponent implements ControlValueAccessor, OnInit, OnDestroy {
+export class PlanetTagInputComponent implements ControlValueAccessor, OnDestroy {
 
   static nextId = 0;
 
@@ -76,10 +74,6 @@ export class PlanetTagInputComponent implements ControlValueAccessor, OnInit, On
   }
   @Output() valueChanges = new EventEmitter<string[]>();
 
-  get empty() {
-    return this.tagInput.nativeElement.value === '' && this._value.length === 0;
-  }
-
   private _placeholder: string;
   @Input()
   get placeholder() {
@@ -90,22 +84,13 @@ export class PlanetTagInputComponent implements ControlValueAccessor, OnInit, On
     this.stateChanges.next();
   }
 
-  get shouldLabelFloat() {
-    return this.focused || !this.empty;
-  }
-
-  @ViewChild('tagInput') tagInput: ElementRef;
-  @ViewChild('tagAuto') tagAutocomplete: MatAutocomplete;
-
+  shouldLabelFloat = false;
   onTouched;
   stateChanges = new Subject<void>();
-  tagChanges$ = new Subject<string>();
-  private onDestroy$ = new Subject<void>();
   tags: string[] = [];
-  filteredTags: Observable<string[]>;
   inputControl = new FormControl();
   focused = false;
-  separatorKeyCodes = [ ENTER, COMMA ];
+  tooltipLabels = '';
 
   constructor(
     @Optional() @Self() public ngControl: NgControl,
@@ -120,50 +105,21 @@ export class PlanetTagInputComponent implements ControlValueAccessor, OnInit, On
     this.tagsService.getTags().subscribe((tags: string[]) => {
       this.tags = tags;
     });
-    this.filteredTags = this.inputControl.valueChanges.pipe(
-      startWith(null),
-      map((value: string | null) => value ? this.tagsService.filterTags(this.tags, value) : this.tags),
-      map((fullList) => fullList.slice(0, 5))
-    );
-    this.focusMonitor.monitor(elementRef.nativeElement, true).subscribe(origin => {
-      this.focused = !!origin;
-      this.stateChanges.next();
-    });
-  }
-
-  ngOnInit() {
-    this.tagChanges$.pipe(takeUntil(this.onDestroy$), auditTime(200)).subscribe((newTag) => {
-      this.addTag(newTag);
-    });
   }
 
   ngOnDestroy() {
     this.stateChanges.complete();
-    this.onDestroy$.next();
-    this.onDestroy$.complete();
     this.focusMonitor.stopMonitoring(this.elementRef.nativeElement);
   }
 
   onChange(_: any) {}
 
-  inputAddTag(event: any) {
-    this.tagChanges$.next(event.value);
-  }
-
-  autocompleteAddTag(event: any) {
-    this.tagChanges$.next(event.option.viewValue);
-  }
-
   addTag(newTag: string) {
     if (this.value.indexOf(newTag.trim()) > -1) {
       return;
     }
-    const input = this.tagInput.nativeElement;
     if (newTag.trim()) {
       this.writeValue(this.value.concat([ newTag.trim() ]));
-    }
-    if (input.value) {
-      input.value = '';
     }
   }
 
@@ -173,6 +129,7 @@ export class PlanetTagInputComponent implements ControlValueAccessor, OnInit, On
 
   writeValue(tags) {
     this.value = tags;
+    this.tooltipLabels = tags.join(', ');
   }
 
   registerOnChange(fn: (_: any) => void) {
