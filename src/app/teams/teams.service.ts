f2f53371a273a2c298fbaaa1e7ef5689e43fba14
singharpita <singh.arpi92@gmail.com>
2018-11-22 06:02:28 +0545

    Notify member added to team (fixes #2539) (#2551)

--- src/app/teams/teams.service.ts
+++ src/app/teams/teams.service.ts
@@ -118,12 +118,29 @@ export class TeamsService {
     const notifications = members.filter((user: any) => {
       return this.userService.get().name !== user.name && user.name !== 'satellite';
     }).map((user: any) => {
-      if (type === 'request') {
-        return this.requestNotification(user._id, notificationParams);
+      switch (type) {
+        case 'request':
+          return this.requestNotification(user._id, notificationParams);
+        case 'added':
+          return this.addedToTeamNotification(user._id, notificationParams);
+        default:
+          return this.memberAddNotification(user._id, notificationParams);
       }
-      return this.memberAddNotification(user._id, notificationParams);
     });
     return this.couchService.post('notifications/_bulk_docs', { docs: notifications });
+  }
+
+  addedToTeamNotification(userId, { team, url }) {
+    return {
+      'user': userId,
+      'message': 'You have been added to ' + team.name + ' team.',
+      'link': url,
+      'item': team._id,
+      'type': 'team',
+      'priority': 1,
+      'status': 'unread',
+      'time': Date.now()
+    };
   }
 
   memberAddNotification(userId, { team, url, newMembersLength }) {
