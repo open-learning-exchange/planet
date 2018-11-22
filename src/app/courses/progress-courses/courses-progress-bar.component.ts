4eb884b58f79d460247d8678406064d4ca471102
Jingting Chen <jchen136@hawk.iit.edu>
2018-11-22 04:18:06 -0600

    Courses step from progress bar (fixes #2421) (#2513)

--- src/app/courses/progress-courses/courses-progress-bar.component.ts
+++ src/app/courses/progress-courses/courses-progress-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'planet-courses-progress-bar',
@@ -11,8 +12,17 @@ export class CoursesProgressBarComponent implements OnChanges {
   @Input() courseProgress: any = { stepNum: 0 };
   completed = false;
 
+  constructor(
+    private router: Router
+  ) { }
+
   ngOnChanges() {
     this.completed = this.course.steps.length === this.courseProgress.stepNum && this.courseProgress.passed;
   }
 
+  routing(completedStepNum, i) {
+    if (i < completedStepNum) {
+      this.router.navigate([ '/courses/view', this.course._id, 'step', i + 1 ]);
+    }
+  }
 }
