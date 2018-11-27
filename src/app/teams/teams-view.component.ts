6e5ba42e8297ce248667fe4685537072cc3d1cda
Laxman Maharjan <email@mlaxman.com.np>
2018-11-27 23:46:55 +0545

    reduce line

--- src/app/teams/teams-view.component.ts
+++ src/app/teams/teams-view.component.ts
@@ -143,9 +143,7 @@ export class TeamsViewComponent implements OnInit, OnDestroy {
         return this.couchService.post('shelf/_bulk_docs', { docs: newShelves });
       }),
       switchMap((notifyShelf) => {
-        this.team.requests = this.team.requests.filter(reqId => {
-          return selectedIds.indexOf(reqId) === -1;
-        });
+        this.team.requests = this.team.requests.filter(reqId => selectedIds.indexOf(reqId) === -1);
         return forkJoin([
           this.teamsService.updateTeam(this.team),
           this.teamsService.sendNotifications('added', selected, {
