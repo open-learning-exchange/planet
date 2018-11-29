0c6b4c18c14108d1f8e7303f84026bd1fe91e899
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-11-29 11:29:31 +0545

    Don't count stepless course for join selected (fixes #2522) (#2532)

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -238,7 +238,13 @@ export class CoursesComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   countSelectNotEnrolled(selected: any) {
-    this.selectedNotEnrolled = selected.reduce((count, id) => count + (this.userShelf.courseIds.indexOf(id) === -1 ? 1 : 0), 0);
+    this.selectedNotEnrolled = selected.reduce((count, id) => {
+      return this.hasSteps(id) ? count + (this.userShelf.courseIds.indexOf(id) === -1 ? 1 : 0) : count;
+    }, 0);
+  }
+
+  hasSteps(id: string) {
+    return this.courses.data.find((course: any) => course._id === id && course.steps.length > 0);
   }
 
   onFilterChange(filterValue: string, field: string) {
@@ -268,7 +274,7 @@ export class CoursesComponent implements OnInit, AfterViewInit, OnDestroy {
     this.couchService.put('shelf/' + this.user._id, newShelf).subscribe((res) => {
       newShelf._rev = res.rev;
       this.userService.shelf = newShelf;
-      this.setupList(this.courses.data,  this.userShelf.courseIds);
+      this.setupList(this.courses.data, this.userShelf.courseIds);
       this.planetMessageService.showMessage(message + ' myCourses');
     }, (error) => (error));
   }
