99e29c35e426e9c85c2bd0c82d8cc343a3eebaaa
yubaraj poudel <yubarajpoudel708@gmail.com>
2018-12-06 14:35:55 +0545

    2655-record-survey-error-fixed

--- src/app/exams/exams-view.component.ts
+++ src/app/exams/exams-view.component.ts
@@ -204,7 +204,9 @@ export class ExamsViewComponent implements OnInit, OnDestroy {
   setAnswerForRetake(answer: any) {
     switch (this.question.type) {
       case 'selectMultiple':
-        this.setSelectMultipleAnswer(answer.value);
+        if (answer.value) {
+          this.setSelectMultipleAnswer(answer.value);
+        }
         break;
       case 'select':
         this.answer = this.question.choices.find((choice) => choice.text === answer.value.text);
