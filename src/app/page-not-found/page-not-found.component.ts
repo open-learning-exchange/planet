84d3d8f5028c4cb26b056936019fc551959ebd27
Svitlana Lesiv <lesivsvitlana@gmail.com>
2018-12-10 14:53:28 -0500

    change markup and add style (fixes #2645)

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'planet-page-not-found',
-  templateUrl: './page-not-found.component.html'
+  templateUrl: './page-not-found.component.html',
+  styles: [ `
+    .error {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 10vh;
+    }
+  ` ]
 })
 export class PageNotFoundComponent {
 
