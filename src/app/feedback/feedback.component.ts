f0e6015007a53badeaba451f49020282557c386d
Jingting <jchen136@hawk.iit.edu>
2018-12-17 13:00:56 -0600

    remove extra line

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -42,7 +42,6 @@ export class FeedbackComponent implements OnInit, AfterViewInit, OnDestroy {
     this.feedback.filter = value ? value : this.dropdownsFill();
     this._titleSearch = value;
   }
-
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   user: any = {};
