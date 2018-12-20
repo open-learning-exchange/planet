724284bb71820874e87fae00f2a3844745d6ddf3
Paul <paul.r.albert@gmail.com>
2018-12-19 23:56:23 -0500

    Remove unused property from CoursesService

--- src/app/courses/courses.service.ts
+++ src/app/courses/courses.service.ts
@@ -24,7 +24,6 @@ export class CoursesService {
   stepIndex: any;
   returnUrl: string;
   currentParams: any;
-  myCourseIndexes: any = [];
 
   constructor(
     private couchService: CouchService,
