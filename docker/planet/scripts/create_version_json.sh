f7ed49d2351a8dd00047024d8623bb60a87f5f08
Laxman Maharjan <email@mlaxman.com.np>
2018-12-25 18:36:48 +0545

    Add build code for apk

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -1,6 +1,8 @@
 PLANET_VERSION=$(cat package.json | jq -r .version)
-LATEST_APK_VERSION="v0.2.27"
+LATEST_APK_VERSION="v0.2.29"
 MIN_APK_VERSION="v0.2.27"
-APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.2.27/myPlanet.apk"
+APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.2.29/myPlanet.apk"
+LATEST_APK_VER_CODE="229"
+MIN_APK_VER_CODE="227"
 
-echo '{"appname":"planet","minapk":"'$MIN_APK_VERSION'","latestapk":"'$LATEST_APK_VERSION'","planetVersion":"'$PLANET_VERSION'","apkpath":"'$APK_PATH'"}' > dist/versions
+echo '{"appname":"planet","minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"planetVersion":"'$PLANET_VERSION'","apkpath":"'$APK_PATH'"}' > dist/versions
