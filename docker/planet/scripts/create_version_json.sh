bd577f8095a8b37e60f02a4b00c055bc90bd6b78
Laxman Maharjan <email@mlaxman.com.np>
2018-12-25 21:19:59 +0545

    Update create_version_json.sh

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -5,4 +5,4 @@ MIN_APK_VERSION="v0.2.27"
 MIN_APK_VER_CODE="227"
 APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.2.29/myPlanet.apk"
 
-echo '{"appname":"planet","minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"planetVersion":"'$PLANET_VERSION'","apkpath":"'$APK_PATH'"}' > dist/versions
+echo '{"appname":"planet","planetVersion":"'$PLANET_VERSION'","latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"apkpath":"'$APK_PATH'"}' > dist/versions
