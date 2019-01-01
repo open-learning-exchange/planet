e27848574c9d27231d0abe8fabb6d4e5661f06f6
Paul <paul.r.albert@gmail.com>
2019-01-01 17:01:30 -0500

    Refactor table-helpers for deep properties

--- src/app/shared/table-helpers.ts
+++ src/app/shared/table-helpers.ts
@@ -9,8 +9,9 @@ const dropdownArray = (fieldValue: any, values: string[]) => {
 };
 
 const checkFilterItems = (data: any) => ((includeItem: boolean, [ field, val ]) => {
+  const dataField = getProperty(data, field);
   // If field is an array field, check if one value matches.  If not check if values match exactly.
-  const noMatch = val instanceof Array ? dropdownArray(data[field], val) : dropdownString(data[field], val);
+  const noMatch = val instanceof Array ? dropdownArray(dataField, val) : dropdownString(dataField, val);
   if (val && noMatch) {
     return false;
   }
@@ -21,17 +22,17 @@ const checkFilterItems = (data: any) => ((includeItem: boolean, [ field, val ]) 
 export const filterSpecificFields = (filterFields: string[]): any => {
   return (data: any, filter: string) => {
     for (let i = 0; i < filterFields.length; i++) {
-      const keys = filterFields[i].split('.');
-      if (getProperty(data, keys).toLowerCase().indexOf(filter.trim().toLowerCase()) > -1) {
+      if (getProperty(data, filterFields[i]).toLowerCase().indexOf(filter.trim().toLowerCase()) > -1) {
         return true;
       }
     }
   };
 };
 
-// Takes an object and array of property keys.  Returns the nested value of the succession of
+// Takes an object and string of dot seperated property keys.  Returns the nested value of the succession of
 // keys or undefined.
-function getProperty(data: any, propertyArray: string[]) {
+function getProperty(data: any, fields: string) {
+  const propertyArray = fields.split('.');
   return propertyArray.reduce((obj, prop) => (obj && obj[prop]) ? obj[prop] : undefined, data);
 }
 
@@ -47,8 +48,7 @@ export const filterDropdowns = (filterObj: any) => {
 export const filterFieldExists = (filterFields: string[], trueIfExists: boolean): any => {
   return (data: any, filter: string) => {
     for (let i = 0; i < filterFields.length; i++) {
-      const keys = filterFields[i].split('.');
-      return trueIfExists === (getProperty(data, keys) !== undefined);
+      return trueIfExists === (getProperty(data, filterFields[i]) !== undefined);
     }
     return true;
   };
@@ -60,8 +60,7 @@ const matchAllItems = (filterItems: string[], propItems: string[]) => {
 
 export const filterArrayField = (filterField: string, filterItems: string[]) => {
   return (data: any, filter: string) => {
-    const keys = filterField.split('.');
-    return matchAllItems(filterItems, getProperty(data, keys) || []);
+    return matchAllItems(filterItems, getProperty(data, filterField) || []);
   };
 };
 
