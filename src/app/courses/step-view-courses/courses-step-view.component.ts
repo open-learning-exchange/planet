e4b1f5c2dafd5b1ee8e874f5516c639af7b2ac4b
Paul <paul.r.albert@gmail.com>
2019-01-15 16:58:40 -0500

    Use types in function params instead of type coercion

--- src/app/courses/step-view-courses/courses-step-view.component.ts
+++ src/app/courses/step-view-courses/courses-step-view.component.ts
@@ -83,8 +83,8 @@ export class CoursesStepViewComponent implements OnInit, OnDestroy {
         user: this.userService.get(),
         type: 'exam' });
     }
-    this.stepDetail.resources.sort(function (a, b) {
-      return ('' + a.title).localeCompare(b.title);
+    this.stepDetail.resources.sort(function (a: { title: string }, b: { title: string }) {
+      return a.title.localeCompare(b.title);
     });
     this.resource = this.stepDetail.resources ? this.stepDetail.resources[0] : undefined;
   }
