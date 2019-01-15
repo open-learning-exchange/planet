ba096f9ce6e5d584b5a690adea904c50f4baac13
Leonard Maximus Irenous Mensah <leomaxi@outlook.com>
2019-01-15 04:02:51 +0000

    myPlanet_v0.2.36 (#2975)

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -1,8 +1,8 @@
 PLANET_VERSION=$(cat package.json | jq -r .version)
-LATEST_APK_VERSION="v0.2.34"
-LATEST_APK_VER_CODE="234"
+LATEST_APK_VERSION="v0.2.36"
+LATEST_APK_VER_CODE="236"
 MIN_APK_VERSION="v0.2.34"
 MIN_APK_VER_CODE="234"
-APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.2.34/myPlanet.apk"
+APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.2.36/myPlanet.apk"
 
 echo '{"appname":"planet","planetVersion":"'$PLANET_VERSION'","latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"apkpath":"'$APK_PATH'"}' > dist/versions
