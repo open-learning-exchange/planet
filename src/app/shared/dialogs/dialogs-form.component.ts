465f1fc56b60225a69e0fc1a04bca7ad11bd75d6
Paul <paul.r.albert@gmail.com>
2019-01-25 12:19:35 -0500

    Add alternative function for opening dialogs form

--- src/app/shared/dialogs/dialogs-form.component.ts
+++ src/app/shared/dialogs/dialogs-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
-import { MatDialogRef } from '@angular/material';
-import { FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { DialogsLoadingService } from './dialogs-loading.service';
 
 @Component({
   templateUrl: './dialogs-form.component.html'
@@ -11,6 +12,7 @@ export class DialogsFormComponent {
   public fields: any;
   public modalForm: any;
   passwordVisibility = new Map();
+  isSpinnerOk = true;
 
   private markFormAsTouched (formGroup: FormGroup) {
     (<any>Object).values(formGroup.controls).forEach(control => {
@@ -21,13 +23,30 @@ export class DialogsFormComponent {
     });
   }
 
-  constructor(public dialogRef: MatDialogRef<DialogsFormComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<DialogsFormComponent>,
+    private fb: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private dialogsLoadingService: DialogsLoadingService
+  ) {
+    if (this.data.formGroup) {
+      this.modalForm = this.data.formGroup instanceof FormGroup ? this.data.formGroup : this.fb.group(this.data.formGroup);
+      this.title = this.data.title;
+      this.fields = this.data.fields;
+      this.isSpinnerOk = false;
+    }
+  }
 
   onSubmit(mForm, dialog) {
-    if (mForm.valid) {
-      dialog.close(mForm.value);
-    } else {
+    if (!mForm.valid) {
       this.markFormAsTouched(mForm);
+      return;
+    }
+    this.dialogsLoadingService.start();
+    if (this.data.onSubmit) {
+      this.data.onSubmit(mForm.value);
+    } else {
+      dialog.close(mForm.value);
     }
   }
 
