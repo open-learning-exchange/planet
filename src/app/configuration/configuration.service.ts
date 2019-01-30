09d94aec5db4f191c202241ea7348c15cddff2e4
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-01-30 15:40:07 -0800

    change font-weight for notifications (fixes #3008)

--- src/app/configuration/configuration.service.ts
+++ src/app/configuration/configuration.service.ts
@@ -25,7 +25,7 @@ export class ConfigurationService {
     return mergeMap(data => {
       const requestNotification = {
         'user': 'SYSTEM',
-        'message': 'New ' + configuration.planetType + ' "' + configuration.name + '" has requested to connect.',
+        'message': `New ${configuration.planetType} <b>"${configuration.name}"</b> has requested to connect.`,
         'link': '/requests/',
         'linkParams': { 'search': configuration.code },
         'type': 'request',
