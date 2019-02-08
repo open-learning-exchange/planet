370c21f90f0fe3fc2a38dc40600f5f9061cef0ab
Paul <paul.r.albert@gmail.com>
2019-02-08 16:37:36 -0500

    Add toggles to configuration and user profile

--- src/app/shared/beta.directive.ts
+++ src/app/shared/beta.directive.ts
@@ -1,5 +1,6 @@
 import { Directive, TemplateRef, ViewContainerRef, OnInit } from '@angular/core';
 import { StateService } from './state.service';
+import { UserService } from './user.service';
 
 @Directive({
   selector: '[planetBeta]'
@@ -8,12 +9,13 @@ export class PlanetBetaDirective implements OnInit {
 
   constructor(
     private stateService: StateService,
+    private userService: UserService,
     private templateRef: TemplateRef<any>,
     private viewContainer: ViewContainerRef
   ) {}
 
   ngOnInit() {
-    if (this.stateService.configuration.betaEnabled) {
+    if (this.userService.get().betaEnabled) {
       this.viewContainer.createEmbeddedView(this.templateRef);
     } else {
       this.viewContainer.clear();
