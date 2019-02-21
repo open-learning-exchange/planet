190c3c44a60ba14e46a8970857f1beeef95f7152
Paul <paul.r.albert@gmail.com>
2019-02-21 00:47:37 -0500

    Fix spacing

--- src/app/surveys/surveys.component.ts
+++ src/app/surveys/surveys.component.ts
@@ -51,17 +51,18 @@ export class SurveysComponent implements OnInit, AfterViewInit, OnDestroy {
       .subscribe((submissions: any) => {
         this.surveys.data = this.surveys.data.map(
           (survey: any) => ({
-              ...survey,
-              taken: submissions.filter(data => {
-                  return data.parentId === survey._id && (data.status !== 'pending' || data.user);
-              }).length
-            })
+            ...survey,
+            taken: submissions.filter(data => {
+                return data.parentId === survey._id && (data.status !== 'pending' || data.user);
+            }).length
+          })
         );
         this.emptyData = !this.surveys.data.length;
         this.dialogsLoadingService.stop();
       });
     this.couchService.checkAuthorization('exams').subscribe((isAuthorized) => this.isAuthorized = isAuthorized);
   }
+
   ngAfterViewInit() {
     this.surveys.sort = this.sort;
     this.surveys.paginator = this.paginator;
