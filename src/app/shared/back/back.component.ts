e28347f1066f7bbbaf0cc7fd0f6ca21509c4f97c
Arash <arashdai@gmail.com>
2019-02-27 13:31:42 -0800

    use location strategy

--- src/app/shared/back/back.component.ts
+++ src/app/shared/back/back.component.ts
@@ -1,26 +1,19 @@
 import { Component } from '@angular/core';
-import { Router, RoutesRecognized } from '@angular/router';
-import { filter, pairwise } from 'rxjs/operators';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'planet-back',
   templateUrl: './back.component.html',
-  styleUrls: [ './back.component.scss' ]
+  styleUrls: [ './back.component.scss' ],
 })
+
 export class BackComponent {
 
   previousRoute: any;
 
-  constructor(private router: Router) {
-    this.router.events.pipe(
-      filter(e => e instanceof RoutesRecognized),
-      pairwise()
-    ).subscribe((event: any[]) => {
-      this.previousRoute = event[0].urlAfterRedirects;
-    });
-  }
+  constructor(private location: Location) { }
 
   goBack() {
-    this.router.navigate([ this.previousRoute ]);
+    this.location.back();
   }
 }
