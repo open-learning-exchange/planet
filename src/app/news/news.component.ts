0d5a7f309f8852b0047fcec9252c9723ffb75ba0
paulbert <paul.r.albert@gmail.com>
2019-03-14 00:34:58 -0400

    Show posts

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,18 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CouchService } from '../shared/couchdb.service';
 import { StateService } from '../shared/state.service';
 import { UserService } from '../shared/user.service';
 import { PlanetMessageService } from '../shared/planet-message.service';
+import { findDocuments } from '../shared/mangoQueries';
+import { environment } from '../../environments/environment';
 
 @Component({
   templateUrl: './news.component.html',
   styleUrls: [ './news.scss' ]
 })
-export class NewsComponent {
+export class NewsComponent implements OnInit {
 
   private dbName = 'news';
   newMessage = '';
   configuration = this.stateService.configuration;
+  newsItems: any[] = [];
+  imgUrlPrefix = environment.couchAddress + '/' + '_users' + '/';
 
   constructor(
     private couchService: CouchService,
@@ -20,6 +24,19 @@ export class NewsComponent {
     private userService: UserService,
     private planetMessageService: PlanetMessageService
   ) {}
+
+  ngOnInit() {
+    this.getMessages();
+  }
+
+  getMessages() {
+    this.couchService.findAll(this.dbName, findDocuments({ createdOn: this.configuration.code })).subscribe(newsItems => {
+      this.newsItems = newsItems.map(item => {
+        const filename = item.user._attachments && Object.keys(item.user._attachments)[0];
+        return { ...item, avatar: filename ? this.imgUrlPrefix + item.user._id + '/' + filename : 'assets/image.png' }
+      });
+    });
+  }
 
   postMessage() {
     this.couchService.updateDocument(this.dbName, {
@@ -32,6 +49,7 @@ export class NewsComponent {
     }).subscribe(() => {
       this.planetMessageService.showMessage('Thank you for submitting your news')
       this.newMessage = '';
+      this.getMessages();
     });
   }
 
