ad181c27fcf9d7acd57ef1f7d83384570773c68b
Paul <paul.r.albert@gmail.com>
2019-03-13 20:42:20 -0400

    Outline of NewsComponent

---
+++ src/app/news/news.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './news.component.html',
+  styleUrls: [ './news.scss' ]
+})
+export class NewsComponent {
+
+  newMessage = '';
+
+  constructor() {}
+
+  postMessage() {}
+
+}
