27d4e9a0a61b1e6471cdb6d48799f6f88eb30a4d
paulbert <paul.r.albert@gmail.com>
2019-03-13 23:02:44 -0400

    Post news messages

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CouchService } from '../shared/couchdb.service';
+import { StateService } from '../shared/state.service';
+import { UserService } from '../shared/user.service';
+import { PlanetMessageService } from '../shared/planet-message.service';
 
 @Component({
   templateUrl: './news.component.html',
@@ -6,10 +10,29 @@ import { Component } from '@angular/core';
 })
 export class NewsComponent {
 
+  private dbName = 'news';
   newMessage = '';
+  configuration = this.stateService.configuration;
 
-  constructor() {}
+  constructor(
+    private couchService: CouchService,
+    private stateService: StateService,
+    private userService: UserService,
+    private planetMessageService: PlanetMessageService
+  ) {}
 
-  postMessage() {}
+  postMessage() {
+    this.couchService.updateDocument(this.dbName, {
+      message: this.newMessage,
+      time: this.couchService.datePlaceholder,
+      createdOn: this.configuration.code,
+      parentCode: this.configuration.parentCode,
+      user: this.userService.get(),
+      viewableBy: 'community'
+    }).subscribe(() => {
+      this.planetMessageService.showMessage('Thank you for submitting your news')
+      this.newMessage = '';
+    });
+  }
 
 }
