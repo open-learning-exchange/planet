a87fb140c03278972df03bf64fe19133273556f1
yubaraj poudel <yubarajpoudel708@gmail.com>
2019-03-17 12:15:48 +0545

    markdown post fixes #3412

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -5,25 +5,37 @@ import { UserService } from '../shared/user.service';
 import { PlanetMessageService } from '../shared/planet-message.service';
 import { findDocuments } from '../shared/mangoQueries';
 import { environment } from '../../environments/environment';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+import { CustomValidators } from '../validators/custom-validators';
+import { ConnectedOverlayPositionChange } from '@angular/cdk/overlay';
 
 @Component({
   templateUrl: './news.component.html',
   styleUrls: [ './news.scss' ]
 })
 export class NewsComponent implements OnInit {
-
   private dbName = 'news';
-  newMessage = '';
   configuration = this.stateService.configuration;
   newsItems: any[] = [];
   imgUrlPrefix = environment.couchAddress + '/' + '_users' + '/';
+  newsPost: FormGroup;
 
   constructor(
+    private fb: FormBuilder,
     private couchService: CouchService,
     private stateService: StateService,
     private userService: UserService,
     private planetMessageService: PlanetMessageService
-  ) {}
+
+  ) {
+    this.createNewsForm();
+  }
+
+  createNewsForm() {
+    this.newsPost = this.fb.group({
+        newMessage: [ '', CustomValidators.required ],
+    });
+  }
 
   ngOnInit() {
     this.getMessages();
@@ -41,7 +53,7 @@ export class NewsComponent implements OnInit {
 
   postMessage() {
     this.couchService.updateDocument(this.dbName, {
-      message: this.newMessage,
+      message: this.newsPost.controls.newMessage.value,
       time: this.couchService.datePlaceholder,
       createdOn: this.configuration.code,
       parentCode: this.configuration.parentCode,
@@ -49,9 +61,15 @@ export class NewsComponent implements OnInit {
       viewableBy: 'community'
     }).subscribe(() => {
       this.planetMessageService.showMessage('Thank you for submitting your news');
-      this.newMessage = '';
+      this.resetNewsForm(this.newsPost.get('newMessage'));
       this.getMessages();
     });
+  }
+
+  resetNewsForm(control: AbstractControl) {
+    control.reset('');
+    control.markAsPristine();
+    control.markAsUntouched();
   }
 
 }
