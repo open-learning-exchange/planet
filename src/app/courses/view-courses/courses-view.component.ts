47453e16f3dc9a4cc530dfffe2bea6dd75d56f66
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-03-18 16:34:06 -0700

    lint (fixes #2719)

--- src/app/courses/view-courses/courses-view.component.ts
+++ src/app/courses/view-courses/courses-view.component.ts
@@ -54,8 +54,8 @@ export class CoursesViewComponent implements OnInit, OnDestroy {
         this.courseId = params.get('id');
         this.coursesService.requestCourse(
         { courseId: this.courseId, forceLatest: true, parent: this.parent }
-      ), (error) => { console.log(error) }
-    });
+      )}
+    );
   }
 
   ngOnDestroy() {
