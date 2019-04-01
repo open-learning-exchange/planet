3458de9512eef30402e9f12a69e0a27fe28e03c2
Svitlana Lesiv <lesivsvitlana@gmail.com>
2019-04-01 14:25:01 -0700

    Achievements: filter missing resources (fixes #3216) (#3506)

--- src/app/users/users-achievements/users-achievements.service.ts
+++ src/app/users/users-achievements/users-achievements.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CouchService } from '../../shared/couchdb.service';
+import { forkJoin } from 'rxjs';
+import { map, first } from 'rxjs/operators';
+import { ResourcesService } from '../../resources/resources.service';
 
 @Injectable({
   providedIn: 'root'
@@ -7,13 +10,30 @@ import { CouchService } from '../../shared/couchdb.service';
 export class UsersAchievementsService {
 
   readonly dbName = 'achievements';
+  parent = false;
 
   constructor(
-    private couchService: CouchService
+    private couchService: CouchService,
+    private resourcesService: ResourcesService
   ) {}
 
+
   getAchievements(id) {
-    return this.couchService.get(this.dbName + '/' + id);
+    this.resourcesService.requestResourcesUpdate(this.parent);
+    return forkJoin([
+      this.couchService.get(this.dbName + '/' + id),
+      this.resourcesService.resourcesListener(this.parent).pipe(first())
+    ]).pipe(
+      map(([ userAchievements, resources ]: any[]) => {
+        const resourceIds = resources.map((res: any) => res._id);
+        userAchievements.achievements.forEach((achievement: any) => {
+          if (achievement.resources.length !== 0) {
+            achievement.resources = achievement.resources.filter(res => resourceIds.indexOf(res._id) !== -1);
+          }
+        });
+        return userAchievements;
+      })
+    );
   }
 
   isEmpty(achievement) {
