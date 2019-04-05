f4977104a040c742c4673b063ef873de09677a47
Laxman Maharjan <email@mlaxman.com.np>
2019-04-05 21:29:56 +0545

    remove comments

--- docker/planet/nginx/upgrade_planetapk.sh
+++ docker/planet/nginx/upgrade_planetapk.sh
@@ -15,14 +15,9 @@ tag=$(echo "$releaseUrl" | sed -r 's/.*\/(.*)/\1/')
 
 downloadUrl="$repoUrl/releases/download/$tag/$repoApkName"
 
-#curl -s "$downloadUrl" -o "$localApkName.tmp" -L
-
-if [ -f "$localApkName" ] && echo "$(curl -s "$repoUrl/releases/download/$tag/$sha256File" | head -c64)  $localApkName" | sha256sum -s -c -; then
-#if [ -f "$localApkName" ] && echo "$(sha256sum $localApkName | head -c64)  $localApkName.tmp" | sha256sum -s -c -; then
+if [ -f "$localApkName" ] && echo "$(curl -s "$repoUrl/releases/download/$tag/$sha256File" | head -c64) $localApkName" | sha256sum -s -c -; then
   echo "The file hasn't changed."
-  #rm -rf "$localApkName.tmp"
 else
   echo "New file found."
   curl -s "$downloadUrl" -o "$localApkName" -L
-  #mv "$localApkName.tmp" "$localApkName"
 fi
