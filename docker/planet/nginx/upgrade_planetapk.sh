67185ce5392e0d7da57107b8765e5a389165567f
Laxman Maharjan <email@mlaxman.com.np>
2019-04-10 20:18:09 +0545

    Check sha256sum to download

--- docker/planet/nginx/upgrade_planetapk.sh
+++ docker/planet/nginx/upgrade_planetapk.sh
@@ -5,19 +5,24 @@ echo "Content-type: text/plain"
 echo ""
 
 repoUrl="https://github.com/open-learning-exchange/myplanet"
-repoApkName="myPlanet.apk"
-sha256File="$repoApkName.sha256"
-localApkName="/usr/share/nginx/html/fs/myPlanet.apk"
+repoApkName="myPlanet-release.apk"
+localApkName="myPlanet.apk"
+sha256File="myPlanet.apk.sha256"
+localShaFile="$repoApkName.sha256"
 
 latestReleaseUrl="$repoUrl/releases/latest"
 releaseUrl=$(curl -L "$latestReleaseUrl" -w "%{url_effective}" -o /dev/null -s)
 tag=$(echo "$releaseUrl" | sed -r 's/.*\/(.*)/\1/')
-
+echo "Last release version: $tag"
 downloadUrl="$repoUrl/releases/download/$tag/$repoApkName"
 
-if [ -f "$localApkName" ] && echo "$(curl -s "$repoUrl/releases/download/$tag/$sha256File" | head -c64) $localApkName" | sha256sum -s -c -; then
+cd fs
+curl -s "$repoUrl/releases/download/$tag/$sha256File" -o "$localShaFile" -L
+echo "Getting SHA256 file"
+if [ -f "$repoApkName" ] && [ -f "$localShaFile" ] && sha256sum "$repoApkName" | sha256sum -c "$localShaFile" 2>&1 | grep OK; then
   echo "The file hasn't changed."
 else
-  echo "New file found."
-  curl -s "$downloadUrl" -o "$localApkName" -L
+  echo "Downloading new version"
+  echo "$(curl -# "$downloadUrl" -o "$repoApkName" -L)"
+  cp "$repoApkName" "$localApkName"
 fi
--- docker/planet/nginx/upgrade_planetapk_fromnation.sh
+++ docker/planet/nginx/upgrade_planetapk_fromnation.sh
@@ -4,14 +4,20 @@ echo "HTTP/1.0 200 OK"
 echo "Content-type: text/plain"
 echo ""
 
-nationUrl="$(curl -s http://couchdb:5984/configurations/$(curl http://couchdb:5984/configurations/_all_docs -s | jq .rows[0].id -r) | jq .parentDomain -r | sed -e 's|^[^/]*//||' -e 's|/.*$||')/fs"
-apkName="myPlanet.apk"
-sha256File="$apkName.sha256"
-localApkName="/usr/share/nginx/html/fs/myPlanet.apk"
+repoApkName="myPlanet-release.apk"
+localApkName="myPlanet.apk"
+sha256File="myPlanet.apk.sha256"
+localShaFile="$repoApkName.sha256"
 
-if [ -f "$localApkName" ] && echo "$(curl -s "$nationUrl/$sha256File" | head -c64)  $localApkName" | sha256sum -s -c -; then
+nationUrl="$(curl -s http://couchdb:5984/configurations/$(curl http://couchdb:5984/configurations/_all_docs -s | jq .rows[0].id -r) | jq .parentDomain -r | sed -e 's|^[^/]*//||' -e 's|/.*$||')"
+downloadUrl="$nationUrl/fs/$repoApkName"
+
+cd fs
+curl -s "$nationUrl/fs/$sha256File" -o "$localShaFile" -L
+if [ -f "$repoApkName" ] && [ -f "$localShaFile" ] && sha256sum "$repoApkName" | sha256sum -c "$localShaFile" 2>&1 | grep OK; then
   echo "The file hasn't changed."
 else
-  echo "New file found."
-  curl -s "$nationUrl/$apkName" -o "$localApkName" -L
+  echo "Downloading new version"
+  echo "$(curl -# "$downloadUrl" -o "$repoApkName" -L)"
+  cp "$repoApkName" "$localApkName"
 fi
