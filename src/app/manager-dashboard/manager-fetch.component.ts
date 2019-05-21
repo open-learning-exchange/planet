5a4584a2ef973345e3d6893c9d08a7237a552e45
Paul <paul.r.albert@gmail.com>
2019-05-20 21:31:20 -0400

    Manager: Fix missing fetch items components (fixes #3852)

--- src/app/manager-dashboard/manager-fetch.component.ts
+++ src/app/manager-dashboard/manager-fetch.component.ts
@@ -40,7 +40,7 @@ export class ManagerFetchComponent implements OnInit, AfterViewInit {
 
   ngOnInit() {
     this.managerService.getPushedList().subscribe((pushedList: any) => {
-      this.pushedItems.data = pushedList.docs;
+      this.pushedItems.data = pushedList;
       this.emptyData = !this.pushedItems.data.length;
     });
   }
