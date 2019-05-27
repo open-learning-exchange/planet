96bd69c3edd03cf426d09d70619d5da41b2e5070
Laxman Maharjan <email@mlaxman.com.np>
2019-05-27 19:53:52 +0545

    Credentials update: Sanitize password (fixes #3833) (#3889)

--- docker/planet/nginx/credentials.sh
+++ docker/planet/nginx/credentials.sh
@@ -6,16 +6,19 @@ echo ""
 YML_PATH=credentials/credentials.yml
 PLANET_USER=${PLANET_CREDENTIALS%%,*}
 PLANET_PASS=${PLANET_CREDENTIALS##*,}
+PLANET_PASS=$(echo $PLANET_PASS | sed -e 's/\$/\$\$/g')
 if [ -f "$YML_PATH" ]; then
   OLD_USER=$(grep COUCHDB_USER $YML_PATH | sed -e 's/.*=//')
 fi
 
 if [ -z "$OLD_USER" ] || [ "$PLANET_USER" == "$OLD_USER" ]; then
   rm credentials/credentials.yml
-  echo "services:
-    db-init:
-      environment:
-        - COUCHDB_USER=$PLANET_USER
-        - COUCHDB_PASS=$PLANET_PASS
-  version: \"2\"" >> $YML_PATH
+  {
+    echo "services:"
+    echo "  db-init:"
+    echo "  environment:"
+    echo "    - COUCHDB_USER=$PLANET_USER"
+    echo "    - COUCHDB_PASS=$PLANET_PASS"
+    echo "version: \"2\""
+  } >> $YML_PATH
 fi
