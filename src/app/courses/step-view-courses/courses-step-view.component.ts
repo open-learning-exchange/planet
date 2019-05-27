5900b591765ee8ccad2d5b4af05f21f98885d081
Arpita Singh <singh.arpi92@gmail.com>
2019-05-27 21:34:18 +0545

    [#3882] preview test and survey routing

--- src/app/courses/step-view-courses/courses-step-view.component.ts
+++ src/app/courses/step-view-courses/courses-step-view.component.ts
@@ -127,7 +127,8 @@ export class CoursesStepViewComponent implements OnInit, OnDestroy {
 
   goToExam(type = 'exam', preview = false) {
     this.router.navigate(
-      [ 'exam', { questionNum: type === 'survey' ? 1 : this.examStart, type, preview: true, examId: this.stepDetail.exam._id } ],
+      [ 'exam', { questionNum: type === 'survey' ? 1 : this.examStart, type, preview: true,
+      examId: type === 'survey' ? this.stepDetail.survey._id : this.stepDetail.exam._id } ],
       { relativeTo: this.route }
     );
   }
