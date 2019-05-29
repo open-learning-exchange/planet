9350644ad06307404477407cce760e5ff9af1f60
Laxman Maharjan <email@mlaxman.com.np>
2019-05-29 13:44:24 +0545

    [#3920] Delete sent items

--- src/app/manager-dashboard/reports/pending-table.component.ts
+++ src/app/manager-dashboard/reports/pending-table.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnChanges, AfterViewInit, ViewChild, Input } from '@angular/core';
+import { Component, OnChanges, AfterViewInit, ViewChild, Input, Output, EventEmitter } from '@angular/core';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
+import { CouchService } from '../../shared/couchdb.service';
+import { PlanetMessageService } from '../../shared/planet-message.service';
 
 @Component({
   selector: 'planet-pending-table',
@@ -8,10 +10,16 @@ import { MatTableDataSource, MatPaginator } from '@angular/material';
 export class PendingTableComponent implements OnChanges, AfterViewInit {
 
   @Input() data = [];
+  @Output() requestUpdate = new EventEmitter<void>();
   items = new MatTableDataSource();
   displayedColumns = [ 'item' ];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(
+    private couchService: CouchService,
+    private planetMessageService: PlanetMessageService
+  ) {}
 
   ngOnChanges() {
     this.items.data = this.data;
@@ -21,4 +29,11 @@ export class PendingTableComponent implements OnChanges, AfterViewInit {
     this.items.paginator = this.paginator;
   }
 
+  deleteItem(item) {
+    const { _id: id, _rev: rev } = item;
+    this.couchService.delete(`send_items/${id}?rev=${rev}`).subscribe(
+      () => this.requestUpdate.emit(),
+      () => this.planetMessageService.showAlert('There was a problem deleting item')
+    );
+  }
 }
