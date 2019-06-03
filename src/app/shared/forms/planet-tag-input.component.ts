c25ef566a8593fa5f7153edfb8d4e6fcdbb00b04
Arpita Singh <singh.arpi92@gmail.com>
2019-06-04 00:15:28 +0545

    Collections: Fix editing tag double add and unwanted selection (fixes #3878) (#3945)

--- src/app/shared/forms/planet-tag-input.component.ts
+++ src/app/shared/forms/planet-tag-input.component.ts
@@ -8,6 +8,7 @@ import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Subject } from 'rxjs';
 import { TagsService } from './tags.service';
 import { PlanetTagInputDialogComponent } from './planet-tag-input-dialog.component';
+import { dedupeShelfReduce } from '../utils';
 
 @Component({
   'selector': 'planet-tag-input',
@@ -104,7 +105,7 @@ export class PlanetTagInputComponent implements ControlValueAccessor, OnInit, On
   initTags(editedId?: string) {
     this.tagsService.getTags(this.db, this.parent).subscribe((tags: any[]) => {
       this.tags = tags;
-      const newValue = this.value.concat(editedId).filter(tagId => tags.some(tag => tag._id === tagId));
+      const newValue = this.value.concat(editedId).filter(tagId => tags.some(tag => tag._id === tagId)).reduce(dedupeShelfReduce, []);
       this.value = newValue;
       this.resetDialogData();
     });
