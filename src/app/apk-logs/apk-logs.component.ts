b49a1963940b63d5a66e4b1a1aa262fec5385812
Anish Regmee <anishregmee@gmail.com>
2019-06-21 11:56:23 +0545

    Create a view to see all crash logs of myplanet (fixes 4071)

--- src/app/apk-logs/apk-logs.component.ts
+++ src/app/apk-logs/apk-logs.component.ts
@@ -2,17 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { CouchService } from '../shared/couchdb.service';
 
 @Component({
-  selector: 'app-apk-logs',
+  selector: 'planet-apk-logs',
   templateUrl: './apk-logs.component.html'
 })
 export class ApkLogsComponent {
 
-  apk_logs = [];
+  apklogs = [];
 
   constructor(
     private couchService: CouchService,
   ) {
-    this.couchService.findAll('apk_logs').subscribe(apk_logs => this.apk_logs = apk_logs);
+    this.couchService.findAll('apk_logs').subscribe(apk_logs => this.apklogs = apk_logs);
   }
 
 }
