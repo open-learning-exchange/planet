8aa7af7f17838eefbfb0c5da2a4a594b57ca88ab
Liew211 <samliew211@outlook.com>
2019-07-02 21:26:23 -0400

    (Issue #4184)

--- src/app/teams/teams-view.component.ts
+++ src/app/teams/teams-view.component.ts
@@ -32,7 +32,7 @@ export class TeamsViewComponent implements OnInit, OnDestroy {
   dialogRef: MatDialogRef<DialogsListComponent>;
   user = this.userService.get();
   news: any[] = [];
-  leftTileContent: 'description' | 'news' = 'news';
+  leftTileContent: 'description' | 'news' = 'description';
   isRoot = true;
 
   constructor(
@@ -92,7 +92,7 @@ export class TeamsViewComponent implements OnInit, OnDestroy {
     }
     this.userStatus = this.requests.some((req: any) => req.userId === user._id) ? 'requesting' : this.userStatus;
     this.userStatus = this.members.some((req: any) => req.userId === user._id) ? 'member' : this.userStatus;
-    this.leftTileContent = this.userStatus !== 'member' ? 'description' : 'news';
+    this.leftTileContent = this.userStatus == 'member' ? 'news' : 'description';
   }
 
   toggleMembership(team, leaveTeam) {
