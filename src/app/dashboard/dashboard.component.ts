6d56c26b4b3a39f1bc7e6d0e9d8debafeef99685
Arpita singh <singh.arpi92@gmail.com>
2019-07-15 16:21:53 +0545

    [#4283] Add learner role on dashboard as default

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -9,6 +9,7 @@ import { findDocuments } from '../shared/mangoQueries';
 import { environment } from '../../environments/environment';
 import { SubmissionsService } from '../submissions/submissions.service';
 import { StateService } from '../shared/state.service';
+import { dedupeShelfReduce } from '../shared/utils';
 
 @Component({
   templateUrl: './dashboard.component.html',
@@ -19,7 +20,7 @@ export class DashboardComponent implements OnInit {
   urlPrefix = environment.couchAddress + '/_users/org.couchdb.user:' + this.userService.get().name + '/';
   displayName: string = this.userService.get().firstName !== undefined ?
     this.userService.get().firstName + ' ' + this.userService.get().lastName : this.userService.get().name;
-  displayRole: string = this.userService.get().roles.join(', ').replace('_', '');
+  displayRole: string;
   planetName: string;
 
   dateNow: any;
@@ -40,6 +41,8 @@ export class DashboardComponent implements OnInit {
     private submissionsService: SubmissionsService,
     private stateService: StateService
   ) {
+    const currRoles = this.userService.get().roles;
+    this.displayRole = currRoles.reduce(dedupeShelfReduce, currRoles.length ? [ 'learner' ] : [ 'Inactive' ]).join(', ').replace('_', '');
     this.userService.shelfChange$.pipe()
       .subscribe(() => {
         this.ngOnInit();
