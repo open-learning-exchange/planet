ebaf14381bdf718b1611f98584a9f397454e2b58
Arpita singh <singh.arpi92@gmail.com>
2019-07-15 11:35:03 +0545

    [#4290] Change team leader

--- src/app/teams/teams-view.component.ts
+++ src/app/teams/teams-view.component.ts
@@ -285,4 +285,12 @@ export class TeamsViewComponent implements OnInit, OnDestroy {
       .subscribe(() => this.planetMessageService.showMessage(`${resource.resource.title} removed`));
   }
 
+  makeLeader(member) {
+    const currentLeader = this.members.find(mem => mem.userId === this.leader);
+    forkJoin([
+      this.teamsService.updateMembershipDoc(this.team, false, { ...member, isLeader: true }),
+      this.teamsService.updateMembershipDoc(this.team, false, { ...currentLeader, isLeader: false })
+    ]).subscribe(() => this.planetMessageService.showMessage(`${member.name} has been made Leader`));
+  }
+
 }
