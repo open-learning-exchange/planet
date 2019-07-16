31639375d9077de582cd8a2f654d31959a2b50fc
Paul <paul.r.albert@gmail.com>
2019-07-16 13:37:30 -0400

    TeamsView: Handle missing resources (fixes #4326) (#4344)

--- src/app/teams/teams.service.ts
+++ src/app/teams/teams.service.ts
@@ -150,7 +150,9 @@ export class TeamsService {
       linkDocs.map(linkDoc => ({
         linkDoc,
         resource: resources.find(resource => resource._id === linkDoc.resourceId)
-      })).sort((a, b) => a.resource.title.toLowerCase() > b.resource.title.toLowerCase() ? 1 : -1)
+      }))
+        .filter(resource => resource.linkDoc.title || resource.resource && resource.resource.title)
+        .sort((a, b) => (a.resource || a.linkDoc).title.toLowerCase() > (b.resource || b.linkDoc).title.toLowerCase() ? 1 : -1)
     ));
   }
 
@@ -219,7 +221,7 @@ export class TeamsService {
     const { teamPlanetCode, teamType } = team;
     const links = resources.map(
       resource => ({
-        resourceId: resource.doc._id, sourcePlanet: resource.doc.sourcePlanet,
+        resourceId: resource.doc._id, sourcePlanet: resource.doc.sourcePlanet, title: resource.doc.title,
         teamId: team._id, teamPlanetCode, teamType, docType: 'resourceLink'
       })
     );
