87612b367caca15b75c38fb198874e9d0f7dcd3c
Varsha Patel <coder8102@gmail.com>
2019-07-17 12:55:17 -0500

    TeamsView: Show more button for long message (fixes #4170) (#4227)

--- src/app/news/news-list-item.component.ts
+++ src/app/news/news-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ChangeDetectorRef, AfterViewChecked } from '@angular/core';
 import { UserService } from '../shared/user.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { UserService } from '../shared/user.service';
   templateUrl: 'news-list-item.component.html',
   styleUrls: [ './news-list-item.scss' ]
 })
-export class NewsListItemComponent {
+export class NewsListItemComponent implements AfterViewChecked {
 
   @Input() item;
   @Input() replyObject;
@@ -14,11 +14,27 @@ export class NewsListItemComponent {
   @Output() changeReplyViewing = new EventEmitter<any>();
   @Output() updateNews = new EventEmitter<any>();
   @Output() deleteNews = new EventEmitter<any>();
+  @ViewChild('content') content;
+  contentHeight = 0;
   currentUser = this.userService.get();
+  showLess = true;
 
   constructor(
-    private userService: UserService
+    private userService: UserService,
+    private cdRef: ChangeDetectorRef
   ) {}
+
+  ngAfterViewChecked() {
+    const offsetHeight = this.content && this.content.nativeElement.children[0].children[0].offsetHeight;
+    if (offsetHeight && offsetHeight !== this.contentHeight) {
+      this.contentHeight = offsetHeight;
+      this.cdRef.detectChanges();
+    }
+  }
+
+  remToPx(rem) {
+    return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+  }
 
   addReply(news) {
     this.updateNews.emit({
