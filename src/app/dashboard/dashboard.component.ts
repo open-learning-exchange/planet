e4ce5ba085a5b8d156b1eebaab1be2c99ec8a960
Arpita singh <singh.arpi92@gmail.com>
2019-07-19 17:04:57 +0545

    [#4306] No resource team invisible

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -74,7 +74,6 @@ export class DashboardComponent implements OnInit {
     const userShelf = this.userService.shelf;
     if (this.isEmptyShelf(userShelf)) {
       this.data = { resources: [], courses: [], meetups: [], myTeams: [] };
-      return;
     }
 
     forkJoin([
