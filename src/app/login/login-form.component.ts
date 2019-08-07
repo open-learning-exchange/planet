9b55135c138186590398edabbd40efbdd6c17663
Paul <paul.r.albert@gmail.com>
2019-08-07 14:49:16 -0400

    Navigate first after login for e2e test

--- src/app/login/login-form.component.ts
+++ src/app/login/login-form.component.ts
@@ -130,8 +130,8 @@ export class LoginFormComponent {
   login({ name, password }: { name: string, password: string }, isCreate: boolean) {
     const configuration = this.stateService.configuration;
     this.pouchAuthService.login(name, password).pipe(
-      switchMap(() => forkJoin(this.pouchService.replicateFromRemoteDBs())),
       switchMap(() => isCreate ? from(this.router.navigate([ 'users/update/' + name ])) : from(this.reRoute())),
+      switchMap(() => forkJoin(this.pouchService.replicateFromRemoteDBs())),
       switchMap(this.createSession(name, password)),
       switchMap((sessionData) => {
         const adminName = configuration.adminName.split('@')[0];
