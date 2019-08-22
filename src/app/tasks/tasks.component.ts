4f27993c97bc1c0d11aeeb2f7e8088e65125108d
Anish_Regmee <anishregmee@gmail.com>
2019-08-22 10:44:09 +0545

    Tasks: Unassigning member (fixes #4598) (#4624)

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -47,9 +47,11 @@ export class TasksComponent implements OnInit {
     event.stopPropagation();
   }
 
-  addAssignee(task, assignee) {
-    const filename = assignee.userDoc._attachments && Object.keys(assignee.userDoc._attachments)[0];
-    assignee = { ...assignee, avatar: filename ? `/_users/${assignee.userDoc._id}/${filename}` : undefined };
+  addAssignee(task, assignee: any = '') {
+    if (assignee !== '') {
+      const filename = assignee.userDoc._attachments && Object.keys(assignee.userDoc._attachments)[0];
+      assignee = { ...assignee, avatar: filename ? `/_users/${assignee.userDoc._id}/${filename}` : undefined };
+    }
     this.tasksService.addTask({ ...task, assignee }).subscribe((res) => {
       this.tasksService.getTasks();
     });
