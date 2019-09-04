b482600dcfb0e250de36bd8319800970b5d46453
Jay Son <sjson421@gmail.com>
2019-09-04 15:20:53 -0400

    Notifications: "Read All" in nav bar menu (fixes #4705) (#4717)

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,6 +10,8 @@ import { debug } from '../debug-operator';
 import { findDocuments } from '../shared/mangoQueries';
 import { PouchAuthService } from '../shared/database';
 import { StateService } from '../shared/state.service';
+import { PlanetMessageService } from '../shared/planet-message.service';
+import { NotificationsService } from '../notifications/notifications.service';
 
 @Component({
   templateUrl: './home.component.html',
@@ -58,7 +60,9 @@ export class HomeComponent implements OnInit, DoCheck, AfterViewChecked, OnDestr
     private router: Router,
     private userService: UserService,
     private pouchAuthService: PouchAuthService,
-    private stateService: StateService
+    private stateService: StateService,
+    private planetMessageService: PlanetMessageService,
+    private notificationsService: NotificationsService
   ) {
     this.userService.userChange$.pipe(takeUntil(this.onDestroy$))
       .subscribe(() => {
@@ -188,6 +192,13 @@ export class HomeComponent implements OnInit, DoCheck, AfterViewChecked, OnDestr
     this.couchService.put('notifications/' + notification._id, updateNotificaton).subscribe((data) => {
       this.userService.setNotificationStateChange();
     }, (err) => console.log(err));
+  }
+
+  /**
+   * Used for marking all notifications as read from navigation bar
+   */
+  readAllNotification() {
+    this.notificationsService.setNotificationsAsRead(this.notifications);
   }
 
   sizeChange(forceModern: boolean) {
---
+++ src/app/notifications/notifications.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { UserService } from '../shared/user.service';
+import { CouchService } from '../shared/couchdb.service';
+import { PlanetMessageService } from '../shared/planet-message.service';
+
+@Injectable()
+export class NotificationsService {
+
+  constructor(
+    private userService: UserService,
+    private couchService: CouchService,
+    private planetMessageService: PlanetMessageService
+  ) {}
+
+  setNotificationsAsRead(notifications: any) {
+    const unreadArray = notifications.filter(notification => notification.status === 'unread')
+      .map(notification => ({ ...notification, status: 'read' }));
+    this.couchService.bulkDocs('notifications', unreadArray).subscribe(() => {
+      this.userService.setNotificationStateChange();
+    }, (err) => this.planetMessageService.showAlert('There was a problem marking all as read'));
+  }
+}
