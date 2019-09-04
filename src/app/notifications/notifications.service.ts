a05ea7721d738df6296d744b421584999e37e5ca
paulbert <paul.r.albert@gmail.com>
2019-09-04 13:18:24 -0400

    Clean up spacing

--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -5,16 +5,16 @@ import { PlanetMessageService } from '../shared/planet-message.service';
 
 @Injectable()
 export class NotificationsService {
-    constructor(
-        private userService: UserService,
-        private couchService: CouchService,
-        private planetMessageService: PlanetMessageService
-    ) { }
 
-    setNotificationsAsRead (unreadArray) {
-        this.couchService.bulkDocs('notifications', unreadArray)
-        .subscribe(() => {
-            this.userService.setNotificationStateChange();
-        }, (err) => this.planetMessageService.showAlert('There was a problem marking all as read'));
-    }
+  constructor(
+    private userService: UserService,
+    private couchService: CouchService,
+    private planetMessageService: PlanetMessageService
+  ) {}
+
+  setNotificationsAsRead(unreadArray) {
+    this.couchService.bulkDocs('notifications', unreadArray).subscribe(() => {
+      this.userService.setNotificationStateChange();
+    }, (err) => this.planetMessageService.showAlert('There was a problem marking all as read'));
+  }
 }
