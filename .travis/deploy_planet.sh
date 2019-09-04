e1960f9d377abd1d2e111aaf055d055d6c067b9c
paulbert <paul.r.albert@gmail.com>
2019-09-04 11:44:56 -0400

    Fix COPY directory

--- .travis/deploy_planet.sh
+++ .travis/deploy_planet.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-echo "$(ls /home/travis/build/open-learning-exchange/planet/ng-app/dist/usr/share/nginx/html)"
-echo "The current directory is: $(pwd)"
 source ./.travis/travis_utils.sh
 prepare_ci
 prepare_planet_test
