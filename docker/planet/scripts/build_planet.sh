e1d9fad68eecb9f2ca7dd885c2638a12394bb194
paulbert <paul.r.albert@gmail.com>
2019-09-04 13:35:38 -0400

    Fix base-href

--- docker/planet/scripts/build_planet.sh
+++ docker/planet/scripts/build_planet.sh
@@ -20,6 +20,6 @@ build_single(){
   if [ -z "${LANG}" ]; then
     npm run ng-high-memory -- build --prod --base-href /eng/ --output-path=dist/eng
   else
-    npm run ng-high-memory -- build --prod --base-href /ara/ --output-path=dist/$LANG --aot --i18n-file=src/i18n/messages.$LANG.xlf --i18n-locale=$LANG2 --i18n-format=xlf
+    npm run ng-high-memory -- build --prod --base-href /$LANG/ --output-path=dist/$LANG --aot --i18n-file=src/i18n/messages.$LANG.xlf --i18n-locale=$LANG2 --i18n-format=xlf
   fi
 }
