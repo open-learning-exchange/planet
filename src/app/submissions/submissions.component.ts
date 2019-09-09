980052f84e1f7fc702c41d8c2db7f52eb1b293b0
Samuel Chen <43349314+samuelchen1213@users.noreply.github.com>
2019-09-09 15:00:01 -0400

    Survey/mySurveys: Add empty message for data of 0 length (fixes #4391) (#4474)

--- src/app/submissions/submissions.component.ts
+++ src/app/submissions/submissions.component.ts
@@ -75,7 +75,6 @@ export class SubmissionsComponent implements OnInit, AfterViewInit, OnDestroy {
       this.submissions.data = submissions.map(submission => ({
         ...submission, submittedBy: this.submissionsService.submissionName(submission.user)
       }));
-      this.emptyData = !this.submissions.data.length;
       this.dialogsLoadingService.stop();
       this.applyFilter('');
     });
@@ -115,6 +114,7 @@ export class SubmissionsComponent implements OnInit, AfterViewInit, OnDestroy {
     this.filter[field] = filterValue === 'All' ? '' : filterValue;
     // Force filter to update by setting it to a space if empty
     this.submissions.filter = this.submissions.filter || ' ';
+    this.emptyData = !this.submissions.filteredData.length;
   }
 
   dropdownsFill() {
