5c9cc5e2f273ae35a51f276c91f925e6f8027850
Laxman Maharjan <email@mlaxman.com.np>
2019-09-10 20:37:24 +0545

    Promote user to super admin

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -262,26 +262,34 @@ export class UsersComponent implements OnInit, OnDestroy, AfterViewInit {
     };
   }
 
-  setRoles(user, roles, event) {
+  setRoles(user, roles, event, wasSuperAdmin = false) {
+    const makeSuperAdmin = roles.indexOf('_admin') > -1;
+    const planetConfig = this.stateService.configuration;
     const tempUser = {
       ...user,
-      roles,
+      roles: roles.filter(r => r !== '_admin'),
       oldRoles: [ ...user.roles ] || [ 'learner' ],
-      isUserAdmin: roles.indexOf('manager') > -1 || roles.indexOf('_admin') > -1
+      isUserAdmin: roles.indexOf('manager') > -1 || makeSuperAdmin
     };
     const obs = [ this.couchService.put('_users/org.couchdb.user:' + tempUser.name, tempUser) ];
-    if (tempUser.oldRoles.indexOf('_admin')) {
+    if (wasSuperAdmin) {
       obs.push(this.couchService.delete('_node/nonode@nohost/_config/admins/' + tempUser.name));
     }
-    if (tempUser.roles.indexOf('_admin')) {
-      obs.push(this.couchService.put('_node/nonode@nohost/_config/admins/' + tempUser.name, 'password'));
+    if (makeSuperAdmin) {
+      obs.push(this.couchService.put(
+        '_node/nonode@nohost/_config/admins/' + tempUser.name,
+        '-' + tempUser.password_scheme + '-' + tempUser.derived_key + ',' + tempUser.salt + ',' + tempUser.iterations
+      ));
+      const adminName = tempUser.name + '@' + planetConfig.parentCode;
+      const userDetail = { ...user, requestId: planetConfig._id, isUserAdmin: false, roles: [], name: adminName };
+      obs.push(this.couchService.updateDocument('_users', { '_id': 'org.couchdb.user:' + adminName, ...userDetail }, {
+        domain: planetConfig.parentDomain
+      }));
     }
-    forkJoin(obs).pipe(switchMap(() => {
-      if (tempUser.isUserAdmin) {
-        return this.removeFromTabletUsers(tempUser);
-      }
-      return of({ });
-    })).subscribe(() => {
+    if (tempUser.isUserAdmin) {
+      obs.push(this.removeFromTabletUsers(tempUser));
+    }
+    forkJoin(obs).subscribe(() => {
       console.log('Success!');
       this.initializeData();
     }, (error) => {
