d4084270f8686cd4dd60b3f322208ea6015eea5d
paulbert <paul.r.albert@gmail.com>
2019-09-09 21:49:46 -0400

    Create all languages in parallel

--- .travis/deploy_planet.sh
+++ .travis/deploy_planet.sh
@@ -3,7 +3,7 @@ set -e
 source ./.travis/travis_utils.sh
 prepare_ci
 prepare_planet_test
-LANGUAGES=('eng' 'ara')
+LANGUAGES=('eng' 'ara' 'spa' 'fra' 'nep' 'som')
 compose_languages "${LANGUAGES[@]}"
 
 deploy_docker './docker/planet/Dockerfile' $PLANET_TEST $PLANET_TEST_LATEST
