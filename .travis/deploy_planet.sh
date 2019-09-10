0b23b665c8b8d85dc70fa4e9f3a760ddf02213af
paulbert <paul.r.albert@gmail.com>
2019-09-10 01:29:10 -0400

    Only build all languages for tags

--- .travis/deploy_planet.sh
+++ .travis/deploy_planet.sh
@@ -3,7 +3,11 @@ set -e
 source ./.travis/travis_utils.sh
 prepare_ci
 prepare_planet_test
-LANGUAGES=('eng' 'ara' 'spa' 'fra' 'nep' 'som')
+if [ ! -z "$gtag" ] || [ ! -z "$TRAVIS_TAG" ]; then
+  LANGUAGES=('eng' 'ara' 'spa' 'fra' 'nep' 'som')
+else
+  LANGUAGES=('eng')
+fi
 compose_languages "${LANGUAGES[@]}"
 
 deploy_docker './docker/planet/Dockerfile' $PLANET_TEST $PLANET_TEST_LATEST
