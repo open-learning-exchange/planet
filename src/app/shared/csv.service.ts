294c3df04ca05de90ce1e7d65d6c8226d12aa429
Arpita singh <singh.arpi92@gmail.com>
2019-09-23 20:42:57 +0545

    [#4874] Empty date

--- src/app/shared/csv.service.ts
+++ src/app/shared/csv.service.ts
@@ -24,7 +24,7 @@ export class CsvService {
     const formattedData = data.map(({ _id, _rev, resourceId, type, createdOn, parentCode, ...dataToDisplay }) =>
       Object.entries(dataToDisplay).reduce((object, [ key, value ]: [ string, any ]) => ({
         ...object,
-        [key]: key.toLowerCase().indexOf('time') === -1 ? value : new Date(value).toString()
+        [key]: key.toLowerCase().indexOf('time') === -1 ? value : value ? new Date(value).toString() : ''
       }), {}
     ));
     this.generate(formattedData, options);
