68c39f311ffe16212d43a8521b8ed665b8d6774a
Paul <paul.r.albert@gmail.com>
2019-10-02 16:01:47 -0400

    Add toolbar and background

--- src/app/manager-dashboard/manager-dashboard.module.ts
+++ src/app/manager-dashboard/manager-dashboard.module.ts
@@ -19,8 +19,6 @@ import { PendingTableComponent } from './reports/pending-table.component';
 import { ReportsMyPlanetComponent } from './reports/reports-myplanet.component';
 import { MyPlanetTableComponent } from './reports/myplanet-table.component';
 import { SharedComponentsModule } from '../shared/shared-components.module';
-import { CertificationComponent } from '../certification/certification.component';
-import { CertificationModule } from '../certification/certification.module';
 
 @NgModule({
   imports: [
@@ -34,8 +32,7 @@ import { CertificationModule } from '../certification/certification.module';
     HttpClientModule,
     HttpClientJsonpModule,
     ConfigurationModule,
-    SharedComponentsModule,
-    CertificationModule
+    SharedComponentsModule
   ],
   declarations: [
     ManagerDashboardComponent,
