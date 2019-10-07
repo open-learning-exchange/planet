ab89963553e5b8a82d3a5f25359d4c67c6293076
Paul <paul.r.albert@gmail.com>
2019-10-07 15:27:42 -0400

    Clean up TasksService

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Input } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { CouchService } from '../shared/couchdb.service';
 import { finalize, map } from 'rxjs/operators';
 import { DialogsFormService } from '../shared/dialogs/dialogs-form.service';
@@ -14,6 +14,7 @@ import { findDocuments } from '../shared/mangoQueries';
   providedIn: 'root'
 })
 export class TasksService {
+
   private dbName = 'tasks';
   private tasksUpdated = new Subject<any>();
 
