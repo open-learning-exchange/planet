2936cc545f9a511ba9517226ccce4198f7b22403
Paul <paul.r.albert@gmail.com>
2019-10-09 15:45:13 -0400

    Store health key on user document

--- src/app/health/health.service.ts
+++ src/app/health/health.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 import { CouchService } from '../shared/couchdb.service';
+import { UserService } from '../shared/user.service';
+import { switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,19 +21,47 @@ export class HealthService {
     this.events = [];
   }
 
-  key = '0102030405060708090001020304050607080900010203040506070809000102';
-  iv = '00010203040506070809000102030405';
+  key: string;
+  iv: string;
 
   constructor(
-    private couchService: CouchService
-  ) {}
+    private couchService: CouchService,
+    private userService: UserService
+  ) {
+    this.userService.userChange$.subscribe(() => {
+      this.key = this.userService.credentials.key;
+      this.iv = this.userService.credentials.iv;
+    });
+  }
 
   getHealthData(userId) {
+    if (this.key === undefined || this.iv === undefined) {
+      return of({ doc: {} });
+    }
     return this.couchService.post(`health/_design/health/_show/decrypt/${userId}`, { key: this.key, iv: this.iv });
   }
 
   postHealthData(data) {
-    return this.couchService.put('health/_design/health/_update/encrypt', { ...data, key: this.key, iv: this.iv });
+    return (this.key === undefined || this.iv === undefined ?
+      this.userService.updateUser(
+        { ...this.userService.get(), ...this.userService.credentials, key: this.createNewKey(32), iv: this.createNewKey(16) }
+      ) :
+      of({})
+    ).pipe(switchMap(() =>
+      this.couchService.put('health/_design/health/_update/encrypt', { ...data, key: this.key, iv: this.iv })
+    ));
+  }
+
+  createNewKey(size: number) {
+    if (size % 16 !== 0) {
+      console.error('Invalid key size');
+      return;
+    }
+    const hexDigits = '0123456789abcdef';
+    const keyArray = new Uint8Array(size);
+    window.crypto.getRandomValues(keyArray);
+
+    return keyArray.reduce((hexString, number) => `${hexString}${hexDigits[Math.floor(number / 16)]}${hexDigits[number % 16]}`, '');
   }
 
 }
