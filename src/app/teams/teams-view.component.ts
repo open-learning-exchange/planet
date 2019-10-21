78e73c5b14c4b039e9f79002497ff91a014e5161
wanglihe2010 <wanglihe2010@gmail.com>
2019-10-21 14:27:14 -0500

    Team: Fix sending request notification (fixes #5011) (#5028)

--- src/app/teams/teams-view.component.ts
+++ src/app/teams/teams-view.component.ts
@@ -190,8 +190,10 @@ export class TeamsViewComponent implements OnInit, AfterViewChecked, OnDestroy {
       ...member,
       tasks: this.tasksService.sortedTasks(tasks.filter(({ assignee }) => assignee && assignee.userId === member.userId), member.tasks)
     }));
-    const tasksForCount = this.leader === this.user._id ? tasks : this.members.find(member => member.userId === this.user._id).tasks;
-    this.taskCount = tasksForCount.filter(task => task.completed === false).length;
+    if (this.userStatus === 'member') {
+      const tasksForCount = this.leader === this.user._id ? tasks : this.members.find(member => member.userId === this.user._id).tasks;
+      this.taskCount = tasksForCount.filter(task => task.completed === false).length;
+    }
   }
 
   resetData() {
