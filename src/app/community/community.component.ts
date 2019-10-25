fcc5735a8c744a84c1c665d32bcf5ad59378c01c
Paul <paul.r.albert@gmail.com>
2019-10-25 13:19:11 -0400

    Add toolbar and news sections

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 import { StateService } from '../shared/state.service';
+import { NewsService } from '../news/news.service';
 
 @Component({
-  templateUrl: './community.component.html'
+  templateUrl: './community.component.html',
+  styleUrls: [ './community.scss' ]
 })
-export class CommunityComponent {
+export class CommunityComponent implements OnInit, OnDestroy {
 
   configuration = this.stateService.configuration;
   teamId = `${this.stateService.configuration.code}@${this.stateService.configuration.parentCode}`;
+  news: any[] = [];
+  onDestroy$ = new Subject<void>();
 
   constructor(
-    private stateService: StateService
+    private stateService: StateService,
+    private newsService: NewsService
   ) {}
+
+  ngOnInit() {
+    this.newsService.requestNews({ viewableBy: 'teams', viewableId: this.teamId });
+    this.newsService.newsUpdated$.pipe(takeUntil(this.onDestroy$)).subscribe(news => this.news = news);
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
 
 }
