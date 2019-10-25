a6c704e745a11d9b30c3efe15d64a83b3bd875c7
Paul <paul.r.albert@gmail.com>
2019-10-25 13:45:14 -0400

    Full news section

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
+import { takeUntil, finalize } from 'rxjs/operators';
 import { StateService } from '../shared/state.service';
 import { NewsService } from '../news/news.service';
+import { DialogsFormService } from '../shared/dialogs/dialogs-form.service';
+import { DialogsLoadingService } from '../shared/dialogs/dialogs-loading.service';
 
 @Component({
   templateUrl: './community.component.html',
@@ -17,17 +19,39 @@ export class CommunityComponent implements OnInit, OnDestroy {
 
   constructor(
     private stateService: StateService,
-    private newsService: NewsService
+    private newsService: NewsService,
+    private dialogsFormService: DialogsFormService,
+    private dialogsLoadingService: DialogsLoadingService
   ) {}
 
   ngOnInit() {
-    this.newsService.requestNews({ viewableBy: 'teams', viewableId: this.teamId });
+    this.newsService.requestNews({ createdOn: this.configuration.code, viewableBy: 'community' });
     this.newsService.newsUpdated$.pipe(takeUntil(this.onDestroy$)).subscribe(news => this.news = news);
   }
 
   ngOnDestroy() {
     this.onDestroy$.next();
     this.onDestroy$.complete();
+  }
+
+  openAddMessageDialog(message = '') {
+    this.dialogsFormService.openDialogsForm(
+      'Add News',
+      [ { name: 'message', placeholder: 'Message', type: 'markdown', required: true } ],
+      { message },
+      { autoFocus: true, onSubmit: this.postMessage.bind(this) }
+    );
+  }
+
+  postMessage(message) {
+    this.newsService.postNews({
+      viewableBy: 'community',
+      messageType: 'sync',
+      messagePlanetCode: this.configuration.code,
+      ...message
+    }, 'Message has been posted successfully').pipe(
+      finalize(() => this.dialogsLoadingService.stop())
+    ).subscribe(() => this.dialogsFormService.closeDialogsForm());
   }
 
 }
