64e9742621fb5fbb2cd5d9f973e0e9a81ef98551
Paul <paul.r.albert@gmail.com>
2019-10-31 08:12:41 -0400

    Health: Sort exams ascending and scroll (fixes #5098) (#5110)

--- src/app/health/health.component.ts
+++ src/app/health/health.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MatTableDataSource, MatDialog } from '@angular/material';
 import { UserService } from '../shared/user.service';
@@ -10,8 +10,9 @@ import { environment } from '../../environments/environment';
   templateUrl: './health.component.html',
   styleUrls: [ './health.scss' ]
 })
-export class HealthComponent implements OnInit {
+export class HealthComponent implements OnInit, AfterViewChecked {
 
+  @ViewChild('examsTable', { static: false }) examsTable: ElementRef;
   userDetail = this.userService.get();
   healthDetail: any = {};
   events: any[] = [];
@@ -19,6 +20,7 @@ export class HealthComponent implements OnInit {
   displayedColumns: string[] = [];
   imageSrc = '';
   urlPrefix = environment.couchAddress + '/_users/';
+  initializeEvents = true;
 
   constructor(
     private userService: UserService,
@@ -41,6 +43,14 @@ export class HealthComponent implements OnInit {
     });
   }
 
+  ngAfterViewChecked() {
+    if (this.initializeEvents === false || this.examsTable === undefined) {
+      return;
+    }
+    this.initializeEvents = false;
+    this.examsTable.nativeElement.scrollLeft = this.examsTable.nativeElement.scrollWidth - this.examsTable.nativeElement.clientWidth;
+  }
+
   goBack() {
     this.router.navigate([ '..' ], { relativeTo: this.route });
   }
@@ -57,7 +67,7 @@ export class HealthComponent implements OnInit {
 
   setEventData() {
     this.eventTable.data = this.events
-      .sort((a, b) => b.date - a.date)
+      .sort((a, b) => a.date - b.date)
       .reduce((eventRows, event) => eventRows.map(item => ({ ...item, [event.date]: event[item.label] })), [
         { label: 'temperature' }, { label: 'pulse' }, { label: 'bp' }, { label: 'height' },
         { label: 'weight' }, { label: 'vision' }, { label: 'hearing' }
