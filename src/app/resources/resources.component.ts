1371f3bacb8071134c90098d3dd413c4859801bc
Anish Regmee <anishregmee@gmail.com>
2019-11-07 17:00:35 +0545

    user-personals deleted but the condition for filtering still not working (fixing #4575)

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -111,7 +111,8 @@ export class ResourcesComponent implements OnInit, AfterViewInit, OnDestroy {
     ).subscribe((resources) => {
       this.resources.data = resources.filter(
         (resource: any) => this.excludeIds.indexOf(resource._id) === -1
-        && this.route === '/myPersonals' ? resource.doc.privateFor : resource.doc.private !== true || this.route.snapshot.data.myPersonals === true
+        && this.route === '/myPersonals' ? resource.doc.privateFor :
+          resource.doc.private !== true || this.route.snapshot.data.myPersonals === true
         && (resource.doc.privateFor || {}).users === this.userService.get()._id
       );
       this.emptyData = !this.resources.data.length;
