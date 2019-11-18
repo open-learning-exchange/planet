5e26ce8d4eba10c957c3b3e183ba0782b5b82086
Charles J Perry <cjperry85@outlook.com>
2019-11-18 12:43:26 -0500

    Users: Show Name instead of username (fixes #5093)(#5160)

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -124,7 +124,7 @@ export class UsersComponent implements OnInit, OnDestroy, AfterViewInit {
       ),
       filterDropdowns(this.filter),
       filterFieldExists([ 'doc.requestId' ], this.filterType === 'associated'),
-      filterSpecificFields([ 'doc.name' ])
+      filterSpecificFields([ 'fullName' ])
     ]);
     this.allUsers.filter = this.allUsers.filter || ' ';
     this.searchChange.pipe(debounceTime(500)).subscribe((searchText) => {
@@ -200,12 +200,12 @@ export class UsersComponent implements OnInit, OnDestroy, AfterViewInit {
         // Removes current user and special satellite user from list.  Users should not be able to change their own roles,
         // so this protects from that.  May need to unhide in the future.
         return this.userService.get().name !== user.name && user.name !== 'satellite';
-      }).concat(childUsers)
-      .map((user: any) => {
+      }).concat(childUsers).map((user: any) => {
         const userInfo = {
           _id: user._id,
           doc: user,
           imageSrc: '',
+          fullName: user.firstName + ' ' + user.lastName,
           visitCount: this.userLoginCount(user, loginActivities),
           lastLogin: this.userLastLogin(user, loginActivities),
           roles: this.toProperRoles(user.roles)
