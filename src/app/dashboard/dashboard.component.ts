c3525ae7b359ca1cd5ad952899949cc10b783bcc
Paul <paul.r.albert@gmail.com>
2019-12-02 15:02:50 -0500

    Add formatted roles to dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -25,7 +25,7 @@ export class DashboardComponent implements OnInit {
   urlPrefix = environment.couchAddress + '/_users/org.couchdb.user:' + this.userService.get().name + '/';
   displayName: string = this.userService.get().firstName !== undefined ?
     this.userService.get().firstName + ' ' + this.userService.get().lastName : this.userService.get().name;
-  displayRole: string;
+  roles: string[];
   planetName: string;
 
   dateNow: any;
@@ -52,7 +52,7 @@ export class DashboardComponent implements OnInit {
     private stateService: StateService
   ) {
     const currRoles = this.userService.get().roles;
-    this.displayRole = currRoles.reduce(dedupeShelfReduce, currRoles.length ? [ 'learner' ] : [ 'Inactive' ]).join(', ').replace('_', '');
+    this.roles = currRoles.reduce(dedupeShelfReduce, currRoles.length ? [ 'learner' ] : [ 'Inactive' ]);
     this.userService.shelfChange$.pipe()
       .subscribe(() => {
         this.ngOnInit();
