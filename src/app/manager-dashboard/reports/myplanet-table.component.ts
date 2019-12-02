be7cd9786abfaf4eb0921ed27e955cc7c0c5d287
Arpita Singh <singh.arpi92@gmail.com>
2019-12-02 21:46:48 +0545

    [#5274] Myplanet report sort

--- src/app/manager-dashboard/reports/myplanet-table.component.ts
+++ src/app/manager-dashboard/reports/myplanet-table.component.ts
@@ -9,13 +9,19 @@ export class MyPlanetTableComponent implements OnChanges, AfterViewInit {
 
   @Input() data = [];
   myPlanets = new MatTableDataSource();
-  displayedColumns = [ 'id', 'name', 'lastSynced', 'version', 'count' ];
+  displayedColumns = [ 'id', 'name', 'last_synced', 'versionName', 'count' ];
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
 
   ngOnChanges() {
     this.myPlanets.data = this.data;
+    this.myPlanets.sortingDataAccessor = (item: any, property) => {
+      switch (property) {
+        case 'name': return (item.customDeviceName || item.deviceName).toLowerCase();
+        default: return typeof item[property] === 'string' ? item[property].toLowerCase() : item[property];
+      }
+    };
   }
 
   ngAfterViewInit() {
