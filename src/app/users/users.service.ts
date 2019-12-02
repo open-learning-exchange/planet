0a435f50336f05f07a2bf5731e70e0d70c5a4e7d
Laxman Maharjan <email@mlaxman.com.np>
2019-12-03 00:18:47 +0545

    Users: Fix list loading (fixes #5269) (#5281)

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -34,16 +34,18 @@ export class UsersService {
   ) {
     const checkIfLocal = (data: { newData, planetField, db }) => data && data.planetField === 'local';
     const dataToUse = (oldData, data: { newData, planetField, db }, isLocal) => isLocal ? data.newData : oldData;
-    combineLatest([
-      this.stateService.couchStateListener('login_activities'),
-      this.stateService.couchStateListener('child_users')
-    ]).pipe(switchMap(([ loginActivities, childUsers ]) => {
-      const [ loginLocal, childLocal ]: boolean[] = [ checkIfLocal(loginActivities), checkIfLocal(childUsers) ];
-      const [ loginData, childData ]: any[] = [
-        dataToUse(this.data.loginActivities, loginActivities, loginLocal), dataToUse(this.data.childUsers, childUsers, childLocal)
-      ];
-      return loginLocal || childLocal ? forkJoin([ this.couchService.findAll(this.dbName), of(loginData), of(childData) ]) : of([]);
-    })).subscribe(([ users, loginActivities, childUsers ]) => {
+    combineLatest([ this.stateService.couchStateListener('login_activities'), this.stateService.couchStateListener('child_users') ]).pipe(
+      switchMap(([ loginActivities, childUsers ]) => {
+        const [ loginLocal, childLocal ]: boolean[] = [ checkIfLocal(loginActivities), checkIfLocal(childUsers) ];
+        const [ loginData, childData ]: any[] = [
+          dataToUse(this.data.loginActivities, loginActivities, loginLocal), dataToUse(this.data.childUsers, childUsers, childLocal)
+        ];
+        return loginLocal || childLocal ? forkJoin([ this.couchService.findAll(this.dbName), of(loginData), of(childData) ]) : of([]);
+      })
+    ).subscribe(([ users, loginActivities, childUsers ]) => {
+      if (users === undefined) {
+        return;
+      }
       this.data = { users, loginActivities, childUsers };
       this.usersUpdated.next(
         this.data.users.filter((user: any) => {
@@ -63,7 +65,6 @@ export class UsersService {
   }
 
   requestUsers() {
-    this.stateService.requestData(this.dbName, 'local');
     this.stateService.requestData('login_activities', 'local');
     this.stateService.requestData('child_users', 'local');
   }
