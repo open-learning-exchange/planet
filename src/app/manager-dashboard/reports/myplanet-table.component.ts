2408fb926765c0dfb01b8b943d57787d0a978362
Arpita Singh <singh.arpi92@gmail.com>
2019-12-05 16:31:18 +0545

    Reports: Sort myplanet report (fixes #5274) (#5280)

--- src/app/manager-dashboard/reports/myplanet-table.component.ts
+++ src/app/manager-dashboard/reports/myplanet-table.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnChanges, AfterViewInit, ViewChild, Input } from '@angular/core';
+import { Component, OnChanges, AfterViewInit, ViewChild, Input, OnInit } from '@angular/core';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'planet-myplanet-table',
   templateUrl: './myplanet-table.component.html'
 })
-export class MyPlanetTableComponent implements OnChanges, AfterViewInit {
+export class MyPlanetTableComponent implements OnInit, OnChanges, AfterViewInit {
 
   @Input() data = [];
   myPlanets = new MatTableDataSource();
-  displayedColumns = [ 'id', 'name', 'lastSynced', 'version', 'count' ];
+  displayedColumns = [ 'id', 'name', 'last_synced', 'versionName', 'count' ];
 
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
+
+  ngOnInit() {
+    this.myPlanets.sortingDataAccessor = (item: any, property) => {
+      switch (property) {
+        case 'name': return (item.customDeviceName || item.deviceName).toLowerCase();
+        default: return typeof item[property] === 'string' ? item[property].toLowerCase() : item[property];
+      }
+    };
+  }
 
   ngOnChanges() {
     this.myPlanets.data = this.data;
