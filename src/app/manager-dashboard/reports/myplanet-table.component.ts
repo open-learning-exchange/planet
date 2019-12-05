389d53f2fdf0dd0e6112e3f50bd04907b0645fc3
Arpita Singh <singh.arpi92@gmail.com>
2019-12-05 16:05:08 +0545

    myplanet sort oninit

--- src/app/manager-dashboard/reports/myplanet-table.component.ts
+++ src/app/manager-dashboard/reports/myplanet-table.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnChanges, AfterViewInit, ViewChild, Input } from '@angular/core';
+import { Component, OnChanges, AfterViewInit, ViewChild, Input, OnInit } from '@angular/core';
 import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 
 @Component({
   selector: 'planet-myplanet-table',
   templateUrl: './myplanet-table.component.html'
 })
-export class MyPlanetTableComponent implements OnChanges, AfterViewInit {
+export class MyPlanetTableComponent implements OnInit, OnChanges, AfterViewInit {
 
   @Input() data = [];
   myPlanets = new MatTableDataSource();
@@ -14,14 +14,17 @@ export class MyPlanetTableComponent implements OnChanges, AfterViewInit {
   @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
   @ViewChild(MatSort, { static: false }) sort: MatSort;
 
-  ngOnChanges() {
-    this.myPlanets.data = this.data;
+  ngOnInit() {
     this.myPlanets.sortingDataAccessor = (item: any, property) => {
       switch (property) {
         case 'name': return (item.customDeviceName || item.deviceName).toLowerCase();
         default: return typeof item[property] === 'string' ? item[property].toLowerCase() : item[property];
       }
     };
+  }
+
+  ngOnChanges() {
+    this.myPlanets.data = this.data;
   }
 
   ngAfterViewInit() {
