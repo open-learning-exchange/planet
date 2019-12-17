840d3b773ba94344f2c5a5b1ddbeb676f1f394ff
Paul <paul.r.albert@gmail.com>
2019-12-16 23:44:01 -0500

    Production app uses en-GB locale (fixes #2794)

--- docker/planet/scripts/build_planet.sh
+++ docker/planet/scripts/build_planet.sh
@@ -4,7 +4,7 @@ build_single(){
   LANG=$1
   LANG2=$2
   if [ -z "${LANG}" ]; then
-    npm run ng-high-memory -- build --prod --base-href /eng/ --output-path=dist/eng
+    npm run ng-high-memory -- build --prod --base-href /eng/ --output-path=dist/eng --aot --i18n-locale=en-GB
   else
     npm run ng-high-memory -- build --prod --base-href /$LANG/ --output-path=dist/$LANG --aot --i18n-file=src/i18n/messages.$LANG.xlf --i18n-locale=$LANG2 --i18n-format=xlf
   fi
