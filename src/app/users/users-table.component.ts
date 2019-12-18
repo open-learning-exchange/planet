36179fcc89e8c73535c9920ea23abd77a8fd7a5b
Vagrant Default User <vagrant@dev>
2019-12-18 07:36:16 +0000

    console removed

--- src/app/users/users-table.component.ts
+++ src/app/users/users-table.component.ts
@@ -114,7 +114,6 @@ export class UsersTableComponent implements OnInit, OnDestroy, AfterViewInit, On
       filterSpecificFieldsByWord([ 'fullName' ])
     ])(data, filter);
     this.usersTable.connect().subscribe(data => {
-      console.log(data)
       data.map((health: any) => health.sort((a, b) => b.lastVisit - a.lastVisit));
       if (this.usersTable.paginator) {
 
