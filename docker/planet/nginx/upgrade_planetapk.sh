b84d82256e0cb168b133be31da52504fe205ddd4
Laxman Maharjan <email@mlaxman.com.np>
2019-12-19 10:00:56 +0545

    myPlanet: Move apkversion to fs (fixes #5261) (#5394)

--- docker/planet/nginx/upgrade_planetapk.sh
+++ docker/planet/nginx/upgrade_planetapk.sh
@@ -45,7 +45,7 @@ if [ -f "$apkName" ] && [ -f "$sha256File" ]; then
       write_status "Success"
       echo "Download successful"
       rm -f "$apkName.tmp"
-      echo $tag > /usr/share/nginx/html/apkversion
+      echo $tag > /usr/share/nginx/html/fs/apkversion
     else
       write_status "Failed"
       echo "Download error"
@@ -59,7 +59,7 @@ else
     write_status "Success"
     echo "Download successful"
     rm -f "$apkName.tmp"
-    echo $tag > /usr/share/nginx/html/apkversion
+    echo $tag > /usr/share/nginx/html/fs/apkversion
   else
     write_status "Failed"
     echo "Download error"
