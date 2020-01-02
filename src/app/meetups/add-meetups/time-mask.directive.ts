66aab84ec4579c4626411bc99f8bdb2f42496189
Paul <paul.r.albert@gmail.com>
2020-01-02 00:42:10 -0500

    Simplify mask

--- src/app/meetups/add-meetups/time-mask.directive.ts
+++ src/app/meetups/add-meetups/time-mask.directive.ts
@@ -1,48 +1,30 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, OnInit } from '@angular/core';
 import { NgControl } from '@angular/forms';
-import { stringify } from 'querystring';
+import { startWith, pairwise } from 'rxjs/operators';
 
 @Directive({
   selector: 'input[planetTimeMask]'
 })
-export class TimeMaskDirective {
-  previousTime = '';
+export class TimeMaskDirective implements OnInit {
 
-  constructor(private controlRef: NgControl) { }
+  constructor(private controlRef: NgControl) {}
 
-  @HostListener('input', [ '$event' ]) onInput($event) {
-    this.controlRef.control.setValue(this.getOutputTime($event.target.value));
+  ngOnInit() {
+    this.controlRef.control.valueChanges.pipe(startWith(null), pairwise()).subscribe(([ prevTime, nextTime ]: [ string, string ]) => {
+      if (prevTime === nextTime) {
+        return;
+      }
+      const inputTime = nextTime.replace(/[^0-9]*/g, '');
+      const regexp = new RegExp('^([0-9]|0[0-9]|1[0-9]|2[0-3])([0-5]|[0-5][0-9])?$');
+      if (inputTime !== '' && !regexp.test(inputTime)) {
+        this.controlRef.control.setValue(prevTime);
+        return;
+      }
+      this.controlRef.control.setValue(inputTime.length > 2 ?
+        inputTime.substring(0, inputTime.length - 2) + ':' + inputTime.substring(inputTime.length - 2, inputTime.length) :
+        inputTime
+      );
+    });
   }
 
-  getOutputTime(inputTime: string): string {
-    inputTime = inputTime.replace(/[^0-9]*/g, '');
-    const regexp = new RegExp('^([0-9]|0[0-9]|1[0-9]|2[0-3])([0-5]|[0-5][0-9])?$');
-    if (inputTime !== '' && !regexp.test(inputTime)) {
-      return this.previousTime;
-    }
-    switch (inputTime.length) {
-      case 4: {
-        this.previousTime = inputTime.substring(0, 2) + ':' + inputTime.substring(2, 4);
-        break;
-      }
-      case 3: {
-        if (+inputTime.substring(1, 3) > 59) {
-          this.previousTime = inputTime.substring(0, 2) + ':' + inputTime.substring(2, 3);
-        } else {
-          this.previousTime = inputTime.substring(0, 1) + ':' + inputTime.substring(1, 3);
-        }
-        break;
-      }
-      case 2: {
-        if (+inputTime.substring(1, 2) > 5) {
-          this.previousTime = inputTime;
-        } else {
-          this.previousTime = inputTime.substring(0, 1) + ':' + inputTime.substring(1, 2);
-        }
-        break;
-      }
-      default: this.previousTime = inputTime;
-    }
-    return this.previousTime;
-  }
 }
