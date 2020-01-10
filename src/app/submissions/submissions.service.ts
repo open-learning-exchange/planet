57c142fea4872f4de63cae0928b4fda3986fcb2d
Arpita Singh <singh.arpi92@gmail.com>
2020-01-10 18:17:12 +0545

    [#5506] PDF response number

--- src/app/submissions/submissions.service.ts
+++ src/app/submissions/submissions.service.ts
@@ -293,8 +293,8 @@ export class SubmissionsService {
         const answerIndexes = this.answerIndexes(questionTexts, submission);
         return `<h3${index === 0 ? '' : ' class="pdf-break"'}>Response from ${new Date(submission.lastUpdateTime).toString()}</h3>  \n` +
           questionTexts.map((question, questionIndex) => (
-            `**Question ${index + 1}:**  \n\n${question}  \n\n` +
-            `**Response ${index + 1}:**  \n\n${this.getAnswerText(submission.answers, index, answerIndexes)}  \n`
+            `**Question ${questionIndex + 1}:**  \n\n${question}  \n\n` +
+            `**Response ${questionIndex + 1}:**  \n\n${this.getAnswerText(submission.answers, questionIndex, answerIndexes)}  \n`
           )).join('  \n');
       }).join('  \n');
       const converter = new showdown.Converter();
