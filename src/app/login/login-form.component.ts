3aa9d4d0850d6ddf1b03752c9d4980e869a5f6e8
Paul <paul.r.albert@gmail.com>
2020-01-13 23:21:03 -0500

    Login: Fix returnUrl always landing on community page (fixes #5529)

--- src/app/login/login-form.component.ts
+++ src/app/login/login-form.component.ts
@@ -67,8 +67,10 @@ export class LoginFormComponent {
   }
 
   createMode: boolean = this.router.url.split('?')[0] === '/login/newmember';
-  returnUrl = this.route.snapshot.queryParams['returnUrl'] ||
-    this.stateService.configuration.planetType === 'community' ? '/' : 'myDashboard';
+  returnUrl = this.route.snapshot.queryParams['returnUrl'] || (this.stateService.configuration.planetType === 'community' ?
+    '/' :
+    'myDashboard'
+  );
 
   onSubmit() {
     if (!this.userForm.valid) {
