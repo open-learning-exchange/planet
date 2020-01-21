bd49d4eff82d08c1c11ab9bfa22405aa098bb8e3
Arpita Singh <singh.arpi92@gmail.com>
2020-01-21 17:51:22 +0545

    [#5469] myPlanet report show hub child communities

--- src/app/manager-dashboard/reports/reports-myplanet.component.ts
+++ src/app/manager-dashboard/reports/reports-myplanet.component.ts
@@ -1,36 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CouchService } from '../../shared/couchdb.service';
-import { forkJoin } from 'rxjs';
+import { forkJoin, Subject, of } from 'rxjs';
 import { StateService } from '../../shared/state.service';
 import { PlanetMessageService } from '../../shared/planet-message.service';
 import { ManagerService } from '../manager.service';
 import { ReportsService } from './reports.service';
 import { filterSpecificFields } from '../../shared/table-helpers';
 import { attachNamesToPlanets } from './reports.utils';
+import { ActivatedRoute } from '@angular/router';
+import { takeUntil, switchMap } from 'rxjs/operators';
 
 @Component({
   templateUrl: './reports-myplanet.component.html'
 })
-export class ReportsMyPlanetComponent implements OnInit {
+export class ReportsMyPlanetComponent implements OnInit, OnDestroy {
 
   private allPlanets: any[] = [];
   searchValue = '';
   planets: any[] = [];
   planetType = this.stateService.configuration.planetType;
+  configuration = this.stateService.configuration;
   get childType() {
     return this.planetType === 'center' ? 'Community' : 'Nation';
   }
+  hubId: string | null = null;
+  hub = { spokes: [] };
+  onDestroy$ = new Subject<void>();
 
   constructor(
     private couchService: CouchService,
     private stateService: StateService,
     private planetMessageService: PlanetMessageService,
     private managerService: ManagerService,
-    private reportsService: ReportsService
+    private reportsService: ReportsService,
+    private route: ActivatedRoute
   ) {}
 
   ngOnInit() {
+    this.route.paramMap.pipe(takeUntil(this.onDestroy$)).subscribe(params => this.hubId = params.get('hubId'));
     this.getMyPlanetList();
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
   }
 
   filterData(filterValue: string) {
@@ -57,13 +70,28 @@ export class ReportsMyPlanetComponent implements OnInit {
   }
 
   getMyPlanetList() {
+    const { planetCode, domain } = this.configuration.planetType === 'community' ?
+      { planetCode: this.configuration.parentCode, domain: this.configuration.parentDomain } :
+      { planetCode: undefined, domain: undefined };
     forkJoin([
-      this.managerService.getChildPlanets(),
+      this.managerService.getChildPlanets(true, planetCode, domain),
       this.couchService.findAll('myplanet_activities')
-    ]).subscribe(([ planets, myPlanets ]) => {
+    ]).pipe(switchMap(([ planets, myPlanets ]: [ any, any ]) => {
+        planets = attachNamesToPlanets(planets).filter((planet: any) => planet.doc.docType !== 'parentName');
+        if (this.hubId) {
+          return this.couchService.findAll('hubs', { 'selector': { 'planetId': this.hubId } }, { domain })
+          .pipe(switchMap((hubs: any) => {
+            this.hub = hubs[0] || { spokes: [] };
+            const selector = { 'selector': { 'createdOn': { '$in': this.hub.spokes } } };
+            return this.couchService.findAll('myplanet_activities', selector, { domain });
+          }), switchMap((hubActivities: any) => {
+            return [ planets.filter(p => this.hub.spokes.indexOf(p.doc.code) > -1), myPlanets.concat(hubActivities) ];
+          }));
+        }
+        return of([ planets, myPlanets ]);
+    })).subscribe(([ planets, myPlanets ]) => {
       this.setAllPlanets(
-        [ { doc: this.stateService.configuration } ].concat(attachNamesToPlanets(planets))
-          .filter((planet: any) => planet.doc.docType !== 'parentName')
+        [ { doc: this.configuration } ].concat(planets)
           .map((planet: any) => ({ ...planet, name: planet.nameDoc ? planet.nameDoc.name : planet.doc.name })),
         myPlanets
       );
