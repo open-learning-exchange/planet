0a75dd6b2a687f3139c4b5a2d55d8337740e4428
Paul <paul.r.albert@gmail.com>
2020-02-03 17:57:59 -0500

    Version 0.10.17 (#5677)

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -1,9 +1,9 @@
 PLANET_VERSION=$(cat package.json | jq -r .version)
-LATEST_APK_VERSION="v0.6.78"
-LATEST_APK_VER_CODE="678"
-MIN_APK_VERSION="v0.6.78"
-MIN_APK_VER_CODE="678"
-APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.6.78/myPlanet.apk"
+LATEST_APK_VERSION="v0.6.85"
+LATEST_APK_VER_CODE="685"
+MIN_APK_VERSION="v0.6.85"
+MIN_APK_VER_CODE="685"
+APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.6.85/myPlanet.apk"
 LOCAL_APK_PATH="/fs/myPlanet.apk"
 
 echo '{"appname":"planet","planetVersion":"'$PLANET_VERSION'","latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"apkpath":"'$APK_PATH'","localapkpath":"'$LOCAL_APK_PATH'"}' > /usr/share/nginx/html/versions
