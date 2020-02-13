8beb8eaef12a38ab3fad2286ea40a143fdcffea7
Paul <paul.r.albert@gmail.com>
2020-02-13 00:55:12 -0500

    Reduce number of lines

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -174,9 +174,7 @@ export class CommunityComponent implements OnInit, OnDestroy {
 
   setLinksAndFinances({ links, finances }) {
     this.links = links;
-    if (this.links.length === 0) {
-      this.deleteMode = false;
-    }
+    this.deleteMode = this.deleteMode && this.links.length === 0;
     this.finances = finances;
   }
 
