2976be53b156c06c598bbdd2bb60bbcb2af783ce
Paul <paul.r.albert@gmail.com>
2020-02-14 12:43:03 -0500

    New community list dialog component

---
+++ src/app/community/community-list-dialog.component.ts
@@ -0,0 +1,32 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  templateUrl: './community-list-dialog.component.html',
+  styles: [ `
+    :host mat-dialog-content {
+      min-width: 33vw;
+    }
+  ` ]
+})
+export class CommunityListDialogComponent {
+
+  selected: any[] = [];
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  listSubmit() {
+    this.data.okClick(this.selected);
+  }
+
+  selectionChange(planet) {
+    if (planet.selected) {
+      this.selected.push(planet);
+    } else {
+      this.selected = this.selected.filter(selectedPlanet => planet._id !== selectedPlanet._id);
+    }
+  }
+
+}
