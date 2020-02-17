0ffbcc3b527d53f81c7737fad8f1cb95148e70c2
Robin Giri <45058288+robinGiri@users.noreply.github.com>
2020-02-18 02:08:40 +0545

    Services: Fix link delete mode completion (fixes #5771) (#5781)

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -178,7 +178,7 @@ export class CommunityComponent implements OnInit, OnDestroy {
 
   setLinksAndFinances({ links, finances }) {
     this.links = links;
-    this.deleteMode = this.deleteMode && this.links.length === 0;
+    this.deleteMode = this.deleteMode && this.links.length !== 0;
     this.finances = finances;
   }
 
