110bd85e1340692474c1821db5d03aa267ae65b4
Arpita Singh <singh.arpi92@gmail.com>
2020-03-06 10:56:07 +0545

    [#5912] Enroll activity date

--- src/app/courses/enroll-courses/courses-enroll.component.ts
+++ src/app/courses/enroll-courses/courses-enroll.component.ts
@@ -33,16 +33,31 @@ export class CoursesEnrollComponent implements OnDestroy {
         this.courseId = paramMap.get('id');
         return forkJoin([
           this.couchService.findAll('shelf', { 'selector': { 'courseIds': { '$elemMatch': { '$eq': this.courseId } } } }),
-          this.usersService.getAllUsers()
+          this.usersService.getAllUsers(),
+          this.coursesService.findProgress([ this.courseId ], { allUsers : true })
         ]);
       }),
       takeUntil(this.onDestroy$)
-    ).subscribe(([ shelfUsers, allUsers ]) => {
+    ).subscribe(([ shelfUsers, allUsers, progresses ]) => {
       this.course = this.coursesService.getCourseNameFromId(this.courseId);
       this.members = allUsers.filter(user => shelfUsers.find((u: any) => u._id === user._id))
-        .map((user: any) => this.usersService.fullUserDoc(user));
+        .map((user: any) => ({
+          ...this.usersService.fullUserDoc(user),
+          activity: this.userProgress(progresses.filter(progress => progress.userId === user._id))
+        }));
       this.emptyData = this.members.length === 0;
     });
+  }
+
+  userProgress(progresses) {
+    return progresses.reduce((activity, progress) => {
+      return ({
+        startDate: (activity.startDate === undefined || activity.startDate > progress.createdDate)
+          ? progress.createdDate : activity.startDate,
+        recentDate: (activity.recentDate === undefined || activity.recentDate < progress.updatedDate)
+          ? progress.updatedDate : activity.recentDate
+      });
+    }, { startDate: undefined, recentDate: undefined });
   }
 
   ngOnDestroy() {
