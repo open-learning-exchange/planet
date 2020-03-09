5c615092b33ac945ccfab3f6374e291c0f3d458d
Leonard Maximus Irenous Mensah <leomaxi@outlook.com>
2020-03-09 18:05:02 +0000

    Update create_version_json.sh

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -1,9 +1,9 @@
 PLANET_VERSION=$(cat package.json | jq -r .version)
-LATEST_APK_VERSION="v0.7.22"
-LATEST_APK_VER_CODE="722"
-MIN_APK_VERSION="v0.7.22"
-MIN_APK_VER_CODE="722"
-APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.7.22/myPlanet.apk"
+LATEST_APK_VERSION="v0.7.24"
+LATEST_APK_VER_CODE="724"
+MIN_APK_VERSION="v0.7.24"
+MIN_APK_VER_CODE="724"
+APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.7.24/myPlanet.apk"
 LOCAL_APK_PATH="/fs/myPlanet.apk"
 
 echo '{"appname":"planet","planetVersion":"'$PLANET_VERSION'","latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"apkpath":"'$APK_PATH'","localapkpath":"'$LOCAL_APK_PATH'"}' > /usr/share/nginx/html/versions
