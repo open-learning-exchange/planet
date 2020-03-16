84902daab50e9ef7bf9097f06d51978f71fa36b9
paulbert <paul.r.albert@gmail.com>
2020-03-16 19:57:51 -0400

    Simpler tooltip text

--- src/app/courses/courses-icon.component.ts
+++ src/app/courses/courses-icon.component.ts
@@ -4,13 +4,13 @@ import { Component, Input } from '@angular/core';
   selector: 'planet-course-icon',
   template: `
     <div [ngSwitch]="icon">
-      <span *ngSwitchCase="'assignment'" i18n-matTooltip matTooltip="The test has question"><mat-icon>assignment</mat-icon></span>
-      <span *ngSwitchCase="'attach_file'" i18n-matTooltip matTooltip="There are resource."><mat-icon>attach_file</mat-icon></span>
-      <span *ngSwitchCase="'description'" i18n-matTooltip matTooltip="There are surveys in this step.">
+      <span *ngSwitchCase="'assignment'" i18n-matTooltip matTooltip="Test"><mat-icon>assignment</mat-icon></span>
+      <span *ngSwitchCase="'attach_file'" i18n-matTooltip matTooltip="Resource(s)"><mat-icon>attach_file</mat-icon></span>
+      <span *ngSwitchCase="'description'" i18n-matTooltip matTooltip="Survey">
         <mat-icon >description</mat-icon>
       </span>
       <span *ngSwitchCase="'done'" i18n-matTooltip matTooltip="This step has been completed."><mat-icon>done</mat-icon></span>
-      <span *ngSwitchCase="'rotate_right'" i18n-matTooltip matTooltip="This step is not complete."><mat-icon >rotate_right</mat-icon></span>
+      <span *ngSwitchCase="'rotate_right'" i18n-matTooltip matTooltip="This step is in progress."><mat-icon >rotate_right</mat-icon></span>
     </div>
   `
 })
