08da3b532bf6fdb4d8e9d7fb6fb5fcc0eee5c8cb
robinGiri <girirobin2018@gmail.com>
2020-03-16 10:11:47 +0545

    switch case used

--- src/app/courses/cousers-icon.component.ts
+++ src/app/courses/cousers-icon.component.ts
@@ -3,11 +3,23 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'planet-course-icon',
   template: `
-    <span *ngIf="step?.exam?.questions.length" i18n-matTooltip matTooltip="The test has question"><mat-icon *ngIf="step?.exam?.questions.length" >assignment</mat-icon></span>
-    <span *ngIf="step?.resources?.length" i18n-matTooltip matTooltip= "There are resource."><mat-icon *ngIf="step?.resources?.length">attach_file</mat-icon></span>
-    <span *ngIf="step?.survey?.questions.length" i18n-matTooltip matTooltip= "There are surveys in this step."><mat-icon *ngIf="step?.survey?.questions.length">description</mat-icon></span>
-    <span *ngIf="step.progress && step.progress" i18n-matTooltip matTooltip= "This step has been completed."><mat-icon *ngIf="step.progress && step.progress" >done</mat-icon></span>
-    <span *ngIf="step.progress && !step.progress" i18n-matTooltip matTooltip= "This step is not complete."><mat-icon *ngIf="step.progress && !step.progress">rotate_right</mat-icon></span>
+  <ng-container [ngSwitch]="icon">
+    <span *ngIf="step?.exam?.questions.length" i18n-matTooltip matTooltip="The test has question">
+      <mat-icon *ngSwitchCase="'assignment'">assignment</mat-icon>
+    </span>
+    <span *ngIf="step?.resources?.length" i18n-matTooltip matTooltip= "There are resource.">
+      <mat-icon *ngSwitchCase="'attach_file'">attach_file</mat-icon>
+    </span>
+    <span *ngIf="step?.survey?.questions.length" i18n-matTooltip matTooltip= "There are surveys in this step.">
+      <mat-icon *ngSwitchCase="'description'">description</mat-icon>
+    </span>
+    <span *ngIf="step.progress && step.progress" i18n-matTooltip matTooltip= "This step has been completed.">
+      <mat-icon *ngSwitchCase="'done'" >done</mat-icon>
+    </span>
+    <span *ngIf="step.progress && !step.progress" i18n-matTooltip matTooltip= "This step is not complete.">
+      <mat-icon *ngSwitchCase="'rotate_right'">rotate_right</mat-icon>
+    </span>
+    </ng-container>
   `
 })
 export class CoursesIconComponent {
