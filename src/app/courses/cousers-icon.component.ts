13c394094cc41fe85f61a3aba003bcd4cc1c3b66
robinGiri <girirobin2018@gmail.com>
2020-03-16 10:54:50 +0545

    improvements as commented

--- src/app/courses/cousers-icon.component.ts
+++ src/app/courses/cousers-icon.component.ts
@@ -3,23 +3,15 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'planet-course-icon',
   template: `
-  <ng-container [ngSwitch]="icon">
-    <span *ngIf="step?.exam?.questions.length" i18n-matTooltip matTooltip="The test has question">
-      <mat-icon *ngSwitchCase="'assignment'">assignment</mat-icon>
+  <div [ngSwitch]="icon">
+    <span *ngSwitchCase="'assignment'" i18n-matTooltip matTooltip="The test has question"><mat-icon>assignment</mat-icon></span>
+    <span *ngSwitchCase="'attach_file'" i18n-matTooltip matTooltip="There are resource."><mat-icon>attach_file</mat-icon></span>
+    <span *ngSwitchCase="'description'" i18n-matTooltip matTooltip="There are surveys in this step.">
+      <mat-icon >description</mat-icon>
     </span>
-    <span *ngIf="step?.resources?.length" i18n-matTooltip matTooltip= "There are resource.">
-      <mat-icon *ngSwitchCase="'attach_file'">attach_file</mat-icon>
-    </span>
-    <span *ngIf="step?.survey?.questions.length" i18n-matTooltip matTooltip= "There are surveys in this step.">
-      <mat-icon *ngSwitchCase="'description'">description</mat-icon>
-    </span>
-    <span *ngIf="step.progress && step.progress" i18n-matTooltip matTooltip= "This step has been completed.">
-      <mat-icon *ngSwitchCase="'done'" >done</mat-icon>
-    </span>
-    <span *ngIf="step.progress && !step.progress" i18n-matTooltip matTooltip= "This step is not complete.">
-      <mat-icon *ngSwitchCase="'rotate_right'">rotate_right</mat-icon>
-    </span>
-    </ng-container>
+    <span *ngSwitchCase="'done'" i18n-matTooltip matTooltip="This step has been completed."><mat-icon>done</mat-icon></span>
+    <span *ngSwitchCase="'rotate_right'" i18n-matTooltip matTooltip="This step is not complete."><mat-icon >rotate_right</mat-icon></span>
+    </div>
   `
 })
 export class CoursesIconComponent {
