d197aa492d10ec9f08e3eaffc44cfff52cc0fafd
Paul <paul.r.albert@gmail.com>
2020-03-30 05:23:00 -0400

    CoursesEnroll: Show Planet name for nation (fixes #6013) (#6045)

--- src/app/courses/enroll-courses/courses-enroll.component.ts
+++ src/app/courses/enroll-courses/courses-enroll.component.ts
@@ -6,6 +6,9 @@ import { CouchService } from '../../shared/couchdb.service';
 import { UsersService } from '../../users/users.service';
 import { CoursesService } from '../courses.service';
 import { TableState } from '../../users/users-table.component';
+import { StateService } from '../../shared/state.service';
+import { ManagerService } from '../../manager-dashboard/manager.service';
+import { attachNamesToPlanets } from '../../manager-dashboard/reports/reports.utils';
 
 
 @Component({
@@ -19,13 +22,22 @@ export class CoursesEnrollComponent {
   members: any[] = [];
   tableState = new TableState();
   emptyData = false;
+  userTableColumns = [
+    'profile',
+    'name',
+    ...(this.stateService.configuration.planetType === 'community' ? [] : [ 'planet' ]),
+    'startDate',
+    'recentDate'
+  ];
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private couchService: CouchService,
     private usersService: UsersService,
-    private coursesService: CoursesService
+    private coursesService: CoursesService,
+    private stateService: StateService,
+    private managerService: ManagerService
   ) {
     this.coursesService.requestCourses();
     this.usersService.requestUserData();
@@ -36,20 +48,14 @@ export class CoursesEnrollComponent {
           this.couchService.findAll('shelf', { 'selector': { 'courseIds': { '$elemMatch': { '$eq': this.courseId } } } }),
           this.coursesService.findProgress([ this.courseId ], { allUsers : true }),
           this.usersService.usersListener(true),
+          this.managerService.getChildPlanets(),
           // Include course listener to ensure requestCourses() is complete.  This updates courses in CoursesService.
           this.coursesService.coursesListener$()
         );
       }),
       take(1)
-    ).subscribe(([ shelfUsers, progresses, users ]) => {
-      this.course = this.coursesService.getCourseNameFromId(this.courseId);
-      this.members = users.map((user: any) => ({
-          ...user,
-          activityDates: this.userProgress(progresses.filter(
-            (progress: any) => progress.createdOn === user.doc.planetCode && progress.userId === (user.doc.couchId || user._id))
-          )
-        })).filter(doc => doc.activityDates.createdDate || shelfUsers.find((u: any) => u._id === doc._id));
-      this.emptyData = this.members.length === 0;
+    ).subscribe((responses) => {
+      this.setMembers(responses);
     });
   }
 
@@ -62,6 +68,19 @@ export class CoursesEnrollComponent {
 
   back() {
     this.router.navigate([ '../..' ], { relativeTo: this.route });
+  }
+
+  setMembers([ shelfUsers, progresses, users, childPlanets, courses ]) {
+    this.course = this.coursesService.getCourseNameFromId(this.courseId);
+    const planets = [ { doc: this.stateService.configuration }, ...attachNamesToPlanets(childPlanets) ];
+    this.members = users.map((user: any) => ({
+        ...user,
+        activityDates: this.userProgress(progresses.filter(
+          (progress: any) => progress.createdOn === user.doc.planetCode && progress.userId === (user.doc.couchId || user._id))
+        ),
+        planet: planets.find(planet => planet.doc.code === user.doc.planetCode)
+      })).filter(doc => doc.activityDates.createdDate || shelfUsers.find((u: any) => u._id === doc._id));
+    this.emptyData = this.members.length === 0;
   }
 
 }
