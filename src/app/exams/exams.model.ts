2eb4cf1918535a0fd7c2531dd562e366cdcea2b8
Paul <paul.r.albert@gmail.com>
2020-04-07 13:36:36 -0400

    Exams: Add data model

---
+++ src/app/exams/exams.model.ts
@@ -0,0 +1,20 @@
+export interface Exam {
+  '_id'?: string;
+  '_rev'?: string;
+  createdDate: number;
+  createdBy: string;
+  name: string;
+  passingPercentage: number;
+  questions: ExamQuestion[];
+  type: 'courses' | 'survey';
+  updatedDate: number;
+  sourcePlanet: string;
+}
+
+export interface ExamQuestion {
+  body: string;
+  type: 'input' | 'textarea' | 'select' | 'selectMultiple';
+  correctChoice: string[];
+  marks: number;
+  choices: { text: string, id: string };
+}
