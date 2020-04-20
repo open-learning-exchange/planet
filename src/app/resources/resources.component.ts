cd0eb847932460eaba9970befdf9736649d6c2b4
Robin Giri <45058288+robinGiri@users.noreply.github.com>
2020-04-21 01:07:24 +0545

    Resource: Single resource delete message (fixes #6144) (#6154)
    Co-authored-by: Paul <paul.r.albert@gmail.com>

--- src/app/resources/resources.component.ts
+++ src/app/resources/resources.component.ts
@@ -198,7 +198,7 @@ export class ResourcesComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   deleteClick(resource) {
-    this.openDeleteDialog(this.deleteResource(resource), 'single', resource.title, 1);
+    this.openDeleteDialog(this.deleteResource(resource), 'single', resource.doc.title, 1);
   }
 
   deleteSelected() {
@@ -210,7 +210,7 @@ export class ResourcesComponent implements OnInit, AfterViewInit, OnDestroy {
       const resource: any = resources[0];
       amount = 'single';
       okClick = this.deleteResource(resource);
-      displayName = resource.title;
+      displayName = resource.doc.title;
     }
     this.openDeleteDialog(okClick, amount, displayName, resources.length);
   }
