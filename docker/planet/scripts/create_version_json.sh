a427bf713d045f0219674c733183d8c25c1fa93d
Paul <paul.r.albert@gmail.com>
2020-04-20 15:26:57 -0400

    Version 0.10.49 (#6152)
    Co-authored-by: Leonard Maximus Irenous Mensah <leomaxi@outlook.com>

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -1,9 +1,9 @@
 PLANET_VERSION=$(cat package.json | jq -r .version)
-LATEST_APK_VERSION="v0.7.52"
-LATEST_APK_VER_CODE="752"
-MIN_APK_VERSION="v0.7.48"
-MIN_APK_VER_CODE="748"
-APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.7.52/myPlanet.apk"
+LATEST_APK_VERSION="v0.7.57"
+LATEST_APK_VER_CODE="757"
+MIN_APK_VERSION="v0.7.52"
+MIN_APK_VER_CODE="752"
+APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.7.57/myPlanet.apk"
 LOCAL_APK_PATH="/fs/myPlanet.apk"
 
 echo '{"appname":"planet","planetVersion":"'$PLANET_VERSION'","latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"apkpath":"'$APK_PATH'","localapkpath":"'$LOCAL_APK_PATH'"}' > /usr/share/nginx/html/versions
