33b21daf4a81085d15dda6832d9c912e993dff4e
Paul <paul.r.albert@gmail.com>
2020-04-20 13:36:42 -0400

    NewsListItem: Wrap text around images (fixes #6148) (#6150)

--- src/app/shared/planet-markdown.component.ts
+++ src/app/shared/planet-markdown.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'planet-markdown',
-  template: '<td-markdown [content]="content" [hostedUrl]="couchAddress"></td-markdown>'
+  template: '<td-markdown [content]="content" [hostedUrl]="couchAddress"></td-markdown>',
+  styleUrls: [ './planet-markdown.scss' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class PlanetMarkdownComponent {
 
