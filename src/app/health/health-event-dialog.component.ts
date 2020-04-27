3e108bd28faf2bcfd89e8431ae1a59e0fc5dd5bf
robinGiri <girirobin2018@gmail.com>
2020-04-27 13:37:16 +0545

    myHealth: Conditions sorted alphabetically (fixes #6190)

--- src/app/health/health-event-dialog.component.ts
+++ src/app/health/health-event-dialog.component.ts
@@ -15,7 +15,7 @@ export class HealthEventDialogComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.event = this.data.event || {};
     this.conditions = Object.entries(this.event.conditions || {})
-      .filter(([ condition, active ]) => active).map(([ condition, active ]) => condition).join(', ');
+      .filter(([ condition, active ]) => active).map(([ condition, active ]) => condition).sort().join(', ');
     this.hasConditionAndTreatment = this.conditionAndTreatmentFields.some(field => this.event[field] !== '');
   }
 
