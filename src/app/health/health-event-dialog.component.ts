9e4006d7ea779c0d11eca9952d38b40b9b78ea0d
Amit Gyawali <gyawaliamit7@gmail.com>
2020-04-27 13:11:17 -0500

    HealthEventDialog: Only show vitals field if data (fixes #6165) (#6199)
    Co-authored-by: Paul <paul.r.albert@gmail.com>

--- src/app/health/health-event-dialog.component.ts
+++ src/app/health/health-event-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { conditionAndTreatmentFields } from './health.constants';
+import { conditionAndTreatmentFields, vitals } from './health.constants';
 
 @Component({
   templateUrl: './health-event-dialog.component.html'
@@ -11,12 +11,14 @@ export class HealthEventDialogComponent {
   hasConditionAndTreatment = false;
   conditionAndTreatmentFields = conditionAndTreatmentFields;
   conditions: string;
+  hasVital = false;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.event = this.data.event || {};
     this.conditions = Object.entries(this.event.conditions || {})
       .filter(([ condition, active ]) => active).map(([ condition, active ]) => condition).join(', ');
     this.hasConditionAndTreatment = this.conditionAndTreatmentFields.some(field => this.event[field] !== '');
+    this.hasVital = vitals.some(vital => this.event[vital]);
   }
 
 }
