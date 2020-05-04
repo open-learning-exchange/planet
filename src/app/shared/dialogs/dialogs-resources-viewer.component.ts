8701d8cfdea02b19e50187dcacad71db0d02b1e3
Amit Gyawali <gyawaliamit7@gmail.com>
2020-05-04 00:08:01 -0500

    DialogsResourcesViewer: View resource button (fixes #6121) (#6181)
    Co-authored-by: Paul <paul.r.albert@gmail.com>

--- src/app/shared/dialogs/dialogs-resources-viewer.component.ts
+++ src/app/shared/dialogs/dialogs-resources-viewer.component.ts
@@ -1,11 +1,33 @@
+import { Router, ActivatedRoute } from '@angular/router';
 import { Component, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
-  template: `<planet-resources-viewer [isDialog]="true" [resourceId]="data.resourceId"></planet-resources-viewer>`
+  template: `
+    <div mat-dialog-title>
+      <button mat-dialog-close mat-icon-button class="close-dialog" i18n>
+        <mat-icon>clear</mat-icon>
+      </button>
+    </div>
+    <planet-resources-viewer [isDialog]="true" [resourceId]="data.resourceId"></planet-resources-viewer>
+    <mat-dialog-actions style="float:right">
+      <button mat-raised-button color="primary" (click)="viewResources()">View Resource</button>
+    </mat-dialog-actions>
+  `,
+  styleUrls: [ './dialogs-resources-viewer.scss' ]
 })
 export class DialogsResourcesViewerComponent {
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private dialogRef: MatDialogRef<DialogsResourcesViewerComponent>,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  viewResources() {
+    this.dialogRef.close();
+    this.router.navigate([ `/resources/view/${this.data.resourceId}` ], { relativeTo: this.route });
+  }
 
 }
