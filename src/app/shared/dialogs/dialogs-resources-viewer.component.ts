eaf410a0bed0ee63099e1b27f71bc40446e334a1
Haoran Hu <travishhr1025@hotmail.com>
2020-05-07 16:36:55 -0400

    Add close button (fixes #6250)

--- src/app/shared/dialogs/dialogs-resources-viewer.component.ts
+++ src/app/shared/dialogs/dialogs-resources-viewer.component.ts
@@ -4,13 +4,9 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 @Component({
   template: `
-    <div mat-dialog-title>
-      <button mat-dialog-close mat-icon-button class="close-dialog" i18n>
-        <mat-icon>clear</mat-icon>
-      </button>
-    </div>
     <planet-resources-viewer [isDialog]="true" [resourceId]="data.resourceId"></planet-resources-viewer>
     <mat-dialog-actions style="float:right">
+      <button mat-dialog-close mat-raised-button i18n>Close</button>
       <button mat-raised-button color="primary" (click)="viewResources()">View Resource</button>
     </mat-dialog-actions>
   `,
