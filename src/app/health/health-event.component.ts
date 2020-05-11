61bc17a2344a986106c69aaacf141d4f86694bc8
Paul <paul.r.albert@gmail.com>
2020-05-11 00:37:24 -0400

    Fix submit button enabled for empty form

--- src/app/health/health-event.component.ts
+++ src/app/health/health-event.component.ts
@@ -65,7 +65,8 @@ export class HealthEventComponent {
   }
 
   isEmptyForm() {
-    return Object.values(this.healthForm.controls).every(control => control.value === '');
+    const isConditionsEmpty = (values) => typeof values === 'object' && Object.values(values).every(value => !value);
+    return Object.values(this.healthForm.controls).every(({ value }) => value === '' || value === null || isConditionsEmpty(value));
   }
 
   goBack() {
