9953e9eb4200e62176c8d3ea930e607bc6ed2ad2
Paul <paul.r.albert@gmail.com>
2020-05-27 23:49:44 -0400

    Truncate decimals before rounding

--- src/app/shared/forms/planet-round.directive.ts
+++ src/app/shared/forms/planet-round.directive.ts
@@ -1,6 +1,5 @@
-import { Directive, Input, HostListener } from '@angular/core';
+import { Directive, Input, HostListener, ElementRef } from '@angular/core';
 import { NgControl } from '@angular/forms';
-import { round } from 'mathjs';
 
 @Directive({
   selector: 'input[planetRound]'
@@ -10,15 +9,18 @@ export class PlanetRoundDirective {
   @Input('planetRound') precision;
 
   constructor(
-    private ngControl: NgControl
+    private ngControl: NgControl,
+    private elementRef: ElementRef
   ) {}
 
   @HostListener('focusout') onFocusOut() {
     if (typeof this.ngControl.value !== 'number') {
       return;
     }
+    const [ integer, decimals ] = this.elementRef.nativeElement.value.split('.');
+    const value = [ integer, (decimals || '0').substring(0, 10) ].join('.');
     const precision = this.precision || 0;
-    const rounded = round(+((this.ngControl.value + Number.EPSILON) + `e${precision}`)) + `e${-precision}`;
+    const rounded = Math.round(+((+value + Number.EPSILON) + `e${precision}`)) + `e${-precision}`;
     this.ngControl.control.setValue(+rounded);
   }
 
