89581b146f983a3d53b62bb48b2ed2dbd402fce7
Robin Giri <45058288+robinGiri@users.noreply.github.com>
2020-05-28 10:27:10 +0545

    HealthEvent: Fix rounding lots of digits (fixes #6391) (#6404)
    Co-authored-by: Paul <paul.r.albert@gmail.com>

--- src/app/shared/forms/planet-round.directive.ts
+++ src/app/shared/forms/planet-round.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Input, HostListener } from '@angular/core';
+import { Directive, Input, HostListener, ElementRef } from '@angular/core';
 import { NgControl } from '@angular/forms';
 
 @Directive({
@@ -6,18 +6,26 @@ import { NgControl } from '@angular/forms';
 })
 export class PlanetRoundDirective {
 
+  // Maximum precision value is 9. Values are truncated before rounding to avoid incorrect values.
   @Input('planetRound') precision;
 
   constructor(
-    private ngControl: NgControl
+    private ngControl: NgControl,
+    private elementRef: ElementRef
   ) {}
 
   @HostListener('focusout') onFocusOut() {
     if (typeof this.ngControl.value !== 'number' || this.ngControl.value === 0 ) {
       return;
     }
+    if (this.precision > 9) {
+      console.error('planetRound has a maximum precision of 9. Numbers will not round correctly if precision set above 9');
+      return;
+    }
+    const [ integer, decimals ] = this.elementRef.nativeElement.value.split('.');
+    const value = [ integer, (decimals || '0').substring(0, 10) ].join('.');
     const precision = this.precision || 0;
-    const rounded = Math.round(+((this.ngControl.value + Number.EPSILON) + `e${precision}`)) + `e${-precision}`;
+    const rounded = Math.round(+((+value + Number.EPSILON) + `e${precision}`)) + `e${-precision}`;
     this.ngControl.control.setValue(+rounded);
   }
 
