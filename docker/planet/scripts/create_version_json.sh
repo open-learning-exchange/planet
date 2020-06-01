a1e072aaa45c247e466bd10a496ec1c9869bc196
Paul <paul.r.albert@gmail.com>
2020-06-01 11:53:24 -0400

    Fix APK path

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -3,7 +3,7 @@ LATEST_APK_VERSION="v0.7.88"
 LATEST_APK_VER_CODE="788"
 MIN_APK_VERSION="v0.7.82"
 MIN_APK_VER_CODE="782"
-APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.7.82/myPlanet.apk"
+APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.7.88/myPlanet.apk"
 LOCAL_APK_PATH="/fs/myPlanet.apk"
 
 echo '{"appname":"planet","planetVersion":"'$PLANET_VERSION'","latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"apkpath":"'$APK_PATH'","localapkpath":"'$LOCAL_APK_PATH'"}' > /usr/share/nginx/html/versions
