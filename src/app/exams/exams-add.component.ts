18b0572e6128ca346200fdbb030a2face2260f7c
Mutugi <48474421+Mutugiii@users.noreply.github.com>
2020-06-08 23:00:52 +0300

    ExamsAdd: Fix "Finish Survey" button text in preview (fixes #5955) (#6445)

--- src/app/exams/exams-add.component.ts
+++ src/app/exams/exams-add.component.ts
@@ -216,7 +216,7 @@ export class ExamsAddComponent implements OnInit {
       this.showErrorMessage();
       return;
     }
-    this.dialog.open(ExamsPreviewComponent, { data: { exam: this.examForm.value }, minWidth: '75vw' });
+    this.dialog.open(ExamsPreviewComponent, { data: { exam: this.examForm.value, examType: this.examType }, minWidth: '75vw' });
   }
 
 }
