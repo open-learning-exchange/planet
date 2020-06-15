6b4a8dac30735df1021b51b38854e5eb1894a935
Haoran Hu <57075986+Haoran-Hu@users.noreply.github.com>
2020-06-15 17:22:28 -0400

    HealthEvent: Show provider name (fixes #6515) (#6548)
    Co-authored-by: Paul <paul.r.albert@gmail.com>

--- src/app/health/health-event-dialog.component.ts
+++ src/app/health/health-event-dialog.component.ts
@@ -1,13 +1,13 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { conditionAndTreatmentFields, vitals } from './health.constants';
-import { HealthService } from './health.service';
 import { Router } from '@angular/router';
+import { UsersService } from '../users/users.service';
 
 @Component({
   templateUrl: './health-event-dialog.component.html'
 })
-export class HealthEventDialogComponent {
+export class HealthEventDialogComponent implements OnInit {
 
   event: any;
   hasConditionAndTreatment = false;
@@ -15,10 +15,12 @@ export class HealthEventDialogComponent {
   conditions: string;
   hasVital = false;
   canUpdate: any;
+  performedBy = '';
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private router: Router
+    private router: Router,
+    private usersService: UsersService
   ) {
     this.event = this.data.event || {};
     this.canUpdate = (new Date(Date.now()).getTime() - new Date(this.event.date).getTime()) <= 300000,
@@ -28,6 +30,16 @@ export class HealthEventDialogComponent {
       this.event.hasInfo === true :
       this.conditionAndTreatmentFields.some(field => this.event[field] !== '');
     this.hasVital = vitals.some(vital => this.event[vital]);
+  }
+
+  ngOnInit() {
+    this.usersService.usersListener(true).subscribe(users => {
+      const user = users.find(u => u._id === this.event.createdBy);
+      this.performedBy = user.fullName;
+    });
+    if (!this.event.selfExamination) {
+      this.usersService.requestUsers();
+    }
   }
 
   editExam(event) {
