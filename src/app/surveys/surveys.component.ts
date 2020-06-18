aed844e18939d51be6a33d293e6807f21d1d5114
robinGiri <girirobin2018@gmail.com>
2020-06-18 14:38:50 +0545

    Surveys: Exclude send member from dialog (fixes #6563)

--- src/app/surveys/surveys.component.ts
+++ src/app/surveys/surveys.component.ts
@@ -69,6 +69,7 @@ export class SurveysComponent implements OnInit, AfterViewInit, OnDestroy {
   message = '';
   configuration = this.stateService.configuration;
   parentCount = 0;
+  excludeIds: any[];
 
   constructor(
     private couchService: CouchService,
@@ -245,14 +246,20 @@ export class SurveysComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   openSendSurveyDialog(survey) {
-    this.dialogRef = this.dialog.open(DialogsAddTableComponent, {
-      width: '80vw',
-      data: {
-        okClick: (selection: any[]) => this.sendSurvey(survey, selection.map(item => item.doc)),
-        excludeIds: [ this.userService.get()._id ],
-        mode: 'users'
-      }
-    });
+    this.submissionsService.getSubmissions(
+      findDocuments({ type: 'survey', 'parent._rev': survey._rev, 'parent._id': survey._id }))
+        .subscribe((submissions: any[]) => {
+          this.excludeIds = submissions.map((submission: any) => submission.user._id);
+          this.dialogRef = this.dialog.open(DialogsAddTableComponent, {
+            width: '80vw',
+            data: {
+              okClick: (selection: any[]) => this.sendSurvey(survey, selection.map(item => item.doc)),
+              excludeIds: [ ...this.excludeIds, this.userService.get()._id ],
+              mode: 'users'
+            }
+          });
+        }
+    );
   }
 
   sendSurvey(survey: any, users: any[]) {
