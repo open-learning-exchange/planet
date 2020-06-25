84ec1fa4ec7ac60e15b3f75fe7cf7782bd47e10c
Paul <paul.r.albert@gmail.com>
2020-06-25 00:49:18 -0400

    Fix initial property value

--- src/app/manager-dashboard/reports/reports-detail.component.ts
+++ src/app/manager-dashboard/reports/reports-detail.component.ts
@@ -44,7 +44,7 @@ export class ReportsDetailComponent implements OnInit, OnDestroy {
   loginActivities = new ReportsDetailData('loginTime');
   resourceActivities = { byDoc: [], total: new ReportsDetailData('time') };
   courseActivities = { byDoc: [], total: new ReportsDetailData('time') };
-  progress = { enrollments: new ReportsDetailData('time'), completions: new ReportsDetailData('time'), steps: [] };
+  progress = { enrollments: new ReportsDetailData('time'), completions: new ReportsDetailData('time') };
   today: Date;
   minDate: Date;
   ratings = { total: new ReportsDetailData('time'), resources: [], courses: [] };
