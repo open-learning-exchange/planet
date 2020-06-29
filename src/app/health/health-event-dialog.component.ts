328a1fc481335e6fabc08293a2e0facf1f8a2c49
Josh Sims <54686871+jsschf@users.noreply.github.com>
2020-06-29 18:30:21 +0000

    HealthEventDialog: Countdown for "Edit" button (fixes #6571) (#6614)
    Co-authored-by: Paul <paul.r.albert@gmail.com>

--- src/app/health/health-event-dialog.component.ts
+++ src/app/health/health-event-dialog.component.ts
@@ -1,13 +1,16 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, OnDestroy } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { conditionAndTreatmentFields, vitals } from './health.constants';
 import { Router } from '@angular/router';
+import { timer, of, combineLatest } from 'rxjs';
+import { switchMap, takeWhile } from 'rxjs/operators';
 import { UsersService } from '../users/users.service';
+import { CouchService } from '../shared/couchdb.service';
 
 @Component({
   templateUrl: './health-event-dialog.component.html'
 })
-export class HealthEventDialogComponent implements OnInit {
+export class HealthEventDialogComponent implements OnInit, OnDestroy {
 
   event: any;
   hasConditionAndTreatment = false;
@@ -16,20 +19,25 @@ export class HealthEventDialogComponent implements OnInit {
   hasVital = false;
   canUpdate: any;
   performedBy = '';
+  minutes: string;
+  seconds: string;
+  timeLimit = 300000;
+  isDestroyed = false;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private router: Router,
-    private usersService: UsersService
+    private usersService: UsersService,
+    private couchService: CouchService
   ) {
     this.event = this.data.event || {};
-    this.canUpdate = (new Date(Date.now()).getTime() - new Date(this.event.updatedDate).getTime()) <= 300000,
     this.conditions = Object.entries(this.event.conditions || {})
       .filter(([ condition, active ]) => active).map(([ condition, active ]) => condition).sort().join(', ');
     this.hasConditionAndTreatment = this.event.hasInfo !== undefined ?
       this.event.hasInfo === true :
       this.conditionAndTreatmentFields.some(field => this.event[field] !== '');
     this.hasVital = vitals.some(vital => this.event[vital]);
+    this.editButtonCountdown();
   }
 
   ngOnInit() {
@@ -42,8 +50,29 @@ export class HealthEventDialogComponent implements OnInit {
     }
   }
 
+  ngOnDestroy() {
+    this.isDestroyed = true;
+  }
+
   editExam(event) {
     this.router.navigate([ 'event', { id: this.data.user, eventId: event._id } ], { relativeTo: this.data.route });
+  }
+
+  editButtonCountdown() {
+    this.couchService.currentTime().pipe(
+      switchMap((currentTime: number) => combineLatest(of(currentTime), timer(0, 1000))),
+      takeWhile(([ time, seconds ]) => {
+        const millisecondsLeft = this.timeLimit + this.event.date - ((seconds * 1000) + time);
+        this.setTimerValue(millisecondsLeft / 1000);
+        return millisecondsLeft > 0 && !this.isDestroyed;
+      })
+    ).subscribe(() => this.canUpdate = true, () => {}, () => this.canUpdate = false);
+  }
+
+  setTimerValue(secondsLeft) {
+    const seconds = Math.floor(secondsLeft % 60).toString();
+    this.minutes = Math.floor(secondsLeft / 60).toString();
+    this.seconds = parseInt(seconds, 10) < 10 ? '0' + seconds : seconds;
   }
 
 }
