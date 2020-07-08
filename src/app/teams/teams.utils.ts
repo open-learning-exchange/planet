994f4247d421ddd572a5c426e77133233ee39d75
Haoran Hu <travishhr1025@hotmail.com>
2020-07-07 23:09:47 -0400

    Sort members alphabetically (fixes #6754)

--- src/app/teams/teams.utils.ts
+++ src/app/teams/teams.utils.ts
@@ -1,10 +1,10 @@
 export const memberCompare = (member1, member2) => member1.userId === member2.userId && member1.userPlanetCode === member2.userPlanetCode;
 
-export const compareFunc = (member1, member2) => {
+export function compareFunc(member1, member2) {
   const name1 = member1.userDoc.doc.lastName;
   const name2 = member2.userDoc.doc.lastName;
   if (name1 && name2) {
     return name1 < name2 ? -1 : 1;
   }
   return member1.userDoc._id < member2.userDoc._id ? -1 : 1;
-};
+}
