e2b940c4405972525293be8add46cc679a3ac9ee
Paul <paul.r.albert@gmail.com>
2020-07-10 14:43:14 -0400

    Fix build error

--- src/app/shared/planet-markdown.component.ts
+++ src/app/shared/planet-markdown.component.ts
@@ -9,7 +9,7 @@ import { environment } from '../../environments/environment';
 })
 export class PlanetMarkdownComponent {
 
-  @Input() content: string | { text: string };
+  @Input() content: any;
   couchAddress = `${environment.couchAddress}/`;
 
 }
