7f1fa79536726f4ba64c741bec83663dd60bde88
Paul <paul.r.albert@gmail.com>
2020-07-13 14:05:14 -0400

    UsersProfile: Correct visits on nation

--- src/app/users/users-profile/users-profile.component.ts
+++ src/app/users/users-profile/users-profile.component.ts
@@ -67,7 +67,8 @@ export class UsersProfileComponent implements OnInit, OnDestroy {
   }
 
   getLoginInfo(name) {
-    this.couchService.findAll('login_activities', findDocuments({ 'user': name }, 0, [ { 'loginTime': 'desc' } ]))
+    const createdOn = this.planetCode || this.stateService.configuration.code;
+    this.couchService.findAll('login_activities', findDocuments({ 'user': name, createdOn }, 0, [ { 'loginTime': 'desc' } ]))
     .subscribe((logins: any) => {
       this.totalLogins = logins.length;
       this.lastLogin = logins.length ? logins[0].loginTime : '';
