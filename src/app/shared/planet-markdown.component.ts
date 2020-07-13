384d2a9fb4cada72dd082094b8f78930949ed596
Paul <paul.r.albert@gmail.com>
2020-07-13 12:10:47 -0400

    PlanetMarkdown: Revert to content always string

--- src/app/shared/planet-markdown.component.ts
+++ src/app/shared/planet-markdown.component.ts
@@ -3,13 +3,13 @@ import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'planet-markdown',
-  template: '<td-markdown [content]="content?.text || content" [hostedUrl]="couchAddress"></td-markdown>',
+  template: '<td-markdown [content]="content" [hostedUrl]="couchAddress"></td-markdown>',
   styleUrls: [ './planet-markdown.scss' ],
   encapsulation: ViewEncapsulation.None
 })
 export class PlanetMarkdownComponent {
 
-  @Input() content: any;
+  @Input() content: string;
   couchAddress = `${environment.couchAddress}/`;
 
 }
