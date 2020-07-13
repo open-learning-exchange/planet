afc19294971d7f0df65d7d80d28ed2e9c8a2ea23
Paul <paul.r.albert@gmail.com>
2020-07-13 16:14:53 -0400

    Remove org.user.couchdb and return all sort values

--- src/app/teams/teams.utils.ts
+++ src/app/teams/teams.utils.ts
@@ -1,7 +1,12 @@
 export const memberCompare = (member1, member2) => member1.userId === member2.userId && member1.userPlanetCode === member2.userPlanetCode;
 
-export const nameCompare = (member1, member2) => {
-  const name1 = member1.userDoc && member1.userDoc.doc.lastName ? member1.userDoc.doc.lastName : member1.userId;
-  const name2 = member2.userDoc && member2.userDoc.doc.lastName ? member2.userDoc.doc.lastName : member2.userId;
-  return name1.localeCompare(name2);
+export const memberNameCompare = (member1, member2) => {
+  const memberName = (member) => (member.userDoc && member.userDoc.doc.lastName) || member.userId.split(':')[1];
+  return memberName(member1).localeCompare(memberName(member2));
 };
+
+export const memberSort = (member1, member2, leader) => memberCompare(member1, leader) ?
+  -1 :
+  memberCompare(member2, leader) ?
+  1 :
+  memberNameCompare(member1, member2);
