b44e748b2c4d78869d7e5c43eedf1aa12d233991
mutugiii <mutugimutuma@gmail.com>
2020-07-16 09:54:03 +0300

    Update the router to profile view

--- src/app/users/users-profile/users-profile-dialog.component.ts
+++ src/app/users/users-profile/users-profile-dialog.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { UserService } from '../../shared/user.service';
+import { UsersProfileComponent } from './users-profile.component';
 
 @Component({
   template: `
@@ -11,24 +11,20 @@ import { UserService } from '../../shared/user.service';
       <planet-users-profile [isDialog]="true" [userName]="name" [planetCode]="planetCode"></planet-users-profile>
     </mat-dialog-content>
     <mat-dialog-actions>
-      <button color="primary" *ngIf="!isHidden" routerLink="/manager/users" mat-raised-button mat-dialog-close i18n>Manager Settings</button>
+      <button color="primary" *ngIf="usersProfileComponent?.user?.isUserAdmin" routerLink="/users/profile/{{usersProfileComponent.user.name}}" mat-raised-button mat-dialog-close i18n>View full profile</button>
       <button mat-raised-button mat-dialog-close i18n>Close</button>
     </mat-dialog-actions>
   `
 })
 export class UserProfileDialogComponent {
 
+  @ViewChild(UsersProfileComponent, { static: false }) usersProfileComponent: UsersProfileComponent;
   name: string;
   planetCode: string;
-  isHidden: boolean;
 
-  constructor(
-  @Inject(MAT_DIALOG_DATA) public data: any,
-  private userService: UserService
-  ) {
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.name = data.member.name;
     this.planetCode = data.member.userPlanetCode;
-    const currentUser = this.userService.get();
-    this.isHidden = !currentUser.isUserAdmin;
   }
+
 }
