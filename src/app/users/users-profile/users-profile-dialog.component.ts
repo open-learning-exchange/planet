466197324f49c509964867cdb259b142498c143c
Mutugi <48474421+Mutugiii@users.noreply.github.com>
2020-07-20 23:01:29 +0300

    UsersProfileDialog: Link to profile for admins/self (fixes #6740) (#6745)
    Co-authored-by: Paul <paul.r.albert@gmail.com>

--- src/app/users/users-profile/users-profile-dialog.component.ts
+++ src/app/users/users-profile/users-profile-dialog.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, ViewChild, AfterViewInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
+import { UsersProfileComponent } from './users-profile.component';
 
 @Component({
   template: `
@@ -10,18 +11,25 @@ import { MAT_DIALOG_DATA } from '@angular/material';
       <planet-users-profile [isDialog]="true" [userName]="name" [planetCode]="planetCode"></planet-users-profile>
     </mat-dialog-content>
     <mat-dialog-actions>
+      <button color="primary" *ngIf="editable" routerLink="/users/profile/{{usersProfileComponent.user.name}}" mat-raised-button mat-dialog-close i18n>View full profile</button>
       <button mat-raised-button mat-dialog-close i18n>Close</button>
     </mat-dialog-actions>
   `
 })
-export class UserProfileDialogComponent {
+export class UserProfileDialogComponent implements AfterViewInit {
 
+  @ViewChild(UsersProfileComponent, { static: false }) usersProfileComponent: UsersProfileComponent;
   name: string;
   planetCode: string;
+  editable = false;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.name = data.member.name;
     this.planetCode = data.member.userPlanetCode;
+  }
+
+  ngAfterViewInit() {
+    this.editable = this.usersProfileComponent && this.usersProfileComponent.editable;
   }
 
 }
