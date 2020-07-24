7ff2e9ea204dee5dcf3ecea844a160e4e1b63a25
Paul <paul.r.albert@gmail.com>
2020-07-24 13:15:16 -0400

    TeamsReports: Initialize start & end date to last month (fixes #6909) (#6936)

--- src/app/teams/teams-reports.component.ts
+++ src/app/teams/teams-reports.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, Output, EventEmitter, OnChanges } from '@angular/core
 import { MatDialog } from '@angular/material';
 import { DialogsFormService } from '../shared/dialogs/dialogs-form.service';
 import { CustomValidators } from '../validators/custom-validators';
+import { CouchService } from '../shared/couchdb.service';
 import { TeamsService } from './teams.service';
 import { DialogsLoadingService } from '../shared/dialogs/dialogs-loading.service';
 import { TeamsReportsDialogComponent } from './teams-reports-dialog.component';
@@ -22,6 +23,7 @@ export class TeamsReportsComponent implements OnChanges {
   rowHeight = '300px';
 
   constructor(
+    private couchService: CouchService,
     private dialog: MatDialog,
     private dialogsFormService: DialogsFormService,
     private dialogsLoadingService: DialogsLoadingService,
@@ -35,21 +37,26 @@ export class TeamsReportsComponent implements OnChanges {
   }
 
   openAddReportDialog(oldReport = {}) {
-    this.dialogsFormService.openDialogsForm(
-      'Add Report',
-      [
-        { name: 'startDate', placeholder: 'Start Date', type: 'date', required: true },
-        { name: 'endDate', placeholder: 'End Date', type: 'date', required: true },
-        { name: 'description', placeholder: 'Summary', type: 'markdown', required: true },
-        { name: 'beginningBalance', placeholder: 'Beginning Balance', type: 'textbox', inputType: 'number' },
-        { name: 'sales', placeholder: 'Sales', type: 'textbox', inputType: 'number' },
-        { name: 'otherIncome', placeholder: 'Other Income', type: 'textbox', inputType: 'number' },
-        { name: 'wages', placeholder: 'Personnel', type: 'textbox', inputType: 'number' },
-        { name: 'otherExpenses', placeholder: 'Non-Personnel', type: 'textbox', inputType: 'number' }
-      ],
-      this.addFormInitialValues(oldReport),
-      { onSubmit: (newReport) => this.updateReport(oldReport, newReport) }
-    );
+    this.couchService.currentTime().subscribe((time: number) => {
+      const currentDate = new Date(time);
+      const lastMonthStart = new Date(currentDate.getFullYear(), currentDate.getMonth() - 1, 1);
+      const lastMonthEnd = currentDate.setDate(0);
+      this.dialogsFormService.openDialogsForm(
+        'Add Report',
+        [
+          { name: 'startDate', placeholder: 'Start Date', type: 'date', required: true },
+          { name: 'endDate', placeholder: 'End Date', type: 'date', required: true },
+          { name: 'description', placeholder: 'Summary', type: 'markdown', required: true },
+          { name: 'beginningBalance', placeholder: 'Beginning Balance', type: 'textbox', inputType: 'number' },
+          { name: 'sales', placeholder: 'Sales', type: 'textbox', inputType: 'number' },
+          { name: 'otherIncome', placeholder: 'Other Income', type: 'textbox', inputType: 'number' },
+          { name: 'wages', placeholder: 'Personnel', type: 'textbox', inputType: 'number' },
+          { name: 'otherExpenses', placeholder: 'Non-Personnel', type: 'textbox', inputType: 'number' }
+        ],
+        this.addFormInitialValues(oldReport, { startDate: lastMonthStart, endDate: lastMonthEnd }),
+        { onSubmit: (newReport) => this.updateReport(oldReport, newReport) }
+      );
+    });
   }
 
   openDeleteReportDialog(report) {
@@ -62,7 +69,7 @@ export class TeamsReportsComponent implements OnChanges {
     });
   }
 
-  addFormInitialValues(oldReport) {
+  addFormInitialValues(oldReport, { startDate, endDate }) {
     const initialValues = {
       description: '',
       beginningBalance: 0,
@@ -71,12 +78,12 @@ export class TeamsReportsComponent implements OnChanges {
       wages: 0,
       otherExpenses: 0,
       ...oldReport,
-      startDate: new Date(oldReport.startDate || Date.now()),
-      endDate: new Date(oldReport.endDate || Date.now())
+      startDate: new Date(oldReport.startDate || startDate),
+      endDate: new Date(oldReport.endDate || endDate)
     };
-    const formControl = (initialValue, endDate = false) => [
+    const formControl = (initialValue, isEndDate = false) => [
       initialValue,
-      [ CustomValidators.required, endDate ? CustomValidators.endDateValidator : () => {} ]
+      [ CustomValidators.required, isEndDate ? CustomValidators.endDateValidator : () => {} ]
     ];
     return Object.entries(initialValues).reduce(
       (formObj, [ key, value ]) => ({ ...formObj, [key]: formControl(value, key === 'endDate') }), {}
