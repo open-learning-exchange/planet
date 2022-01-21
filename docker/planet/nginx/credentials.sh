49731b0248d605b6f1f6f96bd741fb2247b5a5f6
Laxman Maharjan <email@mlaxman.com.np>
2022-01-21 12:36:19 +0545

    nginx proxy for public db

--- docker/planet/nginx/credentials.sh
+++ docker/planet/nginx/credentials.sh
@@ -7,6 +7,7 @@ YML_PATH=credentials/credentials.yml
 PLANET_USER=${PLANET_CREDENTIALS%%,*}
 PLANET_PASS=${PLANET_CREDENTIALS##*,}
 PLANET_PASS=$(echo $PLANET_PASS | sed -e 's/\$/\$\$/g')
+BASE_PASS = $(echo -n $PLANET_USER:$PLANET_PASS | base64)
 if [ -f "$YML_PATH" ]; then
   OLD_USER=$(grep COUCHDB_USER $YML_PATH | sed -e 's/.*=//')
 fi
@@ -21,4 +22,9 @@ if [ -z "$OLD_USER" ] || [ "$PLANET_USER" == "$OLD_USER" ]; then
     echo "      - COUCHDB_PASS=$PLANET_PASS"
     echo "version: \"2\""
   } >> $YML_PATH
+
+  {
+    echo "proxy_set_header Authorization \"Basic $BASE_PASS\";"
+  } > /etc/nginx/conf.d/auth.txt
+
 fi
