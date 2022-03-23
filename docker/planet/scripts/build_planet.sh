b444aafdd20499588cdd14e88e5fe345b2bc8c10
Laxman Maharjan <email@mlaxman.com.np>
2022-03-23 13:24:54 +0545

    update flow for build

--- docker/planet/scripts/build_planet.sh
+++ docker/planet/scripts/build_planet.sh
@@ -4,8 +4,8 @@ build_single(){
   LANG=$1
   LANG2=$2
   if [ -z "${LANG}" ]; then
-    npm run ng-high-memory -- build --prod --aot
+    npm run ng-high-memory -- build --prod --aot --localize=true
   else
-    npm run ng-high-memory -- build --prod --aot
+    npm run ng-high-memory -- build --prod --aot --localize=true
   fi
 }
