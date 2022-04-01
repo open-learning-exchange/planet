d8364467f3ceda1c7b856c747c25434c409422c7
Laxman Maharjan <email@mlaxman.com.np>
2022-04-01 21:58:32 +0545

    create endpoint to update credential auth file (#7058)

--- docker/planet/scripts/docker-entrypoint.sh
+++ docker/planet/scripts/docker-entrypoint.sh
@@ -35,4 +35,6 @@ sed -i -e "s#planet-sync-address#$S_ADDRESS#g" /usr/share/nginx/html/**/main*
 spawn-fcgi -s /run/fcgi.sock -U nginx -G nginx /usr/bin/fcgiwrap
 nginx -g "daemon off;"
 
+/usr/share/nginx/html/credentials.sh
+
 mkdir -p /usr/share/nginx/html/fs
