9b993de9f97c6c52cb593fcc8b0615b31701869f
Laxman Maharjan <email@mlaxman.com.np>
2022-04-09 11:04:39 +0545

    credentials update (#7076)

--- docker/planet/nginx/credentials.sh
+++ docker/planet/nginx/credentials.sh
@@ -8,7 +8,8 @@ PLANET_USER=${PLANET_CREDENTIALS%%,*}
 PLANET_PASS=${PLANET_CREDENTIALS##*,}
 PLANET_PASS=$(echo $PLANET_PASS | sed -e 's/\$/\$\$/g')
 
-BASE_PASS = $(echo -n $PLANET_USER:$PLANET_PASS | base64)
+mkdir -p credentials
+
 if [ -f "$YML_PATH" ]; then
   OLD_USER=$(grep COUCHDB_USER $YML_PATH | sed -e 's/.*=//')
   OLD_PASS=$(grep COUCHDB_PASS $YML_PATH | sed -e 's/.*=//')
@@ -21,9 +22,8 @@ function gen_nginx_conf {
   } > /etc/nginx/conf.d/auth.txt
 }
 
-
-if [ -z "$OLD_USER" ] || [ "$PLANET_USER" == "$OLD_USER" ]; then
-  rm credentials/credentials.yml
+if [ ! -z "$PLANET_USER" ] && ([ -z "$OLD_USER" ] || [ "$PLANET_USER" == "$OLD_USER" ]); then
+  rm -f credentials/credentials.yml
   {
     echo "services:"
     echo "  db-init:"
@@ -32,7 +32,7 @@ if [ -z "$OLD_USER" ] || [ "$PLANET_USER" == "$OLD_USER" ]; then
     echo "      - COUCHDB_PASS=$PLANET_PASS"
     echo "version: \"2\""
   } >> $YML_PATH
-  
+
   gen_nginx_conf $PLANET_USER $PLANET_PASS
 else
   gen_nginx_conf $OLD_USER $OLD_PASS
