7a460356cd587c886ba313eff7f19fd85d0a32ee
mutugiii <mutugimutuma@gmail.com>
2023-05-30 18:15:48 +0300

    Add planet form modules and styling to the page

--- src/app/gpt/gpt.component.ts
+++ src/app/gpt/gpt.component.ts
@@ -1,40 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';
-const { Configuration, OpenAIApi } = require('openai');
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { environment } from "../../environments/environment";
+import { CustomValidators } from "../validators/custom-validators";
+import { showFormErrors } from "../shared/table-helpers";
+
+const { Configuration, OpenAIApi } = require("openai");
 
 @Component({
-  selector: 'planet-gpt',
-  templateUrl: './gpt.component.html',
+  selector: "planet-gpt",
+  templateUrl: "./gpt.component.html",
+  styleUrls: ["./gpt.component.scss"],
 })
 export class GptComponent implements OnInit {
+  // spinnerOn=true;
   promptForm: FormGroup;
-  response: string;
+  questions: any[] = [];
   conversations: any[] = [];
 
-  constructor(private formBuilder: FormBuilder, private http: HttpClient) {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
     this.promptForm = this.formBuilder.group({
-      prompt: '',
+      prompt: ["", CustomValidators.requiredMarkdown],
     });
   }
 
+  goBack() {
+    this.router.navigate(["/"], { relativeTo: this.route });
+  }
+
+  async onSubmit() {
+    if (!this.promptForm.valid) {
+      showFormErrors(this.promptForm.controls);
+      return;
+    }
+
+    // this.spinnerOn=true;
+    await this.submitPrompt();
+    // this.spinnerOn=false;
+  }
+
   async submitPrompt() {
-    const configuration = new Configuration({
-      apiKey: environment.openAIKey,
-    });
-    const openai = new OpenAIApi(configuration);
+    try {
+      const configuration = new Configuration({
+        apiKey: environment.openAIKey,
+      });
+      const openai = new OpenAIApi(configuration);
 
-    const content = this.promptForm.get('prompt').value;
-    this.conversations.push({ role: 'user', content });
+      const content = this.promptForm.get("prompt").value;
+      this.questions.push({ role: "user", content });
 
-    const completion = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: this.conversations,
-    });
+      const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: this.questions,
+      });
 
-    this.response = completion.data.choices[0].message.content;
+      this.conversations.push({
+        query: content,
+        response: completion.data.choices[0].message.content,
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
--- src/app/gpt/gpt.module.ts
+++ src/app/gpt/gpt.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MaterialModule } from '../shared/material.module';
+import { SharedComponentsModule } from '../shared/shared-components.module';
+import { PlanetFormsModule } from '../shared/forms/planet-forms.module';
 
 import { GptComponent } from './gpt.component';
 
@@ -11,7 +14,9 @@ import { GptComponent } from './gpt.component';
         CommonModule,
         FormsModule,
         ReactiveFormsModule,
-        MaterialModule
+        MaterialModule,
+        PlanetFormsModule,
+        SharedComponentsModule
     ],
     exports: [ GptComponent ],
     declarations: [ GptComponent ]
