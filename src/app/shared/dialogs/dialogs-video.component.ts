5b2f15b6057e7f89f33f08d6b68cd68f111381e4
mutugiii <mutugimutuma@gmail.com>
2023-07-03 23:59:54 +0300

    Linting fixes

--- src/app/shared/dialogs/dialogs-video.component.ts
+++ src/app/shared/dialogs/dialogs-video.component.ts
@@ -1,6 +1,6 @@
-import { Component, Inject } from "@angular/core";
-import { MAT_DIALOG_DATA } from "@angular/material/dialog";
-import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   template: `
