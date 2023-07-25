3799137f11154c19e71efff84342252c4b58b5ad
Mutugi <48474421+Mutugiii@users.noreply.github.com>
2023-07-25 13:15:29 +0300

    landing: refactor footer component (fixes #7180) (#7181)

--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -9,7 +9,7 @@ import { LandingComponent } from './landing.component';
 import { LandingNavbarComponent } from './landing-nav/landing-nav.component';
 import { LandingHeroComponent } from './landing-hero/landing-hero.component';
 import { LandingHomeComponent } from './landing-home/landing-home.component';
-import { LandingFooterComponent } from './landing-footer/landing-footer.component';
+import { LandingFooterComponent } from './landing-footer.component';
 import { LandingNewsComponent } from './landing-home/landing-news/landing-news.component';
 import { NewsCardItemComponent } from './landing-home/landing-news/newscard-item/newscard-item.component';
 import { NewsItemDetailsComponent } from './landing-home/landing-news/newsitem-detail/newsitem-detail.component';
