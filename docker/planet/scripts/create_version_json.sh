448bb1c4ccc89a1d89c71e766dbe554a48d9305a
dogi <dogi@users.noreply.github.com>
2023-08-02 11:20:42 -0400

    Update create_version_json.sh

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -1,8 +1,8 @@
 PLANET_VERSION=$(cat package.json | jq -r .version)
 LATEST_APK_VERSION="v0.9.55"
 MIN_APK_VERSION="v0.9.30"
-LATEST_APK_VER_CODE=$(echo $LATEST_APK_VERSION | awk -F'[v.]' '{printf "%d%02d%02d\n", $2, $3, $4}')
-MIN_APK_VER_CODE=$(echo $MIN_APK_VERSION | awk -F'[v.]' '{printf "%d%02d%02d\n", $2, $3, $4}')
+LATEST_APK_VER_CODE=$(echo $LATEST_APK_VERSION | awk -F'[v.]' '{printf "%d%02d%02d\n", $2, $3, $4}' | sed 's#^0*##')
+MIN_APK_VER_CODE=$(echo $MIN_APK_VERSION | awk -F'[v.]' '{printf "%d%02d%02d\n", $2, $3, $4}' | sed 's#^0*##')
 APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/"$LATEST_APK_VERSION"/myPlanet.apk"
 LOCAL_APK_PATH="/fs/myPlanet.apk"
 
