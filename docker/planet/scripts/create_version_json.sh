bcc52423097b2cefe6596ed502d0d0a67148c7bb
dogi <dogi@users.noreply.github.com>
2023-08-02 09:58:07 -0400

    Update create_version_json.sh

--- docker/planet/scripts/create_version_json.sh
+++ docker/planet/scripts/create_version_json.sh
@@ -3,7 +3,7 @@ LATEST_APK_VERSION="v0.9.55"
 LATEST_APK_VER_CODE="955"
 MIN_APK_VERSION="v0.9.30"
 MIN_APK_VER_CODE="930"
-APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/v0.8.89/myPlanet.apk"
+APK_PATH="https://github.com/open-learning-exchange/myplanet/releases/download/"$LATEST_APK_VERSION"/myPlanet.apk"
 LOCAL_APK_PATH="/fs/myPlanet.apk"
 
 echo '{"appname":"planet","planetVersion":"'$PLANET_VERSION'","latestapk":"'$LATEST_APK_VERSION'","latestapkcode":'$LATEST_APK_VER_CODE',"minapk":"'$MIN_APK_VERSION'","minapkcode":'$MIN_APK_VER_CODE',"apkpath":"'$APK_PATH'","localapkpath":"'$LOCAL_APK_PATH'"}' > /usr/share/nginx/html/versions
