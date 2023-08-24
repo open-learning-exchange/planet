7e551836ea840fc39d1c157c939097f935e52cfb
Mutugi <48474421+Mutugiii@users.noreply.github.com>
2023-08-24 20:11:44 +0300

    chat: UI update (fixes #7224) (#7225)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -40,7 +40,7 @@ export class ChatComponent implements OnInit {
 
   createForm() {
     this.promptForm = this.formBuilder.group({
-      prompt: [ '', CustomValidators.requiredMarkdown ],
+      prompt: [ '', CustomValidators.required ],
     });
   }
 
