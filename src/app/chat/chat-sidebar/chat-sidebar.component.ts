b27eaf2eac6e24d75fb250e4c04f6fff74916901
mutugiii <mutugimutuma@gmail.com>
2023-10-31 19:53:11 +0300

    Implement editing functionality

--- src/app/chat/chat-sidebar/chat-sidebar.component.ts
+++ src/app/chat/chat-sidebar/chat-sidebar.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { ChatService } from '../../shared/chat.service';
+import { CouchService } from '../../shared/couchdb.service';
+import { showFormErrors } from '../../shared/table-helpers';
 
 @Component({
   selector: 'planet-chat-sidebar',
@@ -7,10 +11,17 @@ import { ChatService } from '../../shared/chat.service';
   styleUrls: [ './chat-sidebar.scss' ],
 })
 export class ChatSidebarComponent implements OnInit {
+  readonly dbName = 'chat_history';
   conversations: any;
   selectedConversation: any;
+  isEditing: boolean;
+  titleForm: { [key: string]: FormGroup } = {};
 
-  constructor(private chatService: ChatService) {}
+  constructor(
+    private chatService: ChatService,
+    private couchService: CouchService,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit() {
     this.getChatHistory();
@@ -26,10 +37,42 @@ export class ChatSidebarComponent implements OnInit {
     this.selectedConversation = null;
   }
 
+  toggleEditTitle() {
+    this.isEditing = !this.isEditing;
+  }
+
+  updateConversation(conversation, title) {
+    this.couchService.updateDocument(
+      this.dbName, { ...conversation, title: title, updatedTime: this.couchService.datePlaceholder }
+    ).subscribe((data) => {
+      this.getChatHistory();
+      return data;
+    });
+  }
+
+  submitTitle(conversation) {
+    if (this.titleForm[conversation._id].valid) {
+      const title = this.titleForm[conversation._id].get('title').value;
+      this.updateConversation(conversation, title);
+      this.toggleEditTitle();
+    } else {
+      showFormErrors(this.titleForm[conversation._id].controls);
+    }
+  }
+
+  initializeFormGroups() {
+    this.conversations.forEach((conversation) => {
+      this.titleForm[conversation._id] = this.formBuilder.group({
+        title: [ conversation?.title, Validators.required ]
+      });
+    });
+  }
+
   getChatHistory() {
     this.chatService.findConversations([], {}).subscribe(
       (conversations) => {
         this.conversations = conversations;
+        this.initializeFormGroups();
       },
       (error) => console.log(error)
     );
