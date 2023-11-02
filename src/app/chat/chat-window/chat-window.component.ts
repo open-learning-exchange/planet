edbf4ff52d4960b8a67878724e8c988f85a32530
mutugiii <mutugimutuma@gmail.com>
2023-11-02 19:20:00 +0300

    add ngOnDestroy && subscription cleanup in the chat components

--- src/app/chat/chat-window/chat-window.component.ts
+++ src/app/chat/chat-window/chat-window.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 import { CustomValidators } from '../../validators/custom-validators';
 import { ChatService } from '../../shared/chat.service';
@@ -13,6 +15,7 @@ import { showFormErrors } from '../../shared/table-helpers';
   styleUrls: [ './chat-window.scss' ],
 })
 export class ChatWindowComponent implements OnInit {
+  private onDestroy$ = new Subject<void>();
   spinnerOn = true;
   conversations: any[] = [];
   selectedConversationId: any;
@@ -37,16 +40,31 @@ export class ChatWindowComponent implements OnInit {
 
   ngOnInit() {
     this.createForm();
+    this.subscribeToNewChatSelected();
+    this.subscribeToSelectedConversation();
+  }
 
-    this.chatService.newChatSelected$.subscribe(() => {
-      this.selectedConversationId = null;
-      this.conversations = [];
-    });
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
 
-    this.chatService.selectedConversationId$.subscribe((conversationId) => {
-      this.selectedConversationId = conversationId;
-      this.fetchConversation(this.selectedConversationId?._id);
-    });
+  subscribeToNewChatSelected() {
+    this.chatService.newChatSelected$
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(() => {
+        this.selectedConversationId = null;
+        this.conversations = [];
+      });
+  }
+
+  subscribeToSelectedConversation() {
+    this.chatService.selectedConversationId$
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe((conversationId) => {
+        this.selectedConversationId = conversationId;
+        this.fetchConversation(this.selectedConversationId?._id);
+      });
   }
 
   createForm() {
