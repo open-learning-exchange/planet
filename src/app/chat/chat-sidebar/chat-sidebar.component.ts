4394514f0c434e78f22f79375d26c03038acfb4a
mutugiii <mutugimutuma@gmail.com>
2023-11-09 17:19:34 +0300

    Implement the chat search

--- src/app/chat/chat-sidebar/chat-sidebar.component.ts
+++ src/app/chat/chat-sidebar/chat-sidebar.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 import { ChatService } from '../../shared/chat.service';
 import { CouchService } from '../../shared/couchdb.service';
+import { SearchService } from '../../shared/forms/search.service';
 import { showFormErrors } from '../../shared/table-helpers';
 
 @Component({
@@ -10,26 +11,70 @@ import { showFormErrors } from '../../shared/table-helpers';
   templateUrl: './chat-sidebar.component.html',
   styleUrls: [ './chat-sidebar.scss' ],
 })
-export class ChatSidebarComponent implements OnInit {
+export class ChatSidebarComponent implements OnInit, OnDestroy {
   readonly dbName = 'chat_history';
   conversations: any;
+  filteredConversations: any;
   selectedConversation: any;
   isEditing: boolean;
   titleForm: { [key: string]: FormGroup } = {};
+  private _titleSearch = '';
+  get titleSearch(): string { return this._titleSearch.trim(); }
+  set titleSearch(value: string) {
+    this._titleSearch = value;
+    this.recordSearch();
+    this.filterConversations();
+  }
 
   constructor(
     private chatService: ChatService,
     private couchService: CouchService,
+    private searchService: SearchService,
     private formBuilder: FormBuilder
   ) {}
 
   ngOnInit() {
+    this.titleSearch = '';
     this.getChatHistory();
 
     // Listen for any new chats and update
     this.chatService.newChatAdded$.subscribe(() => {
       this.getChatHistory();
     });
+  }
+
+  ngOnDestroy() {
+    this.recordSearch(true);
+  }
+
+  onSearchChange() {
+    this.titleSearch = this.titleSearch;
+  }
+
+  resetFilter() {
+    this.titleSearch = '';
+  }
+
+  recordSearch(complete = false) {
+    this.searchService.recordSearch({
+      type: this.dbName,
+      filter: { 'title': this.titleSearch }
+    }, complete);
+  }
+
+  filterConversations() {
+    if(this.titleSearch.trim() === '' ) {
+      this.getChatHistory();
+    } else {
+      this.conversations = this.conversations.filter(conversation => {
+        const titleMatch = conversation.title?.toLowerCase().includes(this.titleSearch.toLowerCase());
+        const initialQueryMatch = conversation.conversations[0].query?.toLowerCase().includes(
+          this.titleSearch.toLowerCase()
+        );
+
+        return titleMatch || initialQueryMatch;
+      });
+    }
   }
 
   newChat() {
