f5422c215cd79ee5572c6f5a75b9817155aae2c5
mutugiii <mutugimutuma@gmail.com>
2023-12-05 17:53:09 +0300

    Form layout

--- src/app/users/users-archive/users-archive.component.ts
+++ src/app/users/users-archive/users-archive.component.ts
@@ -1,9 +1,42 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { CustomValidators } from '../../validators/custom-validators';
+import { showFormErrors } from '../../shared/table-helpers';
 
 @Component({
-  selector: 'planet-users-archive',
-  templateUrl: './users-archive.component.html'
+  templateUrl: './users-archive.component.html',
+  styles: [ `
+    :host {
+      text-align: center;
+    }
+  ` ]
 })
-export class UsersArchiveComponent {
+export class UsersArchiveComponent implements OnInit {
+  confirmChoice = false;
+  spinnerOn = true;
+  archiveForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.archiveForm = this.formBuilder.group({
+      description: [ '',  CustomValidators.requiredMarkdown ],
+    });
+  }
+
+  onSubmit() {
+    if (this.archiveForm.valid) {
+      const description = this.archiveForm.get('description').value;
+      console.log(description);
+      this.spinnerOn = false;
+
+    } else {
+      showFormErrors(this.archiveForm.controls);
+    }
+  }
 
 }
--- src/app/users/users-router.module.ts
+++ src/app/users/users-router.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { UsersComponent } from './users.component';
+import { UsersArchiveComponent } from './users-archive/users-archive.component';
 import { UsersProfileComponent } from './users-profile/users-profile.component';
 import { UsersUpdateComponent } from './users-update/users-update.component';
 import { UsersAchievementsComponent } from './users-achievements/users-achievements.component';
 
 const routes: Routes = [
   { path: '', component: UsersComponent },
+  { path: 'delete/request', component: UsersArchiveComponent },
   { path: 'profile/:name', component: UsersProfileComponent },
   { path: 'update/:name', component: UsersUpdateComponent },
   { path: 'profile/:name/achievements', component: UsersAchievementsComponent },
