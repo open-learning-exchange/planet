aa046c43a08d27652da543456557a250c3be5c38
mutugiii <mutugimutuma@gmail.com>
2023-12-05 12:30:52 +0300

    Form setup

---
+++ src/app/users/users-archive/users-archive.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'planet-users-archive',
+  templateUrl: './users-archive.component.html'
+})
+export class UsersArchiveComponent {
+
+}
