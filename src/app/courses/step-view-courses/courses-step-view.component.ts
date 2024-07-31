037c5e2de4762ae45fad77d4f238310b6fcd9892
mutugiii <mutugimutuma@gmail.com>
2024-07-31 14:19:21 +0300

    Add optional flex view/grid view for the courses step views

--- src/app/courses/step-view-courses/courses-step-view.component.ts
+++ src/app/courses/step-view-courses/courses-step-view.component.ts
@@ -34,6 +34,7 @@ export class CoursesStepViewComponent implements OnInit, OnDestroy {
   parent = false;
   canManage = false;
   countActivity = true;
+  isGridView = true;
   @ViewChild(MatMenuTrigger) previewButton: MatMenuTrigger;
 
   constructor(
