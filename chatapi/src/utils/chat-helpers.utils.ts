97a24d30cb94420d8a81d2851b7f2fc2752f97d3
mutugiii <mutugimutuma@gmail.com>
2024-09-20 18:25:42 +0300

    Set the entire context var not just the .data

--- chatapi/src/utils/chat-helpers.utils.ts
+++ chatapi/src/utils/chat-helpers.utils.ts
@@ -124,7 +124,7 @@ export async function aiChatNonStream(
   messages: ChatMessage[],
   aiProvider: AIProvider,
   assistant: boolean,
-  context: any,
+  context: any = '',
 ): Promise<string> {
   const provider = models[aiProvider.name];
   if (!provider) {
@@ -150,7 +150,7 @@ export async function aiChatNonStream(
       for (const message of messages) {
         await addToThread(thread.id, message.content);
       }
-      const run = await createRun(thread.id, asst.id, context.data = '');
+      const run = await createRun(thread.id, asst.id, context.data);
       await waitForRunCompletion(thread.id, run.id);
 
       return await retrieveResponse(thread.id);
