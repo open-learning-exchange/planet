534198fcf293f647aa3c2f5881d94c8c59b88da8
mutugiii <mutugimutuma@gmail.com>
2024-10-08 17:04:04 +0300

    Add challenge template guide

--- src/app/shared/dialogs/dialogs-announcement.component.ts
+++ src/app/shared/dialogs/dialogs-announcement.component.ts
@@ -8,20 +8,20 @@ import { MatDialogRef } from '@angular/material/dialog';
 })
 export class DialogsAnnouncementComponent {
   announcement = `
-    ## Planet issues challenge
+  ## Planet issues challenge
 
-    <img src="https://meetgor-cdn.pages.dev/github-filter-issues.png" alt="issues challenge" width="350" height="250">
+  <img src="https://meetgor-cdn.pages.dev/github-filter-issues.png" alt="issues challenge" width="350" height="250">
 
-    Get ready for virtual intern github issues challenge!
+  Get ready for virtual intern github issues challenge!
 
-    **Duration:** 30 days
+  **Duration:** 30 days
 
-    ## Steps to participate:
-    - Find an issue on Planet
-    - Take a screenshot/record a video
-    - Create an issue on our github repository using link below
+  ## Steps to participate:
+  - Find an issue on Planet
+  - Take a screenshot/record a video
+  - Create an issue on our github repository using link below
 
-    [open new Planet issue](https://github.com/open-learning-exchange/planet/issues/new)
+  [open new Planet issue](https://github.com/open-learning-exchange/planet/issues/new)
   `;
 
 
