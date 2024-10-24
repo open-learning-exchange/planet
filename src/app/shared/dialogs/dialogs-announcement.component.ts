92e9412f6d80804b38294f2cb03db7646fe68c6a
mutugiii <mutugimutuma@gmail.com>
2024-10-24 22:05:35 +0300

    Tracking completed users

--- src/app/shared/dialogs/dialogs-announcement.component.ts
+++ src/app/shared/dialogs/dialogs-announcement.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { NewsService } from '../../news/news.service';
+import { findDocuments } from '../../shared/mangoQueries';
+import { SubmissionsService } from '../../submissions/submissions.service';
 
 @Component({
   template: `
     <planet-markdown [content]="announcement"></planet-markdown>
   `
 })
-export class DialogsAnnouncementComponent {
+export class DialogsAnnouncementComponent implements OnInit{
+
+  private onDestroy$ = new Subject<void>();
   announcement = `
   ## Planet issues challenge
 
@@ -27,7 +34,37 @@ export class DialogsAnnouncementComponent {
 
   constructor(
     public dialogRef: MatDialogRef<DialogsAnnouncementComponent>,
+    private newsService: NewsService,
+    private submissionsService: SubmissionsService
   ) {}
+
+  ngOnInit() {
+    this.newsService.newsUpdated$.pipe(takeUntil(this.onDestroy$))
+    .subscribe(news => {
+      news.map(post => ({
+      ...post, public: ((post.doc.viewIn || []).find(view => view._id === 'mutugi@vi') || {}).public
+      }));
+
+      this.submissionsService.getSubmissions(findDocuments({ type: 'survey' }))
+      .subscribe((submissions: any[]) => {
+        const filteredSubmissions = submissions.filter(submission => submission.parentId.includes('d820952d159562a8a6602252390114a4'));
+        const submissionsSet = new Set(filteredSubmissions.map(submission => submission.user.name));
+
+        const filteredNews = news.filter((post) => {
+          const userName = post.doc.user.name.toLowerCase();
+          const isMatch = submissionsSet.has(userName);
+          return isMatch;
+        });
+
+        this.announcement += `\n\n Challenge Submissions: ${filteredNews.length}`;
+      });
+    });
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
 
   onClose(): void {
     this.dialogRef.close();
