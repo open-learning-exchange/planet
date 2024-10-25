d65cc58e1dbe2546cc1b8cac01cc5bebfb654f19
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2024-10-25 15:04:45 -0400

    Update meetups-add.component.ts
    Renamed method to startDateValidator for consistency.

--- src/app/meetups/add-meetups/meetups-add.component.ts
+++ src/app/meetups/add-meetups/meetups-add.component.ts
@@ -88,7 +88,7 @@ export class MeetupsAddComponent implements OnInit {
     this.meetupForm = this.fb.group({
       title: ['', CustomValidators.required],
       description: ['', CustomValidators.required],
-      startDate: ['', [CustomValidators.required, CustomValidators.dateNotInPastValidator()]],
+      startDate: ['', [CustomValidators.required, CustomValidators.startDateValidator()]],
       endDate: [ this.meetup?.endDate ? this.meetup.endDate : '', CustomValidators.endDateValidator() ],      recurring: 'none',
       day: this.fb.array([]),
       startTime: ['', CustomValidators.timeValidator()],
