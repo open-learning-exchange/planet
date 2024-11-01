1da507a18135cf4c87ba58f43e4a23aa7464fce7
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2024-11-01 10:00:38 -0400

    Delete src/app/shared/diacritic-insensitive-match.ts
    I had made a helper function but I don't need it anymore.

--- src/app/shared/diacritic-insensitive-match.ts
+++
@@ -1,5 +0,0 @@
-export function diacriticInsensitiveMatch(value: string, filter: string): boolean {
-    const normalizedValue = value.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-    const normalizedFilter = filter.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase();
-    return normalizedValue.includes(normalizedFilter);
-  }
