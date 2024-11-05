4e1406de311e83feaaf8d7827044d4dee3f59109
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2024-11-05 15:04:17 -0500

    community challenge: popup only appears on fresh login (#7714)
    * popup only appears on fresh login

--- src/app/community/community.component.ts
+++ src/app/community/community.component.ts
@@ -101,11 +101,18 @@ export class CommunityComponent implements OnInit, OnDestroy {
   }
 
   communityChallenge() {
+    // Set the challengeActive status
     this.challengeActive = ((new Date() > new Date(2024, 9, 31)) && (new Date() < new Date(2024, 11, 1)));
-    if (this.challengeActive) {
-      this.openAnnouncementDialog();
+
+    // Check if the popup was shown for this login session
+    const popupShown = localStorage.getItem('announcementPopupShown');
+
+    if (this.challengeActive && !popupShown) {
+        this.openAnnouncementDialog();
+        // Mark popup as shown only after successfully opening the dialog
+        localStorage.setItem('announcementPopupShown', 'true');
     }
-  }
+}
 
   getCommunityData() {
     const setShareTarget = (type) => type === 'center' ? 'nation' : type === 'nation' ? 'community' : undefined;
