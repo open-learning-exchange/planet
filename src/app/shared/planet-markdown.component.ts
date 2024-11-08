ded6fb44b162b0c93405ee5750a60ff38ec7c691
Axel Lorens <axelnlorens@gmail.com>
2024-11-08 12:42:39 -0800

    markdown ability to zoom on image

--- src/app/shared/planet-markdown.component.ts
+++ src/app/shared/planet-markdown.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, ViewEncapsulation, OnChanges } from '@angular/core';
+import { Component, Input, ViewEncapsulation, OnChanges, ElementRef } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { StateService } from './state.service';
+import mediumZoom from 'medium-zoom';
 
 @Component({
   selector: 'planet-markdown',
@@ -16,11 +17,16 @@ export class PlanetMarkdownComponent implements OnChanges {
 
   constructor(
     private stateService: StateService,
+    private el: ElementRef
   ) {}
 
   ngOnChanges() {
     this.couchAddress = this.imageSource === 'parent' ?
       `${environment.parentProtocol}://${this.stateService.configuration.parentDomain}/` :
       `${environment.couchAddress}/`;
+  }
+
+  ngAfterViewInit() {
+    mediumZoom(this.el.nativeElement.querySelectorAll('planet-markdown img'));
   }
 }
