a767b69f0741de032f5180f7b3365f2996d1bcec
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2024-12-16 10:19:41 -0500

    fixed linking and tooltip in myProgress

--- src/app/courses/progress-courses/courses-progress-learner.component.ts
+++ src/app/courses/progress-courses/courses-progress-learner.component.ts
@@ -62,6 +62,7 @@ export class CoursesProgressLearnerComponent implements OnInit, OnDestroy {
   createChartData(courses = [], submissions) {
     return courses.map((course: any) => ({
       label: course.doc.courseTitle,
+      courseId: course._id,
       items: this.courseBySteps(
         course,
         submissions.filter(submission => submission.parentId.indexOf(course._id) > -1)
@@ -105,5 +106,13 @@ export class CoursesProgressLearnerComponent implements OnInit, OnDestroy {
   }
 
   changeData(event) {}
+
+  handleCourseClick(event: any) {
+    if (event.courseId) {
+      this.router.navigate([ '/courses', 'view', event.courseId ]);
+    } else {
+      console.error('Missing Course ID');
+    }
+  }
 
 }
