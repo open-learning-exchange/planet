b910b65f7f7f255c30f694d2b12dca5020c68a64
Gavin Porter <gavporter14@gmail.com>
2024-12-17 13:16:38 -0500

    courses: smoother snackbar (fixes #7941)

--- src/app/shared/planet-message.service.ts
+++ src/app/shared/planet-message.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, HostListener } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Injectable({
@@ -6,11 +6,21 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class PlanetMessageService {
   constructor(
-    private snackBar: MatSnackBar
-  ) { }
+    private snackBar: MatSnackBar,
+  ) { 
+  }
 
+  truncateMessage(message: string) {
+    const maxLength = 45;
+    if (message.length <= maxLength) {
+      return message;
+    } else {
+      return message.slice(0, maxLength) + '...';
+    }
+  }
+  
   showMessage(message: string) {
-    this.snackBar.open(message, undefined, {
+    this.snackBar.open(this.truncateMessage(message), undefined, {
       duration: 3000,
     });
   }
