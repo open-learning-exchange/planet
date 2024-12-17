e9cd3127599c917bb50d8fb025003a05e1f1d28b
Axel Lorens <axelnlorens@gmail.com>
2024-12-17 11:51:40 -0800

    fix function

--- src/app/shared/forms/planet-tag-input-dialog.component.ts
+++ src/app/shared/forms/planet-tag-input-dialog.component.ts
@@ -277,8 +277,8 @@ export class PlanetTagInputDialogComponent {
     return checkValue(this.selected.entries());
   }
 
-  truncateTagName(subTag: { name: string; count?: number }, maxLength: number = 25): string {
-    if (this.deviceType == this.deviceTypes.DESKTOP) { maxLength = 50; }
+  truncateTagName(subTag: { name: string; count?: number }, maxLength: number): string {
+    if (this.deviceType === this.deviceTypes.DESKTOP) { maxLength = 50; } else { maxLength = 25;}
     const truncatedName = subTag.name.length > maxLength ? subTag.name.slice(0, maxLength) + '...' : subTag.name;
     return `${truncatedName} (${subTag.count || 0})`;
   }
