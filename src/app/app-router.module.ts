434196eaad53d091ad8d621aba4fde1fdabd163b
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-01-03 11:01:08 -0500

    linting fix

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -5,13 +5,13 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { AuthService } from './shared/auth-guard.service';
 
 export const routes: Routes = [
-  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivateChild: [ AuthService ]},
+  { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule), canActivateChild: [ AuthService ] },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule), canActivate: [ AuthService ] },
   { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
