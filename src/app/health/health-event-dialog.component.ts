cdb51e2f5716fb9449713c97969b90d99a87b032
Jesse Washburn <jeswashburn@gmail.com>
2025-01-06 12:52:11 -0500

    added user info to header of examination

--- src/app/health/health-event-dialog.component.ts
+++ src/app/health/health-event-dialog.component.ts
@@ -7,6 +7,7 @@ import { switchMap, takeWhile } from 'rxjs/operators';
 import { UsersService } from '../users/users.service';
 import { CouchService } from '../shared/couchdb.service';
 import { UserService } from '../shared/user.service';
+import { HealthService } from './health.service';
 
 @Component({
   templateUrl: './health-event-dialog.component.html'
@@ -14,6 +15,8 @@ import { UserService } from '../shared/user.service';
 export class HealthEventDialogComponent implements OnInit, OnDestroy {
 
   event: any;
+  events: any[] = [];
+  additionalInfo: any = {};
   hasConditionAndTreatment = false;
   conditionAndTreatmentFields = conditionAndTreatmentFields;
   conditions: string;
@@ -24,15 +27,19 @@ export class HealthEventDialogComponent implements OnInit, OnDestroy {
   seconds: string;
   timeLimit = 300000;
   isDestroyed = false;
+  userDetail = this.userService.get();
+  healthDetail: any = {};
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private router: Router,
     private usersService: UsersService,
     private couchService: CouchService,
-    private userService: UserService
+    private userService: UserService,
+    private healthService: HealthService
   ) {
     this.event = this.data.event || {};
+    this.healthDetail = data.healthDetail || {};
     this.conditions = Object.entries(this.event.conditions || {})
       .filter(([ condition, active ]) => active).map(([ condition, active ]) => condition).sort().join(', ');
     this.hasConditionAndTreatment = this.event.hasInfo !== undefined ?
@@ -76,5 +83,36 @@ export class HealthEventDialogComponent implements OnInit, OnDestroy {
     this.minutes = Math.floor(secondsLeft / 60).toString();
     this.seconds = parseInt(seconds, 10) < 10 ? '0' + seconds : seconds;
   }
+
+  initData() {
+    this.healthService.getHealthData(this.userDetail._id).pipe(
+      switchMap(([ { profile, events, userKey } ]: any[]) => {
+        this.userDetail = { ...profile, ...this.userDetail };
+        this.healthDetail = profile;
+        this.events = events || [];
+        return userKey
+          ? this.couchService.findAll('health', { selector: { profileId: userKey } })
+          : of([]);
+      })
+    ).subscribe(eventDocs => {
+      this.events.push(...eventDocs);
+      this.processEventData();
+    });
+  }
+
+
+  processEventData() {
+    this.additionalInfo = this.events.reduce((info, { date, selfExamination, conditions, hasInfo, ...event }) => ({
+      ...info,
+      [date]: {
+        selfExamination,
+        hasConditions: conditions && Object.values(conditions).some(Boolean),
+        hasInfo: hasInfo === true || Object.entries(event).some(
+          ([ key, value ]) => conditionAndTreatmentFields.includes(key) && value
+        )
+      }
+    }), {});
+  }
+
 
 }
