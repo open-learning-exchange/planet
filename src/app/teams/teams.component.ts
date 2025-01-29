629bebeef5ed02af40c9a8f8043bff1e482fb5ce
mutugiii <mutugimutuma@gmail.com>
2025-01-29 22:00:19 +0300

    Add support for getting teams data

--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -4,6 +4,7 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router, ActivatedRoute } from '@angular/router';
+import { SelectionModel } from '@angular/cdk/collections';
 import { UserService } from '../shared/user.service';
 import { CouchService } from '../shared/couchdb.service';
 import { PlanetMessageService } from '../shared/planet-message.service';
@@ -62,6 +63,10 @@ export class TeamsComponent implements OnInit, AfterViewInit {
   isMobile: boolean;
   userNotInShelf = false;
   showFiltersRow = false;
+  selection = new SelectionModel(true, []);
+  get tableData() {
+    return this.teams;
+  }
 
   constructor(
     private userService: UserService,
