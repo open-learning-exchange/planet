bf91b429a0b65667abbca3c2078b43cbb7fbf4f6
Mutugi <48474421+Mutugiii@users.noreply.github.com>
2025-02-05 00:06:07 +0300

    teams: smoother surveys workflow (fixes #8070) (#8164)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- src/app/shared/dialogs/dialogs-add-table.component.ts
+++ src/app/shared/dialogs/dialogs-add-table.component.ts
@@ -3,6 +3,7 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CoursesComponent } from '../../courses/courses.component';
 import { DialogsLoadingService } from './dialogs-loading.service';
 import { UsersComponent } from '../../users/users.component';
+import { TeamsComponent } from '../../teams/teams.component';
 
 @Component({
   templateUrl: 'dialogs-add-table.component.html'
@@ -11,22 +12,31 @@ export class DialogsAddTableComponent implements AfterViewInit {
 
   @ViewChild(CoursesComponent) coursesComponent: CoursesComponent;
   @ViewChild(UsersComponent) usersComponent: UsersComponent;
-  mode: 'courses' | 'users' = 'courses';
+  @ViewChild(TeamsComponent) teamsComponent: TeamsComponent;
+  mode: 'courses' | 'users' | 'teams' = 'courses';
   okDisabled = true;
   get component() {
-    return this.mode === 'courses' ?
-      this.coursesComponent :
-      this.mode === 'users' ?
-      this.usersComponent.usersTable :
+    return this.mode === 'courses' ? this.coursesComponent :
+      this.mode === 'users' ? this.usersComponent.usersTable :
+      this.mode === 'teams' ? this.teamsComponent :
       undefined;
   }
+  selectedLink: { db, title, selector? };
+  links: { db, title, selector? }[] = [
+    { db: 'teams', title: $localize`Teams`, selector: { type: 'team' } },
+    { db: 'teams', title: $localize`Enterprises`, selector: { type: 'enterprise' } }
+  ];
+  teamsSelected = [];
 
   constructor(
     public dialogRef: MatDialogRef<DialogsAddTableComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private dialogsLoadingService: DialogsLoadingService
+    private dialogsLoadingService: DialogsLoadingService,
   ) {
     this.mode = this.data.mode;
+    if (this.mode === 'teams') {
+      this.selectedLink = this.links[0];
+    }
   }
 
   ngAfterViewInit() {
@@ -40,11 +50,26 @@ export class DialogsAddTableComponent implements AfterViewInit {
       this.dialogsLoadingService.start();
     }
     const tableData = this.component.tableData;
-    const selection = this.component.selection.selected;
-    const items = typeof selection[0] === 'string' ?
-      tableData.data.filter((item: any) => selection.indexOf(item._id) > -1) :
-      selection;
-    this.data.okClick(items);
+    if (this.mode === 'teams') {
+      const items = tableData.data.filter((item: any) => this.teamsSelected.indexOf(item.doc._id) > -1);
+      this.data.okClick(items);
+    } else {
+      const selection = this.component.selection.selected;
+      const items = typeof selection[0] === 'string' ?
+        tableData.data.filter((item: any) => selection.indexOf(item._id) > -1) :
+        selection;
+      this.data.okClick(items);
+    }
+  }
+
+  teamSelect({ teamId }) {
+    const index = this.teamsSelected.indexOf(teamId);
+    if (index === -1) {
+      this.teamsSelected.push(teamId);
+    } else {
+      this.teamsSelected.splice(index, 1);
+    }
+    this.okDisabled = this.teamsSelected.length === 0;
   }
 
 }
