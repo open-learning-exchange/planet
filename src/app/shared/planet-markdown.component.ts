cb254cebb70cf65e67c660ebcc6d3f7a234114d3
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-02-07 14:44:27 -0500

    latex somewhat working

--- src/app/shared/planet-markdown.component.ts
+++ src/app/shared/planet-markdown.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, ViewEncapsulation, OnChanges } from '@angular/core';
+import { Component, Input, ViewEncapsulation, OnChanges, AfterViewChecked, ElementRef } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { StateService } from './state.service';
+import * as katex from 'katex';
 
 @Component({
   selector: 'planet-markdown',
@@ -8,7 +9,7 @@ import { StateService } from './state.service';
   styleUrls: [ './planet-markdown.scss' ],
   encapsulation: ViewEncapsulation.None
 })
-export class PlanetMarkdownComponent implements OnChanges {
+export class PlanetMarkdownComponent implements OnChanges, AfterViewChecked {
 
   @Input() content: string;
   @Input() imageSource: 'parent' | 'local' = 'local';
@@ -16,11 +17,46 @@ export class PlanetMarkdownComponent implements OnChanges {
 
   constructor(
     private stateService: StateService,
+    private el: ElementRef
   ) {}
 
   ngOnChanges() {
     this.couchAddress = this.imageSource === 'parent' ?
       `${environment.parentProtocol}://${this.stateService.configuration.parentDomain}/` :
       `${environment.couchAddress}/`;
+    this.renderKaTeX();
+  }
+
+  ngAfterViewChecked() {
+    this.renderKaTeX();
+  }
+
+  private renderKaTeX() {
+    const elements = this.el.nativeElement.querySelectorAll('td-markdown');
+    elements.forEach(element => {
+      const text = element.innerHTML;
+      const inlinePattern = /\((.*?)\)/g;
+      const blockPattern = /\$\$(.*?)\$\$/gs;
+
+      // Render inline LaTeX
+      element.innerHTML = text.replace(inlinePattern, (match, p1) => {
+        try {
+          return katex.renderToString(p1, { throwOnError: false });
+        } catch (error) {
+          console.error('KaTeX render error:', error);
+          return match;
+        }
+      });
+
+      // Render block LaTeX
+      element.innerHTML = element.innerHTML.replace(blockPattern, (match, p1) => {
+        try {
+          return katex.renderToString(p1, { throwOnError: false, displayMode: true });
+        } catch (error) {
+          console.error('KaTeX render error:', error);
+          return match;
+        }
+      });
+    });
   }
 }
