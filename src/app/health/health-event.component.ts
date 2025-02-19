262dcdb3210a923ec2d2ab4ab16feb025e17d65b
Jesse Washburn <jeswashburn@gmail.com>
2025-02-19 10:04:55 -0500

    added last of its kind method for hr

--- src/app/health/health-event.component.ts
+++ src/app/health/health-event.component.ts
@@ -157,4 +157,19 @@ export class HealthEventComponent implements OnInit {
     );
   }
 
+  isLastSection(section: string): boolean {
+    const sections = [
+      'notes',
+      'diagnosis',
+      'treatments',
+      'medications',
+      'immunizations',
+      'xrays',
+      'tests',
+      'referrals'
+    ];
+    const activeSections = sections.filter(sec => this.event[sec]);
+    return activeSections[activeSections.length - 1] === section;
+  }
+
 }
