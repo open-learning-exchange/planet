30155c764f91c0874fa8d4bc69b4fe8804e9ed53
Jesse Washburn <jeswashburn@gmail.com>
2025-02-19 09:09:47 -0500

    added export functionality

--- src/app/health/health-event-dialog.component.ts
+++ src/app/health/health-event-dialog.component.ts
@@ -7,6 +7,9 @@ import { switchMap, takeWhile } from 'rxjs/operators';
 import { UsersService } from '../users/users.service';
 import { CouchService } from '../shared/couchdb.service';
 import { UserService } from '../shared/user.service';
+import * as pdfMake from 'pdfmake/build/pdfmake';
+import * as pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   templateUrl: './health-event-dialog.component.html'
@@ -75,6 +78,41 @@ export class HealthEventDialogComponent implements OnInit, OnDestroy {
     const seconds = Math.floor(secondsLeft % 60).toString();
     this.minutes = Math.floor(secondsLeft / 60).toString();
     this.seconds = parseInt(seconds, 10) < 10 ? '0' + seconds : seconds;
+  }
+
+  exportToPdf(event) {
+    const documentDefinition = {
+      content: [
+        { text: `Health examination on ${new Date(event.date).toLocaleDateString()}`, style: 'header' },
+        { text: [{ text: 'Performed by: ', bold: true }, `${event.selfExamination ? 'Self' : this.performedBy}`], style: 'subheader' },
+        { text: 'Vitals', style: 'sectionHeader' },
+        { text: [{ text: 'Temperature: ', bold: true }, event.temperature ? `${event.temperature} °C` : 'N/A'], style: 'content' },
+        { text: [{ text: 'Pulse: ', bold: true }, event.pulse ? `${event.pulse} bpm` : 'N/A'], style: 'content' },
+        { text: [{ text: 'Blood Pressure: ', bold: true }, event.bp ? `${event.bp}` : 'N/A'], style: 'content' },
+        { text: [{ text: 'Height: ', bold: true }, event.height ? `${event.height} cm` : 'N/A'], style: 'content' },
+        { text: [{ text: 'Weight: ', bold: true }, event.weight ? `${event.weight} kg` : 'N/A'], style: 'content' },
+        { text: [{ text: 'Vision: ', bold: true }, event.vision ? `${event.vision}` : 'N/A'], style: 'content' },
+        { text: [{ text: 'Hearing: ', bold: true }, event.hearing ? `${event.hearing}` : 'N/A'], style: 'content' },
+        { text: '\nConditions', style: 'sectionHeader' },
+        { text: this.conditions || 'N/A', style: 'content' },
+        { text: '\nOther Notes', style: 'sectionHeader' },
+        { text: [{ text: 'Observations & Notes: ', bold: true }, `${event.notes || 'N/A'}`], style: 'content' },
+        { text: [{ text: 'Diagnosis: ', bold: true }, `${event.diagnosis || 'N/A'}`], style: 'content' },
+        { text: [{ text: 'Treatments: ', bold: true }, `${event.treatments || 'N/A'}`], style: 'content' },
+        { text: [{ text: 'Medications: ', bold: true }, `${event.medications || 'N/A'}`], style: 'content' },
+        { text: [{ text: 'Immunizations: ', bold: true }, `${event.immunizations || 'N/A'}`], style: 'content' },
+        { text: [{ text: 'X-rays: ', bold: true }, `${event.xrays || 'N/A'}`], style: 'content' },
+        { text: [{ text: 'Lab Tests: ', bold: true }, `${event.tests || 'N/A'}`], style: 'content' },
+        { text: [{ text: 'Referrals: ', bold: true }, `${event.referrals || 'N/A'}`], style: 'content' }
+      ],
+      styles: {
+        header: { fontSize: 18, bold: true },
+        subheader: { fontSize: 14, margin: [0, 10, 0, 5] },
+        sectionHeader: { fontSize: 16, bold: true, margin: [0, 10, 0, 5] },
+        content: { fontSize: 12, margin: [0, 2, 0, 2] }
+      }
+    };
+    pdfMake.createPdf(documentDefinition).download(`health_event_${event.date}.pdf`);
   }
 
 }
