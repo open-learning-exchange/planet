bae403d224b751b3fe38487cd4b0d9f17d143781
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-02-27 16:08:46 -0500

    works on pfp change but not on pfp removal

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -5,9 +5,45 @@ import { CustomValidators } from '../validators/custom-validators';
 import { ValidatorService } from '../validators/validator.service';
 import { DialogsLoadingService } from '../shared/dialogs/dialogs-loading.service';
 import { StateService } from '../shared/state.service';
-import { Subject } from 'rxjs';
+import { Subject, of } from 'rxjs';
 import { addDateAndTime, getClockTime } from '../shared/utils';
 import { findDocuments } from '../shared/mangoQueries';
+import { environment } from '../../environments/environment';
+
+interface Attachment {
+  _id: string;
+  userId: string;
+  planetCode: string;
+  _attachments: {
+    [key: string]: {
+      content_type: string;
+      digest: string;
+      length: number;
+      revpos: number;
+      stub: boolean;
+    }
+  };
+}
+
+interface TaskAssignee {
+  userId: string;
+  name: string;
+  attachmentDoc?: Attachment;
+  avatar?: string;
+  teamPlanetCode?: string;
+}
+
+interface Task {
+  _id?: string;
+  title: string;
+  description?: string;
+  deadline: number;
+  completed: boolean;
+  completedTime?: number;
+  status?: string;
+  assignee?: TaskAssignee;
+  link?: any;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -39,14 +75,59 @@ export class TasksService {
   }
 
   tasksListener(link = {}, planetField = 'local') {
-    return this.tasksUpdated.pipe(map((res: any) => res.planetField === planetField ?
-      res.newData.filter(task =>
-        task.link &&
-        Object.entries(task.link).every(([ key, value ]) => value === link[key]) &&
-        task.status !== 'archived'
-      ) :
-      []
-    ));
+    return this.tasksUpdated.pipe(
+      switchMap((res: any) => {
+        if (res.planetField !== planetField) {
+          return of([] as Task[]);
+        }
+        const tasks = res.newData.filter((task: Task) =>
+          task.link &&
+          Object.entries(task.link).every(([ key, value ]) => value === link[key]) &&
+          task.status !== 'archived'
+        );
+        // Get latest attachments for all task assignees
+        const userIds = tasks
+          .filter((task: Task) => task.assignee && task.assignee.userId)
+          .map((task: Task) => task.assignee.userId);
+        if (userIds.length === 0) {
+          return of(tasks);
+        }
+        return this.couchService.findAll('attachments').pipe(
+          map((attachments: Attachment[]) => {
+            return tasks.map((task: Task) => {
+              if (!task.assignee) {
+                return task;
+              }
+              const userId = task.assignee.userId;
+              const attachmentId = `${userId}@${this.stateService.configuration.code}`;
+              const attachment = attachments.find(a => a._id === attachmentId);
+              return {
+                ...task,
+                assignee: {
+                  ...task.assignee,
+                  attachmentDoc: attachment
+                }
+              };
+            });
+          })
+        );
+      })
+    );
+  }
+
+  getTaskAvatar(assignee: any) {
+    if (!assignee) {
+      return 'assets/image.png';
+    }
+    if (assignee.attachmentDoc && assignee.attachmentDoc._attachments) {
+      const filename = Object.keys(assignee.attachmentDoc._attachments)[0];
+      return `${environment.couchAddress}/attachments/${assignee.attachmentDoc._id}/${filename}`;
+    }
+    if (assignee.userDoc && assignee.userDoc._attachments) {
+      const filename = Object.keys(assignee.userDoc._attachments)[0];
+      return `${environment.couchAddress}/_users/${assignee.userDoc._id}/${filename}`;
+    }
+    return 'assets/image.png';
   }
 
   addDialogSubmit(additionalFields, task: any, newTask: any, onSuccess) {
