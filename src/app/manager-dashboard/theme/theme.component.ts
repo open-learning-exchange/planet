024b6ba7a7f071588371983c2afb4447f733af01
paulbert <paul.r.albert@gmail.com>
2025-03-07 16:43:50 -0500

    Add custom logo for theme

--- src/app/manager-dashboard/theme/theme.component.ts
+++ src/app/manager-dashboard/theme/theme.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { ConfigurationService } from '../../configuration/configuration.service';
 import { StateService } from '../../shared/state.service';
+import { DialogsImagesComponent } from '../../shared/dialogs/dialogs-images.component';
 
 @Component({
   template: `
@@ -10,6 +12,7 @@ import { StateService } from '../../shared/state.service';
         <mat-option [value]="'purple'" i18n>Purple</mat-option>
       </mat-select>
     </mat-form-field>
+    <button mat-raised-button (click)="addLogo()">Update Logo</button>
     <button mat-raised-button (click)="updateConfigurationTheme()">Update</button>
   `
 })
@@ -17,21 +20,37 @@ export class ThemeComponent {
 
   configuration = this.stateService.configuration;
   selectedColorTheme: string = this.configuration.customization?.colorTheme || "blue";
+  selectedLogo: string = this.configuration.customization?.logo || "";
   colorThemeValues = ['blue','purple'];
 
   constructor(
     private configurationService: ConfigurationService,
-    private stateService: StateService
+    private stateService: StateService,
+    private dialog: MatDialog
   ) {};
 
   updateConfigurationTheme() {
     const newConfiguration = {
       ...this.configuration,
       customization: {
-        colorTheme: this.selectedColorTheme
+        colorTheme: this.selectedColorTheme,
+        logo: this.selectedLogo
       }
     }
     this.configurationService.updateConfiguration(newConfiguration).subscribe();
+  }
+  
+  addLogo() {
+    this.dialog.open(DialogsImagesComponent, {
+      width: '500px',
+      data: {
+        imageGroup: 'community'
+      }
+    }).afterClosed().subscribe(image => {
+      if (image) {
+        this.selectedLogo = `${image._id}/${image.title}`;
+      }
+    });
   }
 
 }
