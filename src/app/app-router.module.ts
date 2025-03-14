6855f794995398445e62f9c083b6ebc0517dc7c4
mutugiii <mutugimutuma@gmail.com>
2025-03-15 00:10:49 +0300

    router changes

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }) ],
+  imports: [ RouterModule.forRoot(routes, { relativeLinkResolution: 'corrected' }) ],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
