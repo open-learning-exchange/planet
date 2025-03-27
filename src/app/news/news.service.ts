d2a2353603719d93e7580ecb3995c04fe69dcf64
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-03-27 17:54:05 -0400

    clearing voice text for shared AI reflects immediately

--- src/app/news/news.service.ts
+++ src/app/news/news.service.ts
@@ -57,7 +57,8 @@ export class NewsService {
   postNews(post, successMessage = $localize`Thank you for submitting your message`, isMessageEdit = true) {
     const { configuration } = this.stateService;
     const message = typeof post.message === 'string' ? post.message : post.message.text;
-    const images = this.createImagesArray(post, message);
+    const processedMessage = message === '' ? '</br>' : message;
+    const images = this.createImagesArray(post, processedMessage);
     const newPost = {
       docType: 'message',
       time: this.couchService.datePlaceholder,
@@ -65,7 +66,7 @@ export class NewsService {
       parentCode: configuration.parentCode,
       user: this.userService.get(),
       ...post,
-      message,
+      message: processedMessage,
       images,
       updatedDate: isMessageEdit ? this.couchService.datePlaceholder : post.updatedDate,
       viewIn: post.viewIn || []
