7c62c45fba9b85800fcadc50220ccce4b2118f37
Munish Patel <53735021+munishpatel@users.noreply.github.com>
2025-04-30 10:42:57 -0500

    8566: smoother myPlanet link in login dialog (fixes #8566)

--- src/app/login/login-dialog.component.ts
+++ src/app/login/login-dialog.component.ts
@@ -4,7 +4,24 @@ import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 @Component({
   template: `
     <planet-login-form [isDialog]="true" (loginEvent)="login($event)"></planet-login-form>
-  `
+    <div class="myplanet-link" style="margin: 1rem 0; text-align: center;">
+      <span i18n class="margin-lr-3">On Android?</span>
+      <a mat-button target="_blank" rel="noopener noreferrer" 
+         href="https://play.google.com/store/apps/details?id=org.ole.planet.myplanet">
+        <ng-container i18n>Try myPlanet</ng-container>
+        <mat-icon aria-hidden="true" class="margin-lr-3">open_in_new</mat-icon>
+      </a>
+    </div>
+  `,
+  styles: [`
+    .myplanet-link {
+      margin-top: 1rem;
+      text-align: center;
+    }
+    .margin-lr-3 {
+      margin: 0 3px;
+    }
+  `]
 })
 export class LoginDialogComponent {
 
@@ -22,5 +39,4 @@ export class LoginDialogComponent {
       this.dialogRef.close(loginState);
     }
   }
-
 }
