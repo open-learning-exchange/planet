58cf1aa0d013d3753b1d2c0889b39126f0cab4f5
mutugiii <mutugimutuma@gmail.com>
2025-05-01 16:30:24 +0300

    newer design doc to take into account the viewIns

--- design/news/news-design.js
+++ design/news/news-design.js
@@ -1,29 +1,38 @@
 module.exports = {
   "views": {
-    "by_updated_date": {
-      "map": function (doc) {
-        if (doc.docType === 'message') {
-          emit(doc.updatedDate, {
-            message: doc.message,
-            time: doc.time,
-            updatedDate: doc.updatedDate,
-            createdOn: doc.createdOn,
-            labels: doc.labels,
-            user: doc.user ? {
-              name: doc.user.name,
-              firstName: doc.user.firstName,
-              middleName: doc.user.middleName,
-              lastName: doc.user.lastName,
-              planetCode: doc.user.planetCode
-            } : null,
-            viewIn: doc.viewIn,
-            chat: doc.chat,
-            news: doc.chat && doc.news ? {
-              conversations: doc.news.conversations
-            } : null
-          });
+    "by_section_and_id_and_date": {
+      map: function (doc) {
+        if (doc.docType !== 'message' || !Array.isArray(doc.viewIn)) { return; }
+
+        const payload = {
+          message: doc.message,
+          time: doc.time,
+          updatedDate: doc.updatedDate,
+          createdOn: doc.createdOn,
+          labels: doc.labels,
+          user: doc.user ? {
+            name: doc.user.name,
+            firstName: doc.user.firstName,
+            middleName: doc.user.middleName,
+            lastName: doc.user.lastName,
+            planetCode: doc.user.planetCode
+          } : null,
+          viewIn: doc.viewIn,
+          chat: doc.chat,
+          news: doc.chat && doc.news ? {
+            conversations: doc.news.conversations
+          } : null
         }
+
+        doc.viewIn.forEach(function (v) {
+          if (v.section === 'community' || v.section === 'teams') {
+            emit(
+              [ v.section, v._id, doc.updatedDate ],
+              payload
+            );
+          }
+        });
       }
-    }
+    },
   }
 }
