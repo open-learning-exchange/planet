2b02295d64c424d8f6cd351c46470f429f0c061f
mutugiii <mutugimutuma@gmail.com>
2025-06-18 15:04:36 +0300

    cleanup

--- src/app/chat/chat-sidebar/chat-sidebar.component.ts
+++ src/app/chat/chat-sidebar/chat-sidebar.component.ts
@@ -40,8 +40,6 @@ export class ChatSidebarComponent implements OnInit, OnDestroy {
   deviceType: DeviceType;
   deviceTypes: typeof DeviceType = DeviceType;
   titleForm: { [key: string]: FormGroup } = {};
-  isMobile = window.innerWidth <= 768;
-
 
   constructor(
     private chatService: ChatService,
