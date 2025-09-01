d861478e21aef672acd0d321ad50e87539ac58a3
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-09-01 13:22:35 -0400

    added more currencies

--- src/app/shared/currencies.ts
+++ src/app/shared/currencies.ts
@@ -28,5 +28,90 @@ export const currencies = [
     code: 'CNY',
     symbol: '¥',
     label: $localize`Chinese Yuan`
+  },
+  {
+    code: 'GTQ',
+    symbol: 'Q',
+    label: $localize`Guatemalan Quetzal`
+  },
+  {
+    code: 'CAD',
+    symbol: 'C$',
+    label: $localize`Canadian Dollar`
+  },
+  {
+    code: 'AUD',
+    symbol: 'A$',
+    label: $localize`Australian Dollar`
+  },
+  {
+    code: 'BRL',
+    symbol: 'R$',
+    label: $localize`Brazilian Real`
+  },
+  {
+    code: 'MXN',
+    symbol: '$',
+    label: $localize`Mexican Peso`
+  },
+  {
+    code: 'CHF',
+    symbol: 'CHF',
+    label: $localize`Swiss Franc`
+  },
+  {
+    code: 'KRW',
+    symbol: '₩',
+    label: $localize`South Korean Won`
+  },
+  {
+    code: 'RUB',
+    symbol: '₽',
+    label: $localize`Russian Ruble`
+  },
+  {
+    code: 'ZAR',
+    symbol: 'R',
+    label: $localize`South African Rand`
+  },
+  {
+    code: 'NGN',
+    symbol: '₦',
+    label: $localize`Nigerian Naira`
+  },
+  {
+    code: 'EGP',
+    symbol: 'E£',
+    label: $localize`Egyptian Pound`
+  },
+  {
+    code: 'KES',
+    symbol: 'KSh',
+    label: $localize`Kenyan Shilling`
+  },
+  {
+    code: 'GHS',
+    symbol: '₵',
+    label: $localize`Ghanaian Cedi`
+  },
+  {
+    code: 'ARS',
+    symbol: '$',
+    label: $localize`Argentine Peso`
+  },
+  {
+    code: 'COP',
+    symbol: '$',
+    label: $localize`Colombian Peso`
+  },
+  {
+    code: 'CLP',
+    symbol: '$',
+    label: $localize`Chilean Peso`
+  },
+  {
+    code: 'PEN',
+    symbol: 'S/',
+    label: $localize`Peruvian Sol`
   }
 ];
