69d259007cf1b1ea99dad49c7c0d35ea46ef7641
Jesse Washburn <142361664+jessewashburn@users.noreply.github.com>
2025-09-01 13:45:50 -0400

    added translations

--- src/app/shared/currencies.ts
+++ src/app/shared/currencies.ts
@@ -2,116 +2,116 @@ export const currencies = [
   {
     code: 'USD',
     symbol: '$',
-    label: $localize`US Dollar`
+    label: $localize`:@@currency.us.dollar:US Dollar`
   },
   {
     code: 'EUR',
     symbol: '€',
-    label: $localize`Euro`
+    label: $localize`:@@currency.euro:Euro`
   },
   {
     code: 'GBP',
     symbol: '£',
-    label: $localize`British Pound`
+    label: $localize`:@@currency.british.pound:British Pound`
   },
   {
     code: 'INR',
     symbol: '₹',
-    label: $localize`Indian Rupee`
+    label: $localize`:@@currency.indian.rupee:Indian Rupee`
   },
   {
     code: 'JPY',
     symbol: '¥',
-    label: $localize`Japanese Yen`
+    label: $localize`:@@currency.japanese.yen:Japanese Yen`
   },
   {
     code: 'CNY',
     symbol: '¥',
-    label: $localize`Chinese Yuan`
+    label: $localize`:@@currency.chinese.yuan:Chinese Yuan`
   },
   {
     code: 'GTQ',
     symbol: 'Q',
-    label: $localize`Guatemalan Quetzal`
+    label: $localize`:@@currency.guatemalan.quetzal:Guatemalan Quetzal`
   },
   {
     code: 'CAD',
     symbol: 'C$',
-    label: $localize`Canadian Dollar`
+    label: $localize`:@@currency.canadian.dollar:Canadian Dollar`
   },
   {
     code: 'AUD',
     symbol: 'A$',
-    label: $localize`Australian Dollar`
+    label: $localize`:@@currency.australian.dollar:Australian Dollar`
   },
   {
     code: 'BRL',
     symbol: 'R$',
-    label: $localize`Brazilian Real`
+    label: $localize`:@@currency.brazilian.real:Brazilian Real`
   },
   {
     code: 'MXN',
     symbol: '$',
-    label: $localize`Mexican Peso`
+    label: $localize`:@@currency.mexican.peso:Mexican Peso`
   },
   {
     code: 'CHF',
     symbol: 'CHF',
-    label: $localize`Swiss Franc`
+    label: $localize`:@@currency.swiss.franc:Swiss Franc`
   },
   {
     code: 'KRW',
     symbol: '₩',
-    label: $localize`South Korean Won`
+    label: $localize`:@@currency.south.korean.won:South Korean Won`
   },
   {
     code: 'RUB',
     symbol: '₽',
-    label: $localize`Russian Ruble`
+    label: $localize`:@@currency.russian.ruble:Russian Ruble`
   },
   {
     code: 'ZAR',
     symbol: 'R',
-    label: $localize`South African Rand`
+    label: $localize`:@@currency.south.african.rand:South African Rand`
   },
   {
     code: 'NGN',
     symbol: '₦',
-    label: $localize`Nigerian Naira`
+    label: $localize`:@@currency.nigerian.naira:Nigerian Naira`
   },
   {
     code: 'EGP',
     symbol: 'E£',
-    label: $localize`Egyptian Pound`
+    label: $localize`:@@currency.egyptian.pound:Egyptian Pound`
   },
   {
     code: 'KES',
     symbol: 'KSh',
-    label: $localize`Kenyan Shilling`
+    label: $localize`:@@currency.kenyan.shilling:Kenyan Shilling`
   },
   {
     code: 'GHS',
     symbol: '₵',
-    label: $localize`Ghanaian Cedi`
+    label: $localize`:@@currency.ghanaian.cedi:Ghanaian Cedi`
   },
   {
     code: 'ARS',
     symbol: '$',
-    label: $localize`Argentine Peso`
+    label: $localize`:@@currency.argentine.peso:Argentine Peso`
   },
   {
     code: 'COP',
     symbol: '$',
-    label: $localize`Colombian Peso`
+    label: $localize`:@@currency.colombian.peso:Colombian Peso`
   },
   {
     code: 'CLP',
     symbol: '$',
-    label: $localize`Chilean Peso`
+    label: $localize`:@@currency.chilean.peso:Chilean Peso`
   },
   {
     code: 'PEN',
     symbol: 'S/',
-    label: $localize`Peruvian Sol`
+    label: $localize`:@@currency.peruvian.sol:Peruvian Sol`
   }
 ];
