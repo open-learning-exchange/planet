ea9903f8fe68b268488bc4c90ea55101ec567321
mutugiii <mutugimutuma@gmail.com>
2025-09-24 17:05:05 +0300

    cached posts rendered markdown

--- src/app/news/news.service.ts
+++ src/app/news/news.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { Subject, of } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 import { CouchService } from '../shared/couchdb.service';
@@ -7,8 +8,19 @@ import { UserService } from '../shared/user.service';
 import { PlanetMessageService } from '../shared/planet-message.service';
 import { findDocuments } from '../shared/mangoQueries';
 import { environment } from '../../environments/environment';
-import { dedupeObjectArray } from '../shared/utils';
+import { calculateMdAdjustedLimit, converter, dedupeObjectArray, truncateText } from '../shared/utils';
 import { planetAndParentId } from '../manager-dashboard/reports/reports.utils';
+
+interface NewsItemViewModel {
+  _id: string;
+  doc: any;
+  sharedDate?: string;
+  avatar: string;
+  renderedHtml: SafeHtml;
+  renderedPreviewHtml: SafeHtml;
+  previewTruncated: boolean;
+  renderedSource?: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -19,16 +31,21 @@ export class NewsService {
   imgUrlPrefix = environment.couchAddress;
   newsUpdated$ = new Subject<any[]>();
   currentOptions: { selectors: any, viewId: string } = { selectors: {}, viewId: '' };
+  private viewModelById = new Map<string, NewsItemViewModel>();
+  private readonly previewLimit = 500;
+  private readonly imageMarkdownRegex = /!\[[^\]]*\]\((.*?\.(?:png|jpe?g|gif)(?:\?.*?)?)\)/g;
 
   constructor(
     private couchService: CouchService,
     private stateService: StateService,
     private userService: UserService,
-    private planetMessageService: PlanetMessageService
+    private planetMessageService: PlanetMessageService,
+    private sanitizer: DomSanitizer
   ) {}
 
   requestNews({ selectors, viewId } = this.currentOptions) {
     this.currentOptions = { selectors, viewId };
+    this.viewModelById.clear();
     this.couchService.findAll(this.dbName, findDocuments(selectors, 0, [ { 'time': 'desc' } ])).pipe(
       switchMap((newsItems: any[]) =>
         this.couchService.findAttachmentsByIds(this.collectAttachmentIds(newsItems)).pipe(
@@ -39,9 +56,7 @@ export class NewsService {
         )
       )
     ).subscribe(({ newsItems, avatarMap }) => {
-      this.newsUpdated$.next(newsItems.map((item: any) => (
-        { doc: item, sharedDate: this.findShareDate(item, viewId), avatar: this.findAvatar(item.user, avatarMap), _id: item._id }
-      )));
+      this.emitNews(this.buildViewModels(newsItems, avatarMap, viewId));
     });
   }
 
@@ -144,6 +159,75 @@ export class NewsService {
 
   postSharedWithCommunity(post) {
     return post && post.doc && (post.doc.viewIn || []).some(({ _id }) => _id === planetAndParentId(this.stateService.configuration));
+  }
+
+  private buildViewModels(newsItems: any[], avatarMap: Map<string, any>, viewId: string): NewsItemViewModel[] {
+    const nextModels: NewsItemViewModel[] = [];
+    const seenIds = new Set<string>();
+
+    newsItems.forEach(item => {
+      const id = item?._id;
+      if (!id) { return; }
+
+      let model = this.viewModelById.get(id);
+      const rawMessage = typeof item?.message === 'string' ? item.message : '';
+
+      if (!model) {
+        const rendered = this.renderMarkdown(rawMessage);
+        model = {
+          _id: id,
+          doc: item,
+          sharedDate: this.findShareDate(item, viewId),
+          avatar: this.findAvatar(item.user, avatarMap),
+          renderedHtml: rendered.full,
+          renderedPreviewHtml: rendered.preview,
+          previewTruncated: rendered.truncated,
+          renderedSource: rawMessage
+        };
+        this.viewModelById.set(id, model);
+      } else {
+        model.doc = item;
+        model.sharedDate = this.findShareDate(item, viewId);
+        model.avatar = this.findAvatar(item.user, avatarMap);
+        if (model.renderedSource !== rawMessage) {
+          const rendered = this.renderMarkdown(rawMessage);
+          model.renderedHtml = rendered.full;
+          model.renderedPreviewHtml = rendered.preview;
+          model.previewTruncated = rendered.truncated;
+          model.renderedSource = rawMessage;
+        }
+      }
+
+      nextModels.push(model);
+      seenIds.add(id);
+    });
+
+    Array.from(this.viewModelById.keys()).forEach(id => {
+      if (!seenIds.has(id)) {
+        this.viewModelById.delete(id);
+      }
+    });
+
+    return [ ...nextModels ];
+  }
+
+  private emitNews(models: NewsItemViewModel[]) {
+    this.newsUpdated$.next(models);
+  }
+
+  private renderMarkdown(source: string): { full: SafeHtml; preview: SafeHtml; truncated: boolean } {
+    const trimmed = source.trimEnd();
+    const textWithoutImages = trimmed.replace(this.imageMarkdownRegex, '');
+    const scaledContent = textWithoutImages.replace(/^(#{1,6})\s+(.+)$/gm, '**$2**');
+    const adjustedLimit = calculateMdAdjustedLimit(scaledContent, this.previewLimit);
+    const truncated = scaledContent.length > adjustedLimit;
+    const previewText = truncated ? truncateText(scaledContent, adjustedLimit) : scaledContent;
+
+    return {
+      full: this.sanitizer.bypassSecurityTrustHtml(converter.makeHtml(trimmed)),
+      preview: this.sanitizer.bypassSecurityTrustHtml(converter.makeHtml(previewText)),
+      truncated
+    };
   }
 
 }
--- src/app/shared/planet-markdown.component.ts
+++ src/app/shared/planet-markdown.component.ts
@@ -1,23 +1,25 @@
-import { Component, Input, ViewEncapsulation, OnChanges } from '@angular/core';
+import { Component, Input, ViewEncapsulation, OnChanges, ChangeDetectionStrategy } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { environment } from '../../environments/environment';
 import { StateService } from './state.service';
-import { truncateText, calculateMdAdjustedLimit } from './utils';
+import { truncateText, calculateMdAdjustedLimit, converter } from './utils';
 
 @Component({
   selector: 'planet-markdown',
   template: `
     <ng-container *ngIf="previewMode; else noPreview">
-      <td-markdown [content]="limitedContent"></td-markdown>
+      <div [innerHTML]="previewHtml" class="markdown"></div>
       <div class="image-gallery" *ngIf="images?.length">
         <img *ngFor="let image of images" [src]="image" class="minified-image" alt="Preview Image" />
       </div>
     </ng-container>
     <ng-template #noPreview>
-      <td-markdown [content]="content" [hostedUrl]="couchAddress"></td-markdown>
+      <div [innerHTML]="fullHtml" class="markdown"></div>
     </ng-template>
   `,
   styleUrls: [ './planet-markdown.scss' ],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PlanetMarkdownComponent implements OnChanges {
 
@@ -27,32 +29,61 @@ export class PlanetMarkdownComponent implements OnChanges {
   @Input() limit: number;
   couchAddress: string;
   images: string[] = [];
-  limitedContent: string;
-  imageMarkdownRegex = /!\[[^\]]*\]\((.*?\.(?:png|jpe?g|gif)(?:\?.*?)?)\)/g;
+  fullHtml: SafeHtml;
+  previewHtml?: SafeHtml;
+  private imageMarkdownRegex = /!\[[^\]]*\]\((.*?\.(?:png|jpe?g|gif)(?:\?.*?)?)\)/g;
+  private cachedContent?: string;
+  private cachedPreviewMode?: boolean;
+  private cachedLimit?: number;
+  private cachedImageSource?: 'parent' | 'local';
 
   constructor(
     private stateService: StateService,
+    private sanitizer: DomSanitizer
   ) {}
 
   ngOnChanges() {
-    this.couchAddress = this.imageSource === 'parent' ?
+    const normalizedContent = this.content || '';
+    const preview = !!this.previewMode;
+    const limit = typeof this.limit === 'number' ? this.limit : normalizedContent.length;
+    const imageSource = this.imageSource || 'local';
+
+    const contentChanged = this.cachedContent !== normalizedContent;
+    const previewChanged = this.cachedPreviewMode !== preview;
+    const limitChanged = this.cachedLimit !== limit;
+    const sourceChanged = this.cachedImageSource !== imageSource;
+
+    if (!contentChanged && !previewChanged && !limitChanged && !sourceChanged) {
+      return;
+    }
+
+    this.couchAddress = imageSource === 'parent' ?
       `${environment.parentProtocol}://${this.stateService.configuration.parentDomain}/` :
       `${environment.couchAddress}/`;
 
-    this.images = this.extractImageUrls(this.content);
-    const textOnly = this.content.replace(this.imageMarkdownRegex, '');
-
-    if (this.previewMode) {
-      const scaledContent = textOnly.replace(/^(#{1,6})\s+(.+)$/gm, '**$2**');
-      const adjustedLimit = calculateMdAdjustedLimit(scaledContent, this.limit);
-
-      this.limitedContent = truncateText(scaledContent, adjustedLimit);
-    } else {
-      this.limitedContent = truncateText(textOnly, this.limit);
+    if (contentChanged || sourceChanged) {
+      this.images = this.extractImageUrls(normalizedContent);
+      this.fullHtml = this.sanitizer.bypassSecurityTrustHtml(converter.makeHtml(normalizedContent));
     }
+
+    if (preview && (contentChanged || limitChanged || previewChanged || sourceChanged)) {
+      const textOnly = normalizedContent.replace(this.imageMarkdownRegex, '');
+      const scaledContent = textOnly.replace(/^(#{1,6})\s+(.+)$/gm, '**$2**');
+      const adjustedLimit = calculateMdAdjustedLimit(scaledContent, limit);
+      const truncated = truncateText(scaledContent, adjustedLimit);
+      this.previewHtml = this.sanitizer.bypassSecurityTrustHtml(converter.makeHtml(truncated));
+    } else if (!preview) {
+      this.previewHtml = undefined;
+    }
+
+    this.cachedContent = normalizedContent;
+    this.cachedPreviewMode = preview;
+    this.cachedLimit = limit;
+    this.cachedImageSource = imageSource;
   }
 
   extractImageUrls(content: string): string[] {
+    this.imageMarkdownRegex.lastIndex = 0;
     const matches: string[] = [];
     let match: RegExpExecArray | null;
     while ((match = this.imageMarkdownRegex.exec(content)) !== null) {
