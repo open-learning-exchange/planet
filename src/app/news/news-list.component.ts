a87b839f9ed9f4c951fc93798568779bf03a080b
mutugiii <mutugimutuma@gmail.com>
2025-09-24 15:55:09 +0300

    Revert "server side pagination"
    This reverts commit 3b5feb997fc33ceb691e6392bdcea68bf73aa0f5.

--- src/app/news/news-list.component.ts
+++ src/app/news/news-list.component.ts
@@ -67,7 +67,6 @@ export class NewsListComponent implements OnInit, OnChanges, AfterViewInit, OnDe
   }
 
   ngOnChanges() {
-    this.isLoadingMore = false;
     let isLatest = true;
     this.replyObject = {};
     this.items.forEach(item => {
@@ -82,7 +81,7 @@ export class NewsListComponent implements OnInit, OnChanges, AfterViewInit, OnDe
       }
     });
     this.displayedItems = this.replyObject[this.replyViewing._id];
-    this.updateDisplayedItems(true);
+    this.loadPagedItems(true);
     if (this.replyViewing._id !== 'root') {
       this.replyViewing = this.items.find(item => item._id === this.replyViewing._id);
     }
@@ -150,7 +149,7 @@ export class NewsListComponent implements OnInit, OnChanges, AfterViewInit, OnDe
     const news = this.items.find(item => item._id === newsId) || { _id: 'root' };
     this.replyViewing = news;
     this.displayedItems = this.replyObject[news._id];
-    this.updateDisplayedItems(true);
+    this.loadPagedItems(true);
     this.isMainPostShared = this.replyViewing._id === 'root' || this.newsService.postSharedWithCommunity(this.replyViewing);
     this.showMainPostShare = !this.replyViewing.doc || !this.replyViewing.doc.replyTo ||
       (
@@ -286,38 +285,27 @@ export class NewsListComponent implements OnInit, OnChanges, AfterViewInit, OnDe
     };
   }
 
-  loadMoreItems() {
-    if (this.isLoadingMore) {
-      return;
-    }
-    this.updateDisplayedItems();
-  }
-
-  private updateDisplayedItems(reset = false) {
-    const news = this.getCurrentItems();
-    if (reset) {
+  loadPagedItems(initial = true) {
+    let pageSize = this.pageSize;
+    if (initial) {
       this.displayedItems = [];
       this.nextStartIndex = 0;
+      // Take maximum so if fewer posts than page size adding a post doesn't add a "Load More" button
+      pageSize = Math.max(this.pageEnd[this.replyViewing._id] || this.pageSize, this.pageSize);
     }
-
-    const pageSize = reset ? Math.max(this.pageEnd[this.replyViewing._id] || this.pageSize, this.pageSize) : this.pageSize;
+    const news = this.getCurrentItems();
     const { items, endIndex, hasMore } = this.paginateItems(news, this.nextStartIndex, pageSize);
 
-    this.displayedItems = reset ? items : [ ...this.displayedItems, ...items ];
+    this.displayedItems = [ ...this.displayedItems, ...items ];
     this.pageEnd[this.replyViewing._id] = this.displayedItems.length;
     this.nextStartIndex = endIndex;
+    this.hasMoreNews = hasMore;
+    this.isLoadingMore = false;
     this.totalReplies = news.length;
+  }
 
-    const reachedLocalEnd = endIndex >= news.length;
-    const shouldFetchRemote = reachedLocalEnd && !hasMore && this.newsService.canLoadMore();
-
-    if (shouldFetchRemote) {
-      this.isLoadingMore = true;
-      this.newsService.loadMoreNews();
-    } else {
-      this.isLoadingMore = false;
-    }
-
-    this.hasMoreNews = hasMore || shouldFetchRemote || this.newsService.canLoadMore();
+  loadMoreItems() {
+    this.isLoadingMore = true;
+    this.loadPagedItems(false);
   }
 }
