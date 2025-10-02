e94dfa44db6f20a6694e2e2434672c9e0ce5f763
mutugiii <mutugimutuma@gmail.com>
2025-10-02 22:37:44 +0300

    shared myplanet toolbar component

---
+++ src/app/manager-dashboard/reports/myplanet/myplanet-toolbar.component.ts
@@ -0,0 +1,68 @@
+import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { DeviceInfoService, DeviceType } from '../../../shared/device-info.service';
+
+@Component({
+  selector: 'planet-myplanet-toolbar',
+  templateUrl: './myplanet-toolbar.component.html',
+  styleUrls: [ './myplanet.scss' ]
+})
+export class MyPlanetToolbarComponent {
+
+  @Input() title = '';
+  @Input() versions: string[] = [];
+  @Input() selectedVersion = '';
+  @Input() types: string[] = [];
+  @Input() selectedType = '';
+  @Input() showTypeFilter = false;
+  @Input() timeFilterOptions: { label: string; value: string }[] = [];
+  @Input() selectedTimeFilter = '';
+  @Input() formGroup!: FormGroup<{ startDate: FormControl<Date>; endDate: FormControl<Date>; }>;
+  @Input() showCustomDateFields = false;
+  @Input() minDate!: Date;
+  @Input() today!: Date;
+  @Input() searchValue = '';
+  @Input() disableShowAllTime = true;
+  @Output() versionChange = new EventEmitter<string>();
+  @Output() typeChange = new EventEmitter<string>();
+  @Output() timeFilterChange = new EventEmitter<string>();
+  @Output() search = new EventEmitter<string>();
+  @Output() clear = new EventEmitter<void>();
+  @Output() resetDateFilter = new EventEmitter<void>();
+  deviceType: DeviceType;
+  deviceTypes: typeof DeviceType = DeviceType;
+  showFiltersRow = false;
+
+  constructor(private deviceInfoService: DeviceInfoService) {
+    this.deviceType = this.deviceInfoService.getDeviceType({ tablet: 1300 });
+  }
+
+  @HostListener('window:resize')
+  OnResize() {
+    this.deviceType = this.deviceInfoService.getDeviceType({ tablet: 1300 });
+  }
+
+  onVersionChange(value: string) {
+    this.versionChange.emit(value);
+  }
+
+  onTypeChange(value: string) {
+    this.typeChange.emit(value);
+  }
+
+  onTimeFilterChange(value: string) {
+    this.timeFilterChange.emit(value);
+  }
+
+  onSearch(value: string) {
+    this.search.emit(value);
+  }
+
+  onClear() {
+    this.clear.emit();
+  }
+
+  onResetDateFilter() {
+    this.resetDateFilter.emit();
+  }
+}
