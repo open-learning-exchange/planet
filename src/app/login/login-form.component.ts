b630147be9985b7709b918459e0921d82eecdb0f
mutugiii <mutugimutuma@gmail.com>
2025-10-09 00:33:12 +0300

    revert login form updates

--- src/app/login/login-form.component.ts
+++ src/app/login/login-form.component.ts
@@ -186,9 +186,11 @@ export class LoginFormComponent {
         return;
       }
       this.pouchAuthService.login(name, password).pipe(
-        switchMap((userCtx) => (isCreate && !this.isDialog ?
+        switchMap((userCtx) => (this.isDialog ?
+          this.userService.setUserAndShelf(userCtx) :
+          isCreate ?
           from(this.router.navigate([ 'users/update/' + name ])) :
-          this.isDialog ? this.userService.setUserAndShelf(userCtx).pipe(switchMap(() => from(this.reRoute()))) : from(this.reRoute())
+          from(this.reRoute())
         )),
         switchMap(() => forkJoin(this.pouchService.replicateFromRemoteDBs())),
         switchMap(this.createSession(name, password)),
