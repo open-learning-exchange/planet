70eaa99496e962bd7855c75d19e9355be0f89b76
Saniya <37302318+Saby-Bishops@users.noreply.github.com>
2025-10-29 15:08:21 -0500

    Remove itemsShown utility export

--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -51,8 +51,6 @@ export const filterById = (array = [], id: string) => array.filter(item => item.
 
 export const arraySubField = (array: any[], field: string) => array.map(item => item[field]);
 
-export const itemsShown = (paginator: any) => Math.min(paginator.length - (paginator.pageIndex * paginator.pageSize), paginator.pageSize);
-
 export const isInMap = (tag: string, map: Map<string, boolean>) => map.get(tag);
 
 export const mapToArray = (map: Map<string, boolean>, equalValue?) => {
