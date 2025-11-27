21756c39f092e6b3e270d5952d957ac86192f028
mutugiii <mutugimutuma@gmail.com>
2025-11-27 17:05:40 +0300

    update submissions design doc

--- design/submissions/submissions-design.js
+++ design/submissions/submissions-design.js
@@ -1,33 +1,35 @@
 module.exports = {
   "views": {
-    "surveyData": {
+    "submissionCounts": {
       "map": function(doc) {
         if (doc.type === 'survey') {
           var teamId = doc.team && doc.team._id ? doc.team._id : (doc.parent && doc.parent.teamId ? doc.parent.teamId : null);
           var status = doc.status || 'pending';
-
-          // Emit for counting
           emit([doc.parentId, teamId, status], 1);
-
-          // Emit parent metadata
-          if (doc.parent && doc.parent._id) {
-            emit(['parent', doc.parent._id], {
-              parentDoc: {
-                _id: doc.parent._id,
-                _rev: doc.parent._rev,
-                name: doc.parent.name,
-                description: doc.parent.description,
-                questions: doc.parent.questions,
-                type: doc.parent.type,
-                sourcePlanet: doc.parent.sourcePlanet
-              },
-              status: status,
-              teamId: teamId
-            });
-          }
         }
       },
       "reduce": "_count"
+    },
+    "parentSurveys": {
+      "map": function(doc) {
+        if (doc.type === 'survey' && doc.parent && doc.parent._id) {
+          var teamId = doc.team && doc.team._id ? doc.team._id : (doc.parent && doc.parent.teamId ? doc.parent.teamId : null);
+          var status = doc.status || 'pending';
+          emit(['parent', doc.parent._id], {
+            parentDoc: {
+              _id: doc.parent._id,
+              _rev: doc.parent._rev,
+              name: doc.parent.name,
+              description: doc.parent.description,
+              questions: doc.parent.questions,
+              type: doc.parent.type,
+              sourcePlanet: doc.parent.sourcePlanet
+            },
+            status: status,
+            teamId: teamId
+          });
+        }
+      }
     }
   }
 };
