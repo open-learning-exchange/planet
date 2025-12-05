6853042556e4202ef1b7a004c25a85d5193acd3b
mutugiii <mutugimutuma@gmail.com>
2025-12-05 14:26:21 +0300

    Single view setup

--- couchdb-setup.sh
+++ couchdb-setup.sh
@@ -184,7 +184,6 @@ upsert_doc resources _index '{"index":{"fields":[{"title":"asc"}]},"name":"time-
 upsert_doc news _index '{"index":{"fields":[{"time":"desc"}]},"name":"time-index"}' POST
 upsert_doc tags _index '{"index":{"fields":[{"name":"asc"}]},"name":"name-index"}' POST
 upsert_doc team_activities _index '{"index":{"fields":[{"time":"desc"}]},"name":"time-index"}' POST
-upsert_doc submissions _index '{"index":{"fields":["type","parent"]},"name":"parent-submissions-index"}' POST
 # Only insert dummy data and update security on install
 # _users security is set in app and auto accept will be overwritten if set here
 if (($ISINSTALL))
--- design/submissions/submissions-design.js
+++ design/submissions/submissions-design.js
@@ -1,36 +1,26 @@
 module.exports = {
   "views": {
-    "submissionCounts": {
+    "submissionsByParent": {
       "map": function(doc) {
-        if (doc.type === 'survey') {
+        if (doc.type === 'survey' && doc.parentId) {
+          var baseSurveyId = doc.parentId.split('@')[0];
           var teamId = doc.team && doc.team._id ? doc.team._id : (doc.parent && doc.parent.teamId ? doc.parent.teamId : null);
-          var status = doc.status || 'pending';
-          var baseSurveyId = doc.parentId ? doc.parentId.split('@')[0] : doc.parentId;
-          emit([baseSurveyId, teamId, status], 1);
-        }
-      },
-      "reduce": "_count"
-    },
-    "parentSurveys": {
-      "map": function(doc) {
-        if (doc.type === 'survey' && doc.parent && doc.parent._id) {
-          var teamId = doc.team && doc.team._id ? doc.team._id : (doc.parent && doc.parent.teamId ? doc.parent.teamId : null);
-          var status = doc.status || 'pending';
-          emit(['parent', doc.parent._id], {
-            parentDoc: {
+          var parentInfo = null;
+          if (doc.parent && doc.parent.sourcePlanet) {
+            parentInfo = {
               _id: doc.parent._id,
-              _rev: doc.parent._rev,
               name: doc.parent.name,
-              description: doc.parent.description,
-              questions: doc.parent.questions,
-              type: doc.parent.type,
               sourcePlanet: doc.parent.sourcePlanet,
               teamShareAllowed: doc.parent.teamShareAllowed,
               isArchived: doc.parent.isArchived,
               createdDate: doc.parent.createdDate
-            },
-            status: status,
-            teamId: teamId
+            };
+          }
+
+          emit([baseSurveyId, teamId], {
+            status: doc.status || 'pending',
+            teamId: teamId,
+            parent: parentInfo
           });
         }
       }
