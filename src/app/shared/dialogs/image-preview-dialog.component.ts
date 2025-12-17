20e9cb5c3353a176fea9a3c53704d6c0978b76f5
emmanuelbaa <147341761+emmanuelbaa@users.noreply.github.com>
2025-12-17 15:17:16 -0500

    commit to preview uploaded template

---
+++ src/app/shared/dialogs/image-preview-dialog.component.ts
@@ -0,0 +1,50 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
+
+@Component({
+  templateUrl: './image-preview-dialog.component.html',
+})
+export class ImagePreviewDialogComponent implements OnInit {
+
+  previewUrl: any;
+  selectedFile: File;
+
+  constructor(
+    public dialogRef: MatDialogRef<ImagePreviewDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {}
+
+  ngOnInit() {
+    if (this.data && this.data.file) {
+      this.selectedFile = this.data.file;
+      this.updatePreview();
+    }
+  }
+
+  onFileSelected(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      this.selectedFile = event.target.files[0];
+      this.updatePreview();
+    }
+  }
+
+  updatePreview() {
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.previewUrl = reader.result;
+    };
+    reader.readAsDataURL(this.selectedFile);
+  }
+
+  confirm() {
+    this.dialogRef.close(this.selectedFile);
+  }
+
+  remove() {
+    this.dialogRef.close(null);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+}
