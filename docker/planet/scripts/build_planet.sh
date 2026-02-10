abca37468068f3f774bc2fdcd136cc2faae2534d
Saniya <37302318+Saby-Bishops@users.noreply.github.com>
2026-02-10 15:31:01 -0600

    Simplify build_planet language branching

--- docker/planet/scripts/build_planet.sh
+++ docker/planet/scripts/build_planet.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
 build_single(){
-  LANG=$1
-  LANG2=$2
-  if [ -z "${LANG}" ]; then
-    npm run ng-high-memory -- build --configuration production --aot --localize=true
-  else
-    npm run ng-high-memory -- build --configuration production --aot --localize=true
-  fi
+  npm run ng-high-memory -- build --configuration production --aot --localize=true
 }
