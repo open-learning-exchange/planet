a77d531ed163fd70a4aa62539f06aabed6e37683
mutugiii <mutugimutuma@gmail.com>
2026-02-10 22:32:00 +0300

    convert to typed form with control map

--- src/app/manager-dashboard/requests/requests-table.component.ts
+++ src/app/manager-dashboard/requests/requests-table.component.ts
@@ -20,8 +20,8 @@ import { ValidatorService } from '../../validators/validator.service';
 import { ReportsService } from '../reports/reports.service';
 import { findDocuments } from '../../shared/mangoQueries';
 
-interface EditChildNameFormValue {
-  name: string;
+interface EditChildNameFormControls {
+  name: FormControl<string>;
 }
 
 @Component({
@@ -211,13 +211,13 @@ export class RequestsTableComponent implements OnChanges, AfterViewInit, OnDestr
   openEditChildNameDialog(planet) {
     const currentName = planet.nameDoc ? planet.nameDoc.name : planet.doc.name;
     const exceptions = [ currentName ];
-    const form = this.fb.group({
-      name: [currentName, {
+    const form: FormGroup<EditChildNameFormControls> = this.fb.group({
+      name: this.fb.control(currentName, {
         validators: [ CustomValidators.required ],
         asyncValidators: [
           (control: AbstractControl<string>) => this.validatorService.isUnique$(this.dbName, 'name', control, { exceptions })
         ]
-      }]
+      })
     });
     this.dialogsFormService.openDialogsForm(
       $localize`Edit ${this.reportsService.planetTypeText(planet.doc.planetType)} Name`,
@@ -229,7 +229,7 @@ export class RequestsTableComponent implements OnChanges, AfterViewInit, OnDestr
   }
 
   editChildName({ doc, nameDoc }) {
-    return ({ name }: EditChildNameFormValue) => {
+    return ({ name }: { name: string }) => {
       this.couchService.updateDocument(
         this.dbName,
         { ...nameDoc, 'name': name, 'docType': 'parentName', 'planetId': doc._id, createdDate: this.couchService.datePlaceholder }
