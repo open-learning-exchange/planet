913a734150870cf9ba35e79ea38a57eb27f838d1
Saniya <37302318+Saby-Bishops@users.noreply.github.com>
2026-02-16 14:31:15 -0600

    Remove unused e2e user handler implementation

--- e2e/userHandler.ts
+++ e2e/userHandler.ts
@@ -1,31 +1,3 @@
-const rp = require('request-promise');
-
-module.exports = function(timeStamp) {
-  const user = 'e2e_' + timeStamp;
-
-  return {
-    get: function() {
-      return user;
-    },
-    create: function() {
-      return rp({
-        method: 'PUT',
-        uri: 'http://127.0.0.1:5984/_users/org.couchdb.user:' + user,
-        headers: { 'Content-Type': 'application/json' },
-        body: { name: user, password: 'e2e', roles: [ 'learner' ], type: 'user' },
-        json: true
-      });
-    },
-    delete: function() {
-      return rp('http://' + user + ':e2e@127.0.0.1:5984/_users/org.couchdb.user:' + user).then(function(res) {
-        const rev = JSON.parse(res)._rev;
-        return rp({
-          method: 'DELETE',
-          uri: 'http://' + user + ':e2e@127.0.0.1:5984/_users/org.couchdb.user:' + user + '?rev=' + rev,
-          headers: { 'Content-Type': 'application/json' },
-          json: true
-        });
-      });
-    }
-  };
+module.exports = function() {
+  throw new Error('Deprecated e2e helper removed as unused.');
 };
