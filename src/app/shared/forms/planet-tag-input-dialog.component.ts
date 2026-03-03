db50bb2cb36b3c446f7346dd39db423b683563b1
Saniya <37302318+Saby-Bishops@users.noreply.github.com>
2026-03-02 22:42:27 -0600

    courses: less collections sub tag ids is more (fixes #9656) (#9545)
    Co-authored-by: dogi <dogi@users.noreply.github.com>

--- src/app/shared/forms/planet-tag-input-dialog.component.ts
+++ src/app/shared/forms/planet-tag-input-dialog.component.ts
@@ -121,10 +121,6 @@ export class PlanetTagInputDialogComponent {
     }
   }
 
-  subTagIds(subTags: any[]) {
-    return subTags.map(subTag => subTag._id || subTag.name);
-  }
-
   updateFilter(value) {
     this.filterValue = value;
     this.tags = this.filterTags(value);
