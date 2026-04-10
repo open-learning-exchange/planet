cd53929f02115fbd17c6adcb24b0f7b7a88740d1
mutugiii <mutugimutuma@gmail.com>
2026-04-10 23:34:18 +0300

    fix icon first view in users table

--- src/app/users/users-table.component.ts
+++ src/app/users/users-table.component.ts
@@ -102,7 +102,7 @@ export class UsersTableComponent implements OnInit, OnDestroy, AfterViewInit, On
     private deviceInfoService: DeviceInfoService
   ) {
     this.deviceType = this.deviceInfoService.getDeviceType();
-    this.isMobile = this.deviceType === DeviceType.MOBILE;
+    this.isMobile = this.deviceType === DeviceType.MOBILE || this.deviceType === DeviceType.SMALL_MOBILE;
   }
 
   ngOnInit() {
@@ -150,7 +150,7 @@ export class UsersTableComponent implements OnInit, OnDestroy, AfterViewInit, On
 
   @HostListener('window:resize') onResize() {
     this.deviceType = this.deviceInfoService.getDeviceType();
-    this.isMobile = this.deviceType === DeviceType.MOBILE;
+    this.isMobile = this.deviceType === DeviceType.MOBILE || this.deviceType === DeviceType.SMALL_MOBILE;
   }
 
   isAllSelected() {
