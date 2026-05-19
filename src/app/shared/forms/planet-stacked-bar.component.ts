a183392ee8e1fd4563be73a7931bbc46b62ce9e9
mutugiii <mutugimutuma@gmail.com>
2026-05-19 14:35:55 +0300

    fix 0 values

--- src/app/shared/forms/planet-stacked-bar.component.ts
+++ src/app/shared/forms/planet-stacked-bar.component.ts
@@ -37,6 +37,15 @@ export class PlanetStackedBarComponent implements OnChanges {
 
   ngOnChanges() {
     const total = this.data.reduce((t, item) => t + item.amount, 0);
+    if (total === 0) {
+      this.data = this.data.map((item, index) => ({
+        ...item,
+        class: index === 0 ? 'bg-light-grey' : item.class,
+        percent: index === 0 ? 1 : 0
+      }));
+      this.barSizes = '1fr';
+      return;
+    }
     this.data = this.data.map(item => ({ ...item, percent: (item.amount / total) }));
     this.barSizes = this.data.reduce((sizes, item) => sizes + (item.percent + 'fr '), '');
     this.barSizes.trim();
