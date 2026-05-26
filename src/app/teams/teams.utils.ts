64e28d2609d69dcfd3122208d61f204418ddb3d1
mutugiii <mutugimutuma@gmail.com>
2026-05-26 15:52:42 +0300

    switch from alphabetic sorting to chronological

--- src/app/teams/teams.utils.ts
+++ src/app/teams/teams.utils.ts
@@ -1,13 +1,14 @@
 export const memberCompare = (member1, member2) => member1.userId === member2.userId && member1.userPlanetCode === member2.userPlanetCode;
 
 export const memberNameCompare = (member1, member2) => {
-  const memberName = (member) => {
-    const fallbackUserId = member?.userId || '';
-    const splitUserId = fallbackUserId.includes(':') ? fallbackUserId.split(':')[1] : fallbackUserId;
-    return (member.userDoc && member.userDoc.doc.lastName) || splitUserId;
-  };
+  const memberName = (member) =>
+    (member.userDoc && member.userDoc.doc.lastName) || (member.userId || '').split(':')[1] || member.userId || '';
   return memberName(member1).localeCompare(memberName(member2));
 };
+
+export const requestDateCompare = (request1, request2) =>
+  (request1.createdDate || 0) - (request2.createdDate || 0) ||
+  (request1.userId || '').localeCompare(request2.userId || '');
 
 export const memberSort = (member1, member2, leader) => memberCompare(member1, leader) ?
   -1 :
