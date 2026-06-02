e890d0d218c14167e49f7a2942e77eaaa5593987
mutugiii <mutugimutuma@gmail.com>
2026-06-02 17:33:59 +0300

    teams: show report receipt images

--- src/app/teams/teams-reports-detail.component.ts
+++ src/app/teams/teams-reports-detail.component.ts
@@ -1,19 +1,21 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { StateService } from '../shared/state.service';
 import { MatDivider } from '@angular/material/list';
-import { NgIf, NgClass, CurrencyPipe, DatePipe } from '@angular/common';
+import { NgFor, NgIf, NgClass, CurrencyPipe, DatePipe } from '@angular/common';
 import { PlanetMarkdownComponent } from '../shared/planet-markdown.component';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'planet-teams-reports-detail',
   templateUrl: './teams-reports-detail.component.html',
   styleUrls: ['./teams-reports-detail.scss'],
-  imports: [MatDivider, NgIf, NgClass, PlanetMarkdownComponent, CurrencyPipe, DatePipe]
+  imports: [MatDivider, NgIf, NgFor, NgClass, PlanetMarkdownComponent, CurrencyPipe, DatePipe]
 })
 export class TeamsReportsDetailComponent implements OnChanges {
 
   @Input() report;
   @Input() showSummary = false;
+  receiptImages: any[] = [];
   expenses: number;
   income: number;
   net: number;
@@ -25,10 +27,26 @@ export class TeamsReportsDetailComponent implements OnChanges {
   ) {}
 
   ngOnChanges() {
-    this.expenses = this.report.wages + this.report.otherExpenses;
-    this.income = this.report.sales + this.report.otherIncome;
+    this.expenses = this.num(this.report.wages) + this.num(this.report.otherExpenses);
+    this.income = this.num(this.report.sales) + this.num(this.report.otherIncome);
     this.net = this.income - this.expenses;
-    this.endingBalance = this.net + this.report.beginningBalance;
+    this.endingBalance = this.net + this.num(this.report.beginningBalance);
+    this.receiptImages = this.getReceiptImages(this.report);
+  }
+
+  private getReceiptImages(report) {
+    return (Object.entries(report?._attachments || {}) as [ string, any ][])
+      .filter(([ , attachment ]) => attachment?.content_type?.startsWith('image/'))
+      .sort(([ a ], [ b ]) => a.localeCompare(b))
+      .map(([ attachmentKey ]) => ({
+        key: attachmentKey,
+        url: `${environment.couchAddress}/teams/${report._id}/${encodeURIComponent(attachmentKey)}`
+      }));
+  }
+
+  private num(value: any): number {
+    const n = Number(value);
+    return Number.isFinite(n) ? n : 0;
   }
 
 }
