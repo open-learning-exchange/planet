3b8216c620607f531fa573e6b84bd22167317fd2
mutugiii <mutugimutuma@gmail.com>
2026-06-05 22:10:07 +0300

    update finances & reports viewer & editor with attachment support

--- src/app/teams/teams-reports-detail.component.ts
+++ src/app/teams/teams-reports-detail.component.ts
@@ -1,19 +1,21 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { StateService } from '../shared/state.service';
 import { MatDivider } from '@angular/material/list';
-import { NgIf, NgClass, CurrencyPipe, DatePipe } from '@angular/common';
+import { NgFor, NgIf, NgClass, CurrencyPipe, DatePipe } from '@angular/common';
 import { PlanetMarkdownComponent } from '../shared/planet-markdown.component';
+import { TeamsAttachmentsService } from './teams-attachments.service';
 
 @Component({
   selector: 'planet-teams-reports-detail',
   templateUrl: './teams-reports-detail.component.html',
   styleUrls: ['./teams-reports-detail.scss'],
-  imports: [MatDivider, NgIf, NgClass, PlanetMarkdownComponent, CurrencyPipe, DatePipe]
+  imports: [MatDivider, NgIf, NgFor, NgClass, PlanetMarkdownComponent, CurrencyPipe, DatePipe]
 })
 export class TeamsReportsDetailComponent implements OnChanges {
 
   @Input() report;
   @Input() showSummary = false;
+  receiptImages: any[] = [];
   expenses: number;
   income: number;
   net: number;
@@ -21,7 +23,8 @@ export class TeamsReportsDetailComponent implements OnChanges {
   curCode = this.stateService.configuration.currency || {};
 
   constructor(
-    private stateService: StateService
+    private stateService: StateService,
+    private teamsAttachmentsService: TeamsAttachmentsService
   ) {}
 
   ngOnChanges() {
@@ -29,6 +32,7 @@ export class TeamsReportsDetailComponent implements OnChanges {
     this.income = this.report.sales + this.report.otherIncome;
     this.net = this.income - this.expenses;
     this.endingBalance = this.net + this.report.beginningBalance;
+    this.receiptImages = this.teamsAttachmentsService.receiptAttachments(this.report);
   }
 
 }
