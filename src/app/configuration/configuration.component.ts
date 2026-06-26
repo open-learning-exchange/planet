ba8701ee16c0bf9ecd6cc18cd2fa39ec19b27879
mutugiii <mutugimutuma@gmail.com>
2026-06-26 20:20:50 +0300

    simplify styles

--- src/app/configuration/configuration.component.ts
+++ src/app/configuration/configuration.component.ts
@@ -91,20 +91,8 @@ interface ContactForm {
       margin-bottom: 10px;
     }
 
-    :host ::ng-deep .configuration-stepper .mat-horizontal-stepper-header-container {
-      justify-content: flex-start;
-      width: fit-content;
-      max-width: 100%;
-    }
-
-    :host ::ng-deep .configuration-stepper .mat-horizontal-stepper-header {
-      flex: 0 0 auto;
-      padding: 0 16px;
-    }
-
-    :host ::ng-deep .configuration-stepper .mat-stepper-horizontal-line {
+    :host ::ng-deep .mat-stepper-horizontal-line {
       flex: 0 0 56px;
-      min-width: 56px;
     }
   `],
   imports: [
