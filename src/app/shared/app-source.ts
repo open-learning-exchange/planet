e6814d8594c3515f628fd0614ba7bc5a850a1078
dogi <dogi@users.noreply.github.com>
2026-09-09 19:53:35 -0400

    Update app-source.ts

--- src/app/shared/app-source.ts
+++ src/app/shared/app-source.ts
@@ -4,7 +4,7 @@ export type AppSourceFilter = AppSource | '';
 export const appSources: { value: AppSource, label: string }[] = [
   { value: 'planet', label: $localize`Planet` },
   { value: 'myplanet', label: $localize`myPlanet` },
-  { value: 'myplanet-lite', label: $localize`myPlanet Lite` }
+  { value: 'myplanet-lite', label: $localize`myPlanet lite` }
 ];
 
 const normalizeAppSource = (value: any): AppSource | undefined => {
